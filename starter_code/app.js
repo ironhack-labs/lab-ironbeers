9144ed9ace07a0815cc52943f1102e0a20166be2
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-11 16:48:21 +0200

    Iteration 1 complete

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -11,7 +10,33 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+
+  const data = {
+    title: "Home",
+    lennyFace: "( ͡° ͜ʖ ͡°)"
+  };
+
+  res.render('index', data);
+});
+
+app.get('/beers', (req, res, next) => {
+
+  const data = {
+    title: "Beers",
+    lennyFace: "( ͡~ ͜ʖ ͡°)"
+  };
+
+  res.render('beers', data);
+});
+
+app.get('/random_beers', (req, res, next) => {
+
+  const data = {
+    title: "Random Beers",
+    lennyFace: "( ͡◉ ͜ʖ ͡◉)"
+  };
+
+  res.render('random_beers', data);
 });
 
 
