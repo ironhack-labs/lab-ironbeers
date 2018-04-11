e57289932d46ddd30c697084eb5907a65e69ba3d
esthvega <esthvega@hotmail.es>
2018-04-11 21:03:32 +0200

    pensaba que no habia forkeado

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,9 +9,29 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {arrBeer: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+app.get('/randomBeers', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeers', {arrBeer: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
 });
 
 
