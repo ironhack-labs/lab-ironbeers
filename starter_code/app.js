30706daef38486b34f2d448fc26adf5f9eb90a25
Alejandro <alexibape@hotmail.com>
2018-04-11 20:18:55 +0200

    Casi terminado, falta maquetar

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,11 +8,34 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+    console.log(beers);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/randomBeer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', {randombeer: beers[0]});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
 
 
 app.listen(3000);
