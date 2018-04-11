5d759742a93fc305407ad451d6ad24bdc68830f1
A-Tr <alvarotrancon@gmail.com>
2018-04-11 18:20:19 +0200

    finalizado que enseñe todas las birras

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,14 +10,24 @@ console.log("hola")
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
-});
+  });
 
 app.get('/beers', (req, res) => {
-  res.render('beers');
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    res.render('beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 
