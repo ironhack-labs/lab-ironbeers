05b69e773f4e458f515d0416ea474f6a1a814121
Yago Vega <yagofvv@gmail.com>
2018-04-11 17:40:53 +0200

    initial commit iteration2

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -6,12 +5,20 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
+app.get('/random-beer', (req, res, next) => {
+  res.render('random-beer');
 });
 
 
