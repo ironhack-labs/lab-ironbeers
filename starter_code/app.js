a2405b5faf9a0150ea16d0e8ffb0290ab92d6c02
Dauv Bafna <dauvbafna1@gmail.com>
2018-04-11 23:33:09 +0200

    BCN dauv ironbeers

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,33 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers);
+      res.render('beers', beers);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-app.listen(3000);
+app.get('/randombeers', (req, res, next) => {
+  res.render('randombeers');
+});
+
+app.listen(3000, () => console.log('listening on port 3000'));
