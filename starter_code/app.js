d4b87f4f9352f65c15d4b3c09e62cb9151f9d4a6
Rapha Montenegro <raphaelmmontenegro@gmail.com>
2018-04-11 23:28:12 +0200

    Nothing works. All is shit

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,17 +1,28 @@
 
 const express = require('express');
 const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', path.joing(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  res.render('beers');
 });
 
 
