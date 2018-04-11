a384640fe0cc9b9f2c3d181369fb90893adbad83
A-Tr <alvarotrancon@gmail.com>
2018-04-11 16:49:54 +0200

    finalizada segunda iteracion

--- starter_code/app.js
+++ starter_code/app.js
@@ -12,8 +12,12 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  res.render('beers');
 });
 
 
