d94a52af8e2fb5717f5d5dcae5dcb9022a1dd390
JMBarcelo <jmbbazal@gmail.com>
2018-04-11 18:41:49 +0200

    Falta el random

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,24 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beersArray: beers});
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
+
+app.get('/random-beer', (req, res, next) => {
+  res.render('random-beer');
+});
 
 app.listen(3000);
