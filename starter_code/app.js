13dc6f324b76b7e433661cec6a252b1d9fd996e5
Pere Serra Bosch <pereserrabosch@MacBook-Pro-de-Pere.local>
2018-04-11 22:53:07 +0200

    Fake data

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,68 @@
+'use strict';
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+// -- configure the app
 
-app.get('/', (req, res, next) => {
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
+// -- fake database
+
+const beers = [{
+  name: 'Madrid',
+  price: '9.75 euros'
+}, {
+  name: 'Amsterdam',
+  price: '10.75 euros'
+}, {
+  name: 'Paris',
+  price: '8.75 euros'
+}, {
+  name: 'Barcelona',
+  price: '6.75 euros'
+}];
+
+
+// -- routes
+
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  /*
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers[0]);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  */
+  
+  const data = {
+    allBeers: beers
+  };
+  
+  res.render('beers', data);
+});
 
-app.listen(3000);
+app.get('/random-beer', (req, res) => {
+  let randomNumber = Math.floor(Math.random() * 4);
+  const data = {
+    myRandomBeer: beers[randomNumber]
+  };
+  res.render('random-beer', data);
+});
+
+// -- start the app
+
+app.listen(3000, () => console.log('listening on port 3000!'));
