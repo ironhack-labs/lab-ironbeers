b190b7fa43f877ac348d1315345c8534997f2db3
aacecandev <aacecan.dev@gmail.com>
2018-04-11 18:03:16 +0200

    quinta iteracion

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,13 +6,38 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.get('/', (req, res, next) => {
-  res.render('index');
+
+  const data = {
+    title: "Home"
+  }
+  res.render('index', data);
 });
+
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+    .then(beers => {
+
+      const data = {
+        title: "Beers",
+        beers: beers
+      }
+      res.render('beers', data);
+    })
+
+    .catch(error => {
+      console.log(error);
+    })
+})
 
 
 app.listen(3000, () => console.log("Server running at localhost:3000"));
