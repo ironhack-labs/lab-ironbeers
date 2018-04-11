97b1094560be0f4c879b02f41abfe481b2f34485
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-11 18:41:14 +0200

    Iteration 3,4,5

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,20 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials( __dirname + '/views/partials/' );
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 app.get('/beers', (req, res, next) => {
-  res.render('beers');
+  punkAPI.getBeers()
+    .then( beers => {
+      res.render('beers', {beersArray: beers});
+    })
+    .catch( error => {
+      console.log(error);
+    })
 });
 
 app.get('/random-beers', (req, res, next) => {
