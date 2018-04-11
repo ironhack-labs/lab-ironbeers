eb54086709565b8441a02078284d7e54218b75fc
Pablo García <heypablete@gmail.com>
2018-04-11 18:22:56 +0200

    empezando la iteracion 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,11 +8,42 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
+// Botones
+let btn = [
+  {
+    texto: "Check the Beers!",
+    destino: ""
+  },
+  {
+    texto: "Check a Random Beer",
+    destino: ""
+  }
+]
+
+// Rutas 
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()
+  .then(beers => {
+    const data = { beers: beers }
+    res.render('beers', data);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  
+});
+
+app.get('/random-beers', (req, res) => {
+  res.render('random-beers');
+});
 
 app.listen(3000);
