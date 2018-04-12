56a5bcd8a7401c41d474522c8833e919a5c0a1cf
Alvaro Reguera <alvaro.reguera@gmail.com>
2018-04-12 09:20:03 +0200

    Upload pre flama layout

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,13 +6,34 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beer', (req, res, next) => {
+punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beer', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
