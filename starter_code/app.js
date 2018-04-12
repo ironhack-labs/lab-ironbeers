f260555c4b567dd76ab8c980337b0204ea319d28
fscheugenpflug <fs.scheugenpflug@hotmail.de>
2018-04-12 10:49:17 +0200

    updated API

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const express = require('express');
 const hbs     = require('hbs');
@@ -11,47 +12,31 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(path.join(__dirname, "/views/partials"));
 
-//-- Fake Data
-
-const beers = [
-  {
-    name: "San Miguel",
-    description: "A beer that you can drink.",
-    tagline: "San Miguel is the shittiest beer but you love it!"
-  },
-  {
-    name: "Heineken",
-    description: "A beer that comes in a green bottle.",
-    tagline: "Getting Americans drunk since a long time ago."
-  },
-  {
-    name: "Corona",
-    description: "A beer for when you want to feel like you are in Tijuana.",
-    tagline: "Put a lime in it and suck it!"
-  },
-  {
-    name: "Chang",
-    description: "Contains an unknown amount of alcohol.",
-    tagline: "For when you want to play drunk roulette in Asian hostels"
-  }
-];
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 app.get('/beers', (req, res, next) => {
-  const data = {
-    allBeers: beers,
-  }
-  res.render('beers', data);
+  punkAPI.getBeers()
+    .then(beers => {
+      const data = {beers} // ¡¡¡IMPORTANT to do that while accessing API => has to be OBJECT!!!
+      res.render("beers", data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 app.get('/random-beers', (req, res, next) => {
-    const data = { 
-      randomBeer: beers[Math.floor(Math.random() * beers.length)]
-     };
-      res.render('randombeers', data);
+  punkAPI.getRandom()
+    .then(randomBeers => {
+      const data = {randomBeers: randomBeers[0]} // ¡¡¡IMPORTANT to do that while accessing API => has to be OBJECT!!!
+      console.log(randomBeers);
+      res.render("randombeers", data);
+    })
+    .catch(error => {
+      console.log(error);
+    });  
 });
 
 
