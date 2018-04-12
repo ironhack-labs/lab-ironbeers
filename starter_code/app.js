2863da4bba1b28f3f32c52fde467f7036fdb065f
Yaats <trang.billy@live.fr>
2018-04-12 10:33:08 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,11 +8,27 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("layout", __dirname + "/views/layout.hbs");
+
+app.use(express.static(path.join(__dirname, '/public')));
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
 });
 
+app.get("/beers", (req, res, next) => {
+  
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+      res.locals.beers = beers;
+      res.render("beers.hbs");
+   })
+  .catch(error => {
+     console.log(error)
+  })
+});
 
 app.listen(3000);
+
