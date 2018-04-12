800c35b8cce1fdf208dacba8debcc13da3a97af1
RISTO TAPANI RAIHALA <tyokone@RISTOs-MacBook-Pro.local>
2018-04-12 09:13:22 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,42 +1,40 @@
-
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
-hbs.registerPartials(__dirname + '/views/partials')
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-app.get('/beers', (req, res, next)=> {
-  punkAPI.getBeers()
-  .then(beers => {
-    res.render('beers', {arrBeers: beers});
-  })
-  
-  .catch(error => {
-    console.log(error)
-  })
-  
-})
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render("beers", { arrBeers: beers });
+    })
 
-app.get('/random', (req, res, next)=> {
-  punkAPI.getRandom()
-  .then(beers => {
-    res.render('random', {arrBeers:beers[0]});
-  })
-  .catch(error => {
-    console.log(error)
-  })
- 
-})
+    .catch(error => {
+      console.log(error);
+    });
+});
 
+app.get("/random", (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(beers => {
+      res.render("random", { arrBeers: beers[0] });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 app.listen(3000);
