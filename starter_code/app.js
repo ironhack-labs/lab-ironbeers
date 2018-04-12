d39c8b84ff0547b44915b77d8cb3b974f66d4493
Rapha Montenegro <raphaelmmontenegro@gmail.com>
2018-04-12 09:50:40 +0200

    fixed some typos. css and image still don't work

--- starter_code/app.js
+++ starter_code/app.js
@@ -7,7 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.joing(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
