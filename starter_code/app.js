35277518ad1f48310e224d75935ead9c25a27526
Tanis Pan <e.pan.gar@gmail.com>
2018-04-12 09:38:46 +0200

    I

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,6 +8,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 //Home Page
