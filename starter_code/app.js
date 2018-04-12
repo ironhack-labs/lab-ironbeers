e13c599a976d88ec799eb51696833fe74deb1408
Samantha Ferreira <samanthamferr@gmail.com>
2018-04-12 09:45:14 +0200

    BCN samantha ferreira

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,28 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers');
+      console.log('cbfguiaefchoia');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 app.listen(3000);
