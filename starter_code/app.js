9a36d45862001d629807a3fbc509c76fd80e405e
Richard Batista <stalfos@icloud.com>
2018-04-25 13:05:43 -0400

    Submitting assignment

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,21 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+	res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+	punkAPI.getBeers()
+	.then(beers => {
+		console.log(beers);
+		res.render('beers');
+	})
+	.catch(error => {
+		console.log(error)
+	})
+});
 
 app.listen(3000);
