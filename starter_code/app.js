1ac8cb5e757aa9ee36fc0854d6cb059eb777bdd6
Daniel Lozano <lozanodaniel02@gmail.com>
2018-04-25 17:55:53 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -9,10 +8,30 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    res.render('random-beer', beer[0]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
