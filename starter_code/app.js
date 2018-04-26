362dd18be457d4f73ce720bac34549ec456f6ef4
Beto <zonanabeto@gmail.com>
2018-04-25 23:36:20 -0500

    me falto el random

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,13 +6,49 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+//control
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
+
+//gets
 
 app.get('/', (req, res, next) => {
   res.render('index');
+  
 });
+
+app.get('/beers', (req, res, next) => {
+ 
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+   
+  })
+  
+  .catch(error => {
+    console.log(error)
+  })
+  
+  
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.randomBeer()
+  .then(beers => {
+    res.render('random-beers',{beers});
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+  
+});
+
+
 
 
 app.listen(3000);
