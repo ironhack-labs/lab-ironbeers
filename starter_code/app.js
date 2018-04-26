dc81b1b85161f7db2f52df1910485ee42df73299
rawanalfaiz <rawan.alfaiz@outlook.com>
2018-04-25 23:06:03 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,7 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +14,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+
+  console.log('hi' );
+
+  punkAPI.getBeers()
+  .then(beers => {
+    let data ={
+      myObjects: beers
+    } 
+	// console.log(beers) // so you can see the structure of the data
+	res.render('beers', data) //make sure to put the res.render inside the .then block
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
+
+app.get('/random', (req, res, next) => {
+  
+  punkAPI.getRandom()
+    .then(oneRandomBeer => {
+
+      console.log(oneRandomBeer[0]);
+      res.render('random-beers',{ randomBeer:oneRandomBeer[0] } );
+  
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+
+});
+
 
 
 app.listen(3000);
