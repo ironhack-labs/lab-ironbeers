4b29fe72ac0e148695039eb5d51e78f2346a3f35
Maximo Campo <themaxomoto@gmail.com>
2018-04-25 21:49:02 -0500

    terminado menos formateo de randombeer

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,6 +5,7 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -14,5 +15,27 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
-app.listen(3000);
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beer',{beers})
+    console.log(beers)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
+app.listen(3000,err=>console.log('Ready in 3000'));
