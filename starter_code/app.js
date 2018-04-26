0d93e855b0ed0160321cac02cf1f7d78571164c6
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-04-25 20:42:17 -0400

    updated layout

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,37 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    const data = {theBeers: beers}
+res.render('beers', data);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.get('/randomBeers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    const data = {theBeers: beers[0]}
+    res.render('randomBeers', data);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
+
+
 
 
 app.listen(3000);
