ad5c91b06ce014cb60eaccae0bd96585224c11ff
Jens Kai <jkaih7@gmail.com>
2018-04-25 20:26:30 -0400

    finished most of the assignment

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,9 +10,45 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+  
+    console.log(beers);
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers);
+    res.render('random-beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
+
+
+
+
+
+
+
+
+
 
 app.listen(3000);
+console.log("listening!!!")
