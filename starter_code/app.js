29e303e216a165917c9876a8b3f58db82b6f3b22
Robert Gelbman <rgelbman@aol.com>
2018-04-25 23:03:46 -0400

    MIA - FTWD - Robert Gelbman - lab-ironbeers

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,10 +8,37 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  // res.render('beers');
+});
+
+app.get('/randomBeers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log("-==-==-=-=-=-===-=-=")
+    console.log(beers);
+    res.render('randomBeers', {beers: beers[0]});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  // res.render('random-beers');
 });
 
 
