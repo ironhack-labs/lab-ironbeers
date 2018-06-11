f5a2ca8b23abfb18693d7bb8e9eb7cb5f3ecffa3
Maximilianus Paredes <max0720x@gmail.com>
2018-06-11 19:22:38 -0400

    progress

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,10 +8,44 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  const allBeers = punkAPI.getBeers()
+  
+   allBeers.then(beers => {
+    let data = {
+      myObjects: beers
+    }
+
+    res.render('beers', data);
+  })
+    allBeers.catch(error => {
+    console.log(error)
+  })
+  
+  
+});
+
+app.get('/random-beers', (req, res, next) => {
+  const randomBeer = punkAPI.getRandom();
+
+  randomBeer.then(beer => {
+    let data = {theActualRandomBeer: beer[0]}
+    
+    res.render('random-beers', data)
+  })
+
+  randomBeer.catch(error => {
+    console.log(error)
+  })
+  
+  
 });
 
 
