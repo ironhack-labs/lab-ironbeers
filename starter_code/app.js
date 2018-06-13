c2bed649fd7d76b827c41b50ee38a171c470b9f9
JensErhardt <jens-erhardt@web.de>
2018-06-13 21:40:35 +0200

    BER-Jens

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,44 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+    .then(beers => {
+      
+
+      let data = {
+        beers : beers,
+        name : "TEST",
+      }
+
+      console.log("DEBUG" + data)
+      
+      res.render('beers', data);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+  
+});
+
+app.get('/random-beer', (req, res, next) => {
+  res.render('random-beer');
+});
 
 app.listen(3000);
