fe95edb16567712d3399ce3c02fb5c9158136c29
ahmouda12 <ahmouda12@ufl.edu>
2018-06-13 01:03:43 -0400

    first

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,9 +9,38 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  const beers = punkAPI.getBeers();
+
+  beers.then(beers => {
+    // alert(beer[0].name);
+    // console.log(beers)
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error);
+  });
+// console.log(beers)
+//   res.render('beers', beers);
+});
+
+app.get('/random-beer', (req, res, next) => {
+  const randomBeers = punkAPI.getRandom();
+
+  randomBeers.then(beers => {
+    // alert(beer[0].name);
+    console.log(randomBeers);
+    res.render('randomBeer', randomBeers);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 });
 
 
