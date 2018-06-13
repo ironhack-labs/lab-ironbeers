11ee9362dbbe44d0d2700456ddc6d040b0f7c4b1
Lars Hansen <lars1702@gmail.com>
2018-06-13 22:27:22 +0200

    ERROR: to be seen in class

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,67 @@
-
+/*jshint esversion: 6 */
 const express = require('express');
 const hbs     = require('hbs');
-const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+const app     = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 
-app.listen(3000);
+app.get('/beers', (request, response) => 
+{
+  punkAPI.getBeers()
+  .then(beers => {
+    response.render(`beers`, {beers});
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
+
+app.get('/:beerid', (request, response) => 
+{
+  let bid = request.params.beerid
+  punkAPI.getBeer(bid)
+  .then(beers => {
+    let beer = beers[0];
+    console.log(beers)
+    response.render(`randomBeer`, {beer});
+  })
+  .catch(error => {
+    console.log("DEBUG: ",error);
+  });
+});
+
+
+
+
+app.get('/random-beer', (request, response) => 
+{
+  // punkAPI.getRandom()
+  // .then(beers => {
+  //   let beer = beers[0];
+  //   console.log("DEBUG beer", beer)
+  //   response.render(`randomBeer`, {beer});
+  // })
+  // .catch(error => {
+  //   console.log("DEBUG RANDOM: ",error);
+  // });
+});
+
+app.listen(3000, () => {
+  
+  console.log('My first app listening on port 3000!')
+});
