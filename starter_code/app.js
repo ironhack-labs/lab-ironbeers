b18d7597582bddd1afca0b5ec9f502462ae7d518
cameron skene <cameron.t.skene@gmail.com>
2018-06-14 17:03:32 +0200

    BER Cameron - 95% complete

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,8 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,5 +16,25 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
+app.get('/random', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random', beers[0]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
