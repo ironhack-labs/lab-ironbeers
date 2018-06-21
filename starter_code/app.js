8097cf19384031c2d8f79966ca38c1508452adc8
MundoGister <mundogister@gmail.com>
2018-06-21 23:41:06 +0200

    Done! :fire

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,8 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +15,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {
+        beers: beers
+      });
+      console.log(beers)
+    });
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beer => {
+      res.render('randomBeer', { beer });
+    });
+});
+
+
 
 
 app.listen(3000);
