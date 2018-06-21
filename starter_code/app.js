71ee06dfccfce1eaa8c0525ee62c1ce5bb103f84
Marta Soler <marta@MacBook-Air-de-Barrabes.local>
2018-06-21 20:49:02 +0200

    Iteration 1 and 2

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,7 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+app.use(express.static('public'));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +14,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
-
 
 app.listen(3000);
