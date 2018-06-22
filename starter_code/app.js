26886eb7acf15d131edc0950a2dd69c03389c4e1
aparna pattathil <aparnapattathil@hotmail.com>
2018-06-22 13:35:50 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,9 +9,29 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
+//route
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+//beers
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+    console.log(beers);
+    
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+ 
+});
+//random beer
+app.get('/random-beer', (req, res, next) => {
+  res.render('random-beer');
 });
 
 
