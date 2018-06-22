9bc9fc0ae71dce69fec2c471952554140c60ba9f
Paula Quitián <pquitian@idealista.com>
2018-06-22 08:18:12 +0200

    Paula_Miguel-falta ultima iteracion

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,13 +6,29 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers().then(beers => {
+    res.render('beers', {beers});
+  })
+});
+
+app.get('/randombeer', (req, res, next) => {
+  punkAPI.getRandom().then(beers => {
+    console.log(beers);
+    res.render('randomBeer', {beers});
+  })
+});
+
 
 
 app.listen(3000);
