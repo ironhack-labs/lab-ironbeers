e67ab1d2228578ac530f662c1a6ef66cc760549c
fran <fmls1989@gmail.com>
2018-06-22 19:49:09 +0200

    iteration 1-6

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,17 +2,50 @@
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
-const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const PORT = 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(express.static(__dirname +'/public'));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+
+  res.render('home');
+
 });
 
+app.get('/beers', (req, res, next) => {
+  // hacer llamada a la API
+  punkAPI.getBeers()
+  .then(beers=>{       
+    res.render('beers', {beers: beers} );
+    
+  })
+  .catch(err=>{
+    console.log(err);
+  });
+  
+});
 
-app.listen(3000);
+app.get('/randombeer', (req, res, next) => {
+  // data = {layout:false} //para que no muestre el layout
+  
+  punkAPI.getBeers()
+  .then(beers=>{  
+
+    let random = beers[Math.floor(Math.random() * beers.length)];
+    
+    res.render('randombeers', {beers: random} );
+    
+  })
+  .catch(err=>{
+    console.log(err);
+  });
+
+});
+
+app.listen(PORT, ()=> console.log('running on port' + PORT));
