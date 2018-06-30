cfe5d96b47548288e4797346cdd3984c335f0d2d
alekz7 <ing.aragon.ipn@gmail.com>
2018-06-29 21:15:18 -0500

    Empezando lo del partial

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,10 +1,13 @@
 
 const express = require('express');
 const hbs     = require('hbs');
-const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const cows    = require('cows');
 const punkAPI = new PunkAPIWrapper();
+
+const app     = express();
+const vacas   = cows();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -14,5 +17,19 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers:beers});
+    })
+    .catch(error => {
+      console.log(error);
+  })
+});
+
+app.get('/randomBeers', (req, res, next) => {
+  res.render('randomBeers');
+});
 
 app.listen(3000);
+console.log('App inicianlizada');
