29be4000eb80701181529a104d33304e21fdf30b
Abel Alonso <abel.alonso@gmail.com>
2018-07-04 19:32:27 +0200

    iteration 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,12 +6,31 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers[0]);
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
+
+app.get('/random-beer', (req, res) => {
+
+  res.render('random-beer');
 });
 
 
