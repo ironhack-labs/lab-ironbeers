0076e6770ebdde416777b99ece78c4ebbbec3c22
Ali Sattar <alirsattar@gmail.com>
2018-07-04 16:10:01 -0400

    getBeers() object now working

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,24 +6,40 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+// SETTING THE VIEW ENGINE AS HSB, SO WE CAN USE .HBS FILES AND LANGUAGE
+
 app.set('view engine', 'hbs');
+
+// SETTING THE 'VIEWS' REDIRECT TO GO TO OUR /VIEWS FOLDER
+
 app.set('views', __dirname + '/views');
+
+// SETTING OUR 'PUBLIC' FOLDER TO AUTOMATICALLY JOIN WITH THE MAIN
+// DIRECTORY, ELIMINATING NEED TO USE /PUBLIC/
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// REGISTERING OUR PARTIALS ROUTE TO /PARTIALS/
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+// MAIN INDEX.HBS ROUTE
 
 app.get('/', (req, res, next) => {
   res.render("index");
 });
 
-app.get('/blah', (req, res, next) => {
+
+// BEERS PAGE ROUTE
+
+app.get('/beers', (req, res, next) => {
+  
   punkAPI.getBeers()
   .then(beers => {
-
+    res.render('beers', {beers: beers});
   })
 
-
-  
-  
-  res.render('blah');
 });
 
 app.listen(3000);
