6f5e2680217becee0d99a87d966b261afdbc7100
Gabriel Garcia <gabogarciam71@gmail.com>
2018-07-04 21:49:45 +0200

    API working

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,20 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 app.get('/beers', (req, res, next) => {
-  res.render('beers');
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 app.get('/randombeers', (req, res, next) => {
