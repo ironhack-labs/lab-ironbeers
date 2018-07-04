6eebddbcf4e5064be4eaec1970147ff8ee15a630
Minyu Li <minyuli1997@gmail.com>
2018-07-04 19:33:53 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,32 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  });
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers', {beer: beers[0]});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.listen(3000, () => {
+  console.log("My first app listening on port 3000!");
+});
