0445f920cb810e8868e3b0defbfe9299e5e131b7
Ruben <rubenvilchesperez1985@hotmail.com>
2018-07-04 21:05:53 +0200

    iteracion 1

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,8 +10,31 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/randombeer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randombeer', beers[0]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
 });
 
 
