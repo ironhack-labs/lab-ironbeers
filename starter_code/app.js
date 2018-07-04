69a7cade15c616712eba423a764d8096cb134c60
Giannini Charles <12gcharles@gmail.com>
2018-07-04 14:32:40 -0400

    MIA Giannini requesting assistance for how to check if the array was successfully passed.

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,27 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
+
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers');
+  })
+});
+
+
+app.get('/randomBeers', (req, res, next)=> {
+  res.render('randomBeers');
+});
+
+
+
 
 
 app.listen(3000);
