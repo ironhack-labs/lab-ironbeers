e2f403736808e8bc946a1ea2dec07737d74ec07b
Niko <nkouuu@hotmail.com>
2018-07-04 21:06:54 +0200

    almost done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,9 +9,30 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{beers});
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    res.render('randomBeer',{beer});
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 
