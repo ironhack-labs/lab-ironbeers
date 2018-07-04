210b52dc535d0d18d5cd8138760b00f0c148dc1f
dmarinsa <dmarinsanchez@gmail.com>
2018-07-04 18:51:05 +0200

    2nd iteration

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,15 +5,18 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname+'/views/partials');
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+
 
 app.use(express.static(__dirname+'/node_modules/bootstrap/dist'))
 app.use(express.static(__dirname+'/node_modules/jquery/dist'))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('partials/index');
 });
 
 
