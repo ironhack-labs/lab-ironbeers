bb27d68b8da4ee491d9560ae66b8f2055c0b8b2f
Sarai Vasquez <svmarinez@hotmail.com>
2018-07-04 19:31:49 +0200

    first commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -14,5 +14,14 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+});
