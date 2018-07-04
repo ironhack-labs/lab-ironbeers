7ff811d1d42c39c4a69f796f49a20ef24f01c9bf
Antonio Esteban <aecdigitaltransformer@gmail.com>
2018-07-04 20:13:35 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -6,13 +5,45 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
+
+
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
 
-app.listen(3000);
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+    console.log({beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+
+  
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers', {beers});
+    console.log({beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.listen(3000, () => {
+  console.log("Escuchando");
+});
+
