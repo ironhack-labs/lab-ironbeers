9778dbc6df53ba040d86f9b35fed43f88a030519
Dunnia <dunnia.alvarez@gmail.com>
2018-07-04 20:11:13 +0200

    falta diseño

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,35 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
+
 
 
 app.listen(3000);
