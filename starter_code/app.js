821840055c4c739c4a76db337b9a33eb3aa56852
Montse Badia <mbadia_11@hotmail.com>
2018-07-04 21:48:41 +0200

    api working

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,22 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 app.get('/beers', (req, res, next) => {
-  res.render('beers');
+
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
 });
 
 app.get('/randomBeers', (req, res, next) => {
