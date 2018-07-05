27e3f5809d62021a7749aff80458767fe09fa04e
Paola Arredondo <arredondop03@gmail.com>
2018-07-05 14:33:41 -0400

    done :)

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,35 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+
+
+
+
+    res.render('beers', {beersArray:beers}) //template variable
+  })
+});
+
+app.get('/random-beer', (req, res, next) => {
+
+  punkAPI.getRandom()
+  .then(beers => {
+
+    res.render('randomBeer', {theBeer:beers[0]})
+  })
+});
+
+
 
 
 app.listen(3000);
