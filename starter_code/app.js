7a0869e12e68e46ff573f2097c2ac285b52f26b3
mtrojas <tererojas.i@gmail.com>
2018-07-04 21:09:44 -0500

    "done"

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,48 @@
-
+//express
 const express = require('express');
-const hbs     = require('hbs');
 const app     = express();
+//mongoose
+const mongoose = require('mongoose');
+//sistema de templates handlebars (hbs)
+const hbs     = require('hbs');
+//configuramos hbs
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
 const path    = require('path');
+
+//this will be acting as my database??
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
+app.listen(3000, () => {
+  console.log('hola');
+});
