48187a58895a22027c8c7d0a685888454b63600a
Andrey Barrantes <abarrantes@clavisco.com>
2018-07-05 08:59:39 -0400

    cant get it to work yet

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,40 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
+  
+  
+  
+  
   res.render('index');
+
 });
 
+
+app.get('/beers', (req, res, next) => {
+  
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('index',{beers});
+
+  })
+
+  
+  
+  
+
+});
+
+
+app.get('/random-beers', (req, res, next) => {
+  
+  
+  
+  
+  res.render('index');
+
+});
 
 app.listen(3000);
