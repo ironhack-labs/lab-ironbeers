25cec6fd04d3fbe340b28d2bcdf0fcd3ed564b71
Andrey Barrantes <abarrantes@clavisco.com>
2018-07-05 10:41:52 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -12,37 +12,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
-  
-  
-  
-  
+
   res.render('index');
-
 });
-
 
 app.get('/beers', (req, res, next) => {
-  
+
   punkAPI.getBeers()
-  .then(beers => {
-    res.render('index',{beers});
+    .then(beers => {
+      res.render('beers', { beers });
 
-  })
-
-  
-  
-  
-
+    })
 });
 
-
 app.get('/random-beers', (req, res, next) => {
-  
-  
-  
-  
-  res.render('index');
-
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('randombeers', { theBeer:beers[0] });
+    })
 });
 
 app.listen(3000);
