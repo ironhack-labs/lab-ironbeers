893ae9f15d902ced3d5f2616b0f35e947848c25d
John Doe <johndoe@example.com>
2018-07-05 10:36:32 -0400

    Final IronBeers

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,25 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res, next) => {  
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', { beers });
+    console.log(beers);
+  }) 
+});
 
-app.listen(3000);
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beer', { theBeer: beers[0] });
+  }) 
+});
+
+app.listen(3000)
