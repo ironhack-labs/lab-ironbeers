18c27f626f280e5574f4ee630663960372526dd7
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-07-05 09:07:48 +0200

    blah

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,8 +10,28 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('beers', __dirname + '/beers');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('random-beers', __dirname + '/random-beers');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.send('not-found');
+});
+
+app.use((err, req, res, next) => {
+  console.error('ERROR', req.method, req.path, err);
+
+  if (!res.headersSent) {
+    res.status(500);
+    res.send('error');
+  }
 });
 
 
