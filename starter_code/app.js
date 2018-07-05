bfbef5bf575150adda72e851df1d016b4aa54b39
Beth Shook <beth.shook@gmail.com>
2018-07-04 23:11:00 -0500

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,9 +10,32 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req,res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.get('/randomBeer', (req,res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 
 app.listen(3000);
