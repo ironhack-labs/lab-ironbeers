29b64197c76dd41a6a26444c588a9bd9ed680382
Rodrigo Ortiz <roloortizgarcia@gmail.com>
2018-07-05 10:52:46 +0200

    first commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,39 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (rep, res, next) => {
+    
+  punkAPI.getBeers()
+      .then(beers => {
+      console.log("Beers")
+      res.render('beers', {beers});
+  })
+      .catch(error => {
+      console.log(error)
+  })
+    
+  });
 
-app.listen(3000);
+  app.get('/randomBeers', (rep, res, next) => {
+      punkAPI.getRandom()
+          .then(beers => {
+          console.log("Random beer")
+          let randomBeer =  beers[0]
+          res.render("randomBeers", {beer:randomBeer})
+      })
+          .catch(error => {
+          console.log(error)
+      })
+    });
+
+
+
+app.listen(3000, ()=>{
+  console.log("Estoy escuchando...")
+});
