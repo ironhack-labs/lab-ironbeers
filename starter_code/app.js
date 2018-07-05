f1505d173262b661a336bc7091df60bcd9589129
Pablo Rezola <pablorezola20@yahoo.es>
2018-07-05 07:32:53 +0200

    iteración5

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,25 +1,41 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
+
   res.render('index');
 });
 
 app.get('/beers', (req, res, next) => {
-  res.render('index');
+  punkAPI.getBeers().then(beers => {
+    console.log(beers);
+    res.render('beers', {
+      beers
+    });
+  })
 });
 
-app.get('/random-beers', (req, res, next) => {
-  res.render('index');
-});
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random-beer', {
+        beers
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+})
 
 app.listen(3000);
