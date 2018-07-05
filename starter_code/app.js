d79735607488b8b93983cf9d959ef53e4b81a888
Ricardo Campos <ricamposm@gmail.com>
2018-07-04 23:17:21 -0500

    DE MEX Ricardo Campos

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,6 +5,8 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials')
+
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -13,6 +15,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req,res)=>{
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beer', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+app.get('/random',(req,res)=>{
+  punkAPI.getRandom()
+  .then(beers => {
+  res.render('random', {beers});
+})
+  .catch(error => {
+    console.log(error)
+  })
+});
 
-
-app.listen(3000);
+app.listen(3000, ()=>{
+  console.log('Listening...')
+});
