81d25448dd16b77d1b1b3b491f4b5b76d83636af
Sobaillo <vcg20193@hotmail.com>
2018-07-05 09:39:41 +0200

    listo y preparado

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,7 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,5 +15,24 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {beers});
+    })
+    .catch(error => {
+      console.log(error)
+    });
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+      res.render('random-beers', {beers});
+    })
+    .catch(error => {
+      console.log(error)
+    });
+});
 
 app.listen(3000);
