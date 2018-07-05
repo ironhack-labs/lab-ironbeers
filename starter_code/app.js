0fa096144c7db6044d4ceac621934084acc2c430
EdMijangos <mloeza2010@hotmail.com>
2018-07-05 08:28:25 -0500

    DE MEX - Eduardo Mijangos

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,33 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res)=>{
+  punkAPI.getBeers()
+  .then(beers => {
+    const beerArr = beers;
+    res.render('beers', {beerArr});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.get('/random-beers', (req, res)=>{
+  punkAPI.getRandom()
+  .then(beers => {
+    const randBeer = beers;
+    res.render('randomBeer', {randBeer});
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 app.listen(3000);
