9a1f0cc04018c5d23fd04ef0368a70c65d38b659
Montserrat Monroy <montserratmonroy@MacBook-Pro-de-Montserrat.local>
2018-07-08 13:03:50 -0500

    PP MEX Montserrat Monroy

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -6,13 +5,41 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
-app.get('/', (req, res, next) => {
+
+
+
+app.get('/',(req, res, next) => {
   res.render('index');
 });
 
+app.get('/random-beer', (req,res, next)=>{
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers)
+  res.render('random-beer', { beers });
+})
+  .catch(error => {
+    console.log(error)
+})
+});
 
-app.listen(3000);
+
+
+  
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers)
+      res.render('beers',{ beers });
+    })
+});
+
+app.listen(3000, ()=>{
+console.log('esta corriendo el server')});
