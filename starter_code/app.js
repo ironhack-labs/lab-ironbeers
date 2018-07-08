1469d5dae04aa60b7e3138147c3ef4d8974ea89c
dragonINXS <davidmudd@outlook.com>
2018-07-08 12:28:48 -0400

    MIA David Mudd

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
-const hbs     = require('hbs');
 const app     = express();
+const hbs     = require('hbs');
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
@@ -10,9 +10,53 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/layout', (req, res, next) => {
+  res.render('layout');
+});
+
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+    .then(someResponse => {
+      console.log("hey", someResponse);
+      res.render('beers', { beers: someResponse });
+
+    })
+    
+    .catch(error => {
+      console.log(error);
+    });
+
+});
+
+app.get('/random-beers', (req, res, next) => {
+
+  punkAPI.getRandom()
+    .then(beers => {
+
+      // console.log(beers[0]);
+
+      res.render("randomBeers", {theBeer:beers[0]});
+
+    })
+    // .catch(error => {
+    //   console.log(error);
+    // });
+
+  // res.render('randomBeers');
+});
+
+
 
 
 app.listen(3000);
