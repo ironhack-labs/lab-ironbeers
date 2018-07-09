5cec30cc7ab9004bdd6baee86cd9a1d6ad12c57d
Patrick de la Guardia <patrickdelaguardia@Patricks-MacBook.local>
2018-07-08 23:04:18 -0500

    1st commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,10 +1,11 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -14,5 +15,25 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+
+app.get('/random', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random', beers[0]);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
 app.listen(3000);
