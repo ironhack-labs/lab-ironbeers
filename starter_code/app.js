7fe96997780a4ac71568a040a3cc0ba882bb1b78
Javier Chacón <j.chacon.alvarez@gmail.com>
2018-07-13 19:25:09 +0200

    finished index page

--- starter_code/app.js
+++ starter_code/app.js
@@ -7,7 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app     = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
