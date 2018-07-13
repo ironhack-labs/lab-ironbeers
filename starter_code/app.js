4aa2ca891d3428bb1957e2fdf244be69bec1cf9a
Javier Chacón <j.chacon.alvarez@gmail.com>
2018-07-13 18:12:29 +0200

    added views and routes

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,22 +1,26 @@
 
 const express = require('express');
-
 const hbs     = require('hbs');
-
-const app     = express();
 const path    = require('path');
-
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+const app     = express();
 const punkAPI = new PunkAPIWrapper();
 
+app.use(express.static(path.join(__dirname, './public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, views));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
+
+app.get('/random', (req, res, next) => {
+  res.render('random');
+});
 
 app.listen(3000);
