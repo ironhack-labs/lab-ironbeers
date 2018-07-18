83451b7cbb571bba3360d1d4655c7a3ad894836c
Marc Pósito <marcposito@gmail.com>
2018-07-18 23:41:26 +0200

    work done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,10 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
+
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,5 +18,25 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{beers: beers});
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('beers',{beers:beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
+app.listen(3002);
