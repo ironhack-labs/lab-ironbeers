50c9bb513039a7444ce1f747ff5cb1ee60a2a966
Anna Fredriksson <leliloro@gmail.com>
2018-07-19 07:35:02 +0200

    add static data

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,11 +8,28 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', { beers: beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.get('/random-beer', (req, res, next) => {
+  res.render('random-beer');
 });
 
 
