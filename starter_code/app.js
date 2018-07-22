6c3f2586d63f7bf41ad181ab6a5d6c6787adfb3f
Oriol Hilari <orihil@icloud.com>
2018-07-22 20:44:53 +0200

    Por acabar

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,13 +6,25 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname, '/views/partials')
+
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('beers', (req, res, next) =>{
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
-app.listen(3000);
+
+app.listen(3300);
