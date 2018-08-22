a83479076fb8673cb2604ea9c4f080e23b6e73d2
Elizaveta <elizaveta.ragozina@mailbox.tu-dresden.de>
2018-08-22 22:09:50 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,17 +1,44 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+app.get("/beers", (req, res) => {
+
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers", {beers})
+    //console.log(beers)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+})
+
+
+app.get("/random-beers", (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      res.render("random-beer", beer[0]);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 
