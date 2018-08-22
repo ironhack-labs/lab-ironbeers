f0c41e03c0a602e9c9d46da11a257d714b4e9403
Cesar Vinken <cesarvinken@hotmail.com>
2018-08-22 21:14:51 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -7,10 +7,37 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.use(express.static(path.join(__dirname, "./public")));
 
 app.get("/", (req, res, next) => {
   res.render("index");
 });
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render("beers", { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+app.get("/random-beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      const beer = getRandomBeer(beers);
+      res.render("random-beers", { beer });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 app.listen(3000);
+
+function getRandomBeer(beers) {
+  return beers[Math.floor(Math.random() * Math.floor(beers.length))];
+}
