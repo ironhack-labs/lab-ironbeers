295882636b5bb2a3efe8bbe1bd86de70e532741a
Nina Drüke <ndrueke@posteo.de>
2018-08-22 19:14:04 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,31 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index',{title: "Home"});
 });
+
+app.get("/beers", (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers", {title: "Beers", beers })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+app.get("/random-beer", (req, res, next) => {
+  punkAPI.getRandom()
+  .then(rbeer => {
+    console.log(rbeer)
+    res.render("randomBeer", {title: "Random Beer", rbeer })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 
 app.listen(3000);
