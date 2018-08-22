2421d7365a3ebf94c004021e0a985c76b56980c0
Nicole Jelonek <nicolejelonek@gmail.com>
2018-08-23 01:29:15 +0200

    Ber Nicole

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,9 +9,39 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
+
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', {
+    title: `Ironbeers`
+  });
 });
 
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers);
+      res.render('beers', {
+        beers
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      console.log(beers);
+      res.render('random-beers', {
+        beers
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
 app.listen(3000);
