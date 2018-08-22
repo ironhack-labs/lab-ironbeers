7b9c46e721475d53ec1a7f84ffffd9e3d3344826
Yuki Sato <sato.youxi@gmail.com>
2018-08-22 20:07:04 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -10,9 +9,40 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+hbs.registerPartials(path.join(__dirname, './views/partials'))
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: "Ironbeers"
+  });
 });
+
+app.get('/beers',(req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers',{
+      beers
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.get('/random-beers',(req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers);
+    res.render('random-beers',{
+      beers
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
 
 
 app.listen(3000);
