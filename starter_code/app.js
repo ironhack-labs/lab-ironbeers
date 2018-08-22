97847d0896ca4d1deaa58ddd9d55c2b34c483eb8
Carla Naujoks <cnaujoks3@gmail.com>
2018-08-22 21:11:38 +0200

    DONE

--- starter_code/app.js
+++ starter_code/app.js
@@ -14,5 +14,35 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+   let data = {
+     beers: beers
+   }
+    res.render("beers", data);
+    
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+   let data2 = {
+     beers: beers
+   }
+    res.render("random-beers", data2);
+    
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
 
 app.listen(3000);
