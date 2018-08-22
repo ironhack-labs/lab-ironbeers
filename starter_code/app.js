a6691e2ee652e600de31472308237eb97585b590
trong nhan nguyen <ntnhan30@gmail.com>
2018-08-22 21:28:19 +0200

    functional

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,34 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    // console.log(beers[0])
+    res.render('beers', {
+      beers: beers
+    });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    res.render('randomBeer', {
+    beer: beer
+  });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
