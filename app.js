1a06d88b30fc84dd524a641352ef48a82750507d
Samuele Maurizi <samuelemaurizi@gmail.com>
2018-08-23 13:30:13 +0200

    needs some style

---
+++ app.js
@@ -0,0 +1,54 @@
+
+const express = require('express');
+const hbs     = require('hbs');
+const app     = express();
+const path    = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials/');
+
+
+
+app.get('/', (req, res, next) => {
+  res.render('index', {title: "Home"});
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log("ALL BEERS:", beers)
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers', {beer: beers[0]});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/beers/:beerId', (req, res, next) => {
+  let beerId = req.params.beerId
+  punkAPI.getBeer(beerId)
+  .then(beers => {
+    res.render('beer-detail', {beer: beers[0]});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Connected...')
+});
