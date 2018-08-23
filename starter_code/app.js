10357d1e4ee012067f33af21ee8a81add970606a
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2018-08-23 09:08:55 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,29 @@
-
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+    res.render("index");
 });
 
+app.get("/beers", (req, res, next) => {
+    punkAPI
+        .getBeers()
+        .then(beers => {
+            res.render("beers");
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000);
