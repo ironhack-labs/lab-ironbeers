c09e1bd935da991388f70fd851b25547780695c9
John Dimitroff <johnmdimitroff@gmail.com>
2018-08-30 16:35:40 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,28 @@
 
-const express = require('express');
-const hbs     = require('hbs');
+const express = require("express");
+const hbs     = require("hbs");
 const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const path    = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.render("index.hbs");
 });
 
+app.get("/beers", (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render("beers.hbs", {beers});
+      console.log(beers);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
 app.listen(3000);
