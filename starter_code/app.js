1ab2a7e66a58c1fe638fcf105de29f9c775e4679
Lauren Cahill <laurencahill@live.com>
2018-09-05 17:42:45 -0400

    finished through iteration 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,13 +5,31 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
-
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  
+  punkAPI.getBeers()
+ 
+  .then(beers => {
+    res.render('beers', {beerList: beers});
+    
+  })
+  .catch(error => {
+    console.log(error)
+})
+
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
 });
 
 
