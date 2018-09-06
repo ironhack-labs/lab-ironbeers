f01d422e97924e4b890c5104ed6ccad6090e9962
InnaYugay <yuinnaa@gmail.com>
2018-09-06 10:17:58 -0400

    almost done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,49 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(theBeers => {
+    console.log(theBeers[0])
+
+      res.render('beers', {theList:theBeers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(randomBeer => {
+    console.log(randomBeer);
+    res.render('random-beers',{randomBeer} )
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+
+
+});
+
 
 
 app.listen(3000);
