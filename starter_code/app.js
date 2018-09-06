c7e85d33e4b812b47ac14dbb9095ada61e73b734
Sat Gavassa <satgavassa@gmail.com>
2018-09-05 21:53:43 -0400

    css continues to be my nightmare

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,9 +10,46 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(theBeers => {
+    
+    console.log(theBeers[0])
+    
+        res.render('beers', {theList:theBeers });
+
+    })
+    .catch(error => {
+      console.log(error)
+      console.log('no beers')
+  });
+});
+
+
+
+
+app.get('/random', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(oneBeer => {
+    let randomBeer= oneBeer[0];
+    console.log('-random beer ------')
+    console.log(randomBeer)
+    console.log('-beer ingredients ------')
+    console.log(randomBeer.ingredients)
+    res.render('random', {randomBeer});
+  })
+  .catch(error => {
+    console.log(error)
+    console.log('no random beer')
+  });
+});
+
 
 
 app.listen(3000);
