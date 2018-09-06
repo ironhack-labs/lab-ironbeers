245f2e06c18d6468205779cc4f181da9ceedb965
batuhaandogan <tipala_1909@hotmail.com>
2018-09-06 11:07:37 -0400

    adsa

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,49 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('');
 });
+
+// app.get('/home', (request, response, next) => {
+//   response.sendFile(__dirname + '/views/home.html')
+// });
+
+
+app.get('/beers', (request, response, next) => {
+
+
+punkAPI.getBeers()
+.then(daBeers => {
+     console.log(daBeers)
+     response.render('beers', {theList:daBeers });
+})
+.catch(error => {
+console.log(error)
+})
+
+
+
+  // response.sendFile(__dirname + '/views/beers.html')
+  
+});
+
+app.get('/random', (request, response, next) => {
+
+  punkAPI.getBeers()
+  .then(randomB => {
+       console.log(daBeers)
+       response.render('random', {theRandom:randomB[0] });
+  })
+  .catch(error => {
+  console.log(error)
+  })
+
+  response.render('randomBeer');
+});
+
 
 
 app.listen(3000);
