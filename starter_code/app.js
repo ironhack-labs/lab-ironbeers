e23854fd2bda45750233d6aa28c0c5b35ff47c9e
HananeDUP <hanane.moualil@gmail.com>
2018-09-11 21:29:03 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,33 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    // console.log(beers)
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      // console.log(beers)
+      res.render('randomBeer', {beers:beers[0]});
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
 app.listen(3000);
