e9197d2912f42bbe386cfc5aba5d76f8ab2ab6d0
agdwm <almudenaguerraswm@gmail.com>
2018-09-12 22:17:31 +0200

    Created beerIteration 5 - Beer Partial - done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,21 +6,24 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views',	 __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('/', (req, res, next) => {
+	res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
 	punkAPI.getBeers()
 		.then(beers => {
-			res.render('index');
+			res.render('beers', {beers});
 		})
 		.catch(error => {
 			console.log(error)
 		})
-});
-
-app.get('/beers', (req, res, next) => {
-	res.render('beers');
 });
 
 app.get('/random-beers', (req, res, next) => {
