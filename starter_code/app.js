d4635357c3875165848cfbafbe2e2f83ed07d8cf
mariaalons <calpurniavee@gmail.com>
2018-09-13 00:23:54 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,11 +8,36 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partial');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+   res.render('beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+   // console.log(beers)
+    res.render('randombeer', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+
+
+
+app.listen(3000)
