9c701dc8a3eac13ec9490d6aa3b0025563be023b
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-09-12 22:24:07 +0200

    Iteration 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,6 +9,8 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
+
 
 app.get('/', (req, res, next) => {
   res.render('');
