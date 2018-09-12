a5ced9ab6bc63c6360e9b9adfeb55da86e5092eb
agdwm <almudenaguerraswm@gmail.com>
2018-09-12 19:58:09 +0200

    First iteration done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -11,8 +10,15 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+	res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+	res.render('beers');
+});
+
+app.get('/random-beers', (req, res, next) => {
+	res.render('random-beers');
+});
 
 app.listen(3000);
