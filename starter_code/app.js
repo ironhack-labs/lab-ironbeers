331e6cc05fb25bff79af1bb5e7a1233a87dac4eb
Francis Wardley <fwardley90@gmail.com>
2018-09-13 09:42:42 +0200

    not finished

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,17 +9,24 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   let data = {
     imageSrc: 'images/beer.png'
-    
+
   }
   res.render('index', data);
 });
 
 app.get('/beers', (req, res, next) => {
-  res.render('beers');
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 app.get('/random-beers', (req, res, next) => {
