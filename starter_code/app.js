0bdc98bfafaeb15e639378b3b6495d055fb2a13c
Esperanza Amaya <eamayag@gmail.com>
2018-09-13 09:10:27 +0200

    uncompleted

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,14 +5,21 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
-
+const port = 3000;
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views'); //en dirname no se pone el folder de app.js? en ese caso, como está fuera de views, sería starter-code, no?
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index');   //duda: ¿cuándo hay que poner res.sender y cuándo res.sendFile con dirname? no encuentro la diferencia...
+});
+
+app.get('/', (req, res, next) => {
+  res.render('layout'); //esta no hacía falta ponerla, no?
+});
+
+app.listen(port, () => {
+  console.log(`Port ${port}`);
 });
 
 
-app.listen(3000);
