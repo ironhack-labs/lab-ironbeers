d467de3adee34e37ce965c0a1eaa83bcb71b5d40
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-09-13 08:17:42 +0200

    Iteration 6

--- starter_code/app.js
+++ starter_code/app.js
@@ -18,7 +18,6 @@ app.get('/', (req, res, next) => {
 app.get('/beers', (req, res, next) => {
   punkAPI.getBeers()
   .then(beers => {
-    console.log(beers)
     res.render('beers',{beers});
   })
   .catch(error => {
@@ -26,7 +25,14 @@ app.get('/beers', (req, res, next) => {
   })
 });
 app.get('/random-beers', (req, res, next) => {
-  res.render('random-beers');
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers)
+    res.render('random-beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 
