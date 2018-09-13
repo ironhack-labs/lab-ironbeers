2647f8175d8ac67f82164c37b0a235296fdf3b0c
joebit213 <joseph@evatic.org>
2018-09-12 21:50:42 -0500

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,9 +10,26 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res, next) => {
+  res.render('randombeers');
+});
+
+
+app.listen(3000, () => console.log('Servidor listo!!!'));
