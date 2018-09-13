4fe80931b515d386fadcc929b908e052ebb4f6b5
pablotaboada <pablo.tabu@hotmail.com>
2018-09-13 09:13:04 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,11 +8,35 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partial')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
+    punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
+  
+});
 
-app.listen(3000);
+app.get('/random_beers', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.listen(3000,() =>{
+console.log('pepe')
+})
