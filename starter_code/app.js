368b76fcb1e67e575023fca80cd3039aebf59f0e
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-09-13 09:13:40 +0200

    almost done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,11 +8,25 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res) => {
+  res.render('random-beers');
+});
 
 app.listen(3000);
