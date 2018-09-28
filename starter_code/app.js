70c0475aecd6c7be68438aea3eee8c5c0cf073bb
sannchez-Mx <sannchez-mx@outlook.com>
2018-09-28 15:11:34 -0500

    terminado

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,9 +10,33 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + "/views/partials")
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/', (req, res, next) => {
+  res.render('layout');
+});
+app.get("/beers", (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render("beers", { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+ app.get("/random-beers", (req, res, next) => {
+  punkAPI.getRandom()
+    .then((beers) => {
+      res.render("random-beers", { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
-app.listen(3000);
+app.listen("4000", () => {
+  console.log("Servidor listo")
+});
