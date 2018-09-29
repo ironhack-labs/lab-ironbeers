98e4e2efa1ae2f7302c9c7e290478b1a767fd27d
Adolfo Vela <vela@tekplanner.com>
2018-09-28 19:36:34 -0500

    Listo iteracion 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,19 +9,27 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// REGISTRO PARTIALS
+hbs.registerPartials(__dirname + "/views/partials/")
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 // BEERS
 app.get('/beers', (req, res) => {
-  punkAPI.getBeers()
+ const beers = punkAPI.getBeers()
+
     .then(beers => {
-      res.render("beers", beers) //Se manda a la vista
+      res.render("beers", { beers }) //Se manda a la vista
+      //console.log(beers)
+
     })
     .catch(error => {
       console.log(error)
     })
+
+
 })
 
 // Servidor en el puerto 3000
