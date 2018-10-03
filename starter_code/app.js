1e057f7a78d3211df598bf20ea430f9a6b419303
Mizraim17 <mizraim.martinez@gmail.com>
2018-10-03 01:21:13 -0500

    ya

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -10,9 +9,34 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Partial
+hbs.registerPartials(__dirname + "/views/partials/")
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beer', (req, res) => {
+    punkAPI.getBeers()
+        .then(beers => {
+            res.render("beer", {
+                beers
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+});
+
+app.get('/random-beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beers => {
+            res.render("random-beers", {
+                beers
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+});
 
 app.listen(3000);
