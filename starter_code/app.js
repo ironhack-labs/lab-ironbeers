d925a6bec7883eb6328760b6a5fec12c3480c557
Alberto Martinez <jose_alberto12216@hotmail.com>
2018-10-08 22:49:33 -0500

    Se finaliza ejercicio de IronBeers by Albert

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,6 +9,8 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + "/views/partials");
+
 
 
 
@@ -16,6 +18,25 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers',(req,res) => {
+  punkAPI.getBeers()
+      .then(beers =>{
+        res.render('beers',{beers});
+      })
+      .catch(error =>{
+        console.log('error at getBeers => ' + error )
+      })
+});
+
+app.get('/random-beers',(req,res) => {
+  punkAPI.getRandom()
+      .then(beer => {
+        res.render('random-beer', {beer});
+      })
+      .catch(error => {
+        console.log('Error at => ' + error);
+      })
+});
 
 
 app.listen(3000, () =>{
