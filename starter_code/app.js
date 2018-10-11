ee270cfe4038423fd2c5bf6bf2efe351db105352
Ana Moreno <anamariamoreno@gmail.com>
2018-10-11 17:01:31 +0200

    beers

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,36 @@
 
 const express = require('express');
-const hbs     = require('hbs');
+const expressLayouts = require('express-ejs-layouts');
 const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/layout');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+app.get('/random-beer', (req, res, next) => {
+  res.render('random-beer');
+});
 
 
 app.listen(3000);
