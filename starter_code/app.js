9d25dbc847932258dcc12e4137021ed77cf0fc33
Alexandra Pedroche <alexandrapedroche@gmail.com>
2018-10-11 17:15:50 +0200

    falta css chachi

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,19 +1,35 @@
-
 const express = require('express');
-const hbs     = require('hbs');
+const expressLayouts = require('express-ejs-layouts');
 const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
 });
 
 
