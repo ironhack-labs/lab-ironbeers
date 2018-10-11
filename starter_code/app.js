aa5d64816ca9bc562254f18d047054345cf2bad5
RaulCote <raulcote@hotmail.com>
2018-10-11 17:00:29 +0200

    Primer push

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,14 +1,22 @@
 
 const express = require('express');
-const hbs     = require('hbs');
+//const ejs     = require('ejs');
 const app     = express();
 const path    = require('path');
+const port = 3000;
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const expressLayouts = require('express-ejs-layouts');
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static('public'));
+
+// configurando ejs
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/layout');
+
 
 
 
@@ -16,6 +24,23 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers})
+    console.log({beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers')
+})
 
 
-app.listen(3000);
+app.listen(port, (error) => {
+  // console.log(`Example app listening on port ${port}!`)
+  error ? console.log(error) : console.log('running on 3000');
+})
