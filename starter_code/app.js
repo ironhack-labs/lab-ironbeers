29ac79dc7c3deac7c9f0b381ee664801f10a9b77
watnfoehn <hoehnefelix@googlemail.com>
2018-11-01 09:16:07 +0100

    first iterations done. it works.

--- starter_code/app.js
+++ starter_code/app.js
@@ -16,6 +16,29 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  
+  punkAPI.getBeers()
+  .then(beers => {
+    
+    
+    res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
 app.listen(3000);
