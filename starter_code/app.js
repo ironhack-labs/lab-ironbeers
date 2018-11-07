94e95145b899826c3694e9eaad74c5ffb27a3d0d
Andre Amancio <amancioandre@gmail.com>
2018-11-07 15:33:46 -0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,25 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  const beers = punkAPI.getBeers();
+  beers.then(beersArr => {
+    res.render('beers', { beersArr })
+  });
+});
 
+app.get('/randomBeer', (req, res, next) => {
+  const randomBeer = punkAPI.getRandom()
+  randomBeer.then(beer => {
+    console.log(...beer);
+    res.render('randomBeer', ...beer);
+  })
+});
 
 app.listen(3000);
