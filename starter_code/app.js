e7926b7d2384cc6a14b16419e02901aa256c31d9
Mohsan <mohsanabbas>
2018-11-07 15:33:28 -0200

    half done

--- starter_code/app.js
+++ starter_code/app.js
@@ -14,8 +14,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+  // res.render('')
 });
+app.get('/beer', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beer',beers);
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  // res.render('')
+});
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+  // res.render('')
+});
+punkAPI.getBeers()
+  .then(beers => {
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
 
-
-app.listen(3000);
+app.listen(3001);
