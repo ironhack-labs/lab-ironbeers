1e14f6f9e4d6995cafe715253d938fbd98b0cdf0
JN Veigel <art.jnveigel@gmail.com>
2018-11-08 12:48:19 +0100

    intial version, working as intended

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,41 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
 
+app.listen(3000);
+
+// #####################################################
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+	res.render('index.hbs');
+});
+
+app.get('/beers', (req, res, next) => {
+
+	const beers = punkAPI.getBeers()
+  		beers.then(beers => {
+  			res.locals.beerList = beers;
+  			res.render('beers.hbs');
+  	});
+
+	beers.catch(error => {
+		console.log(error);
+  	});
+});
+
+app.get('/random-beer', (req, res, next) => {
+	const randomBeer = punkAPI.getRandom();
+		randomBeer.then(randomBeer => {
+			res.locals.rdmBeer = randomBeer[0];
+			res.render('random.hbs');
+		});
+
+		randomBeer.catch(error => {
+			console.log(error);
+		});
 });
 
 
 
-app.listen(3000);
+
