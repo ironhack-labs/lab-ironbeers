b82ac7700289da4345d355ee43529238ab4f9c82
Romulo Franca <romulo2franca@gmail.com>
2018-11-08 09:25:36 -0200

    70%

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,32 @@
-
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
 const punkAPI = new PunkAPIWrapper();
+const PORT = 8080;
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  const beers = punkAPI.getBeers();
+  beers.then((beersArr) => {
+    res.render('beers', { beersArr });
+  });
+});
 
+app.get('/random-beers', (req, res, next) => {
+});
 
-app.listen(3000);
+app.listen(PORT);
