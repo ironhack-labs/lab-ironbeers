21738388c3320971f62f1b083c4c681764085ffc
amine <bouamama.amine@bcg.com>
2018-11-08 12:16:28 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -10,12 +9,33 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {beers});
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('randomBeer', beers[0]);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
 
-app.listen(3000);
+app.listen(5000, () => {
+  console.log("server started")
+});
