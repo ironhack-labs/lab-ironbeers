14b721e1d099744250bcbd955114c8b2c67e23f7
Sarah Connor <sarahchamorro@gmail.com>
2018-11-07 23:54:05 -0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,27 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  const beers = punkAPI.getBeers();
+  beers.then(beersArr => {
+    res.render('beers', { beersArr })
+  });
+});
 
+app.get('/randomBeer', (req, res, next) => {
+  const randomBeer = punkAPI.getRandom()
+  randomBeer.then(beer => {
+    res.render('randomBeer', ...beer);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
-app.listen(3000);
+app.listen(3000, () => console.log("server started!"));
