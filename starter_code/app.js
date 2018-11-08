29ce35b31cfa46ed8673cd2755986bd3981fa889
aemartos <aemartos@gmail.com>
2018-11-09 00:37:30 +0100

    Set files structure, layout with styles, index page, app.js routes and config

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,41 @@
-
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const express = require("express");
+const hbs = require("hbs");
+const _ = require("lodash");
+const app = express();
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
+hbs.registerPartials(__dirname + '/views/partials');
 
-
-
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  let data = {
+    name: "home",
+    home: true
+  };
+  res.render("index", data);
 });
 
+app.get("/beers", (req, res, next) => {
+  let data = {
+    name: "beers",
+    beers: true
+  };
+  res.render("beers", data);
+});
 
+app.get("/random-beers", (req, res, next) => {
+  let data = {
+    name: "randomBeer",
+    randomBeer: true
+  };
+  res.render("random-beers", data);
+});
 
-app.listen(3000);
+const port = 3000;
+app.listen(port,() => {
+  console.log(`Ready on port ${port}`);
+});
