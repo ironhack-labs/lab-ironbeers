d393a21f5046cd59d00e236ed31f2a5abe784de9
Corrado Passera <corradopassera@hotmail.com>
2018-11-08 12:32:58 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,34 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get("/beers", (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.locals.beers = beers;
+    res.render("beers");
+  })
+  .catch(error => {
+    console.log("No beers :'( ", error)
+  })
+})
+
+app.get("/random-beer", (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.locals.beer = beers[0];
+    res.render("randomBeer");
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 
 app.listen(3000);
