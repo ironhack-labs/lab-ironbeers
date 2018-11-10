87536adb76f0f2854bc3496ea99babefe2120d3d
Alfredo Hinarejos <alfredo.hina@gmail.com>
2018-11-10 14:37:29 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,10 +11,49 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+hbs.registerPartials(__dirname + '/partials')
+
+
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  let title = "IronBeers"
+  res.render("index",{
+    title
+  });
 });
+
+app.get('/beers', (req, res, next) => {
+  let title = "Beers";
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers",{
+      title,
+      beers
+    });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
+
+app.get('/randombeer', (req, res, next) => {
+  let title = "Random Beer"
+  punkAPI.getRandom()
+  .then(beers => {
+    let beer = beers[0];
+    res.render("randombeer",{
+      beer,
+      title
+    });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
 
 
 
