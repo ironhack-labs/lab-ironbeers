d268f11a94d23d51c812f6d306fd80c847dbb6f9
sandrarp <sandra.rueda.pr@gmail.com>
2018-11-10 17:24:08 +0100

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
 const path    = require('path');
+const _ = require('lodash');
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -15,6 +19,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+   punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    let beer = _.sample(beers);
+    res.render('randomBeer', {beer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 
 
