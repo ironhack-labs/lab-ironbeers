1200a6f4b37c66ddc06a5f9fa34cd169e1c3fff1
mannysin <sinclair2610@gmail.com>
2018-11-13 10:27:12 -0500

    need to fix paths

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,6 +8,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -16,6 +17,27 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+   res.render('beers', {beers: beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  res.render('/beers');
+});
+
+app.get('/randombeers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(theRandomBeer => {
+    res.render('randombeers');
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
 
 app.listen(3000);
