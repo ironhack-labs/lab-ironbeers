7a0962a68aa42dd62ae1d6aa3c34b8d52c361405
DSauthier <danielsauthier@live.com>
2018-11-13 09:03:18 -0500

    retrieving PunkAPI and styling is 50% complete

--- starter_code/app.js
+++ starter_code/app.js
@@ -13,7 +13,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res, next) => {
+
+
   res.render('index');
+
+
+});
+
+app.get("/beers", (req, res, next) => {
+
+  punkAPI.getBeers()
+    .then((listOfBeers) => {
+      // console.log("==================== ", listOfBeers[0])
+      res.render('beers', { listOfBeers: listOfBeers})
+
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+
+});
+
+app.get("/random-beers", (req, res, next) => {
+
+
+  punkAPI.getRandomBeer()
+    .then((listOfBeers) => {
+      console.log("-=-==--=-=-==--==-"+listOfBeers[0])
+      res.render('random-beers', { listOfBeers: listOfBeers[Math.floor(Math.radnom)* 25] })
+
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  
 });
 
 
