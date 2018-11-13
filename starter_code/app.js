1cc2e3cefd29fd17d4b4cd9934358505e9646756
Javier <jarrindaulivi4@gmail.com>
2018-11-13 11:02:28 -0500

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,6 +8,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -16,6 +17,31 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+//this route is for pulling all the beers from the database
+app.get('/beers', (req, res, next) => {
+  //getBeers is like a function that pulls from the database
+  punkAPI.getBeers()
+  .then(theListOfBeers => {
+
+    res.render('beers', {beers: theListOfBeers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(theRandomBeer => {
+
+    res.render('random-beers', {oneBeer: theRandomBeer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
 app.listen(3000);
