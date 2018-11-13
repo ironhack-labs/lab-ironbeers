d3517e390cd7e6ce3121e2d46deb52e14c2d6e42
Ross Gravitte <Rbgravitte@gmail.com>
2018-11-13 10:17:14 -0500

    list of beers showing up need to add styling and one random beer

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,15 +11,27 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/beers', (req, res, next)=>{
-      res.render('beers');
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
   });
 
 app.get('/', (req, res, next) => {
+ 
+
+
   res.render('index');
+
   });
 
 app.get('/random-beers', (req, res, next)=>{  
       res.render('random-beers');
+
+      
   });
   
 
