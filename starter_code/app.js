a54e9cd1958e70a0b48ce6fec2f832912721a4e8
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-11-14 13:10:52 +0100

    Iteracion 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,35 @@
 
 const express = require('express');
 const hbs     = require('hbs');
+
 const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(`${__dirname}/views`);
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then((beers) => {
+      res.render('beers', { beers });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
 
 app.listen(3000);
