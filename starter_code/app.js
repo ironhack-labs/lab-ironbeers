dfc9fb013295446129638dca601811e7155c5ca5
Jorge Devesa <jrgdevesa@gmail.com>
2018-11-14 16:35:11 +0100

    iteration 4 done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,8 +11,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(__dirname + "/views/partials")
 
 app.get('/', (req, res, next) => {
   res.render('index');
@@ -23,7 +22,6 @@ app.get('/beers', (req, res, next) => {
   punkAPI.getBeers()
   .then(beers => {
     res.render('beers', {beers});
-    console.log({beers})
   })
   .catch(error => {
     console.log(error)
