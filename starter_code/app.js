b8f00f5c7cd79b344d896f16bfd3be7784259e16
Anna Zhuravleva <helloboysandgirls.az@gmail.com>
2018-11-14 12:57:38 +0100

    Iteration 3

--- starter_code/app.js
+++ starter_code/app.js
@@ -14,13 +14,20 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 app.get('/beers', (req, res, next) => {
-  res.render('beers');
+  punkAPI.getBeers()
+ .then(beers => {
+  res.render('beers', {beers});
+ })
+ .catch(error => {
+   console.log(error)
+ });
+
+  
 });
 
 app.get('/random-beers', (req, res, next) => {
