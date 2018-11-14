e2c0bb0e140a698e73f3393349a20dd39f31a399
Anna Zhuravleva <helloboysandgirls.az@gmail.com>
2018-11-14 12:29:37 +0100

    Iteration 1

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
 const path    = require('path');
+
+const PORT = 3000;
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -16,6 +19,16 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
 
-app.listen(3000);
+
+app.listen(PORT, () => {
+  console.log(`listen ${PORT}`)
+});
