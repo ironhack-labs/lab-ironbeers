18f55e3f36fed4e94d1f78ad02abff4cec4eb5ac
adrigt00 <adriangomez@MacBook-Pro-de-Adrian.local>
2018-11-15 09:05:58 +0100

    5 iteration

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
@@ -16,6 +17,24 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+// app.get('/be', (req, res, next) => {
+//   res.render('card');
+// });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
-app.listen(3000);
+
+
+app.listen(4000);
+''
+console.log("norma me he iniciado")
