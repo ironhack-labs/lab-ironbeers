1a83774be86bb420d4f6b6a380819349640ef763
Jack Watkins <jackwatk@Jacks-MacBook-Pro.local>
2018-11-15 14:59:04 +0100

    team sack

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,41 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random-beers', { beer: beers[0] });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-
+app.get('*', (req, res, next) => {
+  res.send('404');
+});
 
 app.listen(3000);
