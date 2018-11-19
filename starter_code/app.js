cb4ed12f227cee0110f7525c7fcf91f7f5d9e93e
Travis Grubbs <flygrubbs@yahoo.com>
2018-11-19 11:25:19 -0500

    added random beer link

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,10 +6,15 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
+// ================ MiddleWare ====================
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
+//  ===============================================
 
 
 app.get('/', (req, res, next) => {
@@ -17,5 +22,52 @@ app.get('/', (req, res, next) => {
 });
 
 
+// =====ROUTE TO GET ALL BEERS============
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(theListOfBeers => {
+    // console.log("===============", theListOfBeers)
+    res.render('beers', {beers: theListOfBeers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+})
+
+
+//======ROUTE TO GET SINGLE RANDOM BEER
+
+
+  
+app.get('/randombeer', (req, res, next) =>{
+    punkAPI.getRandom()
+    .then(theRandomBeer => {
+
+
+      res.render('randombeer', {oneBeer: theRandomBeer})
+    })
+    
+    .catch((err) => {
+      next(err)
+      console.log(err);
+    })
+  })
+    
+  
+
+
+
+
+
+
 
 app.listen(3000);
+
+// punkAPI.getBeers()
+// .then((listOfBeers)=>{
+//   // console.log("the beers list ------ ", listOfBeers);
+//   res.render('beers', {listOfBeers: listOfBeers})
+
+// })
