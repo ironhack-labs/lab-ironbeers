efd47f3ac9ac45b13b2f69b3c6d672674a9a7eee
MefitHp <mefitDev@gmail.com>
2018-11-21 12:48:06 -0600

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -13,9 +13,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+app.get('/randombeer', (req, res) => {
+  punkAPI.getRandom()
+    .then(randomBeer => {
+      console.log(randomBeer)
+      res.render('random', { randomBeer })
+    }).catch(err => {
+      console.log(err);
+    })
+})
 
 
-app.listen(3000);
+
+app.listen(3000, () => {
+  console.log('Server initialized');
+
+});
