3784ead5276151255a888b0b1b0e189e1dfc965b
conviertemx <ro@convierte.mx>
2018-11-21 13:13:22 -0600

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -13,9 +13,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
-});
+  punkAPI.getBeers()
+  .then(beers =>{
+    console.log(beers)
+    res.render('index',{beers});
 
+  })
+})
 
+  app.get('/beers',(req,res)=>{
+    punkAPI.getBeers()
+    .then(beers=>{
+      res.render('beers',{beers});
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+  });
 
-app.listen(3000);
+  app.get('/randombeer',(req,res)=>{
+    punkAPI.getRandom()
+    .then(beer=>{
+      res.render('random',{beer});
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+  });
+
+app.listen(3000,()=>{
+  console.log('listening on 3000')
+})
