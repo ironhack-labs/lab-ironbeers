3ad8499bbeaf24117c5b66bb00b586e359020e7e
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-21 12:58:35 -0600

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -10,12 +9,30 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{beers});
+  })
+  .catch(err => {
+    console.log(err)
+  })
+});
 
+app.get('/randombeer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    res.render('random',{beer});
+  })
+  .catch(err => {
+    console.log(err)
+  })
+});
 
-app.listen(3000);
+app.listen(3000,()=>{
+  console.log('Listening on 3000')
+});
