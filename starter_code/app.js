8acf112eab9a641c15d572e9356ffb4b71ebc261
sabasm <smma1992@gmail.com>
2018-11-21 20:59:26 -0600

    MEX PP Sabás Mendívil IronBeers

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,40 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/',(req,res)=>{
+  punkAPI.getBeers()
+      .then(beers =>{
+        console.log(beers)
+          res.render('index',{beers})
+      })
+      .catch(err => {
+        console.log(err)
+      })
+})
 
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beers =>{
+            console.log(beers)
+            res.render('beers',{beers})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+});
 
-
-
-app.listen(3000);
+app.get('/randombeer',(req,res)=>{
+    punkAPI.getRandom()
+        .then(beer =>{
+            res.render('random',{beer})
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+})
+app.listen(3000,()=>{
+  console.log('Server up and running at port: 3000')
+});
