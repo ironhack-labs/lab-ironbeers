00c503bc2d718f39c2afdef938bda3e5d7aad655
Merwan Guerrib <merwanguerrib@gmail.com>
2018-11-22 21:36:46 +0100

    iteration 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,21 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
 app.listen(3000);
