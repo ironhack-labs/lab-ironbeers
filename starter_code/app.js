9fef5cf39f3d15b85846ae9a1946f17076049920
nicolas prieur <nicolas.prieurdelacomble@gmaile.com>
2018-11-22 21:42:10 +0100

    last commits

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,15 +9,24 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
-
-  
 });
 
+app.get('/beers', (req, res, next) => {
+ punkAPI.getBeers()
+   .then(beers => {
+     // res.send(beers)
+     res.render('beers', {beers});
+   })
+   .catch(error => {
+     console.log(error)
+   })
+});
 
 
 app.listen(3000);
