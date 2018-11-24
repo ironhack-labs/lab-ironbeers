c1e23a6362191fc80dc69eccb75b8861c2e47d6f
unknown <laetitiaroche53@gmail.com>
2018-11-24 11:36:54 +0100

    PAR PT Laëtitia

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,20 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers');  
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
+  hbs.registerPartials(__dirname + '/views/partials')
 
 app.listen(3000);
+
