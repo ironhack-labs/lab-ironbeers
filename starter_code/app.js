da70e3c25247715ff324bcc999ed7483442aee8f
Antoine Ironhack <antoineironhack@MacBook-Pro-de-Antoine.local>
2018-11-24 13:51:50 +0100

    first-push-beers

--- starter_code/app.js
+++ starter_code/app.js
@@ -16,6 +16,29 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      // res.send(beers)
+      res.render('beers', {beers});
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
+
+app.get('/random-beer', (req, res, next) => {
+punkAPI.getRandom()
+  .then(beers => {
+  // res.send(beers)
+  res.render('randomBeer', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.listen(3000);
