eb235ce257bb7e3cb515abfbe3fac0055fa7f466
Axel Heinz <axelmheinz@gmail.com>
2018-12-11 22:01:09 +0100

    done iteration 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,12 +8,28 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', { myBeerArray: beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.get('/randombeer', (req, res, next) => {
+  res.render('randombeer');
 });
 
 
