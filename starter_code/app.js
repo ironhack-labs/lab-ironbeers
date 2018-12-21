2091e66c2e890d421da110a8a7352f3a8b9ffb04
Susanne Tünker <susanne.tuenker@hotmail.de>
2018-12-21 14:58:03 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,12 +9,33 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get("/random-beers", (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render("random-beers", beers[0])
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
 
 
 
