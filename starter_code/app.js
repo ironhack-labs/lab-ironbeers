7daecb8f8a5897a69486bd09935e0df89d3cb5f7
Cdelatorre <carlosdelatorrediaz@gmail.com>
2018-12-28 18:58:50 +0100

    partials & hbs layout structure done

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,11 +5,13 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res, next) => {
@@ -17,5 +19,7 @@ app.get('/', (req, res, next) => {
 });
 
 
+app.listen(PORT, () => {
+  console.info(`App listen ar ${PORT} port`);
+});
 
-app.listen(3000);
