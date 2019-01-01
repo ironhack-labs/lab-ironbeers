700520f30bb68fe75f088acb06de8daf1fa3d41b
Alfredo-Git <algonzalezramirez@gmail.com>
2019-01-01 22:35:27 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,6 +8,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -16,6 +17,27 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
 
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random_beers', (req, res, next) => {
+
+  punkAPI.getRandom()
+  .then(random_beers => {
+    res.render('random_beers',{random_beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
 app.listen(3000);
