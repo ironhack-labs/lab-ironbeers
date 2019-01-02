6a02693ca95a770c7a15ce8e86f3173982f44779
Moises Garcia <moisesgarcia83@gmail.com>
2019-01-02 13:49:02 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,17 +5,43 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  const beers = req.query;
+
+  punkAPI.getBeers(beers)
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  }) 
+});
 
 
-app.listen(3000);
+app.get('/randomBeer', (req, res, next) => {
+  
+  punkAPI.getRandom()
+  .then(beer => {
+    res.render('randomBeer', beer[0])
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
+app.listen(PORT, () => {
+  console.info('ya está operativo')
+})
