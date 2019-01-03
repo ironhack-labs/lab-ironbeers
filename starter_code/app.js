5c1e4e0057ba32ed1cf351b2a4d5c2b490c99c5f
Antonio Colmenares <colmena.anto@gmail.com>
2019-01-03 14:24:30 -0500

    I don't know how to add both bootstrap and css styles together using handlebars but it is done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,13 +8,44 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  const beers = req.query;
+
+  punkAPI.getBeers(beers)
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  }) 
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    res.render('random-beer', beer[0])
+    console.log(beer[0]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
+
+
+
+
+
 
 
 
