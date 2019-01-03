b6dfc35a4431277d6754ba0e1f9f1bb80ea5fcf1
Nerea Aparicio <nerea.aparicio@acuntia.dom>
2019-01-03 17:23:26 +0100

    layout page done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,11 +11,29 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/Beers', (req, res, next) => {
+  res.render('Beers');
+});
 
+// punkAPI.getBeers()
+//   .then(beers => {
+
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
 app.listen(3000);
+
