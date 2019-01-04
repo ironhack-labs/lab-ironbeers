8a197d205e96185a7b4765edfdcdc0bd52989d2e
Nerea Aparicio <nerea.aparicio@acuntia.dom>
2019-01-04 15:08:01 +0100

    it works, need css

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,34 +5,41 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+// const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 app.get('/Beers', (req, res, next) => {
-  res.render('Beers');
+  const {beers} = req.query;
+
+  punkAPI.getBeers(beers)
+  .then(beers => {
+    res.render('Beers',{beers});
+    console.log ({beer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
-// punkAPI.getBeers()
-//   .then(beers => {
-
-//   })
-//   .catch(error => {
-//     console.log(error)
-//   })
 
 app.get('/random-beers', (req, res, next) => {
-  res.render('random-beers');
+
+  punkAPI.getRandom()
+  .then(beer => {
+    res.render('random-beers', beer[0]);
+    console.log (beer[0]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 app.listen(3000);
