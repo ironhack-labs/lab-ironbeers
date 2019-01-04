a437bebfbda4aa96d2dab9c5810aceaf00eb180c
Patricia Defez <ptddds@gmail.com>
2019-01-04 13:29:37 +0100

    almost done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,35 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    //res.send(beers);
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/randomBeer', (req, res, next) => {
+  const beer = punkAPI.getRandom()
+  .then(beer => {
+    
+     //res.send(beer[0]);
+    res.render('randomBeer', beer[0]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
