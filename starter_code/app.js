a79742e88e239893eef242ee34a48892bd7503b7
JZunigaV <j_zuniga4@hotmail.com>
2019-01-05 00:41:42 -0600

    Terminado

--- starter_code/app.js
+++ starter_code/app.js
@@ -30,18 +30,30 @@ hbs.registerHelper("grouped_each", function(every, context, options) {
 });
 
 //Home
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   res.render("index");
 });
 
 //Beer route
-app.get("/beers", (req, res, next) => {
+app.get("/beers", (req, res) => {
   punkAPI
     .getBeers()
     .then(beers => {
       res.render("beers", { beers });
     })
     .catch(console.error);
+});
+
+//Random beer
+app.get("/random-beer", (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beers => {
+      res.render("randomBeer", beers[0]);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 app.listen(3000, () => {
