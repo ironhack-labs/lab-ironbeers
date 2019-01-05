59725e6ef30c1b25f4670f8280747134ad72401f
“leonzlotnik” <zlotnik.leon91@gmail.com>
2019-01-05 12:06:55 -0600

    Logo de cerveza

--- starter_code/app.js
+++ starter_code/app.js
@@ -16,6 +16,23 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', {beers});
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+  
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers')
+});
 
 
-app.listen(3000);
+app.listen(3000, () => console.log('Este servidor esta en el puerto 3000'));
