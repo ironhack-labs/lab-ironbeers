28b1884bbdeb13b089c0e92d1f6be37f342556c7
Lucia Cazorla <a.lucia.cazorla@gmail.com>
2019-01-09 14:02:54 +0100

    in progress

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,17 +5,41 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const PORT = 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers',(req, res, next) => {ç
+  punkAPI.getBeers()
+  .then(beers => {
+    const items = beers;
+    console.log(items);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  res.render('beers',{items});
+})
 
+app.get('/randombeers',(req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    const items = beers;
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  res.render('randombeers',{items});
+})
 
-app.listen(3000);
+app.listen(PORT,() => {
+  console.info(`App listen at port ${PORT}`);
+});
