954883a3277c99ac38520f04b5020c4ef1126c1b
Marco Monter <monter.marco@gmail.com>
2019-01-14 16:45:52 -0600

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,11 +11,43 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next)=>{
+  punkAPI.getBeers()
+  .then(beers => {
+    //console.log(beers)
+    res.render('beers', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random',(req, res, next)=>{
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random', { beers })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+// hbs.registerHelper('each', function(beers, options) {
+//   let randombeer = [];
+//   for(i=0; i<beers.length; i++){
+//       let randombeer = beers[i]
+//       randombeer.push(randombeer)
+//   };
+//   let ri = Math.floor(Math.random() * randombeer.length);
+//   console.log(randombeer[ri])
+//   return randombeer[ri];
+// });
 
 app.listen(3000);
---
+++ starter_code/public/javascripts/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  console.log('IronGenerator JS imported successfully!');
+
+}, false);
+
+
