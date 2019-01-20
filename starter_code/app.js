0de8a289d336615f5989d16dc393ff8bfd333e7a
williamfiallos <wfiallos10@gmail.com>
2019-01-20 13:20:47 -0500

    update hbs files, views/partials

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,32 +5,47 @@ const app = express();
 // package that allows templating and dynamic views
 const hbs = require('hbs');
 
-// sets hbs as default view engine
-app.set('view engine', 'hbs');
-
-// in order to use the partials I have to register them!
-hbs.registerPartials(__dirname + '/views/partials');
-
-
-
-const path    = require('path');
+const path = require('path');
 
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const punkAPI = new PunkAPIWrapper();
 
-
-// routes:
-
+// sets hbs as default view engine
+app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// in order to use the partials I have to register them!
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+// routes:
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', { beerCard:beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/randomBeers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeers', { beerCard:beers })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
-app.listen(3000);
+app.listen(3000, () => console.log("Listening IronBeers on 3000."));
