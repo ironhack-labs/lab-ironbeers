aff5728eaa676e88246d4a519b05a33bfc607df8
mrosas22 <miller.rosas22@gmail.com>
2019-01-22 16:51:19 -0500

    Finished Iteration 6

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,10 +6,14 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+//static configuration
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+// Make everything inside of public/ available
 app.use(express.static(path.join(__dirname, 'public')));
 
+//create a partial folder inside view to store our partials
+hbs.registerPartials(__dirname + '/views/partials');
 
 //First route
 app.get('/', (req, res, next) => {
@@ -17,13 +21,30 @@ app.get('/', (req, res, next) => {
 });
 
 //Second route
-app.get('/beers', (req, res, next) => {
-  res.render('beers');
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      console.log(beers);
+      res.render("beers", { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 //Third route
 app.get('/random-beer', (req, res, next) => {
-  res.render('random-beer');
+  punkAPI
+    .getRandom()
+    .then(beers =>{
+      console.log(beers);
+      res.render("random-beer", { beers });
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+
 });
 
 app.listen(3000);
