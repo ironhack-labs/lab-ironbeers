ea6aee3495c1693874d7ddaf56968d4873433bb6
Christopher Vazquez <c_vazquez08@yahoo.com>
2019-01-23 19:57:05 -0500

    need styling for random-beer page

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,6 +10,8 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 
 app.get('/', (req, res, next) => {
@@ -17,5 +19,33 @@ app.get('/', (req, res, next) => {
 });
 
 
+app.get('/beers', (req,res,next) => {
+  // method from punkAPI
+  punkAPI.getBeers()
+  // response from database 'beers' placeholder
+  .then(beers => { // ====> this holds success callback
+  // rendering the data to page with {beers} object
+  // console.log("Response from DB: " beers);
+  res.render('beers', {beers});    
+  })
+  .catch(error => { // =====> holds error callback
+    console.log(error)
+  })
+});
 
+
+app.get('/random-beer', (req,res,next) => {
+  // getRandom is method from punkAPI
+  punkAPI.getRandom()
+  
+  .then(beers => {
+// rendering object to page
+// beer variable 
+    res.render('random-beers', {beer:beers[0]})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+})
 app.listen(3000);
