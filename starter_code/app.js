e8351c3b2dff31efe02784375a39f1d23b2f423b
Nicolas Castro <nicolascastro@me.com>
2019-01-23 19:58:52 -0500

    Missing Styling

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,24 +3,43 @@ const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
 const path    = require('path');
-//const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-//const punkAPI = new PunkAPIWrapper();
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 app.get('/beers', (req, res, next) => {
-  res.render('beers');
+  punkAPI.getBeers()
+  //beerData is just a placeholder in can be any word
+    .then(beerData => {
+      //console.log(beerData)
+  //beerData is what i will use to call {{#each beerData}} {{/each}}    
+      res.render('beers', { beerData });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
+
 app.get('/randombeer', (req, res, next) => {
-  res.render('randombeer');
+
+  punkAPI.getRandom()
+  .then(beerData => {
+    //console.log(beerData[0])
+    res.render('randombeer', { randomBeer:beerData[0] });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
 });
 
 
