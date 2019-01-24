b8c167553261f650b04f5c2f8e400ff146166477
williamfiallos <wfiallos10@gmail.com>
2019-01-23 19:58:09 -0500

    update in class codealong

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,14 +1,10 @@
 // require all the packages you install
 const express = require('express');
 const app = express();
-
 // package that allows templating and dynamic views
 const hbs = require('hbs');
-
 const path = require('path');
-
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const punkAPI = new PunkAPIWrapper();
 
 // sets hbs as default view engine
@@ -17,7 +13,8 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// in order to use the partials I have to register them!
+// in order to use the PARTIALS I have to REGISTER them!
+// partials are used to avoid repeating code
 hbs.registerPartials(__dirname + '/views/partials');
 
 
@@ -28,20 +25,26 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/beers', (req, res, next) => {
+  // .getBeers => is method we get from punkAPI
   punkAPI.getBeers()
-  .then(beers => {
-    console.log(beers);
-    res.render('beers', beers);
+  // responseFromDB is just a placeholder, can be any word
+  .then(responseFromDB => {  // =======================> ".this()" holds success callback
+    // console.log("Response is: ", responseFromDB); <-working well!
+                          // we are renaming responseFromDB to allBeers variable
+                          // which we'll use in the views
+    res.render('beers-info', { allBeers:responseFromDB });
   })
-  .catch(error => {
+  .catch(error => {  // ===============================> ".catch()" is failure callback
     console.log(error)
   })
 });
 
 app.get('/randomBeers', (req, res, next) => {
+  // .getRandom() is method given by punkAPI
   punkAPI.getRandom()
-  .then(beers => {
-    res.render('randomBeers', beers);
+  .then(someResponse => {
+    console.log("some response =========> ", someResponse[0]);
+    res.render('randomBeers', { randomBeer:someResponse[0] });
   })
   .catch(error => {
     console.log(error)
