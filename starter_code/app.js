a386fefca914c1f7793f5998e6d32b9909a64608
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-01-25 19:45:38 -0500

    Done with all steps - may need to updatestylyng on beers view

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,11 +9,34 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('home');  
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beerData => {
+    res.render('beers', beerData);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+    
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beerData => {
+    res.render('random-beers', beerData[0]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
 });
 
 
