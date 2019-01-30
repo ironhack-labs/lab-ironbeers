f3e41e54ec462354ec6b3e34e8e4545276f1f0c1
jsanz24 <jesus240492@gmail.com>
2019-01-31 00:25:45 +0100

    sin maquetar

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,9 +6,13 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
+
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
@@ -16,6 +20,27 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    
+    res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
-app.listen(3000);
+
+app.listen(2400);
