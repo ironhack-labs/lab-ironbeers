19678a0d3d6384e3337e3a9b026c52e36a0e6b1e
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-01-30 22:34:35 +0100

    almost done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,27 +1,53 @@
 
 const express = require('express');
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(expressLayouts);
+app.set('layout', 'layout');
 
 // app.get('/', (req, res, next) => {
 //   res.render('index');
 // });
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('index');
 });
+
+
 app.get('/beers', (req, res) => {
-  res.render('beers');
+  punkAPI.getBeers()
+    .then((beers) => {
+      console.log(beers);
+      res.render('beers', {
+        beers,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
+
+
 app.get('/random-beers', (req, res) => {
-  res.render('random-beers');
+  punkAPI.getRandom()
+    .then((beer) => {
+      console.log(beer);
+      res.render('random-beers', {
+        beer: beer[0],
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.listen(3000);
