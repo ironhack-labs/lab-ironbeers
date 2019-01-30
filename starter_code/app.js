c916ab08a811fd3fa92d38a80ba6e718d8e3a027
j0nl1 <jonlitech@gmail.com>
2019-01-30 20:54:09 +0100

    First Iteration

--- starter_code/app.js
+++ starter_code/app.js
@@ -16,6 +16,13 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
 
 app.listen(3000);
