8afdfc82f450c3ecdfc3554715046cc1d48e164e
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-01-30 19:39:01 +0100

    change the file from hbs to ejs

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,27 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-app.get('/', (req, res, next) => {
-  res.render('index');
+// app.get('/', (req, res, next) => {
+//   res.render('index');
+// });
+app.get('/', (req, res) => {
+  res.render('home');
 });
-
-
+app.get('/beers', (req, res) => {
+  res.render('beers');
+});
+app.get('/random-beers', (req, res) => {
+  res.render('random-beers');
+});
 
 app.listen(3000);
