355ff9ac36946d4d3e346487babf7aa0995a1da0
David Redondo <rdnd201@gmail.com>
2019-01-30 22:45:05 +0100

    initial version

---
+++ app.js
@@ -0,0 +1,43 @@
+
+const express = require('express');
+const app     = express();
+const path    = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
+const expressLayouts = require('express-ejs-layouts');
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
+app.set('layout extractScripts', true);
+app.set('layout extractStyles', true);
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/Beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beer', {beers});
+
+
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/RandomBeers', (req, res) => {
+  res.render('randomBeers');
+});
+
+
+app.listen(3000);
