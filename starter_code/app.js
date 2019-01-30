2dbe8630e25919552e82d94878d20363afb82db0
Zenchy <alvaro.corral.soilan@gmail.com>
2019-01-30 22:55:35 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partial')
 
 
 
@@ -16,6 +17,27 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
+});
+app.get('/randombeer', (req, res, next) => 
+{
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers)
+    res.render('random-beer',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 app.listen(3000);
