f686a002eccc79a4809a63efaa2902a5bc1abec8
dreazz <agustinsilveira.96@gmail.com>
2019-01-30 23:41:21 +0100

    partial for beers create

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,33 @@
+const express = require("express");
 
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
+
+const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
+app.set("layout", "layouts/layout");
 
 
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.render("index");
 });
-
-
-
+app.get("/beers", (req, res, next) => {
+  var beerArr=[];
+  punkAPI.getBeers()
+  .then(beers =>{
+    res.render("beers",{beers:beers});
+  })
+  .then(()=>{
+    
+  })
+  
+  
+});
 app.listen(3000);
