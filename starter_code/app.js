b6f29c88db8244439e5bcfd733514a5c6028a5dc
Goldziher <nhirschfeld@gmail.com>
2019-01-30 09:37:26 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,34 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const path = require('path');
+const hbs = require('hbs');
+const app = express();
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
-
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.get('/', (req, res, next) => {
+app.get('/', (req,res,nxt) => {
   res.render('index');
 });
 
+app.get('/beers', (req,res,nxt) => {
+  punkAPI.getBeers().then(beers => {
+    res.render('beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/randombeer', (req,res,nxt) => {
+  punkAPI.getBeers().then(beers => {
+    var beer = beers[0]
+    res.render('randombeer', {beer})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
