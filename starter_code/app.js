a2b308d8a7dbeb9a23c2423e63d5d1b51827741e
Didac Bigorda <didacbigorda@gmail.com>
2019-01-30 21:34:03 +0100

    Iteration 1 finished.

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,35 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 3000;
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
 
+app.use(express.static('public'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
 
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
