dbf030b9e23f52c2ec262095782712e3299e9dd0
pablograu <paugf89@gmail.com>
2019-01-31 09:27:45 +0100

    api called

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,19 +1,35 @@
 
 const express = require('express');
-const hbs     = require('hbs');
 const app     = express();
 const path    = require('path');
+const expressLayouts = require('express-ejs-layouts');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.get('/random_beer', (req, res, next) => {
+  res.render('random_beer');
 });
 
 
