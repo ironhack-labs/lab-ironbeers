eb48b428212caa4de9856dbee80c1375ce44da62
Nicolas Lacaze <nicolas.lacaze@gmail.com>
2019-01-31 07:09:56 +0100

    Completed

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,42 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
 const path    = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const app     = express();
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
+
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then( beers => {
+    res.render('beers', { beers} );
+  })
+  .catch(error => {
+    console.log(error)
+  });
+});
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then( beers => {
+    res.render('random-beers', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
