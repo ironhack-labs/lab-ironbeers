6d0d23440dc8fc45fe1dbd9d1bff03493bea5de8
Pedro BM <p.bonilla.miralles@gmail.com>
2019-01-31 09:07:21 +0100

    almost done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,38 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const expressLayouts = require('express-ejs-layouts');
+
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname }/views`);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then((beers) => {
+      console.log(beers);
+      res.render('beers', { beers });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
 
 app.listen(3000);
