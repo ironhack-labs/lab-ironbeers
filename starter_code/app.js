8573ba54693136d1a549f8c1d28bfc539518a73b
Jules <jules.badin@gmail.com>
2019-01-31 15:04:54 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,67 @@
+// SETUP
 
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
 
+hbs.registerPartials(__dirname + "/views/partials");
 
+//ROUTES
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      // res.json(beers);
+      res.locals.beersList = beers;
+      res.render("beers.hbs");
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 
 
-app.listen(3000);
+
+app.get("/random-beers", (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(beers => {
+      res.locals.beersList = beers;
+      res.render("random-beers.hbs");
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("App is ready!");
+});
+
+//=============================
+//Nizar's code
+
+// app.get("/ADDRESS", (request, response, next) => {
+//   getDataFromAnApi()
+//     .then(apiResult => {
+//       // don't forget to send the apiResult to the HBS
+//       response.render("SOME_VIEW.hbs");
+//     })
+//     .catch(error => {
+//       console.log("THERE WAS AN ERROR", error);
+//       response.send("There was an error. Check the TERMINAL.");
+//     });
+// });
