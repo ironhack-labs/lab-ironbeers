580dcbc2675fa6bd51a673feafa18635f4602e21
Nira <niralonso@gmail.com>
2019-01-31 09:06:29 +0100

    .

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,12 +9,39 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/Partials');
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    
+    res.render('Beers',{beers});
+
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(randomBeers => {
+    
+    res.render('random-beer',{randomBeers});
+
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
 
 
 
