5e0309dcdf91d5de20eaf7addd1ef210cd9db2e7
caigma <caigma@gmail.com>
2019-01-31 09:23:31 +0100

    finished pte mejorar css

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,39 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+	res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+	punkAPI
+		.getBeers()
+		.then((birra) => {
+			res.render('beers', { birra });
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
 
+app.get('/random-beer', (req, res, next) => {
+	punkAPI
+		.getRandom()
+		.then((beers) => {
+			res.render('random-beer', { beers });
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
 
 app.listen(3000);
