c973a9333333a761c5eb33334469c9332ec295fc
celinedesnoe <celine.desnoe@skema.edu>
2019-01-31 12:51:15 +0100

    Paris - Bastien & Céline - CSS checking

--- starter_code/app.js
+++ starter_code/app.js
@@ -32,6 +32,7 @@ app.get("/random-beers", (req, res, next) => {
   punkAPI
     .getRandom()
     .then(randomBeer => {
+      // res.json(randomBeer);
       res.render("randomBeers.hbs", { randomBeer });
       console.log("RANDOMBEERS ARE READY", { randomBeer });
     })
@@ -41,16 +42,3 @@ app.get("/random-beers", (req, res, next) => {
 });
 
 app.listen(3000);
-
-// // ------
-// app.get("/ADDRESS", (request, response, next) => {
-//   getDataFromAnApi()
-//     .then(apiResult => {
-//       // don't forget to send the apiResult to the HBS
-//       response.render("SOME_VIEW.hbs");
-//     })
-//     .catch(error => {
-//       console.log("THERE WAS AN ERROR", error);
-//       response.send("There was an error. Check the TERMINAL.");
-//     });
-// });
