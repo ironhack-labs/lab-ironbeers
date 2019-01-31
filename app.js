e2f58017d5f013df2540a6e21a4edc7899588577
Ismael Jaouhar <ismael.jaouhar@gmail.com>
2019-01-31 12:43:04 +0100

    iterance 5 working

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path    = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const ejs = require('ejs');
 
 app.use(expressLayouts);
 app.set('layout', 'layouts/layout');
@@ -12,7 +13,7 @@ app.set('layout', 'layouts/layout');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
-ejs.registerPartials(__dirname + '/views/partials')
+// ejs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
