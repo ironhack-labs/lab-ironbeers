86af5ce52eddf5fc8d1036df0ebece895c676d34
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-01-31 12:42:57 +0100

    not finished

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,18 +2,38 @@
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
+
 const path    = require('path');
+
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+// Set up para hbs
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+
+// Set up para CSS
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', { beers });
+    
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 
