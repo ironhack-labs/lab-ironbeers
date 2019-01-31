98aae0101bb31543fc5d68b43b8affdd54e00a2b
Alvaro Bonilla <alvarobonillavillalba@gmail.com>
2019-01-31 09:25:39 +0100

    done sin maquetar

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,9 +6,11 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+app.use(express.static('public'));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + "/views/partials");
 
 
 
@@ -16,6 +18,30 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get("/beers", (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    let data = {
+      beers:beers
+    }
+    res.render('beers', data);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    let data = {
+      beers:beers
+    }
+    res.render('randomBeer', data);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
