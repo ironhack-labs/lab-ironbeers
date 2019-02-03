6059c60ce79291fd284b075d68a0e0787a32d5f2
Nesc86 <nessc86@gmail.com>
2019-02-03 21:33:18 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,45 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const app = express();
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.set('layout', 'layouts/layout');
+
+app.use(expressLayouts);
+app.use(express.static('public'));
 
 
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers})
+    })
+  .catch(error => {
+    res.render('beers',{error})
+    console.log(error);
+  })
+});
 
+app.get('/random', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers);
+    res.render('random', {beers})
+  })
+  .catch(error => {
+    res.render('random', {error})
+    console.log(error)
+  })
+});
 
 app.listen(3000);
