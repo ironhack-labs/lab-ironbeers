f75e9402a343512e9de6e81113886ec1a48d431b
Diogo Pires Oliveira <diogopiresoliveira@Diogos-MacBook-Air.local>
2019-02-04 02:04:49 -0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +9,38 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Iteration 5 - Beer Partial
+hbs.registerPartials(__dirname + '/views/partials')
 
-
+// Home. ----> Should navigate to /.
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+// Beers. ----> Should navigate to /beers.
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      // console.log(beers);
+      res.render('beers', {
+        beers
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+// Random Beer. ----> Should navigate to /random-beers.
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random-beer', {beers});
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
 
-app.listen(3000);
+app.listen(3001);
