824d6266d0e77d9297dc4a20ef3010e475c0057c
javieramirez90 <javier.ramirez@aiesec.net>
2019-02-06 14:55:26 -0600

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,12 +9,34 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then( beers => {
+      res.render('beers', { beers })
+      // console.log(beers[0])
+    })
+    .catch(e => {
+      console.log(e, "Birras not found.")
+    })
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer',  { beer: beers[0] } )
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 
 
