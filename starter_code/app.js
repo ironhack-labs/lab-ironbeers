24a446ad8ec2f5d10aeeee9e42981d53ea586e5e
Pol Goma <polgoide@gmail.com>
2019-02-06 13:22:47 -0600

    done 🍺

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,42 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(result => {
+    console.log(result)
+  res.render('beers', {result})
+})
+.catch(error => {
+  res.render('beers', {error: "algo salio mal"})
+})
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(result => {
+    console.log(result)
+  res.render('random-beer', {result})
+})
+.catch(error => {
+  res.render('random-beer', {error: "algo salio mal"})
+})
+})
+
+// punkAPI.getBeers()
+// .then(beers => {
+
+// })
+// .catch(error =>{
+//   console.log(error)
+// })
 
 
 app.listen(3000);
