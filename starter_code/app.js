6fea36b7bf54b5578e2e149aa562eba1cf3a4b58
Monica Lopez Gris <monicalopezgris@gmail.com>
2019-02-07 11:42:22 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,43 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const path = require('path');
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('home', {
+    pageTitle: 'home',
+  });
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then((beers) => {
+      res.render('beers', { beers });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
+app.get('/random-beers', (req, res, next) => {
+  const randomBeer = punkAPI.getRandom();
+  randomBeer
+    .then((beer) => {
+      res.render('random', { beer });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 app.listen(3000);
