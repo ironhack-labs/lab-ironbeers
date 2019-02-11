433dd39779787f29007dbf29c33feac9b2930d6a
Henry Doce <henrydoce@protonmail.com>
2019-02-11 17:43:26 -0500

    Iterations done, CSS needed

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,12 @@
 
+
+console.log(__dirname, __filename)
+
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
 const path    = require('path');
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +14,31 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// hbs.registerPartial('showABeer', '{{name}}')
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {data:beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    console.log(beer[0])
+   res.render('random-beers', {data: beer[0]});
+  })
+});
 
 app.listen(3000);
+
