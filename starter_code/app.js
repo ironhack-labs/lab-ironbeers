bde41482bd2ed42237f4e2a21f7531838eed927f
Guillermo X. Aiken <aikengx@gmail.com>
2019-02-11 17:09:50 -0500

    first commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,38 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  //res.send(punkAPI.getBeers({'abv_gt': 25}));
+
+  punkAPI.getBeers()
+  .then(beers => {
+    //console.log(beers);
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+  
+});
 
 
 app.listen(3000);
