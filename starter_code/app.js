bfd4ad0e180c4ba90f9aa1f3451861dbb7580460
Victoria Hickam <vhickam@gmail.com>
2019-02-11 17:56:06 -0500

    first 5 iterations done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,35 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+// const randomBeer = punkAPI.getRandom()
+// const allBeers = punkAPI.getBeers()
+// randomBeer.then(beer => {
+//   //console.log(beer[0].name)
+// })
+
+// allBeers.then(beer => {
+//   console.log(beer)
+// })
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
-
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    //console.log(beers);
+    res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
