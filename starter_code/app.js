f5543b300557b83c27bd87e71747a7606c568e76
Ric Taglietti <rictaglietti@gmail.com>
2019-02-11 20:33:57 -0500

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,15 +5,39 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const randomBeer = punkAPI.getRandom()
+const allBeers = punkAPI.getBeers()
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers);
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.get('/beers-random', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(randombeers => {
+      res.render('beers-random', { randombeers: randombeers[0] })
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 
