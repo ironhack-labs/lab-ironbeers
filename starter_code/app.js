00186e2a17eff57abeb344e0d9163c03382d7cc1
Monroe <mgetz10@gmail.com>
2019-02-11 20:25:28 -0500

    final commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,42 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+
+const randomBeer = punkAPI.getRandom();
+const allBeers = punkAPI.getBeers();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      //console.log(beers);
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(rbeer => {
+      res.render('random-beer', { rbeer: rbeer[0] });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 app.listen(3000);
