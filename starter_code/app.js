7c16538706139865b26e36e8dd2fe2abaf097aa5
Jack Fischer <jrfisch95@gmail.com>
2019-02-14 14:56:53 -0500

    App correctly fetches from the beers API. Still need to build some hbs partials for displaying the beer data and also making a randomizer that pulls a random beer.

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,14 +6,27 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
+
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) =>{
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {beers: beers});
+    })
+    .catch(err => {
+      console.log('There was an error while retrieving beers: ', err)
+    });
 });
 
 
