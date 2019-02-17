4e8e7007801affcc82d60037462f1ffda209971c
ChristopheP96 <protin.christophe@gmail.com>
2019-02-17 19:49:10 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,39 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(expressLayouts);
+app.use(express.static('public'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then((beer) => {
+    res.render('random-beers', { beer } );
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
