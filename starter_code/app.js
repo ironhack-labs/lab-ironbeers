3792b8b1da0129f0a38fa7091a266d90522b4664
Robin <hahne.robin@gmail.com>
2019-02-18 16:25:21 +0100

    iteration 1 done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,43 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.get('/', (req, res, next) => {
   res.render('index');
-});
+}); 
 
+/*
+app.get("/", (req, res, next)=> {
+  res.send(__dirname + "/views/index.hbs", {
+    username: "Rodolpho",
+    body:"<h1>haloooooo</h1>"
+  })
+})
 
+*/
+app.listen(3000, ()=> {
+  console.log("listening in")
+})
 
-app.listen(3000);
+/*
+app.set('views',__dirname + '/views')
+app.set('view engine', 'hbs')
+
+app.get("/", (req, res, next)=> {
+  res.render(__dirname + "/views/index.hbs", {
+    username: "Rodolpho",
+    body:"<h1>haloooooo</h1>"
+  })
+})
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html")
+})
+
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/contact.html")
+})
+
+app.listen(3000, ()=> {
+  console.log("listening in")
+})
+*/
