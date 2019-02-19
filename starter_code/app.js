5da9075944e8b4e298e7adbdd2bcab81f94619eb
Jelte Homminga <jeltehomminga@gmail.com>
2019-02-19 17:00:42 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,42 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
 const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
+const beers   = require('./beers.json');
+const axios   = require('axios');
+
+//toggle the boolean value to use local json file or API
+const useAPI = true;
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
+app.get('/', (req, res) => res.render('index'));
 
+if (useAPI) {
+  app.get('/beers', (req, res) =>{
+    axios.get('https://api.punkapi.com/v2/beers')
+      .then(beers => {
+        console.log(beers.data);
+        res.render('beers', {beers: beers.data} )})
+      .catch(error => console.log(error))
+    });    
+    app.get('/random-beers', (req, res) =>{
+      axios.get('https://api.punkapi.com/v2/beers/random')
+      .then(beers => {
+        let randomBeer = beers.data[0];
+        res.render('random-beers', {randomBeer});
+      })
+      .catch(error => console.log(error))
+    });
+} else {
+    app.get('/beers', (req, res) => res.render('beers', {beers}));
+  app.get('/random-beers', (req, res) =>{
+    let randomBeer = beers[Math.floor(Math.random() * beers.length)];
+    res.render('random-beers', {randomBeer});
+  })
+}
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-
-
-app.listen(3000);
+app.listen(3000, ()=> console.log("ja? wat nou weer!?"));
