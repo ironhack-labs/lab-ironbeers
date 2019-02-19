b81abb20b64f43fb53d445214285895b9cec381d
mnib2xu <b.r.wakker@gmail.com>
2019-02-19 11:07:45 +0100

    finished the beer excercise

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +9,31 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {beers})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    debugger
+    res.render('random-beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 app.listen(3000);
