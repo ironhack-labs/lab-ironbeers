832690b784a77f00d097ed22d8698801ba7b3050
Martin <martin.mihalik.nr@gmail.com>
2019-02-19 12:50:26 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -10,12 +9,33 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {beers});
+    })
+    .catch(error => {
+      console.log('error');
+    })
+})
 
+app.get('/random-beer', (req, res) => {
+  punkAPI.getBeers()
+    .then(beer => {
+      let randomBeer = beer[Math.floor(Math.random() * beer.length)];
+      res.render('randomBeer', {randomBeer});
+    })
+    .catch(error => {
+      console.log('error')
+    })
+})
 
-app.listen(3000);
+app.listen(3000, ()=> {
+  console.log("What is thy bidding my master")
+});
