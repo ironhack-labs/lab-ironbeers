9128ad7c6e356dd5bed2a46346e00c6cc53fdb77
Laura PH <lau.p.hern@hotmail.com>
2019-02-20 01:40:56 +0100

    implemented punk API

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,54 @@
-
 const express = require('express');
-const hbs     = require('hbs');
 const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const hbs     = require('hbs');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper')
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/public'));
 
-
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.listen(3000, () => {
+    console.log('listening')
+});
 
 
-app.listen(3000);
+// //FIRST METHOD USING JSON FILE
+// const beersObject = require('./beers.json');
+
+// app.get('/beers', (req, res) => {
+//     res.render('beers', {beers: beersObject})
+// })
+
+// app.get('/random-beer', (req, res) => {
+//     var getRandomBeer = Math.floor(Math.random() * beersObject.length);
+//     res.render('randomBeer', {randomBeer: beersObject[getRandomBeer]})
+// });
+
+
+//SECOND METHOD WITH PUNK API
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+  .then(beers => {
+      res.render('beers', {beers: beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.get('/random-beer', (req, res) => {
+    punkAPI.getRandom()
+  .then(beers => {
+      var randomBeer = beers[0]
+      res.render('randomBeer', {randomBeer: randomBeer})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
