456f6b6bcb27bdc7726aa4b99776c3ac0ddabbc8
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2019-02-27 22:43:37 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,32 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    res.render('beers', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    console.log(beer)
+    res.render('randombeer', {beer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
