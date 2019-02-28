6efcbaef5fb59f561d20d872f5db90679527f5f3
Carlos <clorezhh@gmail.com>
2019-02-28 08:46:17 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,36 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/partial');
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
+ punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    res.render('beer', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.get('/randome', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(randomeBeers => {
+    console.log(randomeBeers)
+    res.render('randome', {randomeBeers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
 
 
-app.listen(3000);
+app.listen(3000,()=> console.log("App escuchando en puerto " ))
