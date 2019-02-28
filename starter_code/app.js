e36bcd4fbaf07d794c6802528eae29d130841355
Dan Mihai Grigoriciuc <griganu06@gmail.com>
2019-02-28 09:16:08 +0100

    Falta finalizar mquetacion

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,32 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    res.render('beers',{beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/randomBeer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(randomBeer => {
+      console.log(randomBeer)
+      res.render('randomBeer',{randomBeer})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  });
 
 app.listen(3000);
