b895051e93b4e87d323c0328d72c786fc8cad94a
Carlos Martínez Crespo <carlos.martinez.crespo@gmail.com>
2019-02-28 09:41:32 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +9,21 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Registramos el directorio de parciales
+hbs.registerPartials(__dirname + '/views/partials');
 
+app.get('/', (req, res, next) => { res.render('index'); });
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get('/beers', (req, res, next) => {
+    punkAPI.getBeers()
+        .then(beers => { res.render('beers', { beers }) })
+        .catch(error => { console.log(error) })
 });
 
-
+app.get('/random-beers', (req, res, next) => {
+    punkAPI.getRandom()
+        .then(randomBeer => { res.render('randomBeer', { randomBeer }) })
+        .catch(error => { console.log(error) })
+});
 
 app.listen(3000);
