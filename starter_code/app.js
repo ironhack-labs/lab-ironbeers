3001d9bbad06555a0d46c7287bcc27d425d907ff
Fedde <feddetilma@hotmail.com>
2019-03-19 21:40:05 +0100

    Done - first version

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,12 +9,33 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + "/views/partials");
 
-
-
+// Routes
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers() // will get 25 random beers
+    .then(beers => {
+      // console.log(beers[0]);
+      res.render('beers', {beerObject: beers}) // has to be an object (data)
+    })
+    .catch(error => {
+      console.log(error);
+    })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => { 
+    res.render('randomBeer', {beerObject: beers})
+  })
+  .catch(error => {
+    console.log(error);
+  })
+})
 
 
 
