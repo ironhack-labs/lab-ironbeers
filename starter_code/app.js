b612da9f1fb76c29cd611a9f3d418f0d9b8e78a4
Camila Veras <camilavalles@gmail.com>
2019-03-19 21:50:06 +0100

    in progress

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,33 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {x:beers});
+    console.log(beers)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers',  {y:beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
 app.listen(3000);
