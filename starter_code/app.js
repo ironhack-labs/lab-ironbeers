246aadbd88ee5f2ab73aff1da6b8bb4ba4a546ba
Fedde <feddetilma@hotmail.com>
2019-03-20 09:33:28 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,32 +11,33 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(__dirname + "/views/partials");
 
-// Routes
+// Routes - START //
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 app.get('/beers', (req, res, next) => {
   punkAPI.getBeers() // will get 25 random beers
-    .then(beers => {
-      // console.log(beers[0]);
-      res.render('beers', {beerObject: beers}) // has to be an object (data)
-    })
-    .catch(error => {
-      console.log(error);
-    })
+  .then(beers => {
+    // console.log(beers[0]);
+    res.render('beers', {beerObject: beers}) // has to be an object (data)
+  })
+  .catch(error => {
+    console.log(error);
+  })
 });
 
 app.get('/random-beers', (req, res, next) => {
-  punkAPI.getRandom()
+  punkAPI.getRandom() // get one random beer (provided as an array though)
   .then(beers => { 
     res.render('randomBeer', {beerObject: beers})
   })
   .catch(error => {
     console.log(error);
   })
-})
+});
+// Routes - END //
 
-
-
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server started - port 3000")
+});
