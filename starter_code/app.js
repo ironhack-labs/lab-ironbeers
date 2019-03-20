2febefe2ead790988caee3d70071cffe92d89b12
Merle Jansen <merlevanjansen@gmail.com>
2019-03-20 22:14:32 +0100

    initial commit, without css

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,42 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs'); // HBS will be in charge of rendering the HTML:
+app.set('views', __dirname + '/views'); // creates an absolute path pointing to a folder called "views"
+hbs.registerPartials(__dirname + '/views/partials'); // partials
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    //console.log(beers)
+    res.render('beers', {list: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  
+});
+
+app.get('/random', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random', {list:beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
 
 
 app.listen(3000);
+
