c441803f354eae0e18d366f5b0f3bc305f28be91
Jelena Jukic <jelena.dj.pg@gmail.com>
2019-03-20 13:39:45 +0100

    done

---
+++ starter_code/public/buttons.js
@@ -0,0 +1,4 @@
+window.onload = function (){
+  document.getElementById("button-one").addEventListener("click", ()=>window.open("beers"));
+  document.getElementById("button-two").addEventListener("click", ()=>window.open("random-beers"))
+}
