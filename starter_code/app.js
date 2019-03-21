5365651037dd13046d167f884b2cc6f1bf1c9fcb
luisf89 <luisf.martinez89@gmail.com>
2019-03-21 16:11:32 -0600

    partials

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,35 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers',(req, res) =>{
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers')
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
+app.get('/random-beer', (req, res) =>{
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beer')
 
-app.listen(3000);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.listen(3000, () =>{
+  console.log("corriendo el 3000 perro")
+});
