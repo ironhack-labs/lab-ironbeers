2eaf8a3715695085e822c184fc60f9e9f3be265e
David Gallego Gomez <davizs@hotmail.com>
2019-03-27 00:55:39 +0100

    Primer commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,24 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Home'
+  });
 });
 
+app.get('/beers', (req, res, next) => {
+  res.render('beers', {
+    title: 'Beers'
+  });
+});
 
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers', {
+    title: 'Random Beer'
+  });
+});
 
 app.listen(3000);
