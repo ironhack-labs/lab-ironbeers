02671e9c9ec90ade34808d2112febce4a510945f
RedBalloon24 <worldtraveler757@gmail.com>
2019-03-27 15:27:33 +0100

    1st commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,33 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers')
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer')
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("HELLO");
+});
