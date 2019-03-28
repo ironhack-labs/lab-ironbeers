51166b5ba11bc16cd19e4c70551a624734b2a706
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-03-28 21:08:22 +0100

    third iteration checked beers

--- starter_code/app.js
+++ starter_code/app.js
@@ -18,7 +18,18 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 app.get('/beers', (req, res, next) => {
-  res.render('beers');
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {
+      title: 'Beers',
+      beers
+    });
+    console.log('tengo las birras men')
+  })
+  .catch(error => {
+    console.log(error)
+  });
+  
 });
 app.get('/random-beers', (req, res, next) => {
   res.render('random-beers');
