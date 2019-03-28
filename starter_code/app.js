04e8d6c6896ccc249c32e5bdf8e13605fdc7d9c1
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-03-28 20:21:10 +0100

    first iteration fixed with links and menu and footer

--- starter_code/app.js
+++ starter_code/app.js
@@ -17,6 +17,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
 
 
