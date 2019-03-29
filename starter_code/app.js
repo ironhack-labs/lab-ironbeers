d24b293f1ce922606afd2f7b8258880cf7951436
NuriaTired <nuriatired@gmail.com>
2019-03-29 20:51:09 +0100

    Resuelto pero con dudas

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,37 +1,56 @@
-
 const express = require('express');
-const app     = express();
 const hbs = require('hbs');
-
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 
+
+//hbs, handlebass
+//aqui le indicas el motor con el que se va a renderizar
 app.set('view engine', 'hbs');
+//los ficheros de vista que vas a usar, estan en el directorio '/views'
 app.set('views', __dirname + '/views');
-hbs.registerPartials(__dirname + "/views/partials");
-app.use(express.static(__dirname + "/public"));
+//le indicamos que los parciales estan en esta ruta, tira de la const hbs =, que hemos declarado arriba
+hbs.registerPartials(__dirname + '/views/partials')
+//Esta parte no la entiendo muy bien, luego si podemos me explicas
+app.use(express.static(path.join(__dirname, 'public')));
 
 
+//esta es la respuesta que obtenemos al cargar la pagina
+app.get('/', (req, res, next) => {
+  const beerImage = '/images/beer.png';
+  res.render('index', { beerImage });
+});
 
+//esta es la respuesta que obtenemos para ver todas las cervezas
+//mediante un metodo de la punkAPI
+app.get('/beers', (req, res, next) => {
 
-app.get('/', (req, res) => {
-  res.render('index');
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+//esta es la respuesta que obtenemos cuando pulsamos para random beer 
+//mediante un metodo de la punkAPI
+app.get('/random-beers', (req, res, next) => {
+
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random-beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
 });
 
 
 
-
-app.get('/beers', (req, res) => {
-punkAPI.getBeers()
-  .then(beers => {
-    
-  })
-  .catch(error => {
-    console.log(error);
-  })
-})
-
-app.listen(3000, () => {
-  console.log("listening");
-});
+app.listen(3000);
