725e4e6278cd9dcb01d111981c9fbb3baf3b8827
Fernando Lacaba <fernando.lacaba.herrera@gmail.com>
2019-03-31 21:27:23 +0200

    Terminado

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,13 +8,38 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(`${__dirname}/views/partials`);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index',{
+    title: 'Home'
+  })
 });
+
+app.get('/beers',(req, res, next) => {
+  punkAPI.getBeers()
+    .then( beers => {
+      res.render('beers',{
+        title: "Beers",
+        beers
+      })
+    })
+    .catch(error => console.error(error))
+})
+
+app.get('/random-beers',(req, res, next) => {
+  punkAPI.getRandom()
+    .then( randomBeer => {
+      res.render('random-beers', {
+        title: randomBeer[0].name,
+        randomBeer
+      });
+    })
+    .catch(error => console.error(error))
+})
 
 
 
