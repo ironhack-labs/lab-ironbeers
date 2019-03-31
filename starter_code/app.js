5e1245c40445e45e56014284945acece1c36f769
MartaVaqgut <martavaquerogutierrez@gmail.com>
2019-03-31 12:07:02 +0200

    primera iteracion

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const hbs     = require('hbs');
+const PORT = 3000;
 const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
@@ -8,12 +9,24 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.use((req, res, next) => {
+  next();
+})
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('home');
+});
+
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
+
+app.get('/random-beers', (req, rest, next) => {
+  res.render('random-beers');
 });
 
 
