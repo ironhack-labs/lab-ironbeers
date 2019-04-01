dfa3e8e1f6b2edd9a96c12b8fdf34977e6e4cfa7
Victor Garcia <v.garcia.mestre@gmail.com>
2019-04-01 17:47:54 +0200

    Irondone

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,8 +3,17 @@ const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
 const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
+const PORT=3000;
+app.set('view engine','hbs');
+app.set('views',__dirname+'/views');
+hbs.registerPartials(`${__dirname}/views/partials`);
+
+
+const PunkAPIWrapper = require('punkapi-javascript-wrapper')
+const punkAPI = new PunkAPIWrapper()
+
+
+
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -13,9 +22,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Home'
+  });
 });
 
+app.get('/beers',(req, res, next) => {
+  punkAPI.getBeers()
+    .then( beers => {
+      res.render('beers',{
+        title: "Beers",
+        beers
+      })
+    })
+    .catch(error => console.error(error))
+})
 
 
-app.listen(3000);
+
+app.get('/random-beers',(req, res, next) => {
+  punkAPI.getRandom()
+    .then( randomBeer => {
+      res.render('random-beers', {
+        title: randomBeer[0].name,
+        randomBeer
+      });
+    })
+    .catch(error => console.error(error))
+})
+
+
+app.listen(PORT, () => console.info(`Application listen at port ${PORT}`));
+
