54b706ed99604e500bdd36dd6a86e8dd73d3982e
Marcelo Oliveira <marbmo@gmail.com>
2019-04-04 14:28:27 -0300

    Exercício Feito

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,31 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers().then(beers => {
+    var random = Math.round((Math.random()*beers.length) + 1);
+    console.log(random);
+    res.render('beers', {beers});
+  }).catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beer', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
 app.listen(3000);
