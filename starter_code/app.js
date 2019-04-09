0e4302b53575a775f73ed4e37e2546e1fed38fd3
Susan Almassi <susan.z.almassi@gmail.com>
2019-04-09 19:42:17 +0200

    first and only commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,42 @@
-
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
+hbs.registerPartials(__dirname + "/views/partials"); // pour récupérer les partials
 
-
-
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.render("index.hbs");
 });
 
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      // res.json(beers); // to add the data (the beers) in the page as json data = to console.log the data in the browser instead of the terminal
+      res.render("beers.hbs", { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
+app.get("/random-beers", (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      res.render("randomBeer.hbs", { beer });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("my app started at http://localhost:3000");
+});
