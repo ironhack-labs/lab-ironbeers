9690d0f88d5903a826fdceb85666671f8e1350c8
alex fox <to.alexfox@gmail.com>
2019-04-09 16:23:05 +0200

    beers page is working

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
