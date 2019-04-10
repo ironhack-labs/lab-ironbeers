451bfa2e990190b90cbd23a364aa5905c690e069
Luca Manfredi <luca.manfredi1987@gmail.com>
2019-04-10 19:32:56 +0200

    Iteration 2

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 
 const express = require('express');
 const hbs     = require('hbs');
@@ -13,9 +14,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  const data = {
+    sectionTitle: "Home"
+  };
+  
+  res.render('index', data);
+});
+
+app.get('/beers', (req, res, next) => {
+  const data = {
+    sectionTitle: "Beers"
+  };
+
+  res.render('beers', data);
+});
+
+app.get('/random-beers', (req, res, next) => {
+  const data = {
+    sectionTitle: "Random Beers"
+  };
+
+  res.render('random-beers', data);
 });
 
 
 
-app.listen(3000);
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port " + process.env.PORT)
+})
