080a4cf29a9771724facb9abe99e2667b74b0d4d
laura <lauraabad83@gmail.com>
2019-04-10 21:22:25 +0200

    added home,beers

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,19 +1,43 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get('/', (req, res) => {
+    res.render("partials/index");
+});
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beers => {
+            console.log(beers);
+            res.render("partials/beers", { beers });
+
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+});
+
+app.get('/random-beer', (req, res) => {
+    punkAPI.getRandom()
+        .then(beers => {
+            res.render("partials/random-beer", { beers });
+
+        })
+        .catch(error => {
+            console.log(error)
+        })
 });
 
 
