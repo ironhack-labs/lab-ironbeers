3e283dd37e0a8732e6efd9eb76ed5b3dc8feee4d
erFedeRuiz <federicoruiz.ruiz@gmail.com>
2019-04-10 20:47:39 +0200

    Falta css

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,36 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+console.log(__dirname);
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+//app.use(express.static('./public'));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('partials/index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('partials/beers', {beers} );
+  })
+  .catch(error => {
+    console.log(error)
+  })  
+});
 
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    res.render('partials/random-beer', {beer} );
+  })
+  .catch(error => {
+    console.log(error)
+  })  
+});
 
 app.listen(3000);
