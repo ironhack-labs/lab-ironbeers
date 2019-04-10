391d341ce855eab42de2f8ede1def3df801457d2
raveBolognesa <rafael.arrieta.gar@gmail.com>
2019-04-10 18:01:01 +0200

    last commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 
 const express = require('express');
 const hbs     = require('hbs');
@@ -5,17 +6,52 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// punkAPI.getBeers()
+// .then(beers => {
+//   res.render(beers);
+// })
+// .catch(error => {
+//   console.log(error)
+// })
+
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render("index");
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+.then(beers => {
+  res.render("beers", {beers});
+})
+.catch(error => {
+  console.log(error)
+})
+});
+
+app.get('/randomBeer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers)
+    res.render("randomBeer", {beers});
+    
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 
 
-app.listen(3000);
+app.listen(process.env.PORT, ()=>{
+  console.log("Listening on port " + process.env.PORT)
+});
