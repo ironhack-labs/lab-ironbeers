54f4ac3a185a262bdca31fa997b3347c86729ede
ernestogl88 <ernestogomezlopez@gmail.com>
2019-04-10 20:31:15 +0200

    Finalizado a falta de maquetar

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,33 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('partials/index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('partials/beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    res.render('partials/random-beers', {beer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
 app.listen(3000);
