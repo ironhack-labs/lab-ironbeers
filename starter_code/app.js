189f1599d9a4a414b8e49d451323c5a78ebb03d0
Francisco Vaca <fcovacarh@gmail.com>
2019-04-11 09:33:42 +0200

    Done everything but CSS

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,36 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
-
+//Routes
+//Index
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+//Beers
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+//Random beer
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers[0])
+    res.render('randomBeer', beers[0]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
