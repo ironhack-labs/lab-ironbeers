a42c9c403a46aec87f1ede72f7e75c2c9d13a4c2
nathanH <roundingle@gmail.com>
2019-04-11 16:02:54 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,42 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.get('/', (req, res, next) => {
-  res.render('index');
+    res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+    punkAPI
+        .getBeers()
+        .then(beers => {
+            //console.log(beers)
+            res.render('beersPage', { beers });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
 
+app.get('/random-beers', (req, res, next) => {
+    punkAPI
+        .getRandom()
+        .then(beers => {
+            console.log(beers);
+            res.render('randomBeers', { beers });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
 
 app.listen(3000);
