4db7bd333e471ec41b0db79aa54b89fa4cd26663
Ricardo-MaGuz <ricardo@wides.mx>
2019-04-18 09:57:49 -0500

    Como a la mitad

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,15 +6,32 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + "/partials")
 
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
+app.get("/", (req, res) =>{
+  res.render("index")
+})
+
+app.get("/beers", (req, res) =>{
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers", {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.get("/random-beer", (req, res) =>{
+  res.render("random-beer")
+})
 
 
 
