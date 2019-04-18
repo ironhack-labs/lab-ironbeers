fc6ada2122732110ac11eae2cb0143ab2c8de341
frankismael <ismaelfcom93@gmail.com>
2019-04-18 02:12:52 -0500

    ironbeers

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +10,30 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
 
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req,res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
+app.get('/random-beers',(req,res)=> {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers',{beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 app.listen(3000);
