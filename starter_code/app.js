586ff8e3b44089d71e4389403ae91ff48ec289dc
deredhuzent <hakurascot@gmail.com>
2019-04-17 21:04:08 -0500

    Faltan los últimos pasos T_T

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,42 @@
 
-const express = require('express');
+const express = require('express'); 
 const hbs     = require('hbs');
-const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+
+
+const app     = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials') 
 
-
-app.get('/', (req, res, next) => {
+//home
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers)
+      res.render('beers', {beers})
+    })  
+    .catch(error => {
+        res.render('error')
+    })
+})
+
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer')
+})
 
 
-app.listen(3000);
+let PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Corriendo puerto ${PORT}`)
+});
