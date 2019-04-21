b4c9a27c4d7c655fa5a574366bfcd26d01057e78
gustavvopenna <gustavo.pema@gmail.com>
2019-04-21 13:10:47 -0500

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,35 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    //console.log(beers)
+    res.render('beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  // res.render('beers');
+});
+
+app.get('/randomBeer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  //res.render('randomBeer');
+});
 
 
 app.listen(3000);
