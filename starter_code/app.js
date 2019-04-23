63ae32ae524336f282b4070d89f3ef33199db956
Daniela Olano <danolan@Danielas-MacBook-Pro-2.local>
2019-04-23 16:01:20 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -17,5 +17,30 @@ app.get('/', (req, res, next) => {
 });
 
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log("-------", beers);
+    res.render('beers.hbs', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.get('/random-beer', (req, res, next) => { //hit when i go to the page
+  punkAPI.getRandom().then(beer=>{ //the api is called and returns a random beer
+    let data = {
+      image_url: beer[0].image_url,
+      name:beer[0].name,
+      tagline: beer[0].tagline,
+      description: beer[0].description,
+      food_pairing: beer[0].food_pairing,
+      brewers_tips: beer[0].brewers_tips,
+    }
+    res.render('random-beer.hbs',data) //I render my page with the random beer
+  })
+});
 
 app.listen(3000);
