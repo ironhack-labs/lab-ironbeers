a788ca05354d42d8868a5a00e63a89583c2df98f
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-04-23 12:06:59 -0400

    Beers page works and partials are functioning

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,19 +10,30 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/Partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index.hbs');
 });
 
-app.get('/beers', (req, res, next) => {
-  res.render('beers.hbs');
+app.get('/beers', (req, res, next) => { //whats in chrome 
+  
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log("Beer is returning", beers)
+    res.render('Beers.hbs', {beers} ); //refers to the hbs file 
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
 });
 
 app.get('/randomBeer', (req, res, next) => {
   res.render('RandomBeer.hbs');
 });
+
 
 
  
