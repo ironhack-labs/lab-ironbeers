173016b4b17f8f1ca38105d5553ac309b9a1500c
Chris Cardelle <chris.cardelle@gmail.com>
2019-04-23 12:46:58 -0400

    Functioning links and imnages. Structure complete.

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,14 +8,36 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+//Renders Main Page
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+// Renders Beers Page
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers().then(beer => {
+    let data = {beer: beer};
+  res.render('beers' , data);
+});
+});
 
+// Renders Random-Beers Page
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom().then(beer => {
+    
+    
+  res.render('random-beers', {beer});
+})
+.catch(err => {
+  next(err);
+})
+});
 
 app.listen(3000);
+
