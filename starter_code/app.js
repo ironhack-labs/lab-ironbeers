4163e639425d8a99c9c6198b4d24f844a304458f
Jon Riquer <jonriquer@gmail.com>
2019-04-23 11:08:32 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -16,6 +16,29 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    // console.log(beers);
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  // res.render('beers', {beers})
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers', {beers});
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
 
 
 app.listen(3000);
