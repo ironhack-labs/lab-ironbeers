5a4a7d7e602a45e254ec50ec69d1f743de1e4e86
Katy Brown <thisis.katybrown@gmail.com>
2019-04-23 13:01:38 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,11 +10,37 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
 
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+
+//Home 
+
+app.get("/", (req, res, next)=>{
+  res.render("index")
+})
+
+//Beers
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers().then(beer => {
+    console.log(beer)
+    let data = {beer: beer};
+    res.render('beers', data);
 });
+})
+
+
+//Random Beers
+app.get('/random-beers', (req, res, next) => {
+
+  punkAPI.getRandom().then(beers => {
+    console.log(beers[0])
+    let data = beers[0];
+    res.render('random-beers', data);
+});
+})
+
 
 
 
