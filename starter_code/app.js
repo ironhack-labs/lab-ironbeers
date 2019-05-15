ae239c2005d63e9005a9bd26d5cb0bc26ac2d178
Alessio Marinoni <alessio.marinoni1@gmail.com>
2019-05-15 20:50:35 +0200

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -14,6 +15,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beer => {
+      console.log(beer[0].food_pairing)
+      res.render('random-beers', beer[0]);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
 });
 
 
