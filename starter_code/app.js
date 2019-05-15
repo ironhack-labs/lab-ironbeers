95133e3a7bc7d33aca0488e96cfcda584801a829
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-05-15 18:54:24 +0200

    Iteration 1 y 2

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -15,6 +15,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
+
+
+
 
 
 
