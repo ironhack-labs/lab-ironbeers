604e9a6bbaa080e1218bd4aaa271cf5901d8cd53
Noah Wang Teng <NoahRiseUp@Noahs-MacBook-Pro.local>
2019-05-16 09:01:01 +0200

    Menos css

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,45 @@
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+const path = require('path')
+const PunkAPIWrapper = require('punkapi-javascript-wrapper')
+const punkAPI = new PunkAPIWrapper()
+const port = 3000
 
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/views')
+app.use(express.static(path.join(__dirname, 'public')))
 
 
+// Registro directorio partials
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      console.log(beers)
+      res.render('beers', { beers })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
-app.listen(3000);
+// Abrir servidor
+app.listen(port, () => {
+  console.log(`Esta escuchando en el puerto ${port}`)
+})
