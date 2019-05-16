673859bcd012d85f8ccea597e89b225cb6571303
Paula Filipe <pac.tic.es@gmail.com>
2019-05-16 10:26:45 +0200

    Almost finished, but no time to find out how to show the random :(

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,20 +1,43 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers)
+      res.render('beers', {
+        beers
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      console.log(beers)
+      res.render('random-beers', {
+        beers[0]
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
 
 
