69d4217777931c03765a80d4345570efe96c5d13
benjamingarciap <bengarc@gmail.com>
2019-05-16 09:18:12 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
@@ -16,6 +17,32 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    res.render('beers', {beers})})
+   
+  .catch(error => {
+      console.log(error)
+    })
+  })
 
 
-app.listen(3000);
+  
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers)
+    res.render('random-beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+})
+
+
+
+app.listen(3000, ()=> console.log("app is listening"));
