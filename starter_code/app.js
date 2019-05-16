87e50da6fbdc64e6b6957bad2617da00e1f0da00
Tino Gutiérrez <tgf1987@gmail.com>
2019-05-16 09:07:24 +0200

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,10 +11,33 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+hbs.registerPartials(__dirname + '/views/partials')
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
+
+app.get('/', (req, res, next) => res.render('index'))
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers})
+  })   
+  .catch(error => {
+    console.log(error)
+  })
+  
+})
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers[0])
+    res.render('random-beers', beers[0])
+    
+  })   
+  .catch(error => {
+    console.log(error)
+  })
+  
+})
+
 
 
 
