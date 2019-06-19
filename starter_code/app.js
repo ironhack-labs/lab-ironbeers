799e8566f78e95791acd3aa8ac655da6ba2c3ca7
elinlarsen <elin_larsen1@hotmail.fr>
2019-06-19 22:58:45 +0200

    iteration 5 done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,6 +9,9 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs") // which program should render my views ? HBS
+
+hbs.registerPartials(__dirname +"/views/partials") //where partials are stores
 
 
 
@@ -16,6 +19,18 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next)=>{
+  //res.render('beers') // Génère un modèle de vue.
+
+  punkAPI.getBeers()
+        .then(beers => { 
+          res.render('beers', {beers})
+        })
+        .catch(error => {
+          console.log(error)
+        })
+
+})
 
 
 app.listen(3000);
