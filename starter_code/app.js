cd3a32a0924badca7b65cfbda276f9086db673ae
Iván López Ruiz <ivan@MacBook-Air-de-Ivan.local>
2019-06-27 01:18:22 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,45 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const PORT = 3000;
+
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+    console.log({beers})
+  
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
-app.listen(3000);
+// Beers:
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beer',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.listen(PORT);
