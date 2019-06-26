9f95bf2d3f5f115b8050db9416ea7f83d5b72f69
Jesús García Sanchéz <jesusangelgarciasanchez@MacBook-Air-de-Jesus.local>
2019-06-26 21:48:08 +0200

    almost done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,14 +6,37 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + "/views/partials");
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get("/beers", (req, res, next)=> {
+  
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers", {beers})
+  })
+  .catch(error => console.log(error))
+
+})
+
+app.get("/random-beers", (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(beers => {
+      res.render("random-beers", { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 
