3a65d5f4903e96b30eae9dbf248bc9dcc1ae0122
sofia <sofleos@gmail.com>
2019-06-26 15:09:13 -0500

    done sin todos los estilos

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,39 @@
-
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views/");
+app.use(express.static(path.join(__dirname, "public")));
+hbs.registerPartials(path.join(__dirname, "/views/partials"));
 
-
-
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render("beers", { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
-
+app.get("/random", (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(beers => {
+      console.log(beers);
+      res.render("random", { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 app.listen(3000);
