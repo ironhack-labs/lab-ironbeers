2fdbdd1e644dd74cc1fe50380c8130dd0cb6c07b
sergio <stortosa01@gmail.com>
2019-06-27 07:12:40 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -16,6 +16,28 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', { beers })
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      console.log(beers)
+      res.render('random-beers', { beers })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+})
 app.listen(3000);
+
