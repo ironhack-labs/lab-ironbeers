730facee1564a5c814214ec7f6446a33bcbda6aa
Alejandro <alejandro.galvez.rico@gmail.com>
2019-06-26 21:56:41 -0500

    almost done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,33 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+app.use(express.static("public"))
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname+"/views/partials")
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next)=> {
+  punkAPI.getBeers()
+    .then(beers=>{
+      res.render("beers",{beers})
+    })
+    .catch(error =>{
+      console.log(error)
+    })
+})
+app.get('/random-beers', (req, res, next)=> {
+  res.render('random-beers');
+})
 
 
 
-app.listen(3000);
+app.listen(3000, ()=>{
+  console.log("hola")
+});
