5dfa3f37a3737675570f3e41b12dec0e74e40909
Pedro Morissawa <pmorissawa@gmail.com>
2019-07-03 19:45:36 -0300

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,40 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then((beers) => {
+      res.render('beers', { beers });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+    .then((beer) => {
+      res.render('randomBeer', { beer });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 
 app.listen(3000);
