0a021e2d562c109ed07d47936af897344e867dda
Sergio Velikopoljski <sergiovelo17@gmail.com>
2019-07-03 12:32:49 -0400

    lab finished

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,13 +5,33 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+app.get('/beers',(req,res,next)=>{
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers',{theBeers: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+app.get('/random-beers',(req,res,next)=>{
+  punkAPI.getRandom()
+  .then(beer => {
+    console.log(beer);
+    res.render("random-beers",{beer: beer[0]});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 app.get('/', (req, res, next) => {
   res.render('index');
 });
