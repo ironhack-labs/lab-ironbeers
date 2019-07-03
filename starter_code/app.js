febba92e1da2d7ada3065345445bd387fc4f4e1c
mario salvatierra <msalv006@fiu.edu>
2019-07-03 12:02:03 -0400

    added all references button and navbar

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,26 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log("beers api called.. " + beers);
+  })
+  .catch(error => {
+    console.log("result error " + error);
+  })
 
+  res.render('beers');
+});
+app.get('/randomBeers', (req, res, next) => {
+  res.render('randomBeers');
+});
 
 app.listen(3000);
