d9fc0d5b2d2c4c2df8d2ea70d0aeb01ad945eb8e
arstrel <arstrel@gmail.com>
2019-07-03 12:54:31 -0400

    Pablo and Artem friends with CSS

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,43 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
 
 
-app.get('/', (req, res, next) => {
+
+
+
+app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+   punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beerRack: beers});
+  })
+  .catch(error => {
+    console.log(error)
+    res.render('beers', error)
+  })
+  
+});
+app.get('/randomBeers', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeers', {oneBeer: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+app.get('*', (req, res) => {
+  res.render('notFound');
 });
 
 
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Mega componental website is up and running at port 3000');
+})
