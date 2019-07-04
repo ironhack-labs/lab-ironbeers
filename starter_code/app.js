9e08f548548dd88f219cfaa1a6bef386cc309fcb
giovannipc <giovannipaulocunha@hotmail.com>
2019-07-03 22:38:28 -0300

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,12 +9,34 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
-
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    res.render("beers", { bunchBeers: beers });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(beers => {
+    res.render('randombeer', { randomBeer: beers[0] });
+  })
+  .catch(error => {
+    console.log(error)
+  });
+});
+
 
 
 
