b8861dbd4bc79b0ab0798e7155ccb1359e1d08fe
Victor Levy <victorlevygp@gmail.com>
2019-07-04 09:51:14 -0300

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +9,33 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
 
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then((beers) => {
+      res.render('beers', { beers });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+    .then((beers) => {
+      console.log(beers[0]);
+      res.render('randomBeers', beers[0]);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 
 app.listen(3000);
