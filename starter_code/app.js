6f5bd79c8b5527313a8b87e9a6dcea6a909e67dd
Stefanie Cespedes <stefanie.davilaa@gmail.com>
2019-07-03 23:26:02 -0300

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,34 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/Partials');
 
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/randombeer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
