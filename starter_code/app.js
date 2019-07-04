82d478180f9a664198a6986c5fbd9ae5510b195c
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-07-04 01:40:48 -0300

    Project half-done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -16,6 +15,15 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', beers);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
 
 app.listen(3000);
