e42997e76f05c3030d74befe3bf49dfe9844ad04
Matiny <matinylouis@gmail.com>
2019-07-04 16:54:12 -0400

    Displaying all beers!

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,10 +10,23 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error);
+  })
 });
 
 
