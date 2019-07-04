2a5835db5f1c491aefeb259740eeca944d368d6a
David Dornelles <daviddornelles@gmail.com>
2019-07-04 10:00:09 -0300

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -7,6 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,6 +17,26 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then((beers) => {
+      res.render('beers', { beers });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then((beers) => {
+      console.log(beers);
+      /* beers.Math */
+      res.render('randomBeer', { beers });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 app.listen(3000);
