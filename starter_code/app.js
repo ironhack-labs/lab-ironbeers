8cf7ba242a2ce869d1f2bf76759b72b9d5b7c599
Pat Jordao <patriciacj@gmail.com>
2019-07-04 17:26:45 -0300

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,39 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beersList => {
+
+    res.render('beers', {beersList});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    console.log(beer);
+    res.render('random-beers', {beer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
 
 app.listen(3000);
