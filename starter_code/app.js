870b063f5689a48dadd90d1f010fb4393a72decf
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-07-07 18:30:41 -0300

    Project done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
@@ -18,12 +18,23 @@ app.get('/', (req, res, next) => {
 app.get('/beers', (req, res, next) => {
   punkAPI.getBeers()
     .then(beers => {
-      res.render('beers', beers);
+      res.render('beers', { beers });
     })
     .catch(error => {
-      console.log(error)
-    })
+      console.log(error);
+    });
 });
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beer => {
+      res.render('random-beer', { beer });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
 
 
 app.listen(3000);
