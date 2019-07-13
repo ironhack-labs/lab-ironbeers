883b0b7ebf7f0687dc874cb1f5d55c4584eeca51
znokdieline <arogelphoto@gmail.com>
2019-07-13 01:02:23 -0500

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 
@@ -16,6 +17,33 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+ 
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(randomBeer => {
+      console.log(randomBeer)
+      res.render('randomBeer',{randomBeer})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  });
+
+
 
 
 app.listen(3000);
+
