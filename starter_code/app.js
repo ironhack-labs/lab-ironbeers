f2aff925958c4e124ccd3151d3f2e3218c533b38
Mark Trevail <trevail.m@gmail.com>
2019-07-16 11:11:34 +0200

    Added layout

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,11 +9,12 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
 });
 
 
