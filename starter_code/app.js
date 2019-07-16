7f21fa025eb48d7c1b82cb172cc818afd7965cd9
pujasmint <pujasingh.jmd.jh@gmail.com>
2019-07-16 22:06:25 +0200

    initial commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,30 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
+app.get('/', function(req, res){
+  res.send("Hello world!!");
+})
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get('/index', (req, res, next) => {
+  res.render('index.hbs');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+  res.render('beers.hbs', {beerList : beers}); 
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('randomBeer',)
 
 
-app.listen(3000);
+
+
+app.listen(3000, ()=>{console.log('App is running on port 3000!')})
