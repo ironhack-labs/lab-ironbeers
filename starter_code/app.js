8984606d4d8cbffceaeb2226d27125448db9dff0
Tatiane Komrij <tatianekomrij@Tatianes-MacBook-Pro.local>
2019-07-17 14:17:55 +0200

    final one

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,15 +5,40 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
+app.get('/', (require, response) => {
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+    response.render('index');
+});
+
+app.get('/beers', (require, response) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    response.render('beers.hbs', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })  
+});
+
+
+app.get('/randomBeers', (require, response) => {
+  punkAPI.getRandom()
+  .then(randomBeers => {
+    console.log(randomBeers)
+    response.render('randomBeers.hbs', {randomBeers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 
