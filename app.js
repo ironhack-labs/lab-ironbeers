9afb0fd4c4b38c1e81c3c7c22efd46060e594099
Cody Dow <wodydoc@gmail.com>
2019-07-18 14:38:55 +0200

    first commit

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+const path = require('path')
+const PunkAPIWrapper = require('punkapi-javascript-wrapper')
+const punkAPI = new PunkAPIWrapper()
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, '/views'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res, next) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
-
-
-app.listen(3000);
+app.listen(3000)
