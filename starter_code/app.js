3a8c2897ee18025f23a7e459cc130a22c5668d54
ckompier <kompi14@hotmail.com>
2019-07-24 22:35:55 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,10 +10,37 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.get('/', (req, res) => {
 
 
-app.get('/', (req, res, next) => {
+
   res.render('index');
+});
+app.get('/beers',(req,res) => {
+
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers} )
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+} )
+app.get('/random-beers', (req, res) => {
+
+  punkAPI.getRandom()
+  .then(beer => {
+    console.log(beer)
+    res.render('random-beers', {beer: beer[0]})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  ;
 });
 
 
