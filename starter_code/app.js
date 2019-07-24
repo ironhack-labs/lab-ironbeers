62e76c5e76d98b877f2a36e3e9bf7434d20a1946
Rebeca Lopez <rebecalopezsaelices@MacBook-Air-de-REBECA.local>
2019-07-24 20:14:55 +0200

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,35 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
 
+//enrutamiento
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers =>{
+    console.log(beers)
+    res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    console.log({beer:beer[0]})
+    res.render('random-beers', { beer:beer[0] });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
