2b2e8a8bbfbbda72470e716766e675d98a97a58a
Alex Cabo Martinez <alex@MacBook-Air-de-Alex.local>
2019-07-24 20:46:08 +0200

    beers

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +10,35 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
 
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
-});
+})
 
+app.get('/beer', (req, res) => {
+  punkAPI.getBeers()
+    .then(beers => {
 
+      res.render('beer', { beers });
+
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('beer', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+})
 
 app.listen(3000);
