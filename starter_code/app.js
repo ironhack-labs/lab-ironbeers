4ccc855b2452332989a6c595cfd08fc3a1cf0d40
Lu Suelves <luciasuelves@gmail.com>
2019-07-25 09:11:06 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,28 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+punkAPI.getBeers()
+.then(beers => {res.render('beers', {beers})
+console.log(beers)})
+.catch(error => console.log(error))
+})
 
-
-app.listen(3000);
+app.get('/random-beer',(req,res) => {
+punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', {beers})
+    console.log(beers)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+ app.listen(3000);
