41e695f836f4110eec138848db73d16dc7407123
ebgester <ebg.ester@gmail.com>
2019-07-25 08:36:00 +0200

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,33 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => { 
+   // console.log("Estas son las cervezas", beers)
+    res.render('beers', {beers});
+  })
+  .catch(error => {console.log(error)})
+  
+});
+
+app.get('/random-beers', (req, res, next) => {
+
+  punkAPI.getRandom()
+  .then(beers => {
+
+    res.render('randomBeers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
 app.listen(3000);
