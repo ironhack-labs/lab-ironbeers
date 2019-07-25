3e68416a35288b10485f1e8ddf30d5dd6ec9b8f4
fernandorodrigueztig <fernandorodrigueztig@gmail.com>
2019-07-25 10:01:02 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -17,5 +17,29 @@ app.get('/', (req, res, next) => {
 });
 
 
+app.get('/Beers', (req, res, next) => {
+punkAPI.getBeers()
+  .then(beers => {
+    res.render('Beers', {beers});
+    console.log(beers)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+punkAPI.getRandom()
+  .then(beers => {
+    res.render('Random_Beer', {beers});
+    console.log(beers)
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.get('/Random_Beer', (req, res, next) => {
+  res.render('Random Beer');
+});
 
 app.listen(3000);
