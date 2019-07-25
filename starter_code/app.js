20cdc58e6f188598dc6edcec3c6e208fed902983
Maria Pelaez <maria.shecodes@gmail.com>
2019-07-25 09:25:47 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,14 +8,33 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
 
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => { 
+  res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+  .then(beer => { 
+  res.render('randomBeers', {beer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
