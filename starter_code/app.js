fd11ceb6b664f0ed5916f83fe46ef9790a977caf
Dusty Garon <dustygaron@hotmail.com>
2019-08-04 12:08:28 -0400

    finished

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,10 +1,12 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -12,10 +14,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
+//-----------
+// ROUTES
+//-----------
 
+// home
+app.get('/', (req, res, next) => {
+  res.render('index')
+})
+
+// beers 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+// random beer
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      let beer = beers[0]
+      res.render('random-beer', { beer })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
 
 app.listen(3000);
