42484484fc7c148050192221d4472ef1e086f804
Randolph Amaya <randolphjr7@gmail.com>
2019-08-06 15:31:48 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,46 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// in order to use partials we must first register the partials
+hbs.registerPartials(__dirname + `/views/partials`)
 
+
+
+//////////////////////////////OUR ROUTES////////////////////////////
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get(`/beers`, (req, res, next) => {
+  // getBeers method of PUNKAPI, returns an array of 25 beers
+  punkAPI.getBeers()
+  .then(blah => {
+      console.log("/beers punkAPI successfull");
+      res.render(`beers`, { blah });
+
+  })
+  .catch(error => {
+      console.log(error)
+  })
+})
+
+app.get(`/random-beers`, (req, res, next) => {
+  // get random() method of PUNKAPI; retuns a random beer
+  const randomBeer = punkAPI.getRandom();
+
+  randomBeer.then(beer => {
+    let getBeer = beer[0];
+    //console.log(beer);
+    console.log(`/randomBeer punkAPI worked`);
+    console.log(getBeer);
+    res.render(`randomBeer`,  getBeer )
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 
-app.listen(3000);
+
+app.listen(3000, () => console.log("Running on 3000"));
