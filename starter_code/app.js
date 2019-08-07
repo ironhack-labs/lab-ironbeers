a006929d4ffc3c6e7d7c9ffd84ef2ac2e85f3929
Ahernandezl0889 <ahernandezl0889@gmail.com>
2019-08-06 21:52:08 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,54 @@
+//require all the packages you will need
+const myExpress = require('express');
 
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+//package that allows templating and dynamic views
+const PORT = 3000;
+const hbs = require('hbs');
+const app = myExpress();
+// const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(myExpress.static(('public')));
 
-
+// *******************
+// ROUTES
+// *******************
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+// ROUTE FOR GETTING ALL THE BEERS AND IT'S RENDERED ON "/BEERS"
+app.get("/beers", (req, res, next)=>{
+  punkAPI.getBeers()
+  .then(responseFromDB => {
+    // console.log("response is: ", responseFromDB)
+    // (page you want to render) allBeers is the hbs file that's gonna be rendered, it comes from "views" folder
+                            //"beers is thename of a variable we will use in hbs file"
+    res.render("allBeers.hbs", {beers: responseFromDB})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+// ROUTE FOR GETTING ALL THE BEERS AND IT'S RENDERED ON "/RANDOMBBEERS"
+app.get("/random-beer", (req,res,next)=>{
+  punkAPI.getRandom()
+  .then(beers => {
+    // console.log("Random beer: ", beers[0]);
+    const theBeer = beers[0];
+    res.render("random-beer", theBeer);
 
-app.listen(3000);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
+
+app.listen(PORT, () => console.log("🏃‍ on 3000"));
