ed925a655120c7fb77463b12aebb018aa4f36dac
Julia Ramos <juliaramosguedes@gmail.com>
2019-08-14 22:34:24 -0300

    almost done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,33 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers)
+    res.render('randomBeer', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
 app.listen(3000);
