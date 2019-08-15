b6930ae310797ff0457f6dcb460603c71d0295f4
Roberto Camacho <camachof.roberto@gmail.com>
2019-08-15 08:03:46 -0300

    -Completing the following iteration: Iteration 4 - Beers Views Iteration 5 - Beer Partial

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,33 +1,35 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
 const punkAPI = new PunkAPIWrapper();
-
+const getBeers = punkAPI.getBeer();
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
-app.get('/beers',(req, res, next) => {
-  punkAPI.getBeers()
-  .then(beers => {
-    res.render('beers',{beers})
-  })
-  .catch(error => {
-    console.log(error)
-  })
-})
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then((beers) => {
+      console.log(beers.image_url);
+      res.render('beers', { beers });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 app.get('/random-beers', (req, res, next) => {
-  res.render('random-beers')
-})
+  res.render('random-beers');
+});
 
 app.listen(3000);
