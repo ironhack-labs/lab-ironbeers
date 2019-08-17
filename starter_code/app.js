2c2a6199b830cdd22552824e49ce63df40270929
Your Name <josedelacruz@Joses-MacBook-Air.local>
2019-08-17 15:43:26 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,8 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,6 +18,37 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers.hbs',{
+      beers
+    });
+  })
+  .catch(error => {console.log(error)
+})
+});
 
+// app.get("/random-beer", (req, res, next) => {
+//   punkAPI.getRandom()
+//   .then(beers => {
+//     // console.log("Random beer: ", beers[0]);
+//     const theBeer = beers[0];
+//     // console.log('the beer:', theBeer)
+//     res.render("random-beer", theBeer );
+//   })
+//   .catch(error => console.log(error));
+// });
+
+
+
+app.get('/randombeers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    const theBeer = beers[0];
+    res.render("random-beers.hbs", theBeer);
+  })
+  .catch(error => {console.log(error)})
+})
 
 app.listen(3000);
