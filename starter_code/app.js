d0569fb83df77583171a446930ed22b3f9242b32
Bana Hagos <banahagos@banas-mbp.localdomain>
2019-08-26 21:35:57 +0200

    web app created with PunkAPI

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,6 +5,7 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -15,6 +16,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers})
+  })
+  
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    res.render('randomBeer', beer[0])
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+
+})
 
 
 
