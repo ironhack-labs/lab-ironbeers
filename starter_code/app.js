0dd98d25e4d27ca8ec4061e7ffc09d6ef5377eb7
Lena Lau <frau.lena.lau@gmail.com>
2019-08-27 21:48:05 +0200

    iteration 4 and 5 missing

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,13 +1,14 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -16,6 +17,22 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+
+
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
 
 app.listen(3000);
