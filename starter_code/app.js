1554547d8d317d432005926c86d31e1b67ef1094
fairouz <fairouzkoehlerwest@gmail.com>
2019-08-27 21:30:11 +0200

    first commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,34 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+
+hbs.registerPartials(__dirname + '/views/partial')
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.get('/randomBeers', (req, res, next) => {
+  res.render('randomBeers');
+});
 
 
 app.listen(3000);
