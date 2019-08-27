68b4ba29927f56f90656319daa43d71cc2769694
Niklas Babel <niklas@wane.co>
2019-08-27 21:33:12 +0200

    partials

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,11 +9,38 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers)
+      res.render('beers', {
+        beers
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+
+app.get('/randombeers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('randombeers'), {
+        beers
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
 
 
 
