b25f5730c56d25d96b4b99580dcb85eb616ff74b
Christoph Butenschön <c.butenschoen@gmail.com>
2019-08-27 21:20:55 +0200

    working for iteration 3, returnin a single partial, but not iterating over a beers array (iteration 4)

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,13 +1,14 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -16,6 +17,19 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers');
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.get('/randomBeers', (req, res, next) => {
+  res.render('randomBeers');
+});
 
 
 app.listen(3000);
