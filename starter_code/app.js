4640aff3a9c34468cd2349b6fe61697b27f8da14
Minatsu Homma <minatsu.homma@email.com>
2019-08-31 16:20:25 +0200

    init

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,39 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+//set my view engine to hbs
 app.set('view engine', 'hbs');
+//declare where to find views
 app.set('views', __dirname + '/views');
+//register partial views
+hbs.registerPartials(__dirname + "/partials");
+//serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index',{title:"home page"});
+});
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{ beers, title: "beer page" });
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(randBeer => {
+    res.render('random-beer',{randBeer,title: "random-beer page" });
+  })
+  .catch(error => {
+    console.log(error);
+  })
 });
 
-
-
-app.listen(3000);
+app.listen(3000,()=>{
+  console.log("running on port 3000");
+});
