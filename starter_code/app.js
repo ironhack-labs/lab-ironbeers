94a83a2491d296b2f1a0743818d74356a5234b6a
MFFB <marcusffbrandao@hotmail.com>
2019-09-01 18:37:33 -0300

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +10,37 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 
 
 app.get('/', (req, res, next) => {
+  console.log('chamou a rota /')
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log('beers route')
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
+app.get('/randomBeer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beer => {
+      console.log(beer)
+      res.render('randomBeer', { beer });
+    })
+    .catch(error => {
+      console.log(error)
+    });
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('My lab-ironbeers is listening on port 3000!')
+});
