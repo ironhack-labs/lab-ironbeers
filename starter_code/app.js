1722fd6781db27a36cc2d814cac822fd6a131f91
sara sebti <sarasebti@MacBook-Pro-de-sara.local>
2019-09-03 14:28:16 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,44 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
+// app.use("/",(req,res)=>res.redirect("/index"));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', {title: "index"});
 });
 
+app.get('/index', (req, res, next) => {
+  res.redirect("/");
+});
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log("beers successfully imported")
+    console.log(beers)
+    res.render('beers', {beers: beers, title: "beers"});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    console.log(beer)
+    res.render('random-beers',{beer : beer[0], title:"random-beers"});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.listen(4000,()=>{
+  console.log("server is up @ http://localhost:4000");
+});
