d41e486cf382b8222b612973ce82f372debf5797
paulinevalero <paulinevalero@outlook.com>
2019-09-04 15:00:36 -0500

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,38 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static('public'));
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req,res)=>{
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
+app.get('/random-beers', (req,res)=>{
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers[0])
+    res.render('randomBeer', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+})
 
 app.listen(3000);
