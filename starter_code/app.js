f7099c56d0260bea2e9b94d1ae43c8d5b90a3d81
Sonia Meller <mellersonia@gmail.com>
2019-09-04 11:18:54 +0100

    with partials

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,47 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
 
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+
+// Route "/" (http://localhost:3000)
+app.get("/", (req, res, next) => {
+  res.render("index"); // Render /views/index.hbs
 });
 
+// Route "/beers" (http://localhost:3000/beers)
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      // Render /views/beers-list.hbs
+      res.render("beers", {
+        beers: beersFromApi
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.render('error');
+    });
+});
 
+// Route "/random-beer" (http://localhost:3000/random-beer)
+app.get("/random-beer", (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      // beersFromApi is an array with 1 element (I know it's weird)
+      res.render("randomBeer", {
+        beer: beersFromApi[0]
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.render('error');
+    });
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("App running on http://localhost:3000");
+});
