b9436543b18ec40656a2c6cd981c4b63649a6bfc
erivasf <elias_rf@icloud.com>
2019-09-04 18:49:56 -0500

    vavavavavavavavavavvavavavaqva

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,36 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials((`${__dirname}/views/partials`))
 
 
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers()
+  res.render('beers', {
+    beers
+  })
+});
+
+app.get('/random-beer', async (req, res) => {
+  const [beer] = await punkAPI.getRandom()
+  res.render('random-beer', {
+    beer
+  });
+});
 
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Anda jalando, prro')
+});
