4f35ea1b47bbef56b601eb9acedeb4b829741331
kidistca <kidistca@gmail.com>
2019-09-04 23:23:15 +0100

    ironbeers

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,9 +11,37 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    // const data = {
+    //   beers: beers
+    // };
+    
+    console.log("beers for sale");
+    res.render('beers', {beerList: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  });
+  
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers', {beerRandom: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
 });
 
 
