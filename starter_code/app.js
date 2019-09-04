e7443b894fdf11027c508c02c64b3a7ba5f4fdf7
Tiago Silva <tiagosilva94@outlook.com>
2019-09-04 18:58:03 +0100

    Add all 6 iterations done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,42 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  
+  punkAPI.getBeers()
+  .then(beersFromApi => { 
+    const data = {
+      beers: beersFromApi
+    } 
+  res.render('beers', data);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random-beers', (req, res, next) => {
+
+  punkAPI.getRandom()
+  .then(randomFromAPI => {
+    const data = {
+      beer: randomFromAPI[0]
+    }
+    //console.log("DEBUGGING data",data);
+    //console.log("DEBUGGING randomFromAPI",randomFromAPI[0]);
+    res.render('randomBeer', data);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
