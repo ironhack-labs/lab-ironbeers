73da3b04ffbf70e51c745333f212426817c91e81
Andre Almeida <dealmeida_andre@hotmail.com>
2019-09-04 18:51:11 +0100

    Interaction 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,33 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+ });
+
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+  .then(beers => {
+
+    const data = {
+    beers:beers
+    }
+    console.log(beers)
+    res.render('beers',  data);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
 });
 
-
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
 app.listen(3000);
