b85ed2a9933e37ca77402d5f7621e284d56faaf5
Xavier Martinez <luis.martz7@hotmail.com>
2019-09-04 18:52:57 -0500

    done, me costo acomodar los elementos por usar bootstrap, y no conocia el nombre de los objetos ue traia la API

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,41 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
+app.get('/randomBeer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
-app.listen(3000);
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`escuchando desde http://localhost:${PORT}`)
+})
