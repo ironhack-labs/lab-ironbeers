ddf122690a263db3c41af7e9b91e1339123e2815
Kiran Shagoti <kiranmshagoti@Kirans-MacBook-Air.local>
2019-09-05 20:12:42 +0200

    completed

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,13 +8,37 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers().then(beers => {
+    console.log(beers)
+    res.render("beers", {listOfBeers: beers});
+ 
+  }).catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render("random-beer", {listOfBeers: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
+
+
 
 
 
