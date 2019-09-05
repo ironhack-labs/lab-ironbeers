865b1628d8b768b9cc287bf22df347d5222f1c69
Raal9410 <rahm9410@gmail.com>
2019-09-05 01:32:58 -0500

    done :octocat:

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,27 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers()
+  res.render('beers', {
+    beers
+  })
+});
 
+app.get('/random-beer', async (req, res) => {
+  const [beer] = await punkAPI.getRandom()
+  res.render('random-beer', {
+    beer
+  });
+});
 
-app.listen(3000);
+app.listen(3000, ()=>{
+  console.log(`server running on http://localhost:3000`)
+});
