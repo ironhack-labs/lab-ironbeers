072675343ac1d9acd3b11c9bfd00c7352c899d81
Juan Vidal <juanvidaldom@gmail.com>
2019-09-04 22:42:10 -0500

    solo falta el último punto

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,22 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res, next) => {
+  const beers = await punkAPI.getBeers()
+ {res.render('beers', {beers})} 
+});
 
+app.get('/randomBeer', async (req, res, next) => {
+  const beers = await punkAPI.getRandom()
+ {res.render('randomBeer', {beers})} 
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log(`server running at http://localhost:3000`)
+});
