975fa6213f3e73c3801f19eb0f50ee254d2d8f71
KaterynaStadnyk <kateryna.stad@gmail.com>
2019-09-05 18:49:13 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -7,15 +7,48 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + "/views/partials");
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  const images = "/images/beer.png";
+  
+  res.render('index', {img: images});
+});
+
+// app.get('/beers', (req, res, next) => {
+//   res.render('beers');
+// });
+
+
+app.get("/beers", (req, res)=> {
+  punkAPI.getBeers().then(beers => {
+    console.log(beers);
+    res.render("beers", {beers})
+    
+  }).catch(error =>{
+    
+    console.log(error)
+  })
+  
+})
+
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log('random',beers)
+    res.render('random-beers', {beers});
+  
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 
 
-app.listen(3000);
+app.listen(3005);
