c56843821beb092ac32a73094ed9e1029cd58fff
Thuy Le <gitnap@mailbox.org>
2019-09-07 23:39:58 +0200

    Iterations 1-6. Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,41 @@
 
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.render("index");
 });
 
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      console.log(beers);
+      res.render("beers", { beers });
+    })
+    .catch(error => console.log(error));
+});
 
+app.get("/random-beers", (req, res, next) => {
+  punkAPI.getRandom()
+    .then(randomBeer => {
+      console.log(`randomBeer name:`, randomBeer[0].name);
+
+      res.render("random-beers", { beers: randomBeer });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 app.listen(3000);
