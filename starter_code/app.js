07a2081028f4d19c0b39d9b6ad6b15bf8f5e3f34
Elba Chimilio <elbachimilio@gmail.com>
2019-09-10 14:12:24 -0400

    finished iteration 3; moving to iteration 4

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,9 +10,18 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 app.listen(3000);
