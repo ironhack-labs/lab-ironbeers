5757431f225bc7fb986c6b5fd7b83bc724723a8f
chris bolivar <christopherbolivar@hotmail.com>
2019-09-10 13:05:28 -0400

    iteration 5 done partials kinda working

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,13 +10,24 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
+
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 app.get('/beers', (req, res, next) => {
-  res.render('beers');
+
+punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    res.render('beers', beers);
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 app.get('/random', (req, res, next) => {
   res.render('random');
