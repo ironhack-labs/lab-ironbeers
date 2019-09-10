69753d7cf558df2462c35e8367303b902a17966b
Hoshimotao <jthomas302@gmail.com>
2019-09-10 16:09:51 -0400

    create ironbeers lab

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,12 +9,44 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partial');
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+// get beers
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+
+  .then(beers => {
+    console.log('hey there')
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+
+// get random beers
+app.get('/randomBeers', (req, res, next) => {
+
+  punkAPI.getRandom()
+
+  .then(beers => {
+    console.log('hey there')
+    res.render('randomBeers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
 
 
 
