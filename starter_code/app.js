b48f0e7e5b2b1918ddf840feecd36b7bd2dea310
Christian Roque <croqu008@fiu.edu>
2019-09-10 16:32:17 -0400

    75% done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,36 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use(express.static('public'))
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 
+
+app.get('/random-beer', (req, res, next) => {
+  res.render('randomBeer');
+});
+
+
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{beers})
+    console.log(beers)
+  })
+  .catch(error => {
+    console.log(error)
+  });
+
+});
 
 app.listen(3000);
