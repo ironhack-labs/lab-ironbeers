c6931dabb93cd5cf839a836c6f550ed1db72445e
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-10 16:54:10 -0400

    working on random

--- starter_code/app.js
+++ starter_code/app.js
@@ -30,6 +30,18 @@ app.get('/beers', (req, res, next) => {
         })
 });
 
+app.get('/random-beers', (req, res, next) => {
+
+    punkAPI.getBeers()
+
+    punkAPI.getRandom()
+        .then(beers => {
+            res.render('random-beers', { beers });
+        })
+        .catch(error => {
+            console.log(error)
+        })
+});
 
 
 
