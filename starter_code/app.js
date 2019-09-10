1bdb91446937e93868945c707a284ab94fe185c9
Lauren Laskerr <llask@me.com>
2019-09-10 13:53:37 -0400

    it works, it's not beautiful, kind of flexy, isn't really mobile.

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,10 +10,32 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers().then(beers => {
+    console.log(beers)
+    res.render('beers', {beers});
+  }).catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beer', (req, res, next) => {
+
+  punkAPI.getRandom().then(beer => {
+    console.log(beer)
+    res.render('random-beer', {beer});
+  }).catch(error => {
+    console.log(error)
+  })
 });
 
 
