66f6283419bf00462abb1e3b3a0faa7eed405bad
Elba Chimilio <elbachimilio@gmail.com>
2019-09-10 13:33:09 -0400

    Added first and working on second iteration

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -6,16 +5,14 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
-
-
 
 app.listen(3000);
