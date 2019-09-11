11f00b6a35cfba9586638c810d15b2295ba58b65
Brais Vidal <brais.vidal.rodriguez@gmail.com>
2019-09-11 22:27:38 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,41 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.use(express.static("public"));
-
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {
+        beers: beers
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    });
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      const beer = beers[0]
+      res.render('random-beers', {
+        beer
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    });
 });
 
 
