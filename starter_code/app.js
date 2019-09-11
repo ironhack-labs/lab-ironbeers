e1bbd9b7cf6ac715203e6af9dcd88f5e93852f8a
Julio <juhume.exe@gmail.com>
2019-09-11 20:35:55 +0200

    hecho hasta el punto 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,25 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+     res.render('beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
