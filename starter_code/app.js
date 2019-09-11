661d2f6d1b56338944358c3cda861232cd8507b7
Adrian <adrianivsdk@gmail.com>
2019-09-11 09:44:48 -0400

    Finished with the get beers section, the other one was literally the same thing

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,33 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
+  
 });
+app.get('/beers', (req, res, next) => {
+  punkAPI.getB.eers()
+  .then(beers => {
+    console.log(beers)
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+app.get('/randomBeers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(randomBeers => {
+    res.render('randomBeers', {randomBeers});
+    console.log(randomBeers)
 
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
 app.listen(3000);
