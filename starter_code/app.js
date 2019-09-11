75a4eb08a9e675215f153c47580b0c88ca36088b
Marven Mathelier <marven250@hotmail.com>
2019-09-11 09:02:51 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,11 +10,37 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render("index")
+
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+
+      console.log(beers)
+      res.render('beers', {beers});
+
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+});
+
+app.get('/randomBeers', (req, res, next)=> {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('randomBeers', {beers})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
 
 
 
