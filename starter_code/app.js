bba3d3d98bb2d9756c5b380bc05ade2d291a9953
borgila <elbienesbueno@gmail.com>
2019-09-12 00:49:54 +0200

    half

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,24 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
 app.get('/', (req, res, next) => {
+  
   res.render('index');
 });
-
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(birrotesPorElGañote => {
+      // console.log(beers)
+      res.render('beers', {birrotesPorElGañote});
+    })
+    .catch(error => {
+      console.log(error)
+    });
+});
 
 
 app.listen(3000);
