25e1c0695a2de59ecf2c6da2c46336820da7bcc8
Osheit <chang.osheit@gmail.com>
2019-09-10 23:30:10 -0400

    Missing final feature

--- starter_code/app.js
+++ starter_code/app.js
@@ -18,4 +18,44 @@ app.get('/', (req, res, next) => {
 
 
 
+// app.get('/aha',(req,res,next)=>{
+//   res.render('sample')
+// }
+// )
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers().then(beer=> {
+     console.log(beer)
+      res.render('beerPartial', {beer});
+    // console.log("hello")
+  })
+  .catch(error =>{
+    console.log(error)
+  })
+  });
+
+
+  app.get('/random-beers', (req, res, next)=> {
+    punkAPI.getRandom().then(beer => {
+    res.render('randomBeer', {beer});
+  })
+   .catch(error => {
+    console.log(error)
+   })
+  })
+  //   app.get('views/partial',(req,res,next)=>{
+  //     hbs.registerPartials(__dirname + '/views/partials')
+  //     punkAPI.getBeers().then(beer=> {
+  //   res.render('beerPartial', {beer});
+  // })
+  //   })
+
+
+
+
+
+
 app.listen(3000);
