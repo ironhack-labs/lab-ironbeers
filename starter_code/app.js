1bb19768c8f9249a62d3949e6ae72778779d6d53
Ester-Martinez <martinez.ester@gmail.com>
2019-09-12 08:52:16 +0200

    iteration5

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,22 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
 });
 
+app.get("/beers", (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers", {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+})
 
 
 app.listen(3000);
