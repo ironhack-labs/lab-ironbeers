f17ab939c5c16a95bc9805824921450d13275667
Pablo Giral Alonso <pablo@MacBook-Pro-de-Pablo.local>
2019-09-12 09:34:16 +0200

    [MAD DE] Pablo Giral

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,11 +9,35 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    // console.log(beers)
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
+
+app.get('/randombeer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    console.log(beer)
+    res.render('randombeer', {beer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
 });
 
 
