7bd88c42a38ef686436aeb8d579977d5031a6826
rashier <rashier13@gmail.com>
2019-09-12 09:29:20 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const punkAPI = new PunkAPIWrapper();
 
-hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname, "public")));
@@ -18,7 +18,17 @@ app.get("/beers", (req, res, next) => {
   punkAPI
     .getBeers()
     .then(beers => {
-      res.render("beers",{beers});
+      res.render("beers", { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+app.get("/random-beers", (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(beers => {
+      res.render("beers", { beers });
     })
     .catch(error => {
       console.log(error);
