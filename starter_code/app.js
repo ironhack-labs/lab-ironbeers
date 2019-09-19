fd0a9ac1a9bff067b6980aaec461eb5f16550163
angelmc32 <angel.melendez.c@gmail.com>
2019-09-19 18:03:46 -0500

    Finished exercise without mixins

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,42 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.pug');
 });
 
+app.get('/beers', (req, res, next) => {
+  
+  punkAPI.getBeers()
+  .then(beers => {
+    //console.log(JSON.stringify(beers));
+    res.render('beers.pug', {beers : beers});
+  })
+  .catch(error => {
+    console.log(error)
+  });
 
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res, next) => {
+
+  punkAPI.getRandom()
+  .then(beers => {
+    //console.log(JSON.stringify(beers));
+    res.render('random.pug', {beers : beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+})
+
+app.listen(3000, () => {
+  console.log(`Listening in port 3000`);
+});
