238d86536657b7e7b9ba31be3a1278ab06053f09
nehaarora <neha95218@gmail.com>
2019-09-24 16:43:37 +0200

    ironbeers

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,14 +1,14 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + "/views/partials");
 
 
 
@@ -16,6 +16,27 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(function(beers){
+    // console.log(beers);
+    res.render('beers', {beers});
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(function(randombeer){
+    console.log(randombeer);
+    res.render('randomBeer', {randombeer});
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+});
 
 
 app.listen(3000);
