c2b1070aaf6a391b589754086f18c45f4f597eff
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-09-25 18:04:15 -0300

    iteration 3

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,7 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,6 +15,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(data => {
+    res.render('beers',{data});
+  })
+  .catch(error => {
+    throw new Error(error);
+  })
 });
 
 
