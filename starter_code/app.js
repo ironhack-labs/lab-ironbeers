d4f01110f7b7259d029f488bf154b4b53007680e
Marco <marco.modena@protonmail.com>
2019-09-25 19:54:27 -0300

    90% done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +9,31 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res, next) => {
+  const beer = await punkAPI.getBeers();
+  try {
+    res.render("beers", {
+      beer
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
+app.get('/random-beers', async (req, res, next) => {
+  const beer = await punkAPI.getRandom();
+  try {
+    res.send(beer[0]);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 app.listen(3000);
