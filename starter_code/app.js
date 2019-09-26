6ed50c0e619cf0414812cc675412fe9af86504fb
Rafael Dias <rafaels.dias@me.com>
2019-09-26 08:19:03 -0300

    backend done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,14 +8,33 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', beers);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
+
+app.get('/randomBeer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(randomBeer => {
+    res.render('randomBeer', randomBeer);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
 
 
 app.listen(3000);
