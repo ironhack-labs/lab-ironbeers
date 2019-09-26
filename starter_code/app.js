156c4bd4ec68568d773709ee8d291b869a508c46
Millene <millenems06@gmail.com>
2019-09-26 10:18:42 -0300

    not finished

--- starter_code/app.js
+++ starter_code/app.js
@@ -16,6 +16,22 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (request, response, next) => {
+  const beers = async 
+});
 
+app.get('/random-beers', (request, response, next) => {
+  response.sendFile(__dirname + '/views/random-beers.hbs');
+});
+
+punkAPI.getBeers()
+  .then(beers => {
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.listen(3000);
