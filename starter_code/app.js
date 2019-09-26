2aabeafef29b9c767e4b6706bf37a77cba016c96
Guilherme Tadanori Mori <gtm.mori@gmail.com>
2019-09-25 21:07:37 -0300

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,14 +8,44 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(express.static(path.join(__dirname + '/public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(data => {
+    res.render('beers', {data} );
+  })
+  .catch(error => {
+    throw new Error(error)
+  })
+})
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(data => {
+    res.render('randomBeers', data[0] );
+  })
+  .catch(error => {
+    throw new Error(error)
+  })
+});
 
 app.listen(3000);
+
+
+
+
+// const fetchBeer = async () => {
+//   const data = await punkAPI.getBeers();
+//   res.send(data);
+// }
+// try {
+//   fetchBeer();
+// } catch (error) {
+//   throw new Error(error);
+// }
