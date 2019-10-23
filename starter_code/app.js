b1010cc29ef2d36e697a04af9f2ada52d4f601e2
Gabriel Zsigmond <gaazsig@gmail.com>
2019-10-22 22:32:57 -0300

    class ended

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,7 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/components')
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
