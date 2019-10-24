3f06341da0203af8300416ff750f99888040ce0a
marciano <mcnvrodrigues@gmail.com>
2019-10-23 22:28:11 -0300

    v2

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,7 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,10 +17,21 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
-app.get('/beers', (req, res) => {
-  res.render('beers');
+app.get('/beers', (req, res) => {  
+
+  punkAPI.getBeers()
+  .then(beers => {
+    let filterBeers = beers.slice(0, 6);
+    // console.log(beers);
+    res.render('beers', {filterBeers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
-
+app.get('/random-beers', (req, res) => {
+  res.render('randombears');
+});
 
 app.listen(3000);
