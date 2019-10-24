7d7a981a40861c27950a75d52b6bcdcc5a63a660
Gabriel Zsigmond <gaazsig@gmail.com>
2019-10-23 21:14:34 -0300

    bunus on click goes to specific beer

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,35 +1,54 @@
-
-const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 const punkAPI = new PunkAPIWrapper();
 
-hbs.registerPartials(__dirname + '/views/components')
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + "/views/components");
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 
-
-
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-app.get('/beers', (req, res, next)=> {
-  punkAPI.getBeers()
-  .then(beers => {
-    res.render('beers', {beers})
-    console.log(beers)
-  })
-  .catch(error => {
-    console.log(error)
-  })
-})
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render("beers", { beers });
+      console.log(beers);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-app.get('/randome-beers', (req, res, next) => {
-  res.render('randomeBeers')
-})
+app.get("/beer/:id", (req, res, next) => {
+  const id = req.params.id
+  punkAPI
+    .getBeer(id)
+    .then(beer => {
+      res.render("oneBeer", { beer });
+      console.log(beer);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get("/randome-beers", (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      res.render("oneBeer", { beer });
+      console.log(beer)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 app.listen(3000);
