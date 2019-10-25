85c5130a27f8e9dc22d6e68d1dcc55c9a20d0f4f
nasciba <amandanasciba@gmail.com>
2019-10-24 21:18:46 -0300

    lab to be finished

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,26 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get('/', (request, response, next) => {
+    response.render('index');
 });
 
+app.get('/beers', (request, response, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    response.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
+app.get('/random-beer', (request, response, next) => {
+  response.render('random-beer');
+})
 
 app.listen(3000);
+
+
+
