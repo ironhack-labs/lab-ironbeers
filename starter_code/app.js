3093b53ebcf502ad026a5f26394190d57b60ec15
ionpascaru <iionpascaru@gmail.com>
2019-10-26 12:20:19 +0200

    first part

--- starter_code/app.js
+++ starter_code/app.js
@@ -16,6 +16,17 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
+
+app.get('/home', (req, res, next) => {
+  res.render('index');
+});
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
+
 
 
 app.listen(3000);
