eddb97fd60fc56a933a5c67fb14fad519d07de3d
Solomonrp <matheusrp97@hotmail.com>
2019-10-26 00:21:28 -0300

    Feito

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -11,11 +11,33 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.get('/', (request, response, next) => {
+  response.render('index');
 
-app.get('/', (req, res, next) => {
-  res.render('index');
 });
 
+app.get('/beers', (request, response, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+    response.render('beers', {beers});
+
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.get('/random-beers', (request, response, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    let randomBeer = Math.round(Math.random() * beers.length)
+    console.log(beers[randomBeer]);
+    response.render('random-beers', beers[randomBeer]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
 
 app.listen(3000);
