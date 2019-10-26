46580bcdded808dfafc831a03d8cc326c5aaeffe
Cristina Rodríguez <cristina.rd.90@gmail.com>
2019-10-26 15:40:30 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,14 +6,39 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(response => {
+    console.log(response)
+    res.render('beers', {beers: response});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/randomBeers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(response => {
+    console.log(response)
+  res.render('randomBeers', response[0]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
 });
 
 
