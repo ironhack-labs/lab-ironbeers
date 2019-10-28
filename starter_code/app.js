5a5b7f1b20a2a622381c32fd1ab10774cae48bdc
Elizabeth Lombardi <elizabeth.lombardi7@gmail.com>
2019-10-28 21:54:19 +0100

    Missing styles for only one beer template

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,38 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  let dataIndex = {
+    beerImg: "/images/beer.png"
+  }
+  res.render('index', dataIndex);
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', { beers: beers }); //creamos obj todas
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers', { beers: beers[0] }); //return single obj
+  })
+  .catch(error => {
+    console.log(error)
+  })  
 });
 
 
-
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Running on 3000')
+})
