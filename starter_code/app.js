1502e308313d2c78f5173453dbd2f826d1f4d334
Aitor Arina <aitorulz1@gmail.com>
2019-10-30 19:43:16 +0100

    Finally I ve been able to fix the bullet points within random beer

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,45 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  let data = {
+    img: "/images/beer.png",
+    botonTo: "/beers",
+    botonToRandom: "/randomBeer",
+  };
+  res.render('index', data);
+  
 });
 
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers[0])
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  
+});
+
+app.get('/randomBeer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', { beer: beers[0] });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+
+});
 
 
 app.listen(3000);
