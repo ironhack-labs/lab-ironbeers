f190ddaa5d737593605e0a7d940179d77e6a77de
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2019-11-02 18:47:49 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,40 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + "/views/partials")
 
 
-
+// HOME PAGE
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render("index")
 });
 
 
+
+// BEERS
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers", {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+// RANDOM BEERS
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beer => {
+      res.render("randombeers", { beer: beer[0] })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  
+  
+  });
+  
 
 app.listen(3000);
