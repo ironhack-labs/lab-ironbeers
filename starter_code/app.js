e664be37cf87ec68314614ef99f542da84f3dc30
Sergio Iturzaeta <gios@MacBook-Pro-de-Sergio.local>
2019-11-05 17:09:50 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,45 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  
+  let data = {
+    img: "/images/beer.png"
+  };
+
+  res.render('index', data);
 });
 
+
+
+app.get('/beers', (req, res, next) => {
+  
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers", {beers})
+  })
+  .catch(err => {
+
+    console.log("ERROR", err)
+  })
+
+  
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(randomBeers => {
+    res.render("randomBeer", {randomBeers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
 
 
 app.listen(3000);
