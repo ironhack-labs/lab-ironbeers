1dc962ce12f89c848099baec3556a842ce7beb00
camille <camille.grison@gmail.com>
2019-11-11 19:10:28 +0100

    redesign of first page

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,11 +9,15 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 const beerRouter = require("./routes/beers");
+const randombeerRouter = require("./routes/randomBeer");
 app.use(beerRouter);
+app.use(randombeerRouter);
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 app.listen(3000);
--- starter_code/routes/randomBeer.js
+++ starter_code/routes/randomBeer.js
@@ -3,16 +3,16 @@ const router = new express.Router();
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-router.get("/beers", (req, res) => {
-    punkAPI.getBeers()
+router.get("/random-beers", (req, res) => {
+    punkAPI.getRandom()
         .then(apiRes => {
-            res.render("beers", {
-                beers: apiRes
+            res.render("randomBeer", {
+                randomBeers: apiRes[0]
             });
             //res.json(apiRes); // send back the response as a JSON object
+            console.log("inside randomBeers.js promise");
         })
         .catch(apiErr => console.log(apiErr));
 });
-
 
 module.exports = router;
