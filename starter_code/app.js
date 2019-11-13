b2413fb5545a9086a3a3e93a1d025970ac766917
José Carneiro <josemcarneiro@gmail.com>
2019-11-13 14:35:02 +0000

    Refactored starter code

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,18 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
-
-
 
 app.listen(3000);
