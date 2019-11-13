3e315bfb7837a76e916b8d3efc8189c9021834b3
MarvinSequera <marvinsequera@yahoo.com>
2019-11-13 21:52:09 +0100

    Listo

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,41 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
+
+
+//Routing
+app.get('/', (req, res) => res.render('index'))
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers })
+      //console.log(beers)
+    })
+    .catch(error => {
+      console.log("error")
+    })
+})
+
+app.get('/randomBeer', (req, res) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('randomBeer', { beers })
+    })
+    .catch(erro => {
+      console.log("error")
+    })
+})
 
 
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
 
-
-
-app.listen(3000);
+app.listen(3000, console.log("A la escucha"));
