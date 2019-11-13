011b0e113d91627c7bab6c622711694cd20a79ce
Dennis H <deheeu@gmail.com>
2019-11-13 16:28:14 +0000

    done: iteration 1-6, todo: beautifcation

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,8 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
 const path    = require('path');
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +10,35 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {
+      beers
+    });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(randomBeers => {
+    console.log(randomBeers);
+    res.render('random-beers', {
+      randomBeers
+    });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
