48d8d4a4ed8ea643c1edcde6bcea6f447670a845
Rebecca Wood <rebecca@Rebeccas-Air.home>
2019-11-13 23:21:02 +0100

    version1

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,20 +1,52 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// beers route
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {
+        beers
+      })
+    })
+    .catch(error => {
+      console.log("beerror")
+    })
+})
+
+// random beers route
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('randomBeers', {
+        beers
+      });
+    })
+    .catch(error => {
+      console.log(beerror)
+    })
+});
+
+// render random beers ...?
+
+app.get('/random-beers', (req, res) => {
+  res.render('randomBeers');
+})
+
 
 
 
