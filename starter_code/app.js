2a9e7a17804ecf28f19ae3a327eeb75fa00006d7
Camilosorio <tech@camilo.fr>
2019-11-13 11:23:37 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,20 +1,43 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers().then(beers => {
+    console.log(beers)
+    res.render("beers", { listOfBeers: beers });
+
+  }).catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render("random-beer", { listOfBeers: beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+});
+
+
 
 
 
