65cfbaa0b9f289d48e58c5001e94b425cc3ecbf7
Rodrigo Borba <rodrigoborba@MBP-de-Rodrigo.home>
2019-11-13 23:43:40 +0000

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,47 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers", { beers })
+    console.log(beers)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random', {beerPartial: beers[0]});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+});
+
+app.get('/:id', (req, res, next) => {
+  console.log(req.params.id);
+
+  punkAPI.getBeer(req.params.id)
+  .then(beers => {
+    res.render('beersId', {
+      beers
+    });
+  })
+  .catch(error => {
+    console.log(error);
+    next(error);
+  });
+});
 
 app.listen(3000);
