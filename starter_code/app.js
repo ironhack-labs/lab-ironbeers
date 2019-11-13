3fc2df648f0c1d6dd2ccd3895560aa13c07aa494
Sandra Tejedor delso <sandratejedordelso@MacBook-Pro-de-Sandra.local>
2019-11-13 20:37:55 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,14 +1,14 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
 
 
 
@@ -16,6 +16,29 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers)
+      res.render('beers', {
+        beers
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('randomBeer', {
+        beers
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
 app.listen(3000);
