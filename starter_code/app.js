85b62d44b102e2c5f56804d3942b4f21ba772c58
Sonia Cáceres Benítez <soniacaceresbenitez@Air-de-Sonia.home>
2019-11-13 23:34:41 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -15,6 +16,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+app.get("/beers", (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render("beers", { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+app.get("/random-beers", (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beer => {
+      res.render("random-Beers", { beer })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
 
 
 
