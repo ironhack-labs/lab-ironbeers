9ebc0f2e02941e8a5c2bfe136b58a417832fb102
David Daniel Ibarra Reyes <DavidIbarra@MacBook-Pro-de-David.local>
2019-11-13 20:31:55 -0600

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -12,10 +12,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
+
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 
 
-app.listen(3000);
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.hbs");
+// });
+
+app.get("/beers", (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers.hbs", {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+
+
+app.get("/random-beers", (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render("random-beers.hbs", {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+hbs.registerPartials(__dirname + '/views/partials') 
+
+
+
+app.listen(3000, () => console.log("http://localhost:3000"));
