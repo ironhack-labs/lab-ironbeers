f84bc6e265f0abbc1644df963d6df7196fb10786
Berna <berna@email.com>
2019-11-13 18:02:54 -0600

    Faltan estilos

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,29 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+	res.render('index');
 });
 
+app.get('/beers', async (req, res) => {
+	const beers = await punkAPI.getBeers({ abv_gt: 8 });
+	res.render('beers', { beers });
+});
 
+app.get('/random-beer', async (req, res) => {
+	const [ randomBeer ] = await punkAPI.getRandom();
+	console.log(randomBeer.food_pairing[0]);
+	res.render('randomBeer', { randomBeer });
+});
 
-app.listen(3000);
+app.listen(3000, () => console.log(`listen on http://localhost:3000`));
