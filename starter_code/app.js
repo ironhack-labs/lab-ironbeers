2c9c85eb80515e8f954823c129bfd65e6fece32b
Imanol Bernardino Alonso <imanolbernardino92@gmail.com>
2019-11-14 09:31:49 +0100

    Donee

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,12 +9,34 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))             // Registro de partials
 
 
+//ENRUTAMOS
+app.get('/', (req, res, next) => res.render('index'))
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
+app.get('/beers', (req, res) =>
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers)
+      res.render('beers', {beers})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+)
+
+
+app.get('/random-beers', (req, res) =>
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random-beers', {beers}) 
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  
+)
 
 
 
