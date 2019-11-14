04334c227a9053ac10405b328780d8e4b4ceff75
USUARIO <usuario@Macs-MacBook-Pro.local>
2019-11-14 11:53:48 +0100

    hecho, me lie con el bootstrap. Aun no lo controlo y me lio con el .css

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,42 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
 
 
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
+app.get('/', (req, res, next) => res.render('index'));
+app.get('/beers', (req, res) => 
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {beers}
+      )
+    })
+    .catch(error => {
+      console.log(error)
+    })
+);
 
+app.get('/random-beers', (req, res) => 
+punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beers', {beers})
 
+  })
+  .catch(error => {
+    console.log(error)
+  })
+);
 
-app.listen(3000);
+// app.get('/beerPartial', (req, res, next) => {
+//   res.render("beerPartial")
+// });
+
+app.listen(3000)
