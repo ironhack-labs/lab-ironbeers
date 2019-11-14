7541544b30c4ecf6644beb63303b99f4b683187b
Victor <victor@victor.com>
2019-11-14 09:24:37 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,32 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
+app.get('/', (req, res, next) => {res.render('index');});
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers',{beers} )
+      console.log(beers)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  })
+  
+app.get('/random-beers', (req, res,) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random-beers', {beers})
 
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  })
 
 app.listen(3000);
