a8cf66d1b91d1ec6cf7494fd6a09bda615d3143d
EChColorado <edxz7c@gmail.com>
2019-11-13 19:46:13 -0600

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,27 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res, next) => {
+  const beers = await punkAPI.getBeers()//.
+  // then(beers => {
+  //   console.log(beers)
+  // })
+  // .catch(error => {
+  //   console.log(error)
+  // })
+  res.render('beers', {beers});
+});
 
+app.get('/random-beers', async (req, res, next) => {
+  const randomBeers = await punkAPI.getRandom()
+  res.render('randomBeer', randomBeers);
+});
 
 app.listen(3000);
