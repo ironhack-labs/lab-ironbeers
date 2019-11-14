5d5e84642c6901f9e85c32cd4f18659125ab3597
Ninoska Cairos <ninocairos@NinoCairos.local>
2019-11-14 09:26:40 +0100

    cnaskjcn

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,40 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
+
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
-
-
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {
+        beers
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(randomBeers => {
+      res.render('randomBeers', {randomBeers})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 
 app.listen(3000);
