599a647576a337d55bb59513a0a8481914eaefea
Carlos HM <carloshm@MacBook-Pro-de-Carlos-2.local>
2019-11-13 18:49:56 -0600

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,16 +6,30 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers({'abv_gt': 8});
+  
+  res.render('beers', {beers});
+});
+
+app.get('/random-beers', async (req, res) => {
+  const randomBeer = await punkAPI.getRandom();
+  console.log(randomBeer)
+  res.render('random-beers', ...randomBeer);
+});
 
 
-app.listen(3000);
+
+app.listen(3000, () => console.log("http://localhost:3000"));
