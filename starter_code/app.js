96e2337a0b88434484d12c669d9197f821b7c4b4
caprosset <capucinerosset@gmail.com>
2019-11-14 13:08:07 +0100

    Iteration 2

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,11 +11,19 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+// ROUTES
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  res.render('');
+});
+
+app.get('/random-beer', (req, res, next) => {
+  res.render('');
+});
 
 
+// START SERVER
 app.listen(3000);
