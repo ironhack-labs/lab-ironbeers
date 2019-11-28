addf599efb9e9a882d3017ff7e614704a348f03f
abernier <antoine.bernier@gmail.com>
2019-11-28 12:26:34 +0100

    solution

---
+++ solution_code/app.js
@@ -0,0 +1,43 @@
+
+const express = require('express');
+const hbs     = require('hbs');
+const app     = express();
+const path    = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers().then(beers => {
+    console.log('beers', beers);
+    
+    res.render('beers', {
+      beers: beers
+    });
+  }).catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom().then(beers => {
+    console.log('beers', beers);
+    
+    res.render('randomBeer', {
+      beer: beers[0]
+    });
+  }).catch(error => {
+    console.log(error)
+  })
+});
+
+app.listen(3000);
