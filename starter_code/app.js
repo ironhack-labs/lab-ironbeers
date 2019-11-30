84ec2fc6d3a2e7ad8489cbcb213e08ea1e9ec9b9
malva <you@example.com>
2019-11-30 00:49:23 +0000

    closer

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,23 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(responseFromDB => {
+    // console.log("Response is:",  responseFromDB);
+    // allBeers is the hbs file that's gonna be rendered, it comes from "views" folder
+                        // "beers" is the name of a variable we will use in hbs file 
+    res.render("beers.hbs", { beers: responseFromDB });
+  })
+  .catch(error => console.log(error));
 });
 
 app.listen(3000);
