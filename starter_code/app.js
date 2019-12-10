c4297379a52cc1ad75d5ba83630c9d53ef5f2f0d
Monika <mswidzinska87@gmail.com>
2019-12-10 17:10:50 +0100

    beers

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,17 +2,27 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+    res.render('index');
+});
+app.get('/beers', (req, res, next) => {
+    punkAPI.getBeers()
+        .then((beers) => {
+            console.log('beers');
+            res.render('beers', { beers });
+        })
+        .catch(error => {
+            res.render('error');
+            console.log(error);
+        });
 });
 
 app.listen(3000);
