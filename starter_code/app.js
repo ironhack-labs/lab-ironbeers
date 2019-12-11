d9abc5f028f87ab93d07962b14cbd90d1e205ad4
MSHSusanneV <voglsus@mediamarktsaturn.com>
2019-12-11 17:50:19 +0100

    Für Patrick zur Fehlersuche

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,8 +11,23 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {  
+        console.log(beers);
+    res.render('beers', {beers});
+
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
+
 
 app.listen(3000);
