400f4b3f37c50441aca45df410bed7bb548ed3fc
Strapak, Sergej <strapak@mediamarktsaturn.com>
2019-12-11 16:15:14 +0100

    Functional beerPartial

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,13 +6,27 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+ punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error);
+  });
 });
 
 app.listen(3000);
