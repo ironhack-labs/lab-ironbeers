240994a6bcecebfd76e7e2372acb7fcc61ce30fe
Mike Ta <h.michael.ta@gmail.com>
2019-12-11 10:31:21 +0100

    still working on iteration 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,17 +2,42 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then((beers) => {
+    console.log(beers);
+    res.render('beers', { beers });
+  })
+  .catch(error => {
+    res.render('error');
+    console.log(error);
+  });
+});
+
+app.get("/random-beer", (req,res, next)=> {
+  punkAPI.getRandom()
+  .then((beerPartial) => {
+  console.log(beers);
+  res.render("randomBeers", beerPartial[0]);
+  })
+  .catch(error => {
+    console.log(error);
+    res.render('error');
+  });
+  
+})
+
 
 app.listen(3000);
