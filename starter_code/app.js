7c8a0a6b889e820237d8f4afe120f104f7238b83
Bialas <bialaska@media-saturn.com>
2019-12-11 19:19:15 +0100

    iteration 5 started

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,8 +11,22 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+    console.log(beers)})
+  .catch(error => {
+    console.log(error);
+  });
+  
 });
 
 app.listen(3000);
