691e0ffc906605c0a851384ac9a75e78fded8674
Jorch39 <jorch.finanzas@gmail.com>
2019-12-14 17:00:48 -0600

    Lab accomplished

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,11 +8,38 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+	punkAPI
+		.getBeers()
+		.then(beers => {
+			console.log(beers)
+			res.render('beers', {beers});
+		})
+		.catch(error => {
+			console.log(error);
+		});
+})
+
+app.get('/random-beers', (req, res, next) => {
+	punkAPI
+	  .getRandom()
+	  .then(beers => {
+	  	res.render('random-beers', {beers});
+	  })
+	  .catch(error => {
+	    console.log(error);
+	  });
+	
+})
+
 
 app.listen(3000);
