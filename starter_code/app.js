30c1957e9e4f11fbbbe22439d7f3ff923036edca
Servo MacPro <tarzanboycraft@hotmail.com>
2019-12-14 14:29:35 -0600

    beers and index

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,8 +11,45 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/random-beers' , (req,res)=>{
+  punkAPI
+  .getRandom()
+  .then(beers => {
+    console.log({chela: beers[0]})
+    res.render('random-beers', {chela: beers[0]})
+  })
+  .catch(error => {
+    console.log(error);
+  });
+})
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(chelas => {
+    res.render('beers', {chelas});
+    // return chelas;
+    // let chelasResult = [[]]
+    // chelas.forEach((chela, index) => {
+    //   console.log(chelasResult[chelasResult.length-1].length < 4)
+    //   if(chelasResult[chelasResult.length-1].length < 4){
+    //     console.log('primer caso'+chela.length)
+    //     chelasResult[chelasResult.length-1].push(chela)
+    //   }else{
+    //     chelasResult.push([chela])
+    //   }
+    // })
+
+    console.log(chelasResult.length);
+    console.log( '- chelas -', chelasResult )
+    res.render('beers', {chelasResult});
+  })
+  .catch(error => {
+    console.log(error);
+  });
+})
 
 app.listen(3000);
+
