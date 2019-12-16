737067d74b23a0c1451ccbcd13cc20995e5e3570
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2019-12-16 16:26:12 -0600

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -19,7 +19,6 @@ app.get('/beers', (req, res, next) => {
   punkAPI
   .getBeers()
   .then(beers => {
-    // console.log(beers[0]);
     res.render('beers', { beers });
   })
   .catch(error => {
@@ -27,7 +26,14 @@ app.get('/beers', (req, res, next) => {
   });
 });
 app.get('/random-beers', (req, res, next) => {
-  res.render('random-beers');
+  punkAPI
+  .getRandom()
+  .then(beers => {
+    res.render("random-beers", { beers });
+  })
+  .catch(error => {
+    console.log(error);
+  });
 });
 
 app.listen(3000, () => console.log('servidorlisto'));
