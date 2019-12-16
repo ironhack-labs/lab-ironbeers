95420919601ac10eb0f4acfcf886a39d6ab488cf
Rafael Dias <rafaels.dias@me.com>
2019-12-16 12:04:02 -0300

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,6 +6,8 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+const port = 3000
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 hbs.registerPartials(__dirname + '/views/partials')
@@ -19,22 +21,23 @@ app.get('/', (req, res, next) => {
 app.get('/beers', (req, res, next) => {
   punkAPI.getBeers()
   .then(beers => {
-    res.render('beers', beers);
+    res.render('beers', { beers });
   })
   .catch(error => {
     console.log(error);
   })
 });
 
-app.get('/randomBeer', (req, res, next) => {
-  punkAPI.getRandom()
-  .then(randomBeer => {
-    res.render('randomBeer', randomBeer);
-  })
-  .catch(error => {
+app.get('/randomBeer', async(req, res, next) => {
+   const beers = await punkAPI.getRandom()
+  try {
+    res.render('randomBeer', { 
+      beers
+    })
+  }catch(error) {
     console.log(error);
-  })
+  }
 });
 
 
-app.listen(3000);
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
