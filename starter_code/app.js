4af95926c71ffa3a57da9a7cf8af51b452ee93fe
henricasi <henriquecmandruzato@gmail.com>
2019-12-18 17:54:33 -0300

    Iteration 1 done

--- starter_code/app.js
+++ starter_code/app.js
@@ -12,7 +12,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
+});
+
+app.get('/beers', (req, res, next) => {
+  res.render('beers.hbs');
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers.hbs');
 });
 
 app.listen(3000);
