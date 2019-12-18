8ecccbe4107d6d1c2e4ffaa367340d36c54112ac
Leonardo Polon <lpolon@gmail.com>
2019-12-18 18:34:31 -0300

    first iteration done

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,9 +6,11 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// hbs setup
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+// static serving setup
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
