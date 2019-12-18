f824a0c8328b5bf47ba3d28f21e1bd8d483486d5
henricasi <henriquecmandruzato@gmail.com>
2019-12-18 18:41:04 -0300

    Iteration 5 complete

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,12 +11,22 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/', (req, res, next) => {
   res.render('index.hbs');
 });
 
 app.get('/beers', (req, res, next) => {
-  res.render('beers.hbs');
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error);
+  });
 });
 
 app.get('/random-beers', (req, res, next) => {
