98d590297d05b0b9058b8fc0703685d21e563031
Nykolle Malone <nykolle.malone@hotmail.com>
2019-12-18 22:26:20 -0300

    almost done! some css to do

--- starter_code/app.js
+++ starter_code/app.js
@@ -15,4 +15,28 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.get('/beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    throw new Error(error);
+  });
+});
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.get('/random-beers', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(beers => {
+    res.render('random-beers', {beers});
+  })
+  .catch(error => {
+    throw new Error(error);
+  });
+});
+
 app.listen(3000);
