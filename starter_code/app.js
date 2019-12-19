220a51599a789264d6b0ef9ff956661ea09518ea
Reginaldo Barbosa <reginaldobr@gmail.com>
2019-12-18 23:54:06 -0300

    almost done iteration 5 do not show images and can not get any information

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,13 +6,39 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('beerPartial');
+  // res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(data => {
+      res.render('beers', { data });
+    })
+    .catch(error => {
+      throw new Error(error);
+      // console.log(error);
+    })
+});
+
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeers',{beers});
+  })
+  .catch(error => {
+    throw new Error(error);
+  })
 });
 
 app.listen(3000);
