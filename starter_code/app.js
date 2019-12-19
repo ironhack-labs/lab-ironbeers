2522f86db33a47d5c5d558597d1bd01cc37c831f
Giu Roperto <giu.roperto@gmail.com>
2019-12-18 22:53:55 -0300

    adjusting iteration 6

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,8 +11,45 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// how to call the function to see whats inside the Premise
+// punkAPI.getBeers().then(beers => console.log(beers));
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    const data = {
+      beers: beers,
+    }
+    res.render('beers', data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
+
+// how to call the function to see whats inside the Premise
+punkAPI.getRandom().then(beers => console.log(beers[0]));
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(beers => {
+    const data = {
+      beers: beers[0],
+    }
+    res.render('randomBeer', data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
+
 
 app.listen(3000);
