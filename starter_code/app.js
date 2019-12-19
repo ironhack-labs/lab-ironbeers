d7b3c32583a957b8d3b84ea734c29a236d60f345
Joyce Villarroel Mascarenhas <joycevillarroel@MacBook-Air-de-Joyce.local>
2019-12-19 00:36:18 -0300

    iteration 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
@@ -8,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,4 +17,33 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/Beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    res.render('Beers', {beers} );
+
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+  
+});
+
+app.get('/Random-Beer', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(beers => {
+    res.render('Random-Beer', {beers});
+  })
+  .catch(error => {
+    console.log(error);
+  });
+  
+  
+});
+
+
 app.listen(3000);
+
