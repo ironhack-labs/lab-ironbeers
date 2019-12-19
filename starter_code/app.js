9b806489602f89ea49f36f501113fa8774b410be
Juliane <juliane_ch.arruda@live.com>
2019-12-19 00:07:21 -0300

    one to three interaction completed

--- starter_code/app.js
+++ starter_code/app.js
@@ -15,4 +15,22 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+
+      res.render('beers');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI
+  res.render('random-beers');
+});
+
+
 app.listen(3000);
