37c420fff50a4aeb2b2090befe2049342f32f396
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-01-17 01:40:53 -0500

    1st to 3rd iteration done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,11 +8,27 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index', { title: 'Home' });
 });
+
+//Iteration-3
+// Inside the /beers route, call to the getBeers() method of our PunkAPI package.
+// The package will return you an array of 25 beers, and you should pass that array to the beers.hbs view.
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { title: 'Beers', beers });
+    })
+    .catch(error => console.log(error));
+});
+// app.get('/random-beer', (req, res, next) => {
+//   res.render('random-beer', { title: 'Random Beer' });
+// });
 
 app.listen(3000, () => console.log(`Server running`));
