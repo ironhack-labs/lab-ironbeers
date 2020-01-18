aa8191fe26b0a48a74b0dc74d475557bfe750098
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-01-18 01:45:06 -0500

    update

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,26 +11,48 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(__dirname + '/views/partials');
 
+// Handlebars.registerHelper('ifeq', function(a, b, options) {
+//   if (a == b) {
+//     return options.fn(this);
+//   }
+//   return options.inverse(this);
+// });
+
+// /Routes:
 app.get('/', (req, res, next) => {
   res.render('index', { title: 'Home' });
 });
 
-//Iteration-3
+// Iteration - 3;
 // Inside the /beers route, call to the getBeers() method of our PunkAPI package.
 // The package will return you an array of 25 beers, and you should pass that array to the beers.hbs view.
 app.get('/beers', (req, res, next) => {
   punkAPI
     .getBeers()
     .then(beers => {
-      res.render('beers', { title: 'Beers', beers });
+      // console.log('Output for: beers', beers.length);
+
+      res.render('beers', {
+        title: 'Beers',
+        myStyle: ' stylesheets/beers.css',
+        beers,
+        len: false
+      });
     })
     .catch(error => console.log(error));
 });
 app.get('/random-beer', (req, res, next) => {
   punkAPI
     .getRandom()
-    .then(beer => {
-      res.render('randomBeer', { title: 'Random Beer', beer });
+    .then(beers => {
+      // console.log('Output for: beers', beers.length);
+
+      res.render('randomBeer', {
+        title: 'Random Beer',
+        myStyle: 'stylesheets/randomBeer.css',
+        beers,
+        len: true
+      });
     })
     .catch(error => console.log(error));
 });
