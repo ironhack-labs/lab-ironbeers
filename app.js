f6191e0d4a2d7640781778e77604fb7622c73d94
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-01-22 21:11:18 -0500

    Finish iterations 1-3

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Dependecies
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
@@ -6,12 +7,25 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+const myBeer = punkAPI.getBeers();
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// add the routes here
+// Add the routes here
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) =>  {
+    myBeer.then(beer => {
+        res.render('beers', {beer});
+    });
+    myBeer.catch(error => 
+        console.log(error));    
+});
+
+app.get('/random-beer', (req, res) => res.render('random-beer'));
+
+// Start server
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
