599885abbf2fa318455936062687a28ed6899d30
Delvis <deapud@gmail.com>
2020-01-25 10:27:13 -0500

    add ironBeers lab until bonus5

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,10 +8,46 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+// hbs.registerPartial("beerpartial", path.join(__dirname, '/views/partials'));
+
+const myBeers = punkAPI.getBeers();
+const randomBeer = punkAPI.getRandom();
 
 // add the routes here
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+    myBeers.then((beers) => {
+        res.render('beers.hbs', { beersObj: beers });
+    });
+    myBeers.catch((err) => {
+        console.log(err);
+    });
+});
+app.get('/random-beers', (req, res) => {
+    randomBeer.then((beers) => {
+        res.render('random-beer.hbs', { beers });
+    });
+    randomBeer.catch((err) => console.log(err));
+});
+app.get('/beers/:beerId', (req, res) => {
+
+    const bLink = punkAPI.getBeer(req.params.beerId);
+    bLink.then((beerInfo) => {
+        res.render('random-beer.hbs', { beerInfo });
+    });
+    bLink.catch((err) => {
+        console.log(err);
+    });
+});
+
+
+
+app.use(function (req, res, next) {
+    return res.status(404).send({ message: 'Route' + req.url + ' Not found.' });
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
