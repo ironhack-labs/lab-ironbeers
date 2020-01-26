534c15e786228950040897436eb014b192e49ba6
esrfree <esrfree@gmail.com>
2020-01-26 09:25:07 -0500

    data requested

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,8 +10,26 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname + "/views/partials"));
 
 // add the routes here
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+    console.log(req)
+    return res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+    punkAPI
+        .getBeers()
+        .then(beers => res.render('beers', {beers}))
+        .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(beer => res.render('random-beer', beer[0]))
+        .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
