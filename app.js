d6239aabe6b48db6f6a03d54d52d0e566c0216f8
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-01-27 18:01:22 -0500

    Fix 'Beers' page

--- app.js
+++ app.js
@@ -11,18 +11,13 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Partials setup
-hbs.registerPartials(path.join(__dirname, 'views/partials'))
-
-
 // View routes
 app.get('/', (req, res) => res.render('index'));
 
 app.get('/beers', (req, res) =>  {
     const myBeers = punkAPI.getBeers();
     myBeers.then(beers => {
-        console.log(beers);
-        res.render('beers', beers);
+        res.render('beers', {beers});
     });
     myBeers.catch(error => 
         console.log(error));    
@@ -31,7 +26,6 @@ app.get('/beers', (req, res) =>  {
 app.get('/random-beer', (req, res) =>  {
     const myRandomBeer = punkAPI.getRandom();
     myRandomBeer.then(beer => {
-        console.log(beer);
         res.render('random-beer', beer[0]);
     });
     myRandomBeer.catch(error => 
