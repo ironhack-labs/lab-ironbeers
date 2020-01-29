e26a7a43c8dfdc107ac0a3e042f5fbad27fd1f8d
Jorge Aguilar <jaguilar.ice@gmail.com>
2020-01-29 17:43:47 -0600

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers',(req,res)=>{
+    punkAPI.getBeers()
+    .then(beersFromApi=>{
+        res.render('beers',{beersFromApi});
+       // console.log('Beers from database:',cervezas)
+    })
+    .catch(error=>console.log(error))
+
+})
+
+app.get('/random-beer',(req,res)=>{
+    punkAPI.getRandom()
+    .then(randomBeer=>{
+        res.render('random-beer',{randomBeer});
+        console.log('Beers from database:',randomBeer)
+    })
+    .catch(error=>console.log(error))
+
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
