0ad80336fc8fb7a0be17678a3fe100f5b47498e2
Víctor Peña Romero <vic.pero@icloud.com>
2020-01-29 17:13:36 -0600

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+
+    const beers = punkAPI.getBeers()
+        .then(beersFromAPI => {
+            //console.log(beersFromAPI)
+            res.render('beers', {beersFromAPI})
+        })
+        .catch(err => console.log(err))
+});
+
+app.get('/random-beers', (req, res) => {
+
+    const randomBeer = punkAPI.getRandom()
+        .then(randomElement => {
+            const element = randomElement[0]
+            res.render('random-beers', element)
+            console.log(element)
+        })
+        .catch(err => console.log(err))
+
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
