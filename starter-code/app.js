803968073bbde88b55b4a7e5ef0b0a270f69ffb0
RicardoLEGo <ricnoceronte@gmail.com>
2020-01-29 17:23:43 -0600

    PP-WDFT-MEX-EDITH-LOPEZ-RICARDO-LEYVA

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,16 +4,42 @@ const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
-const punkAPI = new PunkAPIWrapper();
+const punkAPI = new PunkAPIWrapper()
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/beers', (req, res) => {
+	punkAPI
+  .getBeers()
+  .then(beer =>res.render('beers', {beer}))
+  .catch(error => res.send('nel'));
+})
+
+
+app.get('/random_beers', (req, res) =>{
+	punkAPI
+	.getRandom()
+	.then(beer =>{
+				console.log(beer);
+			res.render('random_beers', {beer})}
+		)
+	.catch(error => res.send('nel'));
+
+} )
+
+
+
+
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
