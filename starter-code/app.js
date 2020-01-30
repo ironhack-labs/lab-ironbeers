7dc6adcf84b2a02e1762d3234593979f979a2e80
Shahinya Haeckel <sha.haeckel@gmail.com>
2020-01-30 12:42:16 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(__dirname + '/views/partials');
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    let beers = punkAPI.getBeers().then(function (beersFromApi) {
+        // res.send(beersFromApi)
+        res.render('beers.hbs', { beerList: beersFromApi })
+    }).catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+    let randomBeer = punkAPI.getRandom().then(function (randomBeerFromApi) {
+        res.render('random.hbs', randomBeerFromApi[0])
+    }).catch(error => console.log(error))
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
