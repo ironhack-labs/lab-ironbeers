a14cd36af002d7fc83cb960ab025a4ee470d928a
san4eto <59312005+san4eto@users.noreply.github.com>
2020-01-30 13:49:55 +0100

    FTWD BER Sanka Kiyohide
    no styling added

--- starter-code/app.js
+++ starter-code/app.js
@@ -7,13 +7,31 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // add the partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index.hbs'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      //console.log('Beers from the database: ', beersFromApi)
+      res.render('beers.hbs', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('randomBeers.hbs', { randomBeers: responseFromAPI });
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
