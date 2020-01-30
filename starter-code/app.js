944bf1b36bdcd4b65e848d94ebe5f2e5d4f49709
Karlish Vázquez <karlishvazquez@Karlishs-MacBook-Pro-748.local>
2020-01-29 18:17:12 -0600

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req,res) => {
+    punkAPI
+.getBeers().then(data=>res.render('beers', {data}))
+.catch(err=>res.send('ya valio'))
+})
+
+app.get('/random-beers', (req,res)=> {
+    punkAPI.getRandom()
+    .then(randomBeer => res.render('randomBeer', {randomBeer}))
+    .catch(err=> res.send(err))
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
