a7e0afa9cd248ff6d4e9d5cc8ec4dcc52cdfdd41
Jason Franklin <jfrank325@gmail.com>
2020-01-30 13:06:22 +0100

    done without complete styling

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,9 +11,27 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 // add the partials here:
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beer', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { database: beersFromApi }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi =>
+      res.render('random-beers', { random: responseFromApi })
+    )
+    .catch(error => console.log(error));
+});
+
+app.listen(3005, () => console.log('🏃‍ on port 3005'));
