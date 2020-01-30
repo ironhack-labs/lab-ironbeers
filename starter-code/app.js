399e928a8f527466d330309327c35ca698835f4e
akups <appiah-lindberg@outlook.com>
2020-01-30 14:23:56 +0100

    iron beers almost ready

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,33 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+//hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index.hbs'));
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      //console.log(beersFromApi));
+      res.render('beers', { beersList: beersFromApi });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi => {
+      //console.log(beersFromApi));
+      res.render('randomBeers', { randomList: responseFromApi });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
