47b6420cf491ba0aa6ba4ce7db70f02c2621b912
jonathansaudhof <jonathan@jonathansaudhof.de>
2020-01-30 13:27:56 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const hbs = require('hbs');
+const hbs = require('express-handlebars');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -12,8 +12,45 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+app.engine(
+  'hbs',
+  hbs({
+    extname: 'hbs',
+    defaultLayout: 'main.hbs',
+    layoutsDir: path.resolve(__dirname, 'views/layouts')
+  })
+);
+
+app.set();
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromAPI => {
+      //   res.send(beersFromAPI);
+      res.render('beers', { beersList: beersFromAPI, column: true });
+    })
+    .catch(err => console.log(err));
+});
+app.get('/beers/beer-:beerId', (req, res) => {
+  punkAPI
+    .getBeer(req.params.beerId)
+    .then(beersFromAPI => {
+      //   res.send(beersFromAPI);
+      res.render('beers', { beersList: beersFromAPI, extended: true });
+    })
+    .catch(err => console.log(err));
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromAPI => {
+      //   res.send(beersFromAPI);
+      res.render('beers', { beersList: beersFromAPI, extended: true });
+    })
+    .catch(err => console.log(err));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
