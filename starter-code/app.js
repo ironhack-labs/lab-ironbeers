d8d16bca9fbfc4e00ff40c6f8a96f750efb5ae48
MngSebastian <m.sebi97@yahoo.com>
2020-01-31 09:05:38 +0100

    the logic is there but no design

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      //console.log('Beers from the database: ', beersFromApi);
+      res.render('beers.hbs', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+  //   res.send([{ a: beersFromApi }]);
+
+  // res.render('beers.hbs', { beers: beersFromApi });
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      //console.log('Beers from the database: ', beersFromApi);
+      res.render('randombeer.hbs', { randomBeer: responseFromAPI });
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
