28c253bbd3fde074d07eaf40a8c52d901610a97a
abrebion <abrebion@gmail.com>
2020-02-03 16:38:21 +0100

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,38 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+  const beers = punkAPI
+    .getBeers()
+    .then(beers => {
+      console.log(beers);
+      res.render('beers', { beers });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    const beer = await punkAPI.getRandom();
+    res.render('random-beer', ...beer);
+  } catch (error) {
+    console.log(err);
+  }
+});
+
+app.get('/beers/beer-:id', async (req, res) => {
+  try {
+    const beer = await punkAPI.getBeer(req.params.id);
+    res.render('random-beer', ...beer);
+  } catch (error) {
+    console.log(err);
+  }
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
