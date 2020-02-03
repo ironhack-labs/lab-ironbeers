f7b4f2e53532b7a675e544f7c596edcc1fc91097
Pascal Besson <pascalbes@gmail.com>
2020-02-03 16:46:14 +0100

    [PAR-WDFT]-PASCALB

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,47 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // add the partials here:
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+    punkAPI
+    .getBeers()
+    .then(beers => {
+        res.render('beers', {beers})
+    })
+    .catch(error => console.log(error));
+});
+
+
+app.get('/random-beers', (req, res) => {
+
+    punkAPI
+    .getRandom()
+    .then(beerFromAPI => {
+        const beer = beerFromAPI[0];
+        res.render('random-beers', {beer})
+    })
+    .catch(error => console.log(error));
+
+});
+
+app.get('/beers/beer-:id', (req, res) => {
+
+    punkAPI
+    .getBeer(req.params.id)
+    .then(beerFromAPI => {
+        const beer = beerFromAPI[0];
+        res.render('random-beers', {beer})
+    })
+    .catch(error => console.log(error));
+
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
