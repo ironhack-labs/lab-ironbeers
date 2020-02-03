28128dfcd6fa9e3ffae61272152fe055e0b7be18
Cess Flech <cecile.flechon@gmail.com>
2020-02-03 18:22:34 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the partials here:
 
 // add the routes here:
+// index
 app.get('/', (req, res) => res.render('index'));
+
+// beers
+app.get('/beers', (req, res) => {
+	punkAPI
+		.getBeers()
+		.then((beers) => {
+			console.log(beers);
+			res.render('beers', { beers });
+		})
+		.catch((error) => console.log(error));
+});
+
+// random beers
+app.get('/random-beer', (req, res) => {
+	punkAPI
+		.getRandom()
+		.then((beer) => {
+			console.log(beer);
+			res.render('random-beer', { beer });
+		})
+		.catch((error) => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
