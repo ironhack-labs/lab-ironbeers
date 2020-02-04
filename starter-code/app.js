f636bcb23607cc255056849078e2da0b0e2a57a8
Pierre Etryhard <pierre.etryhard.pro@gmail.com>
2020-02-04 09:09:20 +0100

    done with the best styles ever

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,42 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get(['/', '/home'], (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    punkAPI
+        .getBeers()
+        .then(beers => {
+            res.render("beers", {
+                beers
+            });
+        })
+        .catch(dbErr => console.error(dbErr));
+});
+
+app.get('/random-beers', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(beer => {
+            const random = beer[0];
+            res.render("random-beer", {
+                random
+            });
+        })
+        .catch(dbErr => console.error(dbErr));
+});
+
+app.get('/beers/info-beer/:id', (req, res) => {
+    punkAPI
+        .getBeer(req.params.id)
+        .then(beer => {
+            const data = beer[0];
+            res.render("info-beer", {
+                data
+            });
+        })
+        .catch(dbErr => console.error(dbErr))
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
