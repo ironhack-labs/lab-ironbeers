cc80f353afacde77618729ef060bc1b10abea950
Paul Dkn <pauldkn.code@gmail.com>
2020-02-04 11:37:09 +0100

    Done: lab

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+    .then(beersFromApi => {
+        console.log('Beers from the database: ', beersFromApi);
+        res.render('beers', {beersFromApi});
+    })
+    .catch(err => {
+        console.error(err);
+    })
+})
+
+app.get('/random-beer', (req, res) => {
+    punkAPI.getRandom()
+    .then(randomBeer => {
+        console.log('Random beer: ', randomBeer);
+        res.render('random-beer', {randomBeer});
+    })
+    .catch(err => {
+        console.error(err);
+    });
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
