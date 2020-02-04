46dc10acc42ff3955f09e0828aae2226edd709a6
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-02-04 09:23:04 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, "./views/partials"));
+// hbs.registerPartial("footer", "{{footer}}");
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+// Route to Beers Page
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+    .then(beersFromAPI => {
+        console.log({beersFromAPI});
+        res.render('beers', {beersFromAPI})
+    })
+    .catch(error => console.log(error));
+})
+
+// Route to Random Beer Page
+app.get('/random-beer', (req, res) => {
+    punkAPI
+    .getRandom()
+    .then(resAPI => {
+        res.render('random-beer', {randBeer: resAPI[0]})
+    })
+    .catch(error => console.log(error));
+})
+
+app.listen(3000, () => console.log('🏃‍ http://localhost:3000/'));
