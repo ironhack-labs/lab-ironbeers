34322f73af29a90a998f99ce31a5d50d096e51c1
nelson1812 <nelsoncc1812@gmail.com>
2020-02-05 21:22:25 +0100

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,11 +9,27 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beers => res.render('beers', {
+            beers
+        }))
+        .catch(err => console.log('Ha ocurrido un error: ' + err));
+});
+app.get('/random_beers', (req, res) => {
+    punkAPI.getRandom()
+        .then(randomBeer => res.render('random_beers', {
+            random: randomBeer[0]
+        }))
+        .catch(err => console.log("Ha ocurrido un error: " + err))
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
