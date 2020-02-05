2b1b5f5ad2bc4fe2f7dc76f07618f1ba55808923
Carolina Guzman <carolinaguzman76@gmail.com>
2020-02-06 00:55:43 +0100

    despes de maquetacion

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,6 +6,9 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -14,6 +17,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index'))
+app.get('/beers', (req, res) => {
+    
+    
+    punkAPI.getBeers()
+    .then(beersFromApi => {
+        console.log('Beers from the database: ', beersFromApi)
+        res.render('beers', {beersArray: beersFromApi})
+    })
+    .catch(error => console.log(error))
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+})
+app.get('/randomBeers', (req, res) => {
+
+    punkAPI.getRandom()
+    .then(responseFromAPI => {
+    
+        res.render('randomBeers', {randomBeer: responseFromAPI[0]})
+        
+      })
+      .catch(error => console.log(error))
+
+})
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'))
