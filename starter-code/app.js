901f38f09332560ab592d00f82ed2962afb8f9c6
Remy Pham <remy.8P@gmail.com>
2020-02-05 17:34:14 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,42 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+    // beers route :
+app.get('/beers', (req,res) => {
+    punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+        res.render('beers', {beers: beersFromApi})
+    })
+    .catch(error => console.log(error))
+});
+
+
+    // random-beers route :
+app.get('/random-beers', (req,res) => {
+    punkAPI
+    .getRandom()
+    .then(oneBeer => {
+        console.log(oneBeer)
+        res.render('random-beers', {beer: oneBeer})
+    })
+    .catch(error => console.log(error))
+})
+
+    // beer by ID :
+app.get('/beers/:id', (req,res) =>{
+    punkAPI
+    .getBeer(req.params.id)
+    .then(details => {
+        res.render('beer-details', {details})
+    })
+    .catch(erreur => console.log(erreur))
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
