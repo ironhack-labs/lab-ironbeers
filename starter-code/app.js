26be62f2e9f21e13e928ceaba574b6891edd5844
Jaime Hidalgo Fernandez <jaimehidalgofernandez@gmail.com>
2020-02-05 21:34:43 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,22 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(__dirname + '/views/partials');
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beers => res.render('beers', {beers}))
+})
+
+app.get('/random-beer', (req, res) => {
+    punkAPI.getRandom()
+    .then(beer => res.render('random-beer',{beer}))
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
