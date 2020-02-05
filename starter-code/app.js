ff63ea83629a88c6c248358a606b8bd944b4f66f
CrisSugar <aguacero18@hotmail.com>
2020-02-05 21:31:52 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,6 +6,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+app.use(express.static('public'));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -15,5 +17,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beer => res.render('beers', { beer }))
+})
+
+app.get('/random-beers', (req, res) => {
+    punkAPI.getRandom()
+        .then(beer => res.render('random-beers', { beer }))
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
