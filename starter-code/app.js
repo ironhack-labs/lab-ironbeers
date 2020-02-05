9e00ec17781e20982c0495309eb50eab8ef73872
davidvrl <veral.david@gmail.com>
2020-02-05 20:51:36 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,6 +8,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,5 +16,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beersFromApi => res.render('beers', {
+            allBeers: beersFromApi
+        }))
+        .catch(error => console.log(error));
+
+
+
+})
+app.get('/random-beers', (req, res) => {
+    punkAPI.getRandom()
+        .then(randomBeers => res.render('random-beers', {
+            randomBeer: randomBeers
+        }))
+
+        // const randomBeer = punkAPI.getRandom()
+
+        // randomBeer.then(beer => {
+        //         alert(beer[0].name)
+        //     })
+
+
+        .catch(error => console.log(error));
+
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
