80616268194c0d7ed9479a49b74831a917d4df72
Carlos González <cargonamt@gmail.com>
2020-02-05 21:14:04 +0100

    partial done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/Beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi =>
+      res.render('Beers', {
+        allBeers: beersFromApi
+      })
+    )
+    // .then(res.render('Beers', allBeers:beersFromApi))
+    .catch(error => console.log(error));
+});
 
+app.get('/randombeers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI =>
+      res.render('randombeers', {
+        Beer: responseFromAPI
+      })
+    )
+    .catch(error => console.log(error)); //res.render('randombeers'));
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
