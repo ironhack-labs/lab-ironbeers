d64fb7233ae30986988c5b7335bcca9de079fe8f
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-02-05 21:06:20 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,23 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beer => res.render('beers', { beer }))
+})
+
+
+app.get('/random-beers', (req, res) => {
+    punkAPI.getRandom()
+        .then(beer => res.render('random-beers', { beer }))
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
