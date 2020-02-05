392c20dc8a3d02f12d04fa3ff352036613494717
Laura <lauratp02@gmail.com>
2020-02-05 23:13:48 +0100

    First Commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
@@ -12,8 +13,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(__dirname + '/views/partials');
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beers => res.render('beers', {
+            beers
+        }))
+        .catch(error => console.log(error))
+
+})
+
+app.get('/random-beers', (req, res) => {
+    punkAPI.getRandom()
+        .then(beers => res.render('random-beers', { beers}))
+        .catch(error => console.log(error))
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
