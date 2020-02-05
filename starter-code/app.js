d6e5dcb3e192fc0da925f38cbda9fa96fc3ae98c
Juan Antonio Martin <juanantoniomartinb@gmail.com>
2020-02-05 21:52:17 +0100

    mae mia

--- starter-code/app.js
+++ starter-code/app.js
@@ -15,5 +15,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beersFromApi => res.render('beers', {
+            allBeers: beersFromApi
+        }))
+        .catch(error => console.log(error));
+})
+
+app.get('/random-beers', (req, res) => {
+    punkAPI.getRandom()
+        .then(beersFromApi => {
+            res.render('random-beers', {
+                randomBeer: beersFromApi[0]
+            })
+            console.log(beersFromApi[0])
+        }
+        )
+        .catch(error => console.log(error));
+})
+
+// res.render('random-beers'));
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
