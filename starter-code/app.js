59bf696389278d3f53263a5a75548a67fbd75845
Roberto Marrero <robertomarrerop@hotmail.com>
2020-02-06 09:20:51 +0100

    haciendo partials

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+app.set('views', path.join(__dirname, 'views'))                         // Views directory setup
+app.set('view engine', 'hbs')                                           // View engine setup
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'))         // Partials setup
+
 // add the routes here:
+
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beersFromApi => res.render('beers', { allBeers: beersFromApi }))
+        .catch(error => console.log(error));
+});
+app.get('/randomBeers', (req, res) => {
+    punkAPI.getRandom()
+        .then(responseFromAPI => res.render('randomBeers', { responseFromAPI }))
+        .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
