7e7aec4cd5736bb775e865fc29bf7b3e12bfcb21
JuiceDrinker <subramanianaditya42@gmail.com>
2020-02-06 15:38:52 +0100

    bonus 1 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -10,6 +10,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
 
 // add the partials here:
 
