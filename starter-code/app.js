62adc9331566c670bdbef2e372de824b6a211fb3
Ruben Calzacorta <calzacorta.ruben@gmail.com>
2020-02-06 10:55:16 +0100

    ready

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
+
+
+
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -16,4 +20,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+    
+    punkAPI.getBeers()
+    .then(beersFromApi => {
+        console.log(beersFromApi)
+        const apiBeers = beersFromApi
+        res.render('beers', {beers : apiBeers })
+    })
+    .catch(err => console.log(`failed passing the beers to the beers endpoint with ${err}`))
+    
+});
+
+app.get('/random-beers', (req, res) => {
+
+
+    punkAPI.getRandom()
+    .then(beersFromApi => {
+        
+        console.log(beersFromApi[0])
+
+        res.render("random-beers", {randomBeer : beersFromApi[0]})
+        
+    })
+
+})
+
+
+// app.get(`/num`, (req, res) => {
+
+
+//     punkAPI.getBeer(num)
+//     .then(beersFromApi => {
+        
+//         console.log(beersFromApi)
+
+//         res.render("random-beers", {randomBeer : beersFromApi})
+        
+//     })
+
+// })
+
+
+
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'))
