08b9bc728c7b934ad83ee1504a0fdaf91480eb27
almonasterio <a.monasterio.a@gmail.com>
2020-02-06 09:20:28 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,32 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beer', (req, res) => {
+    punkAPI
+        .getBeers()
+        .then(beers => res.render('beer', {
+            beers
+        }))
+        .catch(error => console.log("error"))
+})
+
+
+app.get('/random-beers', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(beers => {
+            let beer = beers[0]
+            res.render('random-beers', {
+                beer
+            })
+
+        })
+        .catch(error => console.log("error"))
+})
+
+app.listen(4000, () => console.log('🏃‍ on port 4000'));
