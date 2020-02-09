74164f16dfac72c499a817e936ed5d17a929bead
America Zurita <americazurita@gmail.com>
2020-02-09 16:49:55 -0600

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,14 +6,32 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-
+hbs.registerPartials(__dirname + '/views/partials');
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beer', (req, res) => {
+    punkAPI
+      .getBeers()
+      .then(beersFromApi => res.render('beers', { database: beersFromApi }))
+      .catch(error => console.log(error));
+  });
+  
+  app.get('/random-beers', (req, res) => {
+    punkAPI
+      .getRandom()
+      .then(responseFromApi =>
+        res.render('random-beers', { random: responseFromApi })
+      )
+      .catch(error => console.log(error));
+  });
+  
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
