7e7681d02d5e0ae42861982ca05c788d2737bd76
unknown <ricardotomaz027@gmail.com>
2020-02-12 16:38:53 +0000

    lab solved without css

--- starter-code/app.js
+++ starter-code/app.js
@@ -10,10 +10,29 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('home'));
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(listOfAllBeers => {
+      res.render('beers', { listOfAllBeers });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(listOfAllBeers => {
+      res.render('random-beer', { listOfAllBeers });
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
