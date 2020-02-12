458d47b680274f41f6653ec30afb44f0647aa942
Manuel Rocha <manueltemidorocha@gmail.com>
2020-02-12 15:45:11 +0000

    solved till bonus

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      const data = {
+        listOfBeers: beersFromApi
+      };
+      res.render('beers', data);
+      console.log(beersFromApi);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  const randomBeer = punkAPI.getRandom();
+  randomBeer
+    .then(beer => {
+      res.render('random-beers', beer[0]);
+    })
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
