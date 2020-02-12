36b64561ddb3fffe57e732d1e3f45ffbe9bf1b2f
ynnep <adwpenalva@gmail.com>
2020-02-12 16:34:03 +0000

    im doing poorly, iterarions 1-3 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
+hbs.registerPartials(__dirname + '/views/partials');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
@@ -15,5 +15,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (request, response) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      response.render('beers', {
+        beerList: beersFromApi
+      });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (request, response) => {
+  const randomBeer = punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      const data = {
+        randomizedBeer: randomBeer
+      };
+      response.render('random-beer', data);
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
