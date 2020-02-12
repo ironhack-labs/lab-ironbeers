a05c1f6f04aa8992e2b2ca6c2ce4e02028a2c8a6
FilipeAVNunes <filipenunes407@hotmail.com>
2020-02-12 16:00:29 +0000

    all the mandatory parts are done but it's ugly

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,34 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(listOfAllBeers => {
+      console.log(listOfAllBeers);
+      res.render('beers', { listOfAllBeers });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(listOfAllBeers => {
+      console.log(listOfAllBeers);
+      res.render('random-beer', { listOfAllBeers });
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
