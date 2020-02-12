926ef30c5c5cbf05d2d7914acc2c5eeef94b242b
Pedro Meaud <meaud23@gmail.com>
2020-02-12 16:37:42 +0000

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi =>
+      res.render('beers', {
+        beers: beersFromApi
+      })
+    )
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      res.render('random-beer', {
+        randombeer: responseFromAPI
+      });
+    })
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
