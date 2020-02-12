1ed992064c84c9544c3dab5768a8f48a6584091c
André Ferreira <mrawesomeferreira@gmail.com>
2020-02-12 16:35:03 +0000

    solved but needs styles

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,45 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+  const beerList = punkAPI
+    .getBeers()
+    .then(beerList => {
+      console.log('Beers from the database: ', beerList);
+
+      res.render('beers', { beerList });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  const randomBeer = punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      console.log(randomBeer);
+      const data = randomBeer[0];
+      res.render('random-beer', data);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/:beerid', (req, res) => {
+  const beerId = req.params.beerid;
+
+  const specificBeer = punkAPI
+
+    .getBeer(beerId)
+    .then(specificBeer => {
+      console.log(specificBeer);
+      const data = specificBeer[0];
+      res.render('beers', data);
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
