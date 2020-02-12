6b139024f7a2e87d301b75d1fe60bc57c94c4c5e
alinepasetti <pasettialine@gmail.com>
2020-02-12 16:39:47 +0000

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,49 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(__dirname + '/views/partials');
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      // console.log('Beers from the database: ', beersFromApi);
+      const listOfBeers = {
+        beers: beersFromApi
+      };
+      res.render('beers', listOfBeers);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      let data = {
+        randomBeer: responseFromAPI[0]
+      };
+      console.log(data);
+      res.render('random-beers', data);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+  const id = req.params.id;
+  punkAPI
+    .then(beerInfo => {
+      const beer = punkAPI.getBeer(id);
+      console.log(punkAPI.getBeer(4));
+      const chosenBeer = {
+        beer: beer
+      };
+      res.render('single', chosenBeer);
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
