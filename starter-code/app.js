c44a98dcb3a735e2b28b6e8a7405a9c1811af4ec
Sofia Franek <sofiafranek@icloud.com>
2020-02-12 15:31:28 +0000

    finsihed bonus

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (request, response) => {
+  response.render('index');
+});
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (request, response) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      // console.log('Beers from the database: ', beersFromApi);
+      response.render('beers.hbs', { beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      // console.log('Beers from the database: ', beersFromApi);
+      res.render('random-beer.hbs', { beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.listen(3000);
