bb4d49e1e687d871a9c16fbf9a8a36409d5c6daf
David Felix <davidfelix1997@hotmail.com>
2020-02-12 16:34:50 +0000

    #

--- starter-code/app.js
+++ starter-code/app.js
@@ -7,7 +7,9 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,5 +17,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (request, response) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log(beersFromApi);
+      response.render('beers', { beersFromApi });
+    })
+    .catch(error => {
+      // If it fails, the function passed to .catch runs
+      // ...
+      console.log('failure');
+      console.log(error);
+    });
+});
+
+app.get('/random-beers', (request, response) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      console.log(beersFromApi);
+      response.render('randomBeer', { responseFromAPI });
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
