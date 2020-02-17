ad3cd35ca5b42be51210194dc50f865e12d1dff2
Pablolo <pablo_olondriz@hotmail.com>
2020-02-17 20:04:45 +0100

    added views

--- starter-code/app.js
+++ starter-code/app.js
@@ -15,5 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => res.render('beers'));
+app.get('/random-beer', (req, res) => res.render('random-beer'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
