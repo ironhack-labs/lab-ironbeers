e4efc60cbf149ec933d548f5df4c77ef0b1bce63
Michaela DeCamara <michaela.decamara@gmail.com>
2020-02-18 12:42:29 -0500

    views added

--- starter_code/app.js
+++ starter_code/app.js
@@ -14,7 +14,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => {
-    console.log('listening on 3000');
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then((response) => {
+    res.render('beers.hbs', {
+      beers: response
+    })
+    // console.log(respose.data);
   });
-  
+  // .then(responseFromApi =>
+  //   //console.log('Beers from database', responseFromApi))
+  //   res.render('beers.hbs', {beers: responseFromApi})
+  // .catch(error => console.log(error))
+});
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom().then(response => {
+    console.log(response)
+    res.render('random-beer.hbs', {
+      beer: response[0]
+    })
+  })
+
+})
+
+app.listen(3000, () => {
+  console.log('listening on 3000');
+});
