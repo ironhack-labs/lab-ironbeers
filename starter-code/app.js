df647a78dc0d402aaded4201bc97a7185445744e
Marc Serra <mserrahidalgo@gmail.com>
2020-02-18 23:02:04 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,37 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beerFromApi => {
+      res.render('random-beer', { beers: beerFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/beer-:beer_id', (req, res) => {
+  const { beer_id } = req.params;
+  punkAPI
+    .getBeer(beer_id)
+    .then(beerFromApi => {
+      res.render('beers', { beers: beerFromApi });
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
