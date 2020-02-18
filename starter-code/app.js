bfd12e564dad6041b87ff8bacfade60bb1709452
Pablolo <pablo_olondriz@hotmail.com>
2020-02-18 20:59:33 +0100

    mostrando birras

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,13 +6,14 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(path.join(__dirname, "/views/partials"));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-hbs.registerPartials(__dirname + "/views/partials");
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
@@ -20,9 +21,11 @@ app.get('/', (req, res) => res.render('index'));
 app.get('/beers', (req, res) => {
   punkAPI
   .getBeers()
-  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', { beersFromApi })
+  })
   .catch(error => console.log(error));
-  res.render('beers');
 });
 
 app.get('/random-beer', (req, res) => res.render('random-beer'));
