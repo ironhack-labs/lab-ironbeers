d0b1738677cfd48ddbe2d74a4679104fd0f7559a
Javier Varela <jvarela@strands.com>
2020-02-18 21:31:11 +0100

    iteration 1 nav

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,9 +11,20 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const texts = {
+    headTitle: "Beers",
+    nav: ["home", "beers", "random-beers"]
+}
+
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+
+app.get("/", (req, res) => res.render("index", texts));
+app.get("/beers", (req, res) => res.render("beers", texts));
+app.get("/random-beers", (req, res) => res.render("random-beers", texts));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+
