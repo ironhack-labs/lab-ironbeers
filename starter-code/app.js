85a239372b42e39d66a89be750514bd4b7c741b0
Carles Galí Bou <carles_ga@hotmail.com>
2020-02-19 10:49:07 +0100

    Iterations 1 and 2 done, CSS also done

--- starter-code/app.js
+++ starter-code/app.js
@@ -15,5 +15,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => res.render('beers'))
+app.get('/random-beers', (req, res) => res.render('random-beers'))
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+
+
+
+
+
+
+
+// app.get('/allbers', (req, res) => {
+//     punkAPI
+//         .getBeers()
+//         .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+//         .catch(error => console.log(error));
+// })
+
+
+// app.get('/allbers', (req, res) => {
+//     punkAPI
+//         .getBeers();
+//         // todo ok, para el then
+//         .then(beersFromApi => res.render('vista', { beersFromApi }))
+//         // algo va MediaList, para el catch
+//         .catch(error => console.log(error));
+// })
