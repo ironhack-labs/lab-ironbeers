b41de73784158800ea6b143dfa81e4b4d4884cad
Michael Mueller <michael@miniguide.co>
2020-02-20 15:53:30 +0100

    Iteration 1 done

---
+++ starter-code/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render("index"));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
