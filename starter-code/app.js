1f5c598e04240e7779c09f589f0f89ade7e13344
alexfc96 <alex@LAPTOP-ELMBT3E0.localdomain>
2020-02-20 20:45:01 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+hbs.registerPartials(__dirname + "/views/partials");
+
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      //const beers = punkAPI.getBeers({ 'abv_gt': 25 })
+      res.render('beers/beers', { beersFromApi })
+    })
+    .catch(error => console.log(error));
+})
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('beers/randombeer', { responseFromAPI })
+    })
+    .catch(error => console.log(error));
+})
+app.get('/beers/beer-:id', (req, res) => {
+  const { id } = req.params;
+  punkAPI.getBeer(id)
+    .then(beer => {
+      res.render('beers/beer', { beer })
+    })
+    .catch(error => console.log(error));
+})
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(2000, () => console.log('start on port 2000'));
