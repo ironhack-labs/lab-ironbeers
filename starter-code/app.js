8bbe0df100a18a568f9b2d3c0878e75a04a36995
Thibault-d <thibaultdelfaud@gmail.com>
2020-02-21 23:14:38 +0100

    all functions are done, just the design is shit as usual!

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,15 +8,36 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
-app.get('/beer', (req, res) => res.render('beer'));
-app.get('/random', (req, res) => res.render('random'));
+app.get('/', (req, res, next) => res.render('index'));
+
+app.get('/beers', (req, res, next) => {
+    punkAPI
+        .getBeers()
+        .then(beersFromApi => {
+            res.render('beers', {
+                beers: beersFromApi
+            });
+        })
+        .catch(error => console.log(error));
+});
+
+app.get('/random', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(beerFromApi  => {
+            res.render('random', {beers: beerFromApi });
+            console.log(beerFromApi );
+        })
+        .catch(error => console.log(error));
+});
 
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
