4312f116b30db6c28a53b5bfb093019d73c1631f
Gerard Sole <gerardsole@MacBook-Pro-de-Gerard-2.local>
2020-02-21 14:16:28 +0100

    iteration 5 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-
+hbs.registerPartials(path.join(__dirname, "/views/partials"));
 // add the routes here:
 app.get('/', (req, res) => res.render(`${__dirname}/views/index`))
 
