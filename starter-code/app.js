315e92581f2ee9f0840194acdf27ceaf7d298558
alejandro <alejimenezgitgithub@gmail.com>
2020-02-21 01:41:39 +0100

    missing the random-beers

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,26 +2,33 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// add the partials here:
-
-
-// add the routes here:
 app.get('/', (req, res) => { res.render('index') });
+
 app.get('/beers', (req, res) => {
     punkAPI
     .getBeers()
-    .then(beersFromApi => res.render('beers')) // console.log(beersFromApi)
+    .then(beersFromApi => res.render('beers', {beersFromApi})) // console.log(beersFromApi)
     .catch(error => console.log(error));
 });
-app.get('/random-beers', (req, res) => res.render('random-beers'));
+
+app.get('/random-beers', (req, res) => {
+    punkAPI
+      .getRandom()
+      .then(responseFromAPI => {
+        res.render('random-beers', {
+          randombeers: responseFromAPI
+        });
+      })
+      .catch(error => console.log(error));
+  });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
