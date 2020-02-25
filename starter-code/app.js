bcbbde53eaefac534640ee0cbfce88a65155fd53
America Zurita <americazurita@gmail.com>
2020-02-24 21:17:13 -0600

    second iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,8 +6,6 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-
-
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -18,20 +16,19 @@ hbs.registerPartials(__dirname + '/views/partials');
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 app.get('/beer', (req, res) => {
-    punkAPI
-      .getBeers()
-      .then(beersFromApi => res.render('beers', { database: beersFromApi }))
-      .catch(error => console.log(error));
-  });
-  
-  app.get('/random-beers', (req, res) => {
-    punkAPI
-      .getRandom()
-      .then(responseFromApi =>
-        res.render('random-beers', { random: responseFromApi })
-      )
-      .catch(error => console.log(error));
-  });
-  
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { database: beersFromApi }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi =>
+      res.render('random-beer', { random: responseFromApi })
+    )
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
