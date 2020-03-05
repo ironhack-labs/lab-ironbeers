542f2efccb2d6e7789a2f9fdda2a21bc951424e1
Jonas Barre <jonasbarre@jonass-mbp.lan>
2020-03-05 16:57:53 +0100

    cheers

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beersFromApi => {
+            console.log('Beers from the database: ', beersFromApi)
+            // transform beersFromApi to an object
+            res.render('beers', { beers: beersFromApi })
+        })
+        .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+    punkAPI.getRandom()
+        .then(beerFromApi => {
+            console.log('Beers from the database: ', beerFromApi)
+            // transform beersFromApi to an object
+            res.render('random-beers', { beer: beerFromApi })
+        })
+        .catch(error => console.log(error));
+});
+
+// app.get('/random-beers', (req, res) => res.render('random-beers'));
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
