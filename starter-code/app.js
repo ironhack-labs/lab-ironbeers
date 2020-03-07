cf676249a32114b7c9908b1aa335a01a72afeca0
Boyuan Shi <sbyshjd@hotmail.com>
2020-03-07 14:27:34 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-
+hbs.registerPartials(path.join(__dirname,'views/partials'));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+        punkAPI
+        .getBeers()
+        .then(beersFromApi => {
+            res.render('beers',{beersFromApi});
+        })
+        .catch(error => console.log(error));
+    
+
+    });
+app.get('/beers/:beerID',(req,res) => {
+    console.dir(req.params.beerID);
+    punkAPI
+        .getBeer(req.params.beerID)
+        .then(selectedBeer => {
+            res.render('random-beers',selectedBeer[0]);
+        })
+        .catch(error => console.log(error));
+    });
+app.get('/random-beers', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(randomBeer => {
+            res.render('random-beers',randomBeer[0]);
+        })
+        .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
