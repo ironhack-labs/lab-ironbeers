15928ae6c92bc9c1fc57fe480800f88c766c622f
miranda <miranda@LAPTOP-R0JVELH6.localdomain>
2020-03-10 15:57:04 +0100

    t

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const hbs = require('hbs');
+const app = express();
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
-const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get("/", (req, res) => {
+    res.render('index')
+});
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get("/beers", (req, res) => {
+    punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+        console.log('Beers from the Database: ', beersFromApi);
+        res.render("beers", {theBeers: beersFromApi});
+    })
+    .catch(error => console.log(error));
+});
+
+
+app.listen(3001, () => { 
+    console.log('🏃‍ on port 3001')
+});
