ac11d45cfc292d1811d0dc5849b78a6b2cfa8636
Alex <alexgarcia1771@outlook.com>
2020-03-11 18:59:59 +0100

    prueba

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,9 +11,34 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// add the partials here:
 
+
+// add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res,next) => res.render('index'));
+app.get('/beers', (req, res,next) => {
+    //llamada
+    punkAPI.getBeers()
+    //que hago con la llamada
+    .then(beersFromApi =>{ 
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', {allbeers: beersFromApi})
+})
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res, next) => {
+    punkAPI.getRandom()
+    .then(responseFromApi =>{
+        console.log(responseFromApi[0])
+        const theBeer = responseFromApi[0];
+        res.render('random', theBeer)
+    })
+    .catch(error => console.log(error));
+
+})
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
