2da30d77bf1ca4f9d0636f2f733966cf49146fb1
Sofia Santa Maria <yolaresofia@gmail.com>
2020-03-11 23:16:13 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    const beersFromApi = punkAPI.getBeers()
+    .then(beersFromApi => {
+        console.log('Beers from the database: ', beersFromApi);
+        res.render('beers', {
+            beersFromApi
+        })
+    })
+    .catch(error => console.log(error))
+})
+
+
+app.get('/random-beer', (req, res) => {
+    const responseFromApi = punkAPI.getRandom()
+  .then(responseFromApi => {
+      console.log('Beers from the database: ', responseFromApi);
+      res.render('random-beer', {
+          responseFromApi
+      })
+  })
+  .catch(error => console.log(error));
+})
+
+app.get('/detailed/:tagId', (req, res) => {
+    punkAPI.getBeer(req.params.tagId)
+    .then(detailedFromApi => {
+        const selectedBeer = detailedFromApi[0];
+      console.log('Beers from the database: ', detailedFromApi);
+      res.render('detailed', selectedBeer)
+  })
+  .catch(error => console.log(error));
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
