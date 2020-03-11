0f268b833a09b6b477899fd1f40fcb3139de96b0
Pau Larrea <paularrea12@hotmail.com>
2020-03-11 18:04:12 +0100

    beers

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,45 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+   
+    punkAPI.getBeers()
+
+    .then(beersFromApi => res.render('beers', {beersFromApi}))
+    
+    .catch(error => console.log(error));
+
+});
+
+app.get('/random-beers', (req, res, next) => {
+    
+    punkAPI.getRandom()
+  .then(responseFromAPI => res.render('random-beers', responseFromAPI[0]))
+  
+  .catch(error => console.log(error));
+    
+});
+
+app.get('/beers/:id', (req, res, next) => {
+    
+    punkAPI.getBeer(req.params.id)
+  .then(responseFromAPI => res.render('random-beers', responseFromAPI[0]))
+  
+  .catch(error => console.log(error));
+    
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
