905f24dd39a91a0c998f25c4c3c50d31819add1f
ingridpitta <ingridpitta056@gmail.com>
2020-03-10 22:30:37 -0300

    LAB | IRONBEERS [WIP]

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      const randomBeer = responseFromAPI[0];
+      res.render('random-beers', randomBeer);
+    })
+    .catch(error => console.log(error));
+});
+app.get('/beers/beer', (req, res) => {
+  const { id } = req.query;
+  punkAPI.getBeer(id).then(beersFromApi => {
+    res.render('beers', { id: id, beers: beersFromApi });
+  });
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
