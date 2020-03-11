ebd40fce75df6cb75571d85547e2b798a976b3cb
Víctor Ronda <victor.ronda@gmail.com>
2020-03-11 18:35:04 +0100

    done but last iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,41 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res, next) => res.render('index'));
+
+
+app.get('/beers', (req, res, next) => {
+	punkAPI
+		.getBeers()
+		.then((beersFromApi) => {
+			console.log(beersFromApi);
+			res.render('beers', { beersFromApi });
+		})
+		.catch((error) => console.log(error));
+});
+
+app.get('/beers/:id', (req, res, next) => {
+    punkAPI.getBeer(req.params.id)	
+		.then((beersFromApi) => {
+            res.render('beer', { beersFromApi });
+		})
+		.catch((error) => console.log(error));
+});
+
+
+
+app.get('/random-beers', (req, res, next) => {
+	punkAPI
+		.getRandom()
+		.then((responseFromAPI) => {
+            // your magic happens here
+            console.log(responseFromAPI);
+            res.render('random-beers', { responseFromAPI });
+		})
+		.catch((error) => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
