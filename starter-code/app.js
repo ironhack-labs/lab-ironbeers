e6081719cad8edeccd62df61be766f2df111040f
Raul Marchan <alountk@gmail.com>
2020-03-11 18:04:28 +0100

    [BCN-WDFT-RAUL]-done...casi

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,13 +5,37 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+const randomBeer = punkAPI.getRandom()
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+app.get('/',(req,res,next)=>{
+    res.render('index');
+});
+app.get('/beers',(req,res,next)=>{
+    punkAPI.getBeers()
+  .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers',{beersFromApi});
+    })
+  
+  .catch(error => console.log(error));
+});
+app.get('/randombeers',(req,res,next)=>{
+    punkAPI.getRandom()
+  .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('randombeers',{beersFromApi});
+    })
+  
+  .catch(error => console.log(error));
+});
+
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
