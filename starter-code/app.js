ee77fe7f21747b753483a8242749754e387468a5
Elena Perrone <elenperrone11@gmail.com>
2020-03-11 15:26:59 +0100

    nav done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(__dirname + '/views/partials')
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get("/", (req, res) => res.render("index"));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get("/beers", (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log("Beers from the database: ", beersFromApi);
+      res.render("beers", { beers: beersFromApi});
+    })
+    .catch(error => console.log(error));
+});
+
+
+app.listen(3000, () => console.log("🏃‍ on port 3000"));x`
