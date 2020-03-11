e14a9a044ca12c252aedb34c9ab851b258797690
Paulo Salles <plsalles@gmail.com>
2020-03-11 13:07:56 -0300

    All Lab items done - pending bonus

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) => {
+
+punkAPI
+    .getBeers()
+    .then( beersFromApi => res.render('beers', {beersFromApi}))
+    .catch( error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+
+    punkAPI
+        .getRandom()
+        .then(responseFromAPI => res.render('random-beer', {responseFromAPI}))
+        .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
