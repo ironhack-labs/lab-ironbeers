58939efcfc5059d431003406a31c002ed39bc625
Paola <paolavee@gmail.com>
2020-03-12 00:02:48 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+
+punkAPI.getBeers()
+.then(allBeers => {
+const someBeers = allBeers.slice(0,25)
+//console.log(someBeers)
+    res.render('beers',  { someBeers });
+})
+.catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+    punkAPI.getRandom()
+    .then(randomBeer => {
+        const beer = randomBeer;
+        res.render('random-beers', beer[0]);
+    })
+    .catch(error => console.log(error));
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
