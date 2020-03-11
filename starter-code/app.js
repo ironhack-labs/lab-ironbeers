259df9f7d19739d0da7c86be5897d4dd28dede43
Zetzher <fyeah16@gmail.com>
2020-03-11 17:10:08 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,14 +6,35 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-
+hbs.registerPartials(__dirname + '/views/partials');
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get("/beers", (req, res) => {
+    punkAPI.getBeers()
+        .then(beersFromApi => {
+            console.log('Beers from the database: ', beersFromApi)
+            res.render("beers", {beersFromApi})
+        })
+        .catch(error => console.log(error));
+
+});
+app.get("/random", (req, res) => {
+    punkAPI.getRandom()
+        .then(responseFromAPI => {
+            let randomBeer = responseFromAPI[0];
+            res.render("randomBeers", {randomBeer})
+        })
+        .catch(error => console.log(error));
+    
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
