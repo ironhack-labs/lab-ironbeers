6e250f1c4df10ae9f1ee47a0b4ecf7bc7822c71e
Diablobait <thor.stg@gmail.com>
2020-03-12 09:17:05 +0100

    final

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,6 +3,8 @@ const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+
+
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
@@ -15,5 +17,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/home', (req, res) => res.render('index'));
+
+
+
+app.get('/beers', (req, res) => {
+    punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+        res.render('beers',{beersFromApi});
+    })
+    .catch(error => console.log(error));
+});
+
+
+app.get('/random-beer', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(randomBeer => {
+
+            res.render('random-beer',randomBeer[0]);
+        })
+        .catch(error => console.log(error));
+});
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
