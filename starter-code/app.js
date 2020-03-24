f18cbf3cbb7ced7a353fc5b88cf4f02c84157378
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-03-24 15:26:55 -0300

    iteration 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,8 +6,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,5 +15,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+    .then(beers => {
+        res.render('beers', {beers})
+        console.log({beers})
+    })
+    .catch(error => console.log(error))
+})
+
+// app.get('/beers', (req, res) => res.render('beers'));
+
+app.get('/random-beers', (req, res) => res.render('random-beers'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
