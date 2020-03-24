01a1df792aca2c48ddd61755462b9fa29acc1036
brunobsb <tavares.bruno@gmail.com>
2020-03-24 19:45:23 -0300

    Seis interações feitas

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-
+hbs.registerPartials(__dirname + '/views/partials');
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+punkAPI.getBeers().then(beers => {
+    res.render('beers',{beers});
+}).catch(error => console.log(error)); 
+    
+});
+app.get('/random-beers', (req, res) => {
+    punkAPI.getRandom().then(beers => {
+        res.render('randomBeers',{beers});
+    }).catch(error => console.log(error)); 
+        
+    });
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
