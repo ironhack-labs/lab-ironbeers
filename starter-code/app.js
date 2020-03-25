6390bf17066036a0e38db072dcf958eb76242922
Gabriel Bohn <gabrielcbohn@gmail.com>
2020-03-25 11:51:40 -0300

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,20 +13,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 hbs.registerPartials(__dirname + '/views/partials');
+
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
-app.get('/beers', (req, res) => {
-punkAPI.getBeers().then(beers => {
-    res.render('beers',{beers});
-}).catch(error => console.log(error)); 
-    
-});
-app.get('/random-beers', (req, res) => {
-    punkAPI.getRandom().then(beers => {
-        res.render('randomBeers',{beers});
-    }).catch(error => console.log(error)); 
-        
-    });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+    punkAPI
+        .getBeer(req.params.id)
+        .then(beers => res.render('randomBeers', { beers }))
+        .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beers => {
+      res.render('randomBeers', { beers });
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
