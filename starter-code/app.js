e51a28bb76e876d5cdb6cea3859d396d502acace
“Marcela <marcelavieirafreire@gmail.com>
2020-03-24 21:58:03 -0300

    half done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+    .then(beers => {
+        res.render('beers', {beers})
+        console.log({beers})
+    })
+    .catch(error => console.log(error));
+})
+
+app.get('/random-beers', (req, res) => {
+    punkAPI.getRandom()
+    .then(randomBeers => {
+        res.render('random-beers', {randomBeers})
+        console.log({randomBeers})
+    })
+    .catch(error => console.log(error));
+})
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
