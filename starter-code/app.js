6aa6c1912b9f4c481fb1686f4b0c27d48b05e0bb
Francesco Saccone <francescosaccone95@live.it>
2020-04-02 21:21:47 +0200

    Francesco WEBDEVPARTTIMEBERLIN IronbeersLAB iteration 0 and 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+
 // add the routes here:
+
+//To the route /
 app.get('/', (req, res) => res.render('index'));
+
+//To the page beers
+app.get('/beers', (req, res) => res.render('beers'));
+
+//To the page random-beer
+app.get('/random-beer', (req, res) => res.render('random-beer'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
