e89619f9f70dafeb9704fbca26767a1dc0a0be07
PiaG <piagmdf@gmail.com>
2020-04-02 21:27:27 +0200

    first partial working, but not yet understanding, how

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers().then( beersFromApi => {
+           res.render('beers', {data: beersFromApi});
+           }).catch(error => console.log(error));
+ });
+
+app.get('/random-beers', (req, res) => res.render('random-beers'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
