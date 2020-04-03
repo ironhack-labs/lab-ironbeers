8d068b08f69c7fa41943ef1af14e65a902c41ce4
Manuel <mbuergel@gmx.de>
2020-04-03 18:11:17 +0200

    except bonus

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) => {
+
+    punkAPI
+        .getBeers()
+        .then(beersFromApi => {
+            res.render('beers', { beersArray : beersFromApi })
+        })
+    })
+
+
+app.get('/random-beers', (req, res) => {
+    
+    punkAPI
+        .getRandom()
+        .then(randomBeerFromApi => {
+            res.render('random-beers', {oneRandomBeer : randomBeerFromApi})
+            console.log('Random brewskie from the database: ', randomBeerFromApi)
+        })
+
+
+    })
+
+
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
