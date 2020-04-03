0d07731c2eae5b44a2613c06704e7c67f1e0f122
Damaris Goebel <damaris.goebel@gmx.de>
2020-04-03 19:47:13 +0200

    first-commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -15,5 +15,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { beersArr: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+app.get('/random-beers', (req, res) => res.render('random-beers'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
