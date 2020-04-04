d383c7a596719179b5f9becba7b7188010a28475
Noubar Topalian <topalian.noubar@gmail.com>
2020-04-04 10:04:57 +0200

    BERLIN-WDPT-Noubar

--- starter-code/app.js
+++ starter-code/app.js
@@ -15,5 +15,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+    punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', { beersArray: beersFromApi })
+    })
+
+    
+});
+app.get('/random-beers', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(responseFromAPI => {
+            // your magic happens here
+            console.log('Random beer',responseFromAPI);
+            res.render('random-beers', { beersArray: responseFromAPI })
+        })
+        .catch(error => console.log(error));
+
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
