5d624956f714548c63cbb37c21ac461317aa1a27
Sonja Lesch <sonjalesch@gmx.de>
2020-04-04 11:22:06 +0200

    beers not showing: it says it does not find my allMyBeersArray

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+
+app.get('/', (req, res) => {
+  res.render('HOME')
+})
+
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers().then((beers) => {
+    //console.log(beer[0])   //Test
+    res.render('beers', { allMyBeersArray: beers })
+  })
+
+/*  //general code 
+    punkAPI
+    .getBeers()
+    .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+    .catch(error => console.log(error)); */
+
+  res.render('BEERS')
+})
+
+app.get('/random-beer', (req, res) => {
+  res.render('RANDOM-BEER')
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
