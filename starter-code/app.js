2e2563f198fd3d1a0ac3f0781438839ef32ed6bc
vitor lopes <lopesvitor959@gmail.com>
2020-04-11 20:31:41 +0100

    LIS-PTWD-FEB2020-Vitor

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+hbs.registerPartials(__dirname + '/views/partials') 
+
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+   const allBeers = punkAPI
+  .getBeers()
+  .then(allBeers => {
+  console.log('Beers from the database: ', allBeers)
+  res.render('beers', { allBeers });
+  })
+  .catch(error => console.log(error));   
+})
+
+app.get('/random-beers', (req, res) => {
+    const randBeer = punkAPI
+    .getRandom()
+    .then(randBeer => {
+    // your magic happens here
+    console.log('A random beer', randBeer)
+    res.render('random-beers', { randBeer });
+     })
+  .catch(error => console.log(error));  
+})
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
