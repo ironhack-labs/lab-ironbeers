977f2b9a46c705f00d711af970b39984f9d5a379
Armando del Rio <d-rio@outlook.com>
2020-04-15 17:54:23 -0500

    almost done

--- starter-code/app.js
+++ starter-code/app.js
@@ -7,13 +7,27 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
 const punkAPI = new PunkAPIWrapper();
 
 // add the partials here:
-
+hbs.registerPartials(`${__dirname}/views/partials`);
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
-app.get('/beers', (req, res) => res.render('beers'));
-app.get('/randombeers', (req, res) => res.render('randombeers'));
 
+//home
+app.get('/', (req, res) => res.render('index'));
+// Beers
+app.get('/beers', async (req, res) => {
+   const beers = await punkAPI.getBeers();
+   beers.pop()
+  res.render('beers', { beers});
+});
+// Random beers
+app.get('/randombeers', async (req, res) => {
+  const beers = await punkAPI.getBeers();
+  beer = beers[Math.floor(Math.random() * beers.length)];
+  res.render('randombeers',  beer );
+  console.log(beer);
+});
+// initialize server
 app.listen(3000, () => console.log('🏃‍ on http://localhost:3000'));
