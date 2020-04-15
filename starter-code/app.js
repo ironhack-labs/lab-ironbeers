b23443de13da15c15fda2adbfd2cc907984aad4e
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-04-15 22:58:36 +0200

    almost done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,24 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
-
+app.get('/beers', (req, res, next) => {
+    punkAPI
+        .getBeers()
+        .then(beersFromApi => res.render('beers', {beersFromApi}))
+        .catch(error => console.log(error));
+    })
+app.get('/random-beer', (req, res, next) => {
+    punkAPI
+        .getRandom()
+        .then(responseFromApi => res.render('randomBeer', responseFromApi[Math.floor(Math.random()*responseFromApi.length)]))
+        .catch(error => console.log(error));
+})
+ 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
