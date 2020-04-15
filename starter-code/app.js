c14e4ddf2952c2103267af39d825de233f281bbd
Julian Kizeno <julianquiceno20@gmail.com>
2020-04-16 01:38:09 +0200

    done without bonus

--- starter-code/app.js
+++ starter-code/app.js
@@ -15,5 +15,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    //.then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+    .then((beersFromApi) => res.render('beers', {beersFromApi} ))
+    .catch((error) => console.log(error));
+});
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then((responseFromAPI) => res.render('random-beers', {responseFromAPI}))
+    .catch((error) => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
