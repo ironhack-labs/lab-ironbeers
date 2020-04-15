8cbd83c8f5b36c2327daeab0d9d0e0207d843fd2
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2020-04-15 21:41:04 +0200

    add files work done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,25 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
 
-// add the partials here:
-
-// add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beersFromApi => res.render('beers', {beersFromApi}))
+        .catch(err => console.log(err));
+})
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/random-beer', (req, res) => {
+    punkAPI.getRandom()
+        .then(responseFromApi => res.render('random-beer', {responseFromApi}))
+        .catch(err => console.log(err))
+})
+
+app.get('/beers/:id', (req, res) => {
+    punkAPI.getBeer(req.params.id)
+        .then(responseFromApi => res.render('random-beer', {responseFromApi}))        
+        .catch(err => console.log(err))
+})
+app.listen(3000, () => console.log('Listening on port 3000'));
