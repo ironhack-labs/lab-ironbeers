8e90580ef7073f367afe0ccc14a5dcaf081b42f6
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-15 18:43:34 +0200

    set endpoints

--- starter-code/app.js
+++ starter-code/app.js
@@ -15,5 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => res.render('beers'));
+app.get('/random-beers', (req, res) => res.render('random-beers'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
