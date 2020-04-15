48d61b7cdb3752d86ef6f757ab640fb36af4fdfb
togeri <gerartoledo@gmail.com>
2020-04-15 22:05:00 +0200

    Unfinished Exercise

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,9 +11,40 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// add the partials here:
+// Partials
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
 
-// add the routes here:
+// Enrouting
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) => {
+    punkAPI
+        .getBeers()
+        .then(beersFromAPI => {
+            console.log("Beers from the database: ", beersFromAPI)
+            return res.render('beers', { beers: beersFromAPI })
+        })
+        .catch(error => console.log(error))
+})
+
+app.get('/random-beers', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(responseFromAPI => {
+            let randomBeer = responseFromAPI[0]
+            // randomBeer.layout = false
+            return res.render('randomBeers', randomBeer)
+        })
+    .catch(error => console.log(error))
+    
+})
+
+app.get('/beers/:id', (req, res) => {
+    punkAPI
+        .getBeer(req.params.id)
+        .then(randomBeer => res.render('randomBeers', {randomBeer}))
+    .catch(error => console.log(error))
+})
+
+// Launching Server
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
