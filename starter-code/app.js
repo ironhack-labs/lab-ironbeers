fb8200d98ad320172be761e64fafb883424e1f36
Julian Bajo <jbajopinedo@gmail.com>
2020-04-16 01:44:31 +0200

    Ejercicio resuelto a falta de poneer mas estilosCSS

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials')) 
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beer', (req, res) => {
+
+    punkAPI.getBeers()
+    .then(beersFromApi => res.render('beer', {beersFromApi}))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+    
+    punkAPI.getRandom()
+    .then(responseFromAPI => res.render('randomBeer', {responseFromAPI}))
+    .catch(error => console.log(error));
+});
+
+app.get('/beer/:id', (req, res) => {
+
+    punkAPI.getBeer(req.params.id)
+    .then(responseFromAPI => res.render('randomBeer', {responseFromAPI}))
+    .catch(error => console.log(error));
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
