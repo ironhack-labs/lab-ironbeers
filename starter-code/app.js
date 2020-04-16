2db83755ea522b7d1ff066de73ab1c7829ffc32b
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-04-16 14:54:29 +0200

    works, no partials

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(__dirname + '/views/partials');
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) =>
+  punkAPI
+    .getBeers()
+    .then((beersFromApi) => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch((error) => console.log(error))
+);
+
+app.get('/random-beer', (req, res) =>
+  punkAPI
+    .getRandom()
+    .then((randomBeer) => {
+      console.log('Beer from the database: ', randomBeer[0]);
+      res.render('random-beer', { beer: randomBeer[0] });
+    })
+    .catch((error) => console.log(error))
+);
+
+app.listen(3000);
