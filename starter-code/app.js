08b3affb4f2d9435a2aeaf3d203c31c975183f3a
Yosra Yousfi <yousfi.yosra@gmail.com>
2020-04-16 15:12:30 +0200

    lab-ironbeers

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('views', __dirname + '/views');
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then((respFromApi) => {
+      res.render('beers', { beers: respFromApi });
+    })
+    .catch((error) => console.log(error));
+});
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then((responseFromAPI) => {
+      res.render('random-beers', { randomBeer: responseFromAPI });
+    })
+    .catch((error) => console.log(error));
+});
+app.get('/:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then((responseFromAPI) => {
+      res.render('random-beers', { randomBeer: responseFromAPI });
+    })
+    .catch((error) => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
