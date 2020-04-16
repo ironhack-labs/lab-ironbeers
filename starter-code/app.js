3ee0e2917539de9e5a214a3ebfa8cc32d369fa21
Phillip Paul <phillippaul@gmx.de>
2020-04-16 15:12:34 +0200

    Done w/o Iteration 7: Styling

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,33 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then((beersFromApi) => res.render('beers', { beersFromApi }))
+    .catch((error) => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then((singleBeerFromApi) =>
+      res.render('single-beer', { singleBeerFromApi })
+    )
+    .catch((error) => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then((randomBeerFromApi) =>
+      res.render('random-beers', { randomBeerFromApi })
+    )
+    .catch((error) => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
