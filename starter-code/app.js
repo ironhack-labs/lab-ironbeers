49338c5e40b30d3aff95c4804cc5a9a8179844b8
psch0rr95 <sebastiannagl@icloud.com>
2020-04-16 14:26:39 +0200

    donezo

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,36 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then((beersFromAPI) => {
+      res.render('beers', { beers: beersFromAPI });
+    })
+    .catch((error) => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then((beersFromAPI) => {
+      res.render('random-beers', { beers: beersFromAPI });
+    })
+    .catch((error) => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then((beersFromAPI) => {
+      res.render('random-beers', { beers: beersFromAPI });
+    })
+    .catch((error) => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
