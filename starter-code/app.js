25c1d1b96bbd191c3367ea3cace431868dc7e209
HeiseMo <tim.james.tanurhan@gmail.com>
2020-04-16 17:55:53 +0200

    fill page with beer ID info done, some styling done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-hbs.registerPartials(__dirname + '/views/partials')
+// hbs.registerPartials(__dirname + '/views/partials')
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
@@ -42,14 +42,15 @@ app.get('/random-beer', function (req, res) {
 })
 
 
-app.get('/beer', (req, res) => {
-    let id = req.query.id;
-
+app.get('/beer/:id', (req, res) => {
     punkAPI
-        .getBeer(id)
-        .then(singleBeer => res.render('random-beer', {
-            singleBeer
-        }))
-        .catch(error => console.log(error));
+        .getBeer(req.params.id)
+        .then((beer) => {
+            res.render('random-beer', {
+                beer
+            });
+        })
+        .catch((error) => console.log(error));
 });
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
