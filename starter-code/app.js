4f341a6f9ec31891dc28af1f3427ba8c3d99f59b
Benjamin Chimoy <benjaminchimoy@benjamins-mbp.localdomain>
2020-04-19 10:57:30 +0200

    bonus coming up

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,34 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+
+app.get('/', (req, res, next) => res.render('index'));
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then((beersFromApi) => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { cerveza: beersFromApi });
+    })
+    .catch((error) => console.log(error));
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then((beersFromApi) => {
+      console.log('Random Beer from the database: ', beersFromApi);
+      res.render('random-beers', { cervezaR: beersFromApi });
+    })
+    .catch((error) => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
