1fc2eb2301c204079f564b950dacc08d774dcd50
Maxime Paolone <paolonemax@gmail.com>
2020-04-20 15:47:06 +0200

    Iteration 4

--- starter-code/app.js
+++ starter-code/app.js
@@ -15,5 +15,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
-
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then((beersFromApi) => {
+      res.render('beers', {
+        theBeers: beersFromApi,
+      });
+    })
+    .catch((error) => console.log(error));
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then((beersFromApi) => {
+      res.render('random-beer', {
+        theBeers: beersFromApi,
+      });
+    })
+    .catch((error) => console.log(error));
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
