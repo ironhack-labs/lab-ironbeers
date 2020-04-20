8accf2b50f16fb70b0befdce3a959bd9fe0bf7ee
vlp90 <vlp90@DESKTOP-VUTPP2C.localdomain>
2020-04-20 16:33:59 +0200

    exercice done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,41 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'Home'
+	});
+});
+
+app.get('/beers', (req, res) => {
+	punkAPI
+		.getBeers()
+		.then((beersFromApi) => {
+			// console.log('Beers from the database: ', beersFromApi);
+			res.render('beers', {
+				title: 'Beers',
+				beers: beersFromApi
+			});
+		})
+		.catch((error) => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+	punkAPI
+		.getRandom()
+		.then((randomBeer) => {
+			console.log('Beers from the database: ', randomBeer[0]);
+			res.render('random-beer', {
+				title: 'Random beer',
+				beer: randomBeer[0]
+			});
+		})
+		.catch((error) => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => res.render('random-beer'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
