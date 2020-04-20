d7075e7234e4ae5c1f32d571a652f40fac2eba33
Florian Jomain <florian.jomain@gmail.com>
2020-04-20 15:46:21 +0200

    Committing lab

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    punkAPI
+        .getBeers()
+        .then((apiResponse) => {
+            res.render('beers.hbs', {
+                beers: apiResponse,
+            });
+        })
+        .catch((apiErr) => console.log(apiErr));
+});
+
+app.get('/beers/:id', (req, res) => {
+    const id = req.params.id;
+    punkAPI
+        .getBeer(id)
+        .then((apiResponse) => {
+            res.render('singleBeer.hbs', {
+                beer: apiResponse[0]
+            });
+        })
+        .catch((apiError) => {
+            console.log(apiError);
+        });
+});
+
+app.get('/random-beer', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then((apiResponse) => {
+            res.render('randomBeer.hbs', {
+                oneBeer: apiResponse[0],
+            });
+        })
+        .catch((apiErr) => console.log(apiErr));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
