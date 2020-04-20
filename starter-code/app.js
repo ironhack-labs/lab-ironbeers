5c78d5828a730fba58728e36c372116218edabcd
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-04-20 16:13:17 +0200

    Complete

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,6 +6,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -13,7 +15,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
-// add the routes here:
+// ROUTES
+// Homepage
 app.get('/', (req, res) => res.render('index'));
+
+// List of Beers
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then((dbRes) => {
+      console.log(dbRes);
+      res.render('beers', { beers: dbRes });
+    })
+    .catch((error) => console.log(error));
+});
+
+// Random Beer
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then((dbRes) => {
+      console.log(dbRes);
+      res.render('beer', { beer: dbRes[0] });
+    })
+    .catch((error) => console.log(error));
+});
+
+// Specific Beer
+app.get('/beer/:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then((dbRes) => {
+      console.log(dbRes);
+      res.render('beer', { beer: dbRes[0] });
+    })
+    .catch((error) => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
