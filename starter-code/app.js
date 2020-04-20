dd4c0537d4cab23d38d680701b8cbf2a25b0402a
HAKIM BEN FADHEL <capzebib2@yahoo.fr>
2020-04-20 17:22:26 +0200

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+// app.get('/beers', (req, res) => res.render('beers'));
+
+app.get('/beers', (req, res) => {
+  console.log("it's ok");
+  let birra = punkAPI.getBeers(req.query);
+  birra
+    .then(apiRes => {
+      //   res.json(beers);
+      console.log(apiRes);
+      res.render('beers.hbs', { birra: apiRes });
+    })
+    .catch(err => console.log(err));
+});
+
+app.get('/random-beer', (req, res) => {
+  const randomBeer = punkAPI.getRandom();
+  randomBeer
+    .then(beer => {
+      const birraRandom = beer[0].name;
+      console.log(birraRandom);
+      res.render('random-beer', { beer });
+    })
+    .catch(err => console.log(err));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
