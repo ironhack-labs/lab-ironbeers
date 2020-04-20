34ff77915229f4eecdde237064ac5370b8aa2a75
Pauline Ducos <paulineducos@me.com>
2020-04-20 23:12:14 +0200

    PAR WD FT PAULINE DUCOS

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
+hbs.registerPartials(path.join(__dirname, "views/partials"));
+
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res)=>{
+   punkAPI
+    .getBeers()
+    .then((apiRes)=>{
+        console.log(apiRes)
+        res.render("beers.hbs", {
+            beers: apiRes
+        })
+    })
+    .catch((apiErr)=>{console.log(apiErr)})
+})
+
+
+
+app.get("/random-beer", (req, res)=>{
+    punkAPI
+        .getRandom()
+        .then((apiRes)=>{
+            res.render("random-beer.hbs",{
+           beer:  apiRes[0]
+            })       
+        })
+        .catch((apiErr)=>{
+            console.log(apiErr)
+        })
+})
+
+app.get("/beers/:id", (req, res) => {
+    const id = req.params.id;
+    punkAPI
+        .getBeer(id)
+        .then((apiRes) => {
+            res.render("random-beer.hbs", {beer: apiRes[0]})
+                  })
+                .catch((err) => {console.log(err)})
+      
+    });
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
