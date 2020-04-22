46c8129ea30f11dc125165b42ac6f41ab5a4ac89
Maxime Lagrange <com.maximelagrange@gmail.com>
2020-04-22 20:32:05 +0200

    Lab done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
-
+hbs.registerPartials(__dirname + "/views/partials")
+// app.use(express.static(path.join(__dirname, 'views/partials')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) =>{
+    punkAPI
+  .getBeers()
+  .then((beersFromApi) => {
+      console.log('Beers from the database: ', beersFromApi),
+  res.render('beers', {allBeers:beersFromApi});
+})
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req,res) =>{
+    punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+        console.log('Random beers from the database: ', responseFromAPI),
+        res.render('random-beer', {randomBeers:responseFromAPI});
+    })
+    .catch(error => console.log(error)); 
+}
+)
+
+
+
+
+
+app.listen(3002, () => console.log('🏃‍ on port 3000'));
