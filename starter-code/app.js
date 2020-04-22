c2d2579d126a8d55d21e937f3035737b8ef261f6
Teddy ROGERS <teddy.rogers.pro@outlook.com>
2020-04-23 00:50:19 +0200

    PAR WDFT 0320 Teddy ROGERS
    mandatory parts done

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the partials here:
 
 // add the routes here:
+
+//  --- iteration 1 ---
 app.get('/', (req, res) => res.render('index'));
 
+
+// --- iteration 3.1 ---
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+
+    .then((beersFromApi) => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', {
+        beerlist: beersFromApi,
+      });
+    })
+    .catch((error) => console.log(error));
+});
+
+
+// --- iteration 4.1 ---
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+
+    .then((beersFromApi2) => {
+      console.log('Beers from the database: ', beersFromApi2);
+      res.render('random-beer', {
+        randomlist: beersFromApi2,
+      });
+    })
+    .catch((error) => console.log(error));
+});
+
+
+// --- server ---
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
