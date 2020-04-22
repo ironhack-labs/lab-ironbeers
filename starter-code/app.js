6c319da18f638429602b5bc9cafcf5a7bd9c417b
Siiri Sikkilä <sisikkil@student.oulu.fi>
2020-04-22 21:07:37 +0300

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,28 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get("/beers", (req, res) => {
+    const getOnly25beers = punkAPI.getBeers(); //{"hops": "amarillo"}
+    getOnly25beers.then( beerArray => {
+        console.log( beerArray.length + 28);
+        res.render("beers", {beers: beerArray})
+
+    } )
+    getOnly25beers.catch( error => console.log(error));
+});
+
+app.get("/random-beer", (req, res) => {
+    const getRandom = punkAPI.getRandom();
+    getRandom.then( beerArray => {
+        console.log('randm beer:', beerArray);
+        res.render("random-beer", {rndmBeer: beerArray[0]})
+    } )
+    getRandom.catch(error => console.log(error))
+});
+
+app.listen(3001, () => console.log('🏃‍ on port 3001 '));
