96d2e740d4bff6961f6a3a4fc6ccb217dcc5f510
bdean05 <benjamin_h_dean@yahoo.com>
2020-04-25 11:11:49 +0200

    My final

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+// app.get('/beers', (req, res) => res.render('beers'))
+
+app.get("/beers", (request, response) => {
+  punkAPI
+    .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    response.render("beers.hbs", { beers: beersFromApi })
+  })
+  .catch(error => console.log(error));
+});
+
+// app.get('/randombeers', (req, res) => res.render('randombeers'));
+
+// random beer 
+
+app.get("/randombeers", (request, response) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      console.log('Beers from the database: ', responseFromAPI)
+      response.render("randombeers.hbs", { randombeers: responseFromAPI })
+    })
+    .catch(error => console.log(error));
+});
+
+
+
+
+
+
+// const randomBeer = punkAPI.getRandom()
+
+// app.get("/randombeers", (request, response) => {
+//  punkAPI
+//  randomBeer.then(randombeer => {
+//    console.log(beer[0].name)
+//  })
+
+  app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+
+
+//Recipe.create(myRecipe)
+//  .then((dbRes) => {
+//    console.log(dbRes)
+//  }).catch((dbErr) => {
+//    console.log(dbErr)
+//  })
+
+//.getBeers()
+//.then((beersFromApi) => {
+//  console.log('Beers from the database: ', beersFromApi)
+//})
+//.catch(error) => {
+//  console.log(error)
+//});
