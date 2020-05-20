737a99010cb8cf3b44aadd97f7845b0356a1fe18
Marcela Vilas Boas <marcela.vilasboas@gmail.com>
2020-05-20 09:28:44 +0200

    1-4 iterations

--- app.js
+++ app.js
@@ -22,4 +22,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then((beersFromApi) => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch((error) => console.log(error));
+}); 
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      res.render('random-beer', { randomBeer: responseFromAPI })
+    })
+    .catch(error => console.log(error));
+
+});
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
