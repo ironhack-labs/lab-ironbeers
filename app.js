95e10de23a96f4f131a44335e2e079b423b40eec
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-05-21 11:30:26 +0200

    done

--- app.js
+++ app.js
@@ -14,12 +14,53 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+//beers route handler:
+app.get('/beers', (req, res) => {
+  const beers = punkAPI.getBeers();
+  beers.then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi);
+    res.render('beers', {
+      beersFromApi
+    });
+  });
+  beers.catch(error => console.log(error));
+});
+
+//ramdom-beers route handler:
+app.get('/random-beer', (req, res) => {
+  const beer = punkAPI.getRandom();
+  beer.then(responseFromAPI => {
+    // your magic happens here
+    console.log('Beers from the database: ', responseFromAPI);
+    res.render('random-beer', {
+      responseFromAPI
+    });
+
+  });
+  beer.catch(error => console.log(error));
+});
+
+//specific beer
+app.get("/beers/beer-:id", (req, res, next) => {
+  const beerid = punkAPI.getBeer(req.params.id);
+  beerid.then(responseFromAPI => {
+    // your magic happens here
+    console.log('Beers from the database: ', responseFromAPI);
+    res.render('random-beer', {
+      responseFromAPI
+    });
+
+  });
+  beerid.catch(error => console.log(error));
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
