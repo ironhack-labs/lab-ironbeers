7e2e8cda7a11091dd39de4b7fa4b4260f542e8b5
Toni <sancheztoni.garcia@gmail.com>
2020-05-21 13:15:31 +0200

    Completadas las iteraciones obligatorias

--- app.js
+++ app.js
@@ -10,16 +10,34 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+//STATIC FILES
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
+//PARTIALS
+hbs.registerPartials(__dirname + '/views/partials');
 // ...
 
-// Add the route handlers here:
-
+//ROUTES
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi.length);
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(e => console.log(e));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom().then(randomBeer => {
+    console.log('random beer', randomBeer[0]);
+    res.render('random-beers', randomBeer[0]);
+  });
+});
+
+//SERVER START
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
