4e4eddd648c93564da5d4a49f7c26b651ddf99e0
Santiago Casa <santiagoacasa@gmail.com>
2020-05-22 21:23:50 +0200

    Hecho - faltan bonus y un poco de styling

--- app.js
+++ app.js
@@ -13,13 +13,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, "/views/partials"));
 
 // ...
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render('index.hbs');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get("/beers", (req, res) => {
+   punkAPI
+     .getBeers()
+     .then(beersFromApi => {
+       res.render('beers.hbs', {beersFromApi: beersFromApi})
+       console.log('Beers from the database: ', beersFromApi)})
+     .catch(error => console.log(error));
+});
+
+app.get("/random-beers", (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('randomBeerPage.hbs', {birraRandom} = responseFromAPI[0]);
+      console.log('Respuesta de getRandom:', responseFromAPI)
+      // your magic happens here
+    })
+    .catch(error => console.log(error));
+  
+});
+
+app.listen(3000, () => console.log('funcionando en el 3000'));
