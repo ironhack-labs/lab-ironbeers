52c130b40eb11f98f8288dc27f756a388b1bb9aa
Arune Martinkeviciute <arunemart@gmail.com>
2020-05-27 18:35:51 +0200

    finished without bonus, did not understand partial

--- app.js
+++ app.js
@@ -6,17 +6,33 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
+app.get('/', function (req, res) {
+  res.render('index.hbs');
+})
+app.get('/beers', function (req, res) {
+punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log(beersFromApi);
+    res.render('beers.hbs', {beersFromApi})
+  })
+  .catch(error => console.log(error));
+})
+app.get('/random-beer', function (req, res) {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer.hbs', {responseFromAPI})
+  })
+  .catch(error => console.log(error));
+})
 
 app.get('/', (req, res) => {
   res.render('index');
