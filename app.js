51e73834263a8cdd12723689c8c56af1f6a9a124
Justo Rivera <justogdrmonterrey@gmail.com>
2020-05-27 17:45:21 +0200

    done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,5 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then(beers => res.render('beers', {beers}))
+})
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom().then(beer => res.render('random-beers', beer[0]))
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
