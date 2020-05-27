a46076eca6969d1d20117f303fd9ddcd0e66fb7d
Michelle Lock <michelleloc@gmail.com>
2020-05-28 00:20:11 +0800

    Finish all mandatory iterations

--- app.js
+++ app.js
@@ -19,7 +19,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+    .then(beersFromApi => res.render('beers.hbs', { beersFromApi }))
+    .catch(error => console.log(error));
+})
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => res.render('random-beer.hbs', { responseFromAPI }))
+  .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
