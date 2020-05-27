3e0128e900e99546d22c2ca585ef262ede2b7b08
Nienke0905 <nienkewestdijk@gmail.com>
2020-05-27 15:43:05 +0200

    mandatory done

--- app.js
+++ app.js
@@ -11,15 +11,24 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  let beers = punkAPI.getBeers();
+  beers.then(beersFromApi => {
+    res.render('beers',beersFromApi);
+  });
+});
+
+app.get('/random-beer', (req, res) => {
+  let randomBeer = punkAPI.getRandom();
+  randomBeer.then(randomBeerReturned => {
+    res.render('random-beer',randomBeerReturned[0]);
+  })
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
