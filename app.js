db090a25ac6be748464bf0607ef62efb6ef5e49e
James Dawson <63356869+polymurph13@users.noreply.github.com>
2020-05-27 19:57:35 +0100

    completed

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
@@ -20,6 +21,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then((apiBeers) => {res.render('beers', {apiBeers});});
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom().then((randomBeer) => {res.render('random-beer', {randomBeer});});
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
