ce983d81d6ec724c12cac450a6942ecfff501ad4
nardokra <nardo.kra@gmail.com>
2020-05-27 22:34:42 +0200

    done, except styling beer pages

--- app.js
+++ app.js
@@ -14,12 +14,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then((beersFromApi) => {
+      res.render('beers', {
+        beers: beersFromApi
+      });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/onebeer:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then((beersFromApi) => {
+      res.render('onebeer', beersFromApi[0]);
+    })
+    .catch(error => console.log(error));
+});
+
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then((beersFromApi) => {
+      res.render('randomBeers',{
+        beers: beersFromApi
+      });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
