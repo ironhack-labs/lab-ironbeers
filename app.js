5f667f7c958870181b779c21edf53a42a464fe78
Justine Benevent <justinebnvent@gmail.com>
2020-05-28 09:47:22 +0300

    mandatory iteration done

--- app.js
+++ app.js
@@ -19,7 +19,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render(__dirname + '/views/home.hbs');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => res.render((__dirname + '/views/beers.hbs'), {beersFromApi}));
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+
+  .then(responseFromAPI => {
+    // your magic happens here
+    punkAPI.getRandom()
+  })
+  .catch(error => console.log(error));
+  res.render(__dirname + '/views/random-beers.hbs');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
