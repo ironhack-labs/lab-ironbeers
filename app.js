e73eb2584ca3f9b55ab24557fde8f405da409529
Ivana Krucaj <ivana.krucaj@gmail.com>
2020-05-28 22:09:52 +0200

    finish bonus without style

--- app.js
+++ app.js
@@ -12,24 +12,32 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
 
-// ...
+// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 
 // Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/beers', (req, res) => {
+
   const getBeers = punkAPI.getBeers()
-  
   getBeers.then(beersFromApi => {
     console.log('Beers from the database: ', beersFromApi)
-    res.render('beers', {beersFromApi})
+    res.render('beers', {randomBeer: beersFromApi})
   }).catch(error => console.log(error));
+});
 
+app.get('/beers/:id', (req, res) => {
+  
+  const getBeer = punkAPI.getBeer(req.params.id)
+  getBeer.then(beerFromApi => {
+    console.log('Beer from the database: ', beerFromApi)
+    res.render('specificbeer', {specificBeer: beerFromApi[0]})
+  }).catch(error => console.log(error));
 });
 
 app.get('/random-beer', (req, res) => {
