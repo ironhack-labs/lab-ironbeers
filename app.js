54a15770fd2287681e8760eefcf1e6acc78e01ce
Miguel Serrano <miguelserrano.sp@gmail.com>
2020-06-23 21:54:01 +0200

    All iterations achieved. Bonus included. Styling not finished

--- app.js
+++ app.js
@@ -7,19 +7,48 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + "/views/partials")
 
 // ...
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/beers', (req, res) => {
+  
+  const strongBeers = punkAPI.getBeers({'abv_gt': 8})
+
+  strongBeers.then(beersFromApi => res.render('beers', {data: beersFromApi}))
+    
+  strongBeers.catch(error => console.log(error))
+})
+
+app.get('/random-beer', (req, res) => {
+
+  const randomBeer = punkAPI.getRandom()
+
+  randomBeer.then(beer => res.render('random-beer', {data: beer}))
+
+  randomBeer.catch(error => console.log(error))
+})
+
+app.get('/beers/:id', (req, res) => {
+
+  const idBeer = punkAPI.getBeer(req.params.id)
+
+  idBeer.then(beer => res.render('beers', {data: beer}))
+
+  idBeer.catch(error => console.log(error))
+})
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
