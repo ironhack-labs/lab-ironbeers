94c8f2e3c4bcaff7875ee8ac7242e2384faf72f4
Rodrigo Perez <user@Rodrigos-MBP.home>
2020-06-23 21:50:48 +0200

    done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(path.join(__dirname + "/views/partials"))
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +21,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+      punkAPI
+        .getBeers()
+        .then(beersFromApi => 
+          res.render('beers', {beersFromApi})
+        .catch(error => console.log(error)));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', responseFromAPI[0])
+      // your magic happens here
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
