678e0677088d3881ea87c25f164a38b9ef6c2c65
ignacioserrano9 <serranoi0910@gmail.com>
2020-06-23 19:54:04 +0200

    bonus till 6

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
+
+
 
 app.get('/', (req, res) => {
   res.render('index');
