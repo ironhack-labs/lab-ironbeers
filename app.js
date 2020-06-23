9184be4e38c442eb75009fc8ae4ae94ae689a38a
MelissaMelendez15 <melissamelendez15@gmail.com>
2020-06-23 22:49:28 +0200

    trabajo realizado

--- app.js
+++ app.js
@@ -14,12 +14,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (req, res) => {res.render('index')});  
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  
+  .catch(error => console.log(error));
+
 });
+app.get('/random-beers', (req, res) =>{res.render('random-beers')});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
