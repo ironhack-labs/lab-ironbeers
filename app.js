61520518e9cf3e0846f0c9d236106e6660fea657
miguelbachi5a <miguelbachi5a@gmail.com>
2020-06-23 20:01:41 +0200

    Bonus 5: PArtial

--- app.js
+++ app.js
@@ -13,11 +13,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.get('/', (req, res) => {
   res.render('index')
