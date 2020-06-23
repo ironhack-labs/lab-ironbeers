873101cf2db1c797624c90f98cbb1c95b67b63d1
ignacioserrano9 <serranoi0910@gmail.com>
2020-06-23 18:11:57 +0200

    finished exercise, working on bonus

--- app.js
+++ app.js
@@ -21,5 +21,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
-
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersApi => {
+      res.render('beers', {
+        beersApi: beersApi
+      })
+      console.log(beersApi)
+    })
+    .catch(error => console.log(error));
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi => {
+      res.render('random-beer', {
+        responseFromApi: responseFromApi
+      })
+      console.log(responseFromApi)
+    })
+    .catch(error => console.log(error));
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
