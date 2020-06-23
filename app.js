0dc4325bfe3c7c3785df0e15f993e3c21a16fea2
miguelbachi5a <miguelbachi5a@gmail.com>
2020-06-23 20:05:51 +0200

    Bonus 7: Style and layout, I added that at the beginning

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-hbs.registerPartials(path.join(__dirname, 'views/partials'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
 
 app.get('/', (req, res) => {
   res.render('index')
