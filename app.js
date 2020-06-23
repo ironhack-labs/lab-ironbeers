558cd2523df004be2b374627bd1fa16552291186
EFdez <escarlafdez@gmail.com>
2020-06-23 22:58:18 +0200

    TODO fix iteration6

--- app.js
+++ app.js
@@ -14,12 +14,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/partials")
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beersFromApi }))
+    .catch(error => console.log(error));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => res.render('random-beers', { beersFromApi }))
+    .catch(error => console.log(error));
+
+})
+
+app.get('/random-beers/:id', (req, res) => {
+
+  punkAPI
+    .getBeer(req.params.id)
+    .then(beersFromApi => res.render('random-beers', { beersFromApi }))
+    .catch(error => console.log(error))
+})
+
+// Volvió a pasar lo mismo que después de instalar el node: me tiraba un error con el puerto, lo cambié a 3003
+app.listen(3003, () => console.log('🏃‍ on port 3000'));
