420897567a0fc8059a9fdbf2e0f9024500892ab8
Laura Sanchez <laura695hotmail.com>
2020-06-24 01:19:04 +0200

    done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,5 +22,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beersFromApi }))
+    .catch(error => console.log(error));
+
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+    .then(randomBeers => res.render('random-beers', {
+      cervezaAleatoria: randomBeers
+    }))
+
+    .catch(error => console.log(error))
+
+
+
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
