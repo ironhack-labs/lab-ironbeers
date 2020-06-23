455bf8a9676780d22e8ce5b6765a3515034a4945
Lovemetal14 <franj.form019@gmail.com>
2020-06-24 00:12:55 +0200

    only until 3rd

--- app.js
+++ app.js
@@ -14,12 +14,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (req, res) => { res.render('index');
 });
+
+
+app.get('/beers', (req, res) => { 
+  punkAPI
+  .getBeers()
+  .then(beersApi => { res.render('beers', {
+    beersApi: beersApi
+  })
+  console.log(beersApi)
+})
+
+.catch(error => console.log(error));
+
+});
+
+
+//app.get('/beers/id:', (req, res) => {
+//  punkAPI
+//  .getBeer(req.params.id)
+//  .then()
+//})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
