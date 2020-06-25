9cdb52f9cc0e35dc9c087893baf0ca20347a937b
Paola Garay <garay.angela@gmail.com>
2020-06-25 15:02:01 +0200

    Iterations 1, 2 and 3

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
+
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -21,5 +24,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+app.get('/beers', (req, res) => {
+  
+  
+  punkAPI.getBeers()
+  .then(
+    PunkAPIWrapper => {
+      console.log(PunkAPIWrapper)
+      res.render('beers', { beers: PunkAPIWrapper})})
+  .catch(error => console.log(error))
+
+});
+
+app.get('/random-beers', (req, res) => {
+  res.render(__dirname + '/views/random-beers.hbs');
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
