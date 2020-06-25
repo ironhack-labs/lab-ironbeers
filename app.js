32be666fbee854245c17e4da738288ab90f1fa17
Eduarda Liu <eduardaliu@gmail.com>
2020-06-25 13:33:15 +0200

    mandatory iterations

--- app.js
+++ app.js
@@ -14,12 +14,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then(
+    beers => res.render('beers', {beers: beers})  // 'beers' é o nome do arquivo beers.hbs
+  )
+});
+
+app.get('/random', (req, res) => {
+  punkAPI.getRandom().then(
+    beers => res.render('random', {beers: beers}) 
+  )
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
