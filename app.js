4e00b30984d123dfcbc3ff344e517d96392a3d81
Izquiano <izquiano@hotmail.com>
2020-06-26 20:23:34 +0200

    Done

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
@@ -21,12 +22,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
 app.get('/beers', (req, res) => {
   punkAPI
   .getBeers()
   .then(beersFromApi => {
     // console.log('Beers from the database: ', beersFromApi)
     res.render('beers', {beersFromApi});
+  })
+  .catch(error => console.log(error));
+  
+});
+
+app.get('/beers/id-:id', (req, res) => {
+  console.log(req.url)
+  const id = req.url.split('-')[1]
+  punkAPI
+  .getBeer(id)
+  .then(responseFromAPI => {
+    console.log('Beers from the database: ', responseFromAPI)
+    res.render('random-beer', {responseFromAPI});
   })
   .catch(error => console.log(error));
   
