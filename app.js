fa8253ce760205915145d3592b6a1e90b80defb9
Nidian Martin <nidianmartin@gmail.com>
2020-06-28 12:50:49 +0200

    Iteration 1 -4, pending styles

--- app.js
+++ app.js
@@ -22,4 +22,19 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeer => res.render('random-beers', randomBeer[0]))
+  .catch(error => console.log(error));
+});
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
