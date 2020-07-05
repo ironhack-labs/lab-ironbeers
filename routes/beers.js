3e325726019ca1296d2a8a346bdff1ab1c0bafe9
Irma Blanco <irblancofdz@gmail.com>
2020-07-05 19:34:24 +0200

    Iteration 3

--- routes/beers.js
+++ routes/beers.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('beers');
-});
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const punkAPI = new PunkAPIWrapper();
+
+router.get('/', (req, res) => {
+  punkAPI.getBeers().then(beers => {
+    res.render('beers', {
+      title: 'IronHack Beers',
+      beers
+    })
+  }).catch(error => {
+    res.render('error', {
+      title: 'Error',
+      error
+    })
+  })
+})
+
 
 module.exports = router;
