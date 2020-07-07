b1b4ca00767ad4151a01c6041f0425cdba8e9705
Alice Prigent <aliprigent@gmail.com>
2020-07-07 20:19:27 +0200

    [PAR-FT-062020] Alice Prigent

--- app.js
+++ app.js
@@ -22,4 +22,28 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get("/beers", (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => { 
+    console.log(beersFromApi)
+   res.render('beers', { beersFromApi })
+   console.log('Beers from the database: ', beersFromApi)
+   })
+  .catch(error => console.log(error));
+});
+
+app.get("/random-beer", (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log('_____')
+   res.render('random-beer', { responseFromAPI })
+   
+  })
+  .catch(error => console.log(error));
+})
+
+
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
