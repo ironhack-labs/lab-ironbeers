a4d8ae5d1d0534953a0b4c204cc13af83e486384
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-07-22 13:02:14 +0200

    finally iteracion 3

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -20,6 +22,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    res.render('beers', {cervezas:beersFromApi});
+  })
+  .catch(error => console.log(error));
+
+  
+});
+
+app.get('/random-beers', (req, res) => {
+  res.render('random-beers');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
