897cc68e202f22123cbeb1f2bb9d036c9815dafd
Noiret <sergio.ko@icloud.com>
2020-07-22 13:03:36 +0200

    Missing bonuses. Randombeers.hbs to be prettified with CSS

--- app.js
+++ app.js
@@ -9,17 +9,43 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + "/views/partials")
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+// Register the location for handlebars partials here
 
-// ...
+// ..
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (req, res, next) => {
+  res.render('index.hbs');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render("beers.hbs", {
+      beersFromApi
+    }))
+    .catch(error => console.log(error));
+});
+
+//Llamamos a la Api y le pasamos el método getRandom()
+//Con la respuesta, lo renderizamos en su hbs correspondiente y
+//pasamos a un objeto y declaramos que la oneRandomBeer es el primer índice del array
+//de la respuesta de la Api
+app.get('/random-beers', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi => {
+      res.render("randomBeers.hbs", {
+        oneRandomBeer: responseFromApi[0]
+      });
+    })
+    .catch(error => console.log(error))
+});
+
+
+app.listen(3001, () => console.log('🏃‍ on port 3001'));
