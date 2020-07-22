365be1fc72dd68717fc9b5cd21cb8e392c6ad676
Natalia Silva <natalia.pupushi@gmail.com>
2020-07-22 15:06:22 -0300

    beers ready

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+const randomBeers = punkAPI.getRandom()
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,8 +21,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.render("index");
 });
+
+
+app.get("/beers", (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers", {
+      beers
+    });
+  })
+  .catch(error => console.log(error))
+  });
+
+
+
+  app.get("/randomBeers", (req, res,) => {
+  punkAPI.getRandom()
+  .then(responseFromAPI => {
+    // your magic happens here
+    console.log(responseFromAPI)
+    res.render("randomBeers", {randomBeer: responseFromAPI[0]});
+    })
+  .catch(error => console.log(error));
+  });
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
