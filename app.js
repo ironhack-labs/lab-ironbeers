e5d66d627367ed11026ad3d58ceaf21ae9a42150
Alejandro Abad <alejandro.abad.noriega@gmail.com>
2020-07-22 13:07:08 +0200

    without css Victoria & Alejandro A

--- app.js
+++ app.js
@@ -7,19 +7,48 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+const getBeers = async () => {
+  
+};
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+//
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers',async (req, res, next) => {
+    try {
+      let beersArray = await punkAPI.getBeers();
+      res.render('beers', {beers: beersArray})
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+
+app.get('/random-beers', async (req, res) => {
+  try {
+    let randomBeer = await punkAPI.getRandom();
+    console.log(randomBeer)
+    res.render('random-beers', {ranBeer: randomBeer});
+
+  } catch (error) {
+    console.log(error)
+  }
+
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
