6c4f8fa331e3d4f48919aaa60951f52b424a01d7
Leandro <leandro.a.loiacono@gmail.com>
2020-07-22 18:31:06 +0200

    done without iteration 6

--- app.js
+++ app.js
@@ -14,12 +14,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render('beers', beersFromApi);
+  })
+  .catch(error => console.log(error))  
+  
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    // your magic happens here
+    let randomBeer = responseFromAPI[0]
+    console.log('Random beer: ', randomBeer);
+    res.render('randomBeers', randomBeer)
+  })
+  .catch(error => console.log(error))   
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
