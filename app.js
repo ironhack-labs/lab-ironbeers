a70b4637668e7590461f45bcc6032e81ff52dad1
alexugo <alex.ugodnikov@gmail.com>
2020-07-25 16:31:47 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -22,4 +22,13 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', beersFromApi))
+  .catch(error => console.log(error));
+  
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
