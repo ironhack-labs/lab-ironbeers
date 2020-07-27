0fe86878f49ae846dad24ad3462510f0c1a45391
Cristina Matamoro <cristina.matamoro@gmail.com>
2020-07-27 22:35:46 +0200

    done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { response } = require('express');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -17,6 +18,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ...
 
 // Add the route handlers here:
+// Iteration 2_route endpoint home
+app.get('/', (req, res) => {
+  res.render('index.hbs');
+});
+
+//Iteration 3.1_route endpoint beers
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ')
+      res.render('beers.hbs',{beersFromApi});
+    })
+    .catch(error => console.log(error));
+});
+
+//Iteration 4.1_route-random-beer
+app.get('/random-beer.hbs', function (req, res) {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer.hbs', {responseFromAPI})
+  })
+  .catch(error => console.log(error));
+})
 
 app.get('/', (req, res) => {
   res.render('index');
