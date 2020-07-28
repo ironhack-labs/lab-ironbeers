5a0f288989fd1f43d41b38b7dc6661daa2e3197f
Matthew Howard <mattalanhoward@gmail.com>
2020-07-28 13:07:59 +0200

    styling

--- app.js
+++ app.js
@@ -12,14 +12,48 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//ROUTES
+app.get('/', (req, res, next) => {
+  res.render('index.hbs');
+});
+
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(responseFromAPI => {
+      // console.log('Beers from database:', responseFromAPI);
+      res.render('beers', { responseFromAPI });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      console.log('Beers from database:', responseFromAPI);
+      res.render('random-beer', { responseFromAPI });
+    })
+    .catch(error => console.log(error));
+});
+
+
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+
+app.get('/views/partials/beerpartial.hbs', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(responseFromAPI => {
+      res.render('beerpartial',{ responseFromAPI });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
