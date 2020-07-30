cb1e6b8f71f386bd5216d0b42e5a9a096e818d6e
AlvaroSL <alvaro.sanchez.lamadrid@gmail.com>
2020-07-30 17:04:43 +0200

    Lab done by Alvaro, Bodei, Lanette

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials/'));
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -19,7 +21,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render(__dirname + '/views/index');
 });
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+    .then((beersFromApi) => {
+      res.render(__dirname + '/views/beers', {beersFromApi});
+    })
+
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+  .then((getRandom) => {
+    res.render(__dirname + '/views/random-beers', {getRandom});
+  })
+
+  .catch(error => console.log(error));
+});
+
+app.get('/beers/:beerId', (req, res) =>{
+
+  let beerId = req.params.beerId;
+  punkAPI.getBeer(beerId)
+    .then((data)=> {
+      let myBeer = data[0]
+      console.log(data[0].image_url)
+    res.render(__dirname + '/views/beers', {beersFromApi: data})
+  })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
