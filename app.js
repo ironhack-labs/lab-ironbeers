22f3c791b2d9c19acb5daaebc5149a1d77e2a315
gonzalo-development <gperalcorrales@gmail.com>
2020-07-30 17:33:46 +0200

    git commit

--- app.js
+++ app.js
@@ -12,14 +12,43 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render('beers',{beer: beersFromApi});})
+  .catch(error => console.log(error));
+
+});
+
+app.get('/random-beer', (req, res) => {
+
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log(responseFromAPI[0].image_url)
+    res.render('random-beer',{beer:responseFromAPI[0]} );
+    // let myData = "Jorge"
+    // { myData }
+    // { myData: myData }
+  })
+  .catch(error => console.log(error));
+
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
