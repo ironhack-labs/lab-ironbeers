417f8e299752b7436646efa29f673a4ffec99d01
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-08-19 15:39:29 +0200

    finish iteration 5

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + '/views/partials');
 
-// ...
 
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
