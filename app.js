a6ff42c5d8948f7147df0c33978f890510c06a93
carct7 <carct7@gmail.com>
2020-08-26 22:01:21 -0500

    first draft

--- app.js
+++ app.js
@@ -14,12 +14,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/partials")
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', async(req, res) => {
+  let cervezas = await punkAPI.getBeers()
+  cervezas.array = cervezas;
+  cervezas.array.randomBeer = false;
+  //console.log(cervezas)
+  res.render('beers', {cervezas});
+});
+
+app.get('/randomBeer', async(req, res) => {
+  let cerveza =  await punkAPI.getRandom()
+  cerveza.array = cerveza;
+  cerveza = cerveza[0];
+  res.render('randomBeer', cerveza);
+});
+
+app.get('/specificBeer', async(req, res) => {
+  let cerveza
+  let id = req.query.id;
+  cerveza =  await punkAPI.getBeer(id);
+  cerveza = cerveza[0];
+  res.render('specificBeer', cerveza);
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
