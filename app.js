f1aa14774ed4d7c04ca48b3e548a6a834fd7ba82
Mai Yamawaki <joooooy031006@icloud.com>
2020-08-26 20:47:43 -0500

    terminamos

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { get } = require('http');
+hbs.registerPartials(__dirname + "/views/partials")
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -13,8 +16,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+// let beers = {}
 
-// ...
+
 
 // Add the route handlers here:
 
@@ -22,4 +26,16 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers=>{res.render("beers", {beers})})
+  .catch(error=>{console.log([error])})
+});
+
+app.get('/randomBeers', (req, res) => {
+  punkAPI.getRandom()
+  .then(cervesas=>{res.render("randomBeers",{cervesas})})
+  .catch(error=>{console.log([error])})
+});
+
+app.listen(3000, () => console.log('🏃‍ on port :http://localhost:3000'));
