db5197a0f1a205d78fbb47c8867783b40b58108c
allecramarcella <allecramarcella.dev@gmail.com>
2020-08-29 17:07:28 +0200

    it 1-6 + styling home

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
 
+
+
+// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
@@ -21,5 +24,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  const allBeersArr = punkAPI.getBeers()
+
+
+  allBeersArr 
+    .then(beers => {
+      // console.log(beers)
+      res.render('beers', {beers})
+    })
+    .catch(error => console.log(error))
+})
+
+app.get('/random-beer', (req, res) => {
+
+  const randomBeer = punkAPI.getRandom()
+
+  randomBeer
+    .then(randomBeer => res.render('random-beer', {randomBeer}))
+    .catch(error => console.log(error))
+})
+
+app.get('/clicked-beer/:id', (req, res) => {
+  const urlId = req.params.id
+  const beerId = punkAPI.getBeer(urlId)
+
+  beerId
+    .then(beer => res.render('clickedBeer', {beer}))
+    .catch(error => console.log(error))
+
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
