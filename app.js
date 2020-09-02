a037098b47f53ea6042b35e59afb4a1c6a411c2a
Rodrigo Muniz <ramunizbh@yahoo.com.br>
2020-09-01 22:03:53 -0300

    parcial do exercicio

--- app.js
+++ app.js
@@ -22,4 +22,17 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  //res.render('beers');
+  
+  //let beers = punkAPI.getBeers();
+  punkAPI.getBeers()
+  //.then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+
+});
+
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
