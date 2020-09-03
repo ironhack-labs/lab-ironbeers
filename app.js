0410d663631710e11c6f39febc84d077ea0f1db4
Sophia Hammer <kontakt@sophia-hammer.de>
2020-09-03 15:36:58 +0200

    lab Ironbeer with partial

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -21,5 +23,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+app.get('/beers', (req, res) => {
+  
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers',{beersFromApi}))
+  .catch(error => console.log(error));
+
+  
+});
+
+
+app.get('/random-beer', (req, res) => {
+
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log(responseFromAPI)
+    res.render('random-beer',{
+    responseFromAPI: responseFromAPI[0]
+  })
+  })
+  .catch(error => console.log(error));
+ 
+
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
