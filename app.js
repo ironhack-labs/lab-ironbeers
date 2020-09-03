2fdd2006f03433e1d4a575e23ce04c902c475ab7
Nadja Reischel <nadja.reischel@outlook.de>
2020-09-03 15:32:47 +0200

    done

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +22,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then((beersFromApi) => {
+    console.log('Beers from the database: ', beersFromApi)
+  res.render('beers', {beersFromApi})
+})
+  .catch((error) => console.log(error));
+});
+
+
+app.get('/randombeers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => res.render('randomBeers', {responseFromAPI}))
+  .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req,res) => {
+  punkAPI
+  .getBeer(req.params.id)
+  .then((beersFromApi) => {
+    console.log(beersFromApi)
+  res.render('beersView', {beer: beersFromApi[0]})
+})
+  .catch((error) => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
