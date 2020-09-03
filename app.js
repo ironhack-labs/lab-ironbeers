0434c6c2e7376502ec3efdb4a068924ed3630c4f
Diana <d.thueroff@gmail.com>
2020-09-03 13:40:52 +0200

    up to iteration 5

--- app.js
+++ app.js
@@ -22,4 +22,28 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+ punkAPI
+ .getBeers()
+ .then(
+   beersFromApi => res.render(
+     'beers', { beersFromApi }
+     )
+ )
+ .catch(error => console.log(error));
+
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(
+    randomBeerFromApi => {
+      const beer = randomBeerFromApi[0];
+      res.render('random-beer', { beer });
+    })
+  .catch(error => console.log(error));
+ 
+ });
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
