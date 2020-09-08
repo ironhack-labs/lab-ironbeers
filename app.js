f1e5395c9967fd96cdff32be4e692c3e3b90d2f6
Javier de Salas <chinostrike@gmail.com>
2020-09-08 22:01:04 +0200

    dinamic page terminada. Layout a medias

--- app.js
+++ app.js
@@ -12,14 +12,50 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, "views/partials")) // Register the location for handlebars partials here:
+
+
 
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
+  res.render('index')
+})
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log(beersFromApi)
+      res.render('beers', {beersFromApi})
+    })
+    .catch(error => console.log(error))
+
+  //res.render('beers', {beerArray})
+
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      console.log(randomBeer)
+      res.render('randomBeer', {...randomBeer})
+    })
+    .catch(error => console.log(error))
+})
+
+app.get('/beers/beer/:id', (req, res) => {
+  const beerId = req.params.id
+  punkAPI
+    .getBeer(beerId)
+    .then(beerFromApi => {
+      res.render('randomBeer', {...beerFromApi})
+    })
+    .catch(error => console.log(error))
+ 
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
