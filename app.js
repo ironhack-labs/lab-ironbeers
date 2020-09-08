b536c275c478195d4ff9a562ec6c4ad3b9039a97
home <home@MacBook-Pro-de-hack.local>
2020-09-08 23:04:45 +0200

    done

--- app.js
+++ app.js
@@ -11,7 +11,40 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(path.join(__dirname, "views", "partials"))
+app.get("/", (req, res) => res.render("index"))
+app.get("/beers", (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', {
+        beersFromApi
+      })
+    })
+    .catch(error => console.log(error));
+})
+app.get("/beers/:id", (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then(beersFromApi => {
+      res.render('beers', {
+        beersFromApi
+      })
+    })
+    .catch(error => console.log(error));
+})
+app.get("/randombeer", (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      console.log('Random Beer: ', responseFromAPI)
+      res.render('randombeer', {
+        responseFromAPI
+      })
+    })
+    .catch(error => console.log(error));
+})
 // Register the location for handlebars partials here:
 
 // ...
