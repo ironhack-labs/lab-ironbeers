9b5d7e5057cea74f638df4cc672c2e280ebecf3a
Cynthia Takiishi <cynthiaakemit@gmail.com>
2020-09-08 19:49:08 -0300

    Iterations done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 
 const hbs = require('hbs');
@@ -20,6 +21,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then((beersFromApi) => {
+      const beers = beersFromApi;
+      console.log(beers);
+      res.render('beers', beers);
+    })
+    .catch((error) => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then((beersFromApi) => {
+      const randomBeer = beersFromApi;
+      console.log(randomBeer);
+      res.render('random-beer', randomBeer);
+    })
+    .catch((error) => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
