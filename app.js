0a8dfbb5dbe9f1092a5b4fdb48dd054c21a38fcd
anabermudezmonteagudo <anabermudezmonteagudo@gmail.com>
2020-09-08 20:33:45 +0200

    falta la iteración 6 y algunos estilos

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
 // Register the location for handlebars partials here:
 
 // ...
@@ -22,4 +22,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => 
+      res.render('beers', {beersFromApi}))
+      //console.log('Beers from the database: ', beersFromApi))
+    .catch(error => console.log(error));
+
+})
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => 
+      res.render('random-beer', {responseFromAPI}))
+  
+  .catch(error => console.log(error))
+
+})
+ 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
