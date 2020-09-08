3c4ae9eef38b078751b6828dfb1be76a9923f193
Cylabeth <cylabeth@gmail.com>
2020-09-08 23:38:16 +0200

    [WEB-MAD DE] Cynthia Elizabeth Gorosito

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials')) 
 // ...
 
 // Add the route handlers here:
@@ -21,5 +21,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then(beersFromApi => res.render('beers',{beers: beersFromApi})).catch(error => console.log(error))});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom().then(responseFromAPI => res.render('random-beer',{randomBeer: responseFromAPI})).catch(error => console.log(error))});
+
+//link experiment
+app.get('/beers/:id', (req, res) => {
+  punkAPI.getBeer(req.param.id).then(beerFromApi => res.render('beers',{beers: beerFromApi})).catch(error => console.log(error))});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
