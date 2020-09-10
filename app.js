88f94a7eaa0b38204669d315b9f6292423c4cce6
Arua Monteiro <aruamonteiro@gmail.com>
2020-09-10 09:56:26 -0300

    done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,6 +21,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers();
+  console.log(beers);
+  res.render('beers', { beers });
+});
+
+app.get('/beers/:beerid', async (req, res) => {
+  const beerId = req.params.beerid;
+  const beer = await punkAPI.getBeer(beerId);
+  res.render('random-beer', { beer });
+});
+
+app.get('/random-beer', async (req, res) => {
+  const beer = await punkAPI.getRandom();
+  res.render('random-beer', { beer });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
