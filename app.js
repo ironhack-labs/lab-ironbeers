cdcae68e637cea804eaeda56396a9803335a8423
sechs <philippe.buteau@skema.edu>
2020-09-18 16:11:45 +0200

    Iteration 3 completed

--- app.js
+++ app.js
@@ -14,12 +14,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/partials")
+
+
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', async (req, res) => {
+  try {
+  const beersFromAPI = await punkAPI.getBeers();
+  res.render('beers', {beersFromAPI});
+  }
+  catch (error) {
+    console.log(error)
+  }
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
