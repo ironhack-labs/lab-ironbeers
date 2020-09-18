dc187409b6c811a24081c0f5a22f1ad9bf7892b7
Rico <rasim.rico@gmail.com>
2020-09-18 16:16:10 +0200

    iteration 3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
+const { nextTick } = require('process');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
@@ -19,8 +20,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
+app.get('/beers', async (req, res) => {
+  try {
+    const apiBeers = await punkAPI.getBeers();
+    res.render('beers', { beers: apiBeers });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get('/random-beers', (req, res) => {
+  res.render('');
+});
 
 app.listen(4000, () => console.log('🏃‍ on port http://localhost:4000'));
