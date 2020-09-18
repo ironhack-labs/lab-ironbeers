825a6249208f50637e5972fbffbf0b50210f321c
Timothée Nicole <timothee.nicole@gmail.com>
2020-09-18 17:21:46 +0200

    Iteration 1 to 5 done, still struggling with iteration 6, I'll maybe do iteration 7 first

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
+hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
 
 // Add the route handlers here:
 
@@ -22,4 +21,48 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res) => { 
+  const beers = await punkAPI.getBeers()
+  try {
+      res.render('beers', {beers} )
+      console.log(beers);  
+  } catch (error) {
+      console.error(error)
+  }
+})
+
+app.get('/random-beers', async (req, res) => {
+  const randomBeers = await punkAPI.getRandom()
+  try {
+    res.render('randomBeers', {randomBeers})
+    console.log(randomBeers)
+  } catch (err) {
+    console.error(err);
+  }
+})
+
+app.get('/beers/beer-:id', (req, res) => {
+  punkAPI.getBeer(:id) 
+  try {
+    res.render("beerPage")
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+
+// app.get('/random-beers', async (req, res) => { 
+  
+//   const randomBeers = await punkAPI.getRandom()
+//   try {
+//       res.render('randomBeers', {randomBeers} )
+//       console.log(randomBeers);  
+//   } catch (error) {
+//       console.error(error)
+//   }
+// })
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+
