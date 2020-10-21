494915c8735794aa25f3773466a300b969fcf7b7
Lexirem <me.vallribera@gmail.com>
2020-10-21 16:12:53 +0200

    minimo hecho

--- app.js
+++ app.js
@@ -13,13 +13,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
 // Add the route handlers here:
-
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) =>{
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom() 
+  .then(randomFromAPI  => res.render('random-beers', {randomFromAPI}))  
+  .catch(error => console.log(error));
+  });
+  
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
