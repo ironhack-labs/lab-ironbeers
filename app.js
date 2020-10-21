03bbb89fe915bf81fca85745e136cd3a948cc074
Esteve Martin <esteve.martin@mpasolutions.es>
2020-10-21 15:59:06 +0200

    Done untill Bonus 5

--- app.js
+++ app.js
@@ -13,13 +13,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers()
+  // console.log(beers)
+  res.render('beers',{beers:beers});
+});
+
+app.get('/random-beer', async (req, res) => {
+  const randomBeer = await punkAPI.getRandom()
+  const currentBeer = randomBeer[0]
+  console.log(currentBeer)
+  res.render('randomBeer',currentBeer);
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
