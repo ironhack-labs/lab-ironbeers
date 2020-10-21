f6df61d4fbb606f038a4b85e3bb7248bcffbb68f
Carla Navia <carlanavia@MBP-de-Carla.home>
2020-10-21 14:37:27 +0200

    [FT-BCN-0920]-Carla Navia

--- app.js
+++ app.js
@@ -14,12 +14,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
+
+app.get("/beers", (req, res, next) => {
+    punkAPI
+    .getBeers() 
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render("beers", { beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get("/random-beers", (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeers => {
+      //  alert(randomBeers[0].name)
+      console.log('Beers from the database: ', randomBeers)
+      res.render("randomBeers", {randomBeers});
+  })
+  .catch(error => console.log(error)); 
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
