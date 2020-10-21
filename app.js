23105df01e1c271afbcbba5a138047294f45fad0
erifylo <erif.evangelou@gmail.com>
2020-10-21 15:47:56 +0200

    falta terminar el bonus 5, y hacer bonuses 6 y 7

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const express = require('express');
-
-const hbs = require('hbs');
-const path = require('path');
+const hbs     = require('hbs');
+const app     = express();
+const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
-const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
-// Register the location for handlebars partials here:
-
-// ...
 
 // Add the route handlers here:
 
+//Route to beers
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log(beersFromApi)
+    res.render('beers.hbs', { beersFromApi});
+  })
+  .catch(error => console.log(error))
+});
+
+
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/randomBeers', (req, res) => {
+  punkAPI.getRandom()
+  .then(responseFromAPI => {
+    console.log(responseFromAPI)
+    res.render('randomBeers', {responseFromAPI});
+  })
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
