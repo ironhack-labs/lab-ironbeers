fc53942de426e67f19e62b3e8824abdbbb522380
Cristina Castro <cristinacastro87@gmail.com>
2020-10-21 16:21:42 +0200

    done

--- app.js
+++ app.js
@@ -12,14 +12,33 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get("/beers", (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', {beersFromApi});
+  })
+  //el catch es posa fora, un cop acabat el then
+  .catch(error => console.log(error));
+  });
+
+
+app.get("/randomBeer", (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log('A random beer from database: ', responseFromAPI)
+    res.render('randomBeer', {responseFromAPI}); 
+  })
+  .catch(error => console.log(error));
+ });
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
