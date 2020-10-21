a41e3d9c1a2184eceab536f01942c6cb677a61a4
Alberto <a.p.r.13287@gmail.com>
2020-10-21 16:13:25 +0200

    done sin bonus

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -18,8 +19,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));  
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom() 
+  .then(randomFromAPI  => res.render('random-beers', {randomFromAPI}))  
+  .catch(error => console.log(error));
+    
+  });
+  
+
+
+
+app.listen(3001, () => console.log('🏃‍ on port 3001'));
