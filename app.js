96b2b41c348b2d690fe2570c39b4087ceda276f7
MarcSadurni <marcsadurni@LAPTOP-R7RNDFA3.localdomain>
2020-10-21 15:48:37 +0200

    done

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + '/views/partials');
 
 // ...
 
@@ -21,5 +22,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+  res.render('beers', { beersFromApi });
+})
+  .catch(error => console.log(error));
+
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeer => {
+    // your magic happens here
+    res.render('random-beers', { randomBeer });
+  })
+  .catch(error => console.log(error));
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
