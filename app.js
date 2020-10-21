d1bcab1be72268bef03cf719f4931d83e1ac0771
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-10-21 16:09:26 +0200

    Done

--- app.js
+++ app.js
@@ -12,14 +12,57 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+// TODO Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-// ...
-
-// Add the route handlers here:
-
+// TODO Add the route handlers here:
+//* Home
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// * Beers Page
+app.get('/beers', async (req, res) => {
+  try {
+    const beers = await punkAPI.getBeers();
+    // console.log(beers);
+    res.render('beers', { beers });
+  } catch (err) {
+    error => console.log(error);
+  }
+});
+
+// * Random-Beers Page
+app.get('/random-beers', async (req, res) => {
+  try {
+    const randomBeers = await punkAPI.getRandom();
+    // console.log(randomBeers);
+    res.render('random-beers', { randomBeers });
+  } catch (err) {
+    error => console.log(error);
+  }
+});
+
+// * Beers from ID
+app.get('/beer/:id', (req, res, next) => {
+  console.log('single beer');
+  var currentId = req.params.id;
+  var numId = parseInt(currentId, 10);
+  var beer;
+  punkAPI
+    .getBeer(numId)
+    .then(res => {
+      console.log(res);
+      //const beer = el[0];
+      beer = res[0];
+    })
+    .then(() => {
+      res.render('beer', {
+        beer
+      });
+    })
+    .catch(err => console.log(err));
+});
+
+// Listener Port
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
