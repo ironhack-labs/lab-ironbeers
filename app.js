bd0a2c88a0ad90c701e91d4658f57476a6d56fbd
Andreu Martínez <martinez.andreu@gmail.com>
2020-10-22 08:45:37 +0200

    [WDFT-BCN-0920] Andreu Mtez - Ex completed incl bonus 5

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// We're saying to the js where the partials are. 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +23,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// Route to Beers
+
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log(beersFromApi)
+    res.render('beers.hbs', {beersFromApi: beersFromApi});
+  })
+  .catch(error => console.log(error));
+});
+
+// Route to random beeers
+
+console.log("Before entering the /random");
+
+app.get('/random', (req, res) => {
+  punkAPI.getRandom()
+  .then(beersFromApi => {
+    console.log("Random: ", beersFromApi)
+    res.render('random.hbs', {beersFromApi: beersFromApi});
+  })
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
