fa58fc34e28ccd7b47239a16209bd5ec3c3a53f4
Sabela <sabela.ons2@gmail.com>
2020-11-03 19:14:57 +0100

    got to end of iteration 3, but it is not working

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+    .catch(error => console.log(error));
+  res.render('Beers', beersFromApi);
+});
+
 app.get('/', (req, res) => {
   res.render('index');
 });
