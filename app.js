96fbb72652a09ca91119254a819cc584fef18a7b
Sabela <sabela.ons2@gmail.com>
2020-11-04 22:42:32 +0100

    most iterations done

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
+const morgan = require('morgan');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const port = 5001;
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
+// view engine will be hbs, 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
+//app.set means configuring something with express, where views are storaged, folder for template 
+app.set('views', path.join(__dirname, './views'));
+//.use public folder to work with express and used it to save static files: css, images, index.html... 
 app.use(express.static(path.join(__dirname, 'public')));
+//dirname related to location of plublic folder
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
 
 // Register the location for handlebars partials here:
 
@@ -21,13 +27,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/beers', (req, res) => {
   punkAPI
     .getBeers()
-    .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+    .then(beersFromApi => {
+      res.render('beers.hbs', {
+        beersFromApi
+      });
+    })
     .catch(error => console.log(error));
-  res.render('Beers', beersFromApi);
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      res.render('random-beer.hbs', {
+        randomBeer
+      });
+    })
+    .catch(error => console.log(error));
 });
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(port, console.log(`Server is running on port: ${port}`));
