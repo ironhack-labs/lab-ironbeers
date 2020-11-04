f141f541c3eb526d747d627e0ccedd95a57d0437
sophiepa <sophiepa@hotmail.com>
2020-11-04 19:02:47 +0100

    having issues with localhost again

--- app.js
+++ app.js
@@ -14,12 +14,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then((beersFromApi) => {res.render('beers.hbs', { beersFromApi });
+    console.log('Beers from the database: ', beersFromApi)
+  })
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+  .then((beersFromRandom) => {res.render('random-beers.hbs', { beersFromRandom });
+  })
+  .catch(error => console.log(error));
+})
