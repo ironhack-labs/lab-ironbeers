f7fa8d0860671bac99c0d56700302bdb873070aa
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-04 10:11:37 +0100

    finally some beers on this beer page

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 hbs.registerPartials(__dirname + "/views/partials");
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/beers', (req, res) => {
-  const allBeers = punkAPI.getBeers().then(beersFromApi =>
-    console.log(beersFromApi)).catch(error => console.log(error))
-  res.render('beers', allBeers);
+  punkAPI.getBeers().then(beersFromApi => res.render('beers', {
+    beersFromApi
+  })).catch(error => console.log(error));
 });
 
 app.get('/random-beer', (req, res) => {
-  const randomBeer = punkAPI.getRandom().then(randomBeer => console.log(randomBeer)).catch(error => console.log(error))
-  res.render('random-beer', randomBeer);
+  punkAPI.getRandom().then(randomBeer => res.render('random-beer', {
+    randomBeer
+  })).catch(error => console.log(error))
 });
 
 app.listen(3000);
