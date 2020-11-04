3d84469aca50b6326e883fd38d90f0d86e3289a7
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-04 09:07:14 +0100

    made a page that doesnt show data

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/beers', (req, res) => {
-  punkAPI.getBeers().then(beersFromApi => console.log('Beers from the database: ', beersFromApi)).catch(error => console.log(error))
-  res.render('beers');
+  const allBeers = punkAPI.getBeers().then(beersFromApi =>
+    console.log(beersFromApi)).catch(error => console.log(error))
+  res.render('beers', allBeers);
 });
 
-app.get('/random-beers', (req, res) => {
-  res.render('random-beers');
+app.get('/random-beer', (req, res) => {
+  const randomBeer = punkAPI.getRandom().then(randomBeer => console.log(randomBeer)).catch(error => console.log(error))
+  res.render('random-beer', randomBeer);
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(3000);
+
+module.exports = app;
