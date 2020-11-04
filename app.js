d2a66881d04061388fbde7b954b2522197620c4f
Issa Nikole Gruber <issanikolegruber@Issas-MacBook-Pro.local>
2020-11-04 13:52:40 +0100

    done

--- app.js
+++ app.js
@@ -14,12 +14,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+//Iteration 3.1 create a beers route
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then((beersFromApi) => {res.render('beers', { beersFromApi });
+    console.log('Beers from the database: ', beersFromApi)
+  })
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then((beersFromRandom) => {res.render('random-beer', { beersFromRandom });
+  })
+  .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
