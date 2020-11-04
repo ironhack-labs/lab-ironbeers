8b1de5051c44d1b606e4f76be3e0ef024534ff79
Elia Chiarucci <elia.chiarucci@gmail.com>
2020-11-04 11:28:12 +0100

    Assignments done

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -21,5 +23,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then((beers) => {
+    res.render('beers', {beers});
+  })
+  .catch(e => res.status(500).send("Something went wrong"));
+})
+
+app.get('/beers/:id', (req, res) => {
+  punkAPI.getBeer(req.params.id)
+  .then((beer) => {
+    res.render('random-beer', {beer});
+  })
+  .catch(e => res.status(500).send("Something went wrong"));
+})
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+  .then((beer) => {
+    res.render('random-beer', {beer});
+  })
+  .catch(e => res.status(500).send("Something went wrong"));
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
