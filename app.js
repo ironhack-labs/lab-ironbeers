c3b1f8f550753023a2ba142bbdcc12dffc39ccf7
Emad Ghnim <emad.ghnim@outlook.com>
2020-11-04 15:28:40 +0100

    done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
@@ -22,4 +22,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers',(req, res) => {
+    punkAPI.getBeers()
+    .then(beersFromApi => {
+      res.render('beers.hbs', { beersFromApi });
+      console.log('Beers from the database: ', beersFromApi);
+    })
+    .catch(err => console.error(err));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+    .then(randomBeer => {
+      res.render('random-beers.hbs', { randomBeer });
+    })
+    .catch(err => console.error(err));
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
