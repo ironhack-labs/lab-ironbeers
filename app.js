bc043aae39faa2d7b14baf9a06d8a2e079eda85b
codebroker <utuman@windowslive.com>
2020-11-06 13:12:25 +0100

    done

--- app.js
+++ app.js
@@ -7,19 +7,35 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then((beerResults) => {
+    res.render('beers', {beerResults});
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then((responseFromAPI) => {
+     res.render('random-beer', { responseFromAPI });
+    })
+  .catch(error => console.log(error))
+});
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
