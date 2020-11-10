ca2ad0fef39e9a589a5f8ce460e6f0261f66e221
Cristy Lopez Piñeiro <aclopezp93@gmail.com>
2020-11-11 00:53:22 +0100

    done

--- app.js
+++ app.js
@@ -18,8 +18,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => res.render('beers', {beers: beersFromApi}))
+  .catch(error => console.log(error))
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(responseFromAPI => res.render('random-beer', {responseFromAPI}))
+    .catch(error => console.log(error))
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
