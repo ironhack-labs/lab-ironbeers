9d94dfd6b27ce7aa3e5c6508ef29acb84b603f5b
Dirk Kelderman <dirkkelderman@gmail.com>
2020-11-10 15:00:01 +0100

    completed mandatory iterations

--- app.js
+++ app.js
@@ -22,4 +22,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    const beers = beersFromApi;
+    res.render('beers', {beers});
+    console.log('Beers from the database: ', beersFromApi)})
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(responseFromAPI => {
+    const randomBeer = responseFromAPI;
+    res.render('random-beer', {randomBeer});
+  })
+  .catch(error => console.log(error));
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
