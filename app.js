61a276b5f896e3c39cf611464cdafa3c09e672ce
TalB <tal.burdman@gmail.com>
2020-11-10 21:38:34 +0100

    done

--- app.js
+++ app.js
@@ -9,14 +9,36 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Register the location for handlebars partials here:
 
 // ...
 
 // Add the route handlers here:
+// app.get('/', (req, res) => res.render('index - page'))
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', {beerList: beersFromApi }))
+    .catch(error => console.log(error));
+   
+})
+
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => res.render('random-beers', {randomBeer: responseFromAPI}))
+     .catch(error => console.log(error));
+    
+})
+    
+  
 
 app.get('/', (req, res) => {
   res.render('index');
