fba6490969ec50e5e53f33e59b95d2e1fcaa3b46
MLuisa Santos <mls_mansell@outlook.com>
2020-11-10 21:31:26 +0100

    done

--- app.js
+++ app.js
@@ -14,12 +14,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 // Add the route handlers here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then(beersFromApi => res.render('beers',{beers: beersFromApi})).catch(error => console.log(error));
+
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom().then(beer => res.render('random-beer', {beerRandom: beer})).catch(error => console.log(error));
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
