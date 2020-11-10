692361eea74c20533be0fee403264de75aa10da2
Heyling Marquez <marquezheyling@gmail.com>
2020-11-10 22:31:44 +0100

    done

--- app.js
+++ app.js
@@ -7,46 +7,54 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-
-hbs.registerPartials(path.join(__dirname, "views/partials"))
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Register the location for handlebars partials here:
+// Register the location for handlebars partials here:
 
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
 
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+
   res.render('index');
+
 });
 
 app.get('/beers', (req, res) => {
-  punkAPI
-    .getBeers()
-    .then(beersFromApi => res.render("beers", { beers: beersFromApi }))
+
+  punkAPI.getBeers()
+    .then(beersFromApi => res.render('beers', { beers: beersFromApi }))
+    .catch(error => console.log(error));
+
+});
+
+app.get('/random-beers', (req, res) => {
+
+  punkAPI.getRandom()
+    .then(responseFromAPI => res.render('random-beers', { responseFromAPI }))
     .catch(error => console.log(error))
 
 });
 
-app.get('/random-beer', (req, res) => {
-  punkAPI
-    .getRandom()
-    .then(responseFromAPI => res.render("random-beer", { responseFromAPI }))
+
+
+// Specific beer information
+app.get('/beers/:id', (req, res) => {
+
+  const id = req.params.id
+
+  punkAPI.getBeer(id)
+    .then(uniqueBeer => res.render('beer', { uniqueBeer }))
     .catch(error => console.log(error))
 
 });
 
-//app.get('/beers/:id', (req, res) => {
-//punkAPI.getBeer(req.params.id).then(beersFromApi => {
-//res.render("beer", { oneBeer: beersFromApi });
-// })
-// .catch(error => console.log('Error to accesing this beer', error))
-//})
 
 
 
