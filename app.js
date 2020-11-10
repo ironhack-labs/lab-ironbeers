94b03dddd16991e04190433ea2ff6d8ec25779d8
Carlos Martin-Salas Larena <carlos_2n3055@hotmail.com>
2020-11-10 23:59:04 +0100

    done

--- app.js
+++ app.js
@@ -12,14 +12,47 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(`${__dirname}/views/partials`)
+
+
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+app.get('/beers', (req, res) => {
+
+  punkAPI
+  .getBeers()
+  .then((beersFromApi) => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', { beersFromApi });
+  })
+
+  .catch (error => console.log(error));
+  
+});
+
+
+app.get('/random-beer', (req, res) => {
+  
+  punkAPI
+  .getRandom()
+  .then((randomBeerFromAPI) => {
+    console.log('Beers from the database: ', randomBeerFromAPI)
+    res.render('random-beer', { randomBeerFromAPI });
+  })
+    
+  .catch(error => console.log(error));
+
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
