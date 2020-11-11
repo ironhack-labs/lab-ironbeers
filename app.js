27646be21e3ad7ac6d518e6a0c8a47e3d54123c4
Roma Dorado <doradoroma@gmail.com>
2020-11-11 01:00:40 +0100

    [WEB-MAD-PP]Jose Dorado

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
 const hbs = require('hbs');
+
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -15,11 +16,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Register the location for handlebars partials here:
 
 // ...
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (req, res) => { res.render('index'); });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers().then(beersFromApi => res.render('beers', { beer: beersFromApi })).catch(error => console.log(error))
 });
+punkAPI.getBeers().then(beersFromApi => console.log('Beers from the database: ', beersFromApi)) .catch(error => console.log(error));
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom().then(beersFromApi => res.render('random-beers', { beer: beersFromApi })).catch(error => console.log(error))
+});
+punkAPI.getBeers().then(beersFromApi => console.log('Beers from the database: ', beersFromApi)) .catch(error => console.log(error));
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
