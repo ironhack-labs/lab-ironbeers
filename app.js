0e7030d7fda6089ede605f8f8945b760500bbef5
Adrian Casas <adrian.casas.fernandez@gmail.com>
2020-11-11 13:17:18 +0000

    Done missing styles & bonus 6. Pending

--- app.js
+++ app.js
@@ -7,19 +7,39 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname + "/views/partials"));
 // ...
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
+
+app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  
+  punkAPI.getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      res.render('random-beer', {beersFromApi})
+    })
+    .catch(error => console.log(error));
+
+})  
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
