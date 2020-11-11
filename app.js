9bfc071d6f1c8bde0ad66313abb70807342cb20a
Jesus Rua <jesus.rua.pareja@gmail.com>
2020-11-11 13:17:38 +0100

    COMPLETE WITHOUT STYLING AND BONUS

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
-const punkAPI = new PunkAPIWrapper();
+const punkAPI = new PunkAPIWrapper(); 
+
+hbs.registerPartials(path.join(__dirname, "/views/partials"))
+
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -18,8 +21,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (request, response, next) => {
+  response.render('index');
 });
+
+app.get("/beers", (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    res.render("beers", {beers});
+  })
+  .catch(error => console.log(error));
+})
+
+app.get("/randomBeers", (req, res, next)=>{
+  punkAPI
+  .getRandom()
+  .then(randomBeers => {
+    res.render("randomBeers", {randomBeers})
+  })
+  .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
