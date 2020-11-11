dd1b625c6474bf765b5c4119d6dec7c6cfd6dacc
RaquelRB <raquelrobles95@gmail.com>
2020-11-11 13:18:22 +0100

    First attempt

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// const randomBeer = PunkAPI.getRandom()
+
+// randomBeer.then(beer => {
+//   alert(beer[0].name)
+// })
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -14,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, "/views/partials"))
 
 // Add the route handlers here:
 
@@ -22,4 +28,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get("/beers", (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    res.render("beers", {beers});
+  })
+  .catch(error => console.log(error));
+})
+
+app.get("/random-beer", (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeers => {
+    res.render("random-beer", {randomBeers});
+  })
+  .catch(error => console.log(error));
+})
+
+app.listen(3001, () => console.log('🏃‍ on port 3001'));
