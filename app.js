e586df4438b90a715b8351f3f20fda54307a36a9
Sergio <srosduran@gmail.com>
2020-11-11 15:03:22 +0100

    done except last bonus

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
+const chalk = require('chalk');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
@@ -14,12 +15,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-// Add the route handlers here:
+
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => console.log(error));
+
+});
+
+app.get('/random-beer', (req, res) => {
+
+  punkAPI
+  .getRandom()
+  .then(beer => {
+    res.render('random-beer', {beer});
+  })
+  .catch(error => console.log(error));
+
+});
+
+app.get('/beers/:id', (req, res) => {
+
+  punkAPI
+  .getBeer(req.params.id)
+  .then(beer => {
+    res.render('beer', {beer});
+  })
+  .catch(error => console.log(error));
+
+});
+
+app.listen(3000, () => console.log(chalk.green.inverse.bold('🏃‍ on port 3000')));
