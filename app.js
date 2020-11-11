84a70206898428fe2d3c8893a707bf5dc8ae016a
Mark <mark.sanjuan.sole@gmail.com>
2020-11-11 12:45:20 +0100

    done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,5 +22,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render('beers', { beersFromApi })})
+  .catch(error => console.log(error))
+})
+app.get('/random-beers', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(beerRandom => res.render('random-beer', { beerRandom }))
+  .catch(error => console.log(error))
+})
+app.get(`/:id`, (req, res, next) => {
+  punkAPI
+  .getBeer(req.params.id)
+  .then(beerFromApi => res.render('beer', {beerFromApi}))
+  .catch(error => console.log(error))
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
