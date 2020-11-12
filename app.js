4211fa8df9c4bb5c0c9b4456b1e559173a1b39b2
“Anna <aniawaszak@yahoo.com>
2020-11-12 21:13:59 +0100

    done

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +22,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// app.get('/beers', (req, res) => {
+//   punkAPI
+//   .getBeers()
+//   .then(beersFromApi => {
+//     res.render('beers', { beerList: beersFromApi } );
+//     // console.log('Beers from the database: ', beersFromApi)
+//   })
+//   .catch(error => console.log(error));
+// });
+
+app.get('/beers', async (req, res) => {
+  let beersFromApi = await punkAPI.getBeers()
+  res.render('beers', { beerList: beersFromApi } );
+});
+
+app.get('/beer/:id', (req, res) => {  
+  // console.log(req.params.id);  
+  // const id = req.params.id;  
+  punkAPI    
+  .getBeer(req.params.id)
+  .then(beerFromAPI => {
+    res.render('beer', { beer: beerFromAPI[0] });    
+  })    
+  .catch(err => console.log(err));
+});
+
+app.get('/random-beer', async (req, res) => {
+  let responseFromAPI = await punkAPI.getRandom()
+  res.render('random-beer', { random: responseFromAPI });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
