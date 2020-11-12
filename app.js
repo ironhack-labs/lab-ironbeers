1a684027085b003f19cac488c65296845acdd907
Kara P <quetzlcoatlus@gmail.com>
2020-11-12 15:32:48 +0100

    basic tasks complete

--- app.js
+++ app.js
@@ -14,12 +14,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(responseFromApi => {
+      res.render('beers', {allBeers: responseFromApi} )
+    })
+    .catch(error => console.log(error));
+});
+
+
+app.get('/randomBeers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeerFromApi => {
+      // console.log(randomBeerFromApi)
+      res.render('randomBeers', {randomBeer: randomBeerFromApi} )
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
