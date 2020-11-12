b6c832d43372c1bc380747931f280fb9d3c77360
Facundo Gomez <gomezvera.f@gmail.com>
2020-11-12 17:00:02 +0100

    all excercises complete. Pending final bonus for styling

--- app.js
+++ app.js
@@ -13,13 +13,36 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  
+  punkAPI
+  .getBeers()
+  .then(ApiBeers => res.render('beers', {ApiBeers}))
+  .catch(error => console.log(error));
+  
+});
+app.get('/random-beer', (req, res) => {
+  
+  punkAPI
+  .getRandom()
+  .then(ApiRandBeer => res.render('random-beer', {ApiRandBeer}))
+  .catch(error => console.log(error));
+  
+});
+app.get('/beers/:id', (req, res) => {
+  punkAPI
+  .getBeer(req.params.id)
+  .then(ApiSpecBeer => res.render('specific-beer', {ApiSpecBeer}))
+  .catch(error => console.log(error));
+  
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
