522d43e9a4501c80773a3c56a40a99143c9c0c6d
Katie Gracey <gracey.kd@gmail.com>
2020-11-12 19:29:44 +0100

    done

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials')
+
+// prints out all the beers here using the functions from the API
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -21,5 +25,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+// put async before function call
+app.get('/beers', async (req, res) => {
+  let beersFromApi = await punkAPI.getBeers()
+    res.render('beers', {apiBeers: beersFromApi});
+    console.log('Beers from the database: ', beersFromApi)
+});
+
+app.get('/random-beer', async (req, res) => {
+  let responseFromAPI = await punkAPI.getRandom()
+    res.render('random-beers', {randomBeer: responseFromAPI});
+    console.log('Random beer: ', responseFromAPI)
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
