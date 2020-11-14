976ec6f9dab7599b516744d64ef7e0ceb3566adf
“Anna <aniawaszak@yahoo.com>
2020-11-14 12:17:45 +0100

    add showing details of soecific chosen beer

--- app.js
+++ app.js
@@ -24,6 +24,35 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res) => {
+  let beersFromApi = await punkAPI.getBeers();
+  res.render('beers', { beerList: beersFromApi } );
+});
+
+// app.get('/beers/:id', async (req, res) => {
+//   const beer = await punkAPI.getBeer(req.params.id);
+//   res.render('random-beer', { oneBeer: beer })
+// })
+
+app.get('/beers/:id', (req, res) => {
+  console.log(req.params.id)
+  punkAPI.getBeer(req.params.id)
+  .then(beer => {
+    console.log('BEER', beer)
+    res.render('random-beer', { oneBeer: beer });
+  })
+  .catch(err => console.log(err));
+})
+
+app.get('/random-beer', async (req, res) => {
+  let responseFromAPI = await punkAPI.getRandom()
+  console.log(responseFromAPI) //
+  res.render('random-beer', { oneBeer: responseFromAPI });
+});
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
 // app.get('/beers', (req, res) => {
 //   punkAPI
 //   .getBeers()
@@ -34,25 +63,10 @@ app.get('/', (req, res) => {
 //   .catch(error => console.log(error));
 // });
 
-app.get('/beers', async (req, res) => {
-  let beersFromApi = await punkAPI.getBeers()
-  res.render('beers', { beerList: beersFromApi } );
-});
-
-app.get('/beer/:id', (req, res) => {  
-  // console.log(req.params.id);  
-  // const id = req.params.id;  
-  punkAPI    
-  .getBeer(req.params.id)
-  .then(beerFromAPI => {
-    res.render('beer', { beer: beerFromAPI[0] });    
-  })    
-  .catch(err => console.log(err));
-});
-
-app.get('/random-beer', async (req, res) => {
-  let responseFromAPI = await punkAPI.getRandom()
-  res.render('random-beer', { random: responseFromAPI });
-});
-
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+// app.get('/beers/:id', (req, res) => {
+//   punkAPI.find(beer => beer.id === req.params.id)
+//   console.log(req.params.id)
+//   .then(beer => {
+//     res.render('random-beer', { oneBeer: beer });
+//   })
+// })
