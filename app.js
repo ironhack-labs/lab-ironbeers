cf96097268e2e11e94c4a3943c46c7c4d2576d1d
Jan <jan.leon.voelker@gmail.com>
2020-11-15 02:15:48 +0100

    random beer works

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(__dirname + '/views/partials');
 // ...
 
 // Add the route handlers here:
@@ -22,15 +21,32 @@ app.get('/', (req, res) => {
 });
 app.get('/beers', (req, res) => {
   punkAPI
-  .getBeers()
-  .then(beersFromApi => { // beersFromApi ist der Name, den wir der ANTWORT der Api geben (die Antwort ist nun also darin gestored, ist vermutlich ein array)
-    res.render('beers', {apiBeers: beersFromApi}); // hier geben wir dem beersFromApi einen neuen Namen (apiBeers), während wir es in ein Objekt verwandeln
-    console.log('Beers from the database: ', beersFromApi)} 
-    )
-  .catch(error => console.log(error));
+    .getBeers()
+    .then(beersFromApi => { // beersFromApi ist der Name, den wir der ANTWORT der Api geben (die Antwort ist nun also darin gestored, ist vermutlich ein array)
+      res.render('beers', {
+        apiBeers: beersFromApi
+      }); // hier geben wir dem beersFromApi einen neuen Namen (apiBeers), während wir es in ein Objekt verwandeln
+      console.log('Beers from the database: ', beersFromApi)
+    })
+    .catch(error => console.log(error));
 });
 app.get('/random-beers', (req, res) => {
-  res.render('random-beers');
+  punkAPI
+    .getRandom()
+    .then(randomBeerFromApi => {
+      res.render('random-beers', {
+        randomBeerFromApi:randomBeerFromApi[0]
+        
+        //ggf hier das [0] wieder rausnehmen
+      });
+      console.log(randomBeerFromApi)
+      console.log('Beers from the database: ', randomBeerFromApi) 
+    })
+    .catch(error => console.log(error));
+
 });
+// app.get('/random-beers', (req, res) => {
+//   res.render('random-beers');
+// });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
