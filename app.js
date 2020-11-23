d489cbe2e1f1c5c0c68157e8bac4c3998abcc4a9
Santiago Contreras <santiagoc213@gmail.com>
2020-11-23 22:07:01 +0100

    done without bonuses

--- app.js
+++ app.js
@@ -13,13 +13,33 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+// hbs.registerPartials(`${__dirname}/views/partials`)
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', async (req, res) => {
+  // getBeers() returns a promise that should be resolved with an array of 25 beers.
+  try {
+    const beers = await punkAPI.getBeers()
+    console.log(beers)
+    res.render('beers', {beers})
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    const randomBeer = await punkAPI.getRandom();
+    console.log(randomBeer)
+    res.render('random-beer', randomBeer[0]);
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
