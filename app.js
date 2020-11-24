da62a87aa568b7cfa86cafa406b2468f3669ff96
Tomy <tomytubert@gmail.com>
2020-11-24 16:24:49 +0100

    Done without the last bonus 6-7

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Bonus Iteracion 5 - Beer Partial
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -22,4 +26,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers();
+  res.render('beers', {beers});
+  console.log(beers)
+});
+
+app.get('/random-beers', async (req, res) => {
+  const beers = await punkAPI.getRandom();
+  res.render('random-beer', {beers});
+  //console.log(beers)
+});
+
+//Bonus Iteracion 6
+app.get('/beers/:beerId', async (req, res) => {
+  const beers = await punkAPI.getBeer(req.params.beerId);
+  res.render('random-beer', beers[0]);
+  console.log(beers[0])
+
+});
+
+
+app.listen(8000, () => console.log('🏃‍ on port 8000'));
