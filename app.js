51534ff9132410406ce82966157af57156e1c60c
AnneVictoria <avduclaux@gmail.com>
2020-12-04 23:59:08 +0100

    done

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+// hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,6 +22,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', async (req, res) => {
+  try {
+    const beers = await punkAPI.getBeers(25);
+    res.render('beers', { beers });
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.get('/random-beers', async (req, res) => {
+  try {
+    const randomBeers = await punkAPI.getRandom();
+    res.render('random-beers', { randomBeers });
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.get('/beers/:id([0-9])', async (req, res) => {
+  try {
+    const oneBeer = await punkAPI.getBeer(req.params.id);
+    res.render('detailsBeer', { oneBeer });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
