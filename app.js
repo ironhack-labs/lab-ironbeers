da680dc7790a2006e8b246f6bf4bf755bbf3f2c7
Thomas Lamy <thomaslamy75011@gmail.com>
2020-12-04 20:00:46 +0100

    done

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Register the location for handlebars partials here:
 
@@ -20,6 +21,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', async (req, res) => {
+  try {
+    const beers = await punkAPI.getBeers();
+    res.render('beers', { beers });
+    //console.log(beers);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    const randomBeers = await punkAPI.getRandom();
+    res.render('random-beer', { randomBeers });
+    //console.log(beers);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get('/beers/beer-:id', async (req, res) => {
+  console.log(req.params);
+  try {
+    const id = await punkAPI.getBeer(req.params.id);
+    //console.log(id);
+    res.render('beerDetail', { id });
+    //console.log(beers);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
