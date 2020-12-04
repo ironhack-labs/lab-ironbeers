de4b9b5f325692f266f8b5c7d9f83157c4688320
yuuu.miao@gmail.com <yuuumiao@miyaas.home>
2020-12-04 18:00:48 +0100

    TBC styles css

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 
 // ...
 
@@ -21,5 +23,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+// #### Iteration 3.1 The `/beers` route
+app.get('/beers', async (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ') //beersFromApi);
+    res.render('beers', { beersFromApi })
+    // res.send("rendering")
+  })
+  .catch(error => console.log(error));
+
+})
+
+
+app.get('/random-beer', (req, res) => {
+  // res.send('this page is rendering');
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    // 4.1 The /random-beer route
+    console.log(responseFromAPI);
+    res.render('random-beer', {responseFromAPI})
+  })
+  .catch(error => console.log(error));
+})
+
+
+app.get('/beers/beer/:id', async (req, res) => {
+  //Bonus: Iteration 6
+ try{
+      const oneBeer = await punkAPI.getBeer(req.params.id);
+      console.log("here", oneBeer);
+      res.render('detail-beer', {oneBeer})
+} catch (err){
+  console.log("err")
+}
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
