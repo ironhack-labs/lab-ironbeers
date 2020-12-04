ca8e553f26e7e3a28ce4276139dbc62be4372e26
Victorien Provenzano <victorien.provenzano@icloud.com>
2020-12-04 16:48:30 +0100

    bonus 5

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
@@ -30,9 +30,12 @@ app.get('/beers', (req, res) => {
 });
 
 app.get('/random-beer', (req, res) => {
-  punkAPI.getRandom().then(responseFromAPI => {
-    res.render('random-beer', responseFromAPI[0]);
-  });
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', responseFromAPI[0]);
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
