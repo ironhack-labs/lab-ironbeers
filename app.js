898e1317d1f812d4e48c342384763a34323bc888
malakai <malek.fouhal@gmail.com>
2020-12-04 17:04:32 +0100

    lab done without bonus

--- app.js
+++ app.js
@@ -14,12 +14,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/beers.hbs")
 
 // Add the route handlers here:
 
+
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beersFromApi });
+      console.log(beersFromApi);
+
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', responseFromAPI[0]);
+      console.log(responseFromAPI[0]);
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
