0fb2abe1e4ea6a3056b5037c04b2fac359784ad3
hunferno <kone.rene@hotmail.fr>
2020-12-05 14:46:38 +0100

    Iteration4 Done

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 
 // Register the location for handlebars partials here:
 
@@ -26,14 +26,20 @@ app.get('/', (req, res) => {
 app.get('/beers', async (req, res) => {
   try {
     const arrOfBeers = await punkAPI.getBeers(25);
-    res.render('beers', arrOfBeers);
+    res.render('beers', { arrOfBeers });
   } catch (error) {
     console.log(error);
   }
 });
 
-app.get('/random-beers', (req, res) => {
-  res.render('random-beers');
+app.get('/random-beers', async (req, res) => {
+  try {
+    const randomOfBeers = await punkAPI.getRandom();
+    console.log(randomOfBeers);
+    res.render('random-beers', { randomOfBeers });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
