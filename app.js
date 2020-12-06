b175ab900e0a6549ae93fcd237d9dd272e6cd45a
Etouma-bela <belaetouma@gmail.com>
2020-12-06 23:30:41 +0100

    done without the bonuses

--- app.js
+++ app.js
@@ -22,4 +22,30 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+// itiration 3
+app.get('/beers', async (req, res) => {
+  // const beersFromApi = await punkAPI.getBeers();
+  // res.render('beers', { beersFromApi });
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+//Iteration 4
+app.get('/random-beers', async (req, res) => {
+  const randomResponseFromAPI = await punkAPI.getRandom();
+  res.render('random-beers', { randomResponseFromAPI });
+  // punkAPI => THIS STRUCTURE DOESN'T WORK. IT'S VERY SLOW AND EVENTUALLY FAILS. I DON'T UNDERSTAND WHY!!!!!!!!!!!!!!!
+  //   .getRandom()
+  //   .then(randomResponseFromAPI => {
+  //     console.log('Random beer here: ', randomResponseFromAPI);
+  //     res.render('random-beers', { randomRresponseFromAPI });
+  //   })
+  //   .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
