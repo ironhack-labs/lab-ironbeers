9307e2039b76cbd4705b552296468d605f08b750
hunferno <kone.rene@hotmail.fr>
2020-12-06 12:32:40 +0100

    First Bonus Done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.urlencoded());
