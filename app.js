b4bcdc018ec1a2a4ba605914565a233144784d1d
Jacqueline <j.jumaga@gmail.com>
2020-12-07 23:34:21 +0100

    express lab with practice linking hbs/js/database and displaying data on the browser

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+//creates a shortcut to public folder - index.hbs line 2 only needs ./images/beer.png
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -19,7 +20,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  // render it to where? index.hbs
+  //route handlers return a promise?
+  res.render('index'); //shows localhost:3000 (home page)
+});
+
+app.get('/beers', (req, res) => {
+  // create a beer route inside the app.js file
+  //res.render('beers'); shows localhost:3000/beers ** I can't render twice - this was uncommented which is what prevented me from showing my beers in my browser
+  punkAPI
+    .getBeers() //calling the .getBeers() method returns a PROMISE that should be resolved with an array of 25 beers
+    .then(beersFromApi => {
+      //this is my promise, I want to get my beers
+      //console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { beersFromApi }); // then I want to render them inside my beers.hbs
+    })
+    .catch(error => console.log(error)); //if it doesn't work, throw an error
+});
+
+app.get('/random-beer', (req, res) => {
+  //route handlers return a promise?
+  //res.render('random-beer'); shows localhost:3000/random-beers
+  //const randomBeer =
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', { responseFromAPI });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
