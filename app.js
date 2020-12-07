7241c90d6639f6c51802ae3f8ad54e6b1ad64dad
Kenton Cargile <49448362+KentonCargile@users.noreply.github.com>
2020-12-07 16:25:10 +0100

    done

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const express = require('express');
 
+
+const express = require('express');
 const hbs = require('hbs');
+const app = express();
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
-const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', beers[0]);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.listen(3000);
