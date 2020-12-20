0d6cfd29e6308681e2ae9efd77bc395db89dde34
Llorenç Palliser <palliser@palliser.es>
2020-12-20 19:51:55 +0100

    Iteracciones hechas y avanzando los bonus

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
--- controllers/common.controller.js
+++ controllers/common.controller.js
@@ -1,14 +1,31 @@
 
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
+
 module.exports.home = (req, res, next) => {
-    res.render('common/home');
-  }
-  
+  res.render('common/home');
+}
 
 module.exports.beers = (req, res, next) => {
-    res.render('common/beers');
-  }
+ const birras = punkAPI.getBeers()
+ birras.then(birra => {
+     res.render('common/beers', {
+      beers: birra,
+           })
+   })
+ }
 
 
 module.exports.random = (req, res, next) => {
-    res.render('common/random');
-  }
+  const randomBeer = punkAPI.getRandom()
+  randomBeer.then(beer => {
+    res.render('common/random', {
+      randomBeer: beer[0].name,
+      tagline: beer[0].tagline,
+      image: beer[0].image_url,
+      description: beer[0].description,
+      food_pairing: beer[0].food_pairing,
+      brewers_tips: beer[0].brewers_tips,
+    })
+  })
+}
