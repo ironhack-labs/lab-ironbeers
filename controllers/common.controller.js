f26bdbbd1a37d3b27951448ca6f40dfd5ad7704c
MkAlba <albacastello74@gmail.com>
2020-12-21 19:15:03 +0100

    feat: iteration 1 to 5

--- controllers/common.controller.js
+++ controllers/common.controller.js
@@ -1,11 +1,29 @@
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
+
 module.exports.index = (req, res, next) => {
-    res.render('index');
-  };
+  res.render('index');
+};
 
-module.exports.beers =  (req, res, next) => {
-    res.render('beers');
-  };
+module.exports.beers = (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', {
+        beersFromApi
+      });
+      console.log('Beers from the database: ', beersFromApi);
+    })
+    .catch(error => console.log(error));
+};
 
-  module.exports.randombeers = (req, res, next) => {
-    res.render('randombeers');
-  }; 
+module.exports.randombeers = (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('randombeers', {
+        responseFromAPI
+      });
+    })
+    .catch(error => console.log(error));
+};
