a29bd2471d1f51d6f39b6c759ef3745f0159bcbf
Dardannya Patente <dardannyapatente@hotmail.com>
2020-12-21 15:26:33 +0100

    changes

--- app.js
+++ app.js
@@ -12,14 +12,45 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+// // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (request, response) => {
+  response.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (request, response) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => response.render('beers', { beersFromApi }))
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get('/beers/:id', (req, res) => {
+  const id = req.params.id;
+  punkAPI
+    .getBeer(id)
+    .then(beerByID => {
+      console.log(beerByID)
+      res.render('beersDetailPage', { beerByID });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (request, response) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      response.render('random-beer', responseFromAPI[0]);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.listen(3000, () => console.log('Run on port 3000'));
