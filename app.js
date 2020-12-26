d14afedb7a1f1ec3889d812d2680d3184dc4704c
RFeldman84 <rachel.feldman84@gmail.com>
2020-12-25 23:43:23 -0500

    finish parts of  iterations and some styling

--- app.js
+++ app.js
@@ -14,9 +14,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/partials")
 
 // Add the route handlers here:
+app.get('/beers',(req, res)=>{
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render("beers.hbs", {beersFromApi}))
+  .catch(error => console.log(error));
+  
+});
+
+app.get('/random-beers',(req, res)=>{
+  res.render("random-beers.hbs")
+});
 
 app.get('/', (req, res) => {
   res.render('index');
