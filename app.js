a0133622fb445f20345065b8c0d0d7f9c1482cfb
Kiko Quesada <franciscoquesadacabello89@gmail.com>
2020-12-29 18:05:35 +0100

    feat: iteration 1 and 2

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
