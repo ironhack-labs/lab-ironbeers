c43b13d9298092d1a4b16957d56a0f08c9aa1f4c
Manuel Carrillo Almoguera <manucaralmo@gmail.com>
2021-01-16 15:00:50 +0100

    First commit

--- app.js
+++ app.js
@@ -10,16 +10,27 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Add the route handlers here:
-
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (req, res, next) => {
+  res.render('home');
 });
+
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
+
+app.get('/random-beer', (req, res, next) => {
+  res.render('random-beer');
+});
+
+app.use((req, res, next) => {
+  res.status(400)
+  res.send('404 ERROR')
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
