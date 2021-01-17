19bdae4dd1040802b129301ad5875ec66648a899
rgarsan <rgarsan84@gmail.com>
2021-01-17 23:53:17 +0100

    iteration2+

--- app.js
+++ app.js
@@ -21,5 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  res.render('beers');
+});
+app.get('/random-beers', (req, res) => {
+  res.render('random-beers');
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
