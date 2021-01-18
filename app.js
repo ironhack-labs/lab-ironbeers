0628dedbfc2665a5fd326c1b430630355bfd6241
rgarsan <rgarsan84@gmail.com>
2021-01-18 23:58:04 +0100

    iteration 3.2

--- app.js
+++ app.js
@@ -22,10 +22,14 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 app.get('/beers', (req, res) => {
-  res.render('beers');
+  punkAPI
+  .getBeers()
+  .then(beersFromApi =>{
+    res.render('beers', { beersFromApi});
+  })
+  .catch(error => console.log(error));
 });
-app.get('/random-beers', (req, res) => {
-  res.render('random-beers');
-});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
