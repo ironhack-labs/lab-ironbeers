4aef307f57c53fd6cbd6e9c6317be786dfbd9c88
Manuel Carrillo Almoguera <manucaralmo@gmail.com>
2021-01-18 12:40:34 +0100

    Lab done

--- app.js
+++ app.js
@@ -10,27 +10,55 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 hbs.registerPartials(__dirname + "/views/partials");
 
-// Add the route handlers here:
+
+// HOME PAGE
 app.get('/', (req, res, next) => {
-  res.render('home');
+  res.render('home', { title: 'Home' });
 });
 
+// BEER LIST
 app.get('/beers', (req, res, next) => {
-  res.render('beers');
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render('beers', { beersFromApi, title: 'Beers' })
+  })
+  .catch(error => console.log(error))
 });
 
+// RANDOM BEER
 app.get('/random-beer', (req, res, next) => {
-  res.render('random-beer');
+  punkAPI
+  .getRandom()
+  .then(randomBeer => {
+    res.render('beer', { beer: randomBeer[0], title: 'Random Beer' });
+  })
+  .catch(error => console.log(error));
 });
 
+
+// BEER BY ID
+app.get('/beer/:id', (req, res, next) => {
+  let beerId = req.params.id
+
+  punkAPI
+  .getBeer(beerId)
+  .then(Beer => {
+    res.render('beer', { beer: Beer[0], title: 'Beer' });
+  })
+  .catch(error => console.log(error));
+});
+
+
+// 404
 app.use((req, res, next) => {
   res.status(400)
-  res.send('404 ERROR')
+  res.render('error', {title: '404 Error'})
 })
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
