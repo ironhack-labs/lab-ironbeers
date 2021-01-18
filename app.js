3d3889e72a31c262497e44051a1d6f3483427c83
Thiarhes <injesusservice@hotmail.com>
2021-01-18 00:43:37 -0300

    struggling with this yet

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -13,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
@@ -22,12 +23,21 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/beers', (req, res) => {
-res.render('beers.hbs')
+app.get('/beers', (req, res, next) => {
+  const beers = punkAPI.getBeers()
+  .then((beers) => {
+    res.render('beers.hbs', {beers})
+  }) 
+.catch(error => console.log(error));
 });
 
-app.get('/random-beers', (req, res) => {
-res.render('random-beers.hbs')
+app.get('/random-beers', (req, res, next) => {
+  const randomBeers = punkAPI.getRandom()
+  .then((randomBeers) => {
+    res.render('random-beers.hbs', randomBeers[0]);
+    console.log(randomBeers[0]);
+  })
+  .catch(error => console.log(error));
 })
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
