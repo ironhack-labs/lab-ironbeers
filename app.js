7a20043ffbadeaa0fda81918e04d15a0476f0470
Ángela Herrador <angelaherrador1991@gmail.com>
2021-01-19 20:44:20 +0100

    Iteration 4 done

--- app.js
+++ app.js
@@ -29,5 +29,12 @@ app.get('/beers', (req, res) => {
     .catch(error => console.log(error))
 });
 
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi => res.render('random-beers', { randomBeer: responseFromApi } ))
+    .catch(error => console.log(error))
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
