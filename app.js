abe8e1612b2cc92ea4dfbc76ba395dcdc387aa42
cisnerosjj <jorgecisnerosgavin@gmail.com>
2021-01-20 12:24:29 +0100

    Done up it 4

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
 const express = require('express');
+const app = express();
 
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
-const app = express();
-const punkAPI = new PunkAPIWrapper();
-
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
+//Create static server for files, like css, imgs, and js
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+// Configure handlebars
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-// ...
+// Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + '/views/partials');
+
+const punkAPI = new PunkAPIWrapper();
+
+// ENDPOINTS
 
 // Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// ITERATION 3.1--------------------------------------------------------------------
+
 app.get('/beers', (req, res) => {
-  punkAPI
-    .getBeers()
-    .then(beersFromApi => res.render('beers', {beersFromApi}))
-    .catch(error => console.log(error));
+ punkAPI.getBeers()
+  .then(beersFromApi => {
+/*     console.log('Beers from the database: ', beersFromApi)
+ */    res.render('beers.hbs', {beers: beersFromApi})
+  }) 
+  .catch(error => console.log(error));
+
 });
+
+// ITERATION 4.1-------------------------------------------------------------------
+
+app.get('/randombeer', (req, res) => {
+  // your magic happens here
+  punkAPI.getRandom()
+    .then(responseFromAPI => {
+      /* console.log('Beers from the database: ', responseFromAPI) */
+      res.render('randombeer', {randomBeer: responseFromAPI})
+    }) 
+    .catch(error => console.log(error));
+  
+}),
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
