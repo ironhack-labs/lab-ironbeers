58ea8b0a293eeaec05b169d089cfe08244a2a077
Discordante <diegocerratos@gmail.com>
2021-01-20 21:29:49 +0100

    Add bonus 5

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+
 const path = require('path');
 const app = express();
 
@@ -14,14 +15,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 // Add the route handlers here:
 
+//index
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+//beers
 app.get('/beers', (req, res) => {
   punkAPI
   .getBeers()
@@ -32,6 +35,7 @@ app.get('/beers', (req, res) => {
   .catch(error => console.log('error'));
 });
 
+//random-beers
 app.get('/random-beers', (req, res) => {
   punkAPI
   .getRandom()
@@ -40,7 +44,6 @@ app.get('/random-beers', (req, res) => {
     res.render('random-beer', {cervecita: responseFromAPI});
   })
   .catch(error => console.log('error'));
-
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
