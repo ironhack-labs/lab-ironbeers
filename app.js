685bf45d4b4984c50a2320d9a4f6b39bdb2287a9
fmuniente <frd.muniente@gmail.com>
2021-02-02 19:11:59 +0100

    done

--- app.js
+++ app.js
@@ -13,13 +13,55 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      const data = {
+        beers: beersFromApi
+      };
+
+      res.render('beers', data);
+    })
+    .then()
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      const data = {
+        beer: beersFromApi
+      };
+
+      res.render('random-beers', data);
+    })
+    .then()
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/beer-:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then(beersFromApi => {
+      const data = {
+        beer: beersFromApi
+      };
+
+      res.render('selected-beer', data);
+    })
+    .then()
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
