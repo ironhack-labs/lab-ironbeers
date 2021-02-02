f65da049ad1977d14572f37e8e35d3e0acd2f6d5
Gaston <gastonsalimbene@gmail.com>
2021-02-02 23:36:25 +0100

    lab-ironbeers

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -22,4 +23,48 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    const data = await punkAPI.getBeers();
+    
+    res.render('beers'/beers.hbs, {data: beersFromApi}
+  )
+.catch(error => console.log(error));
+  
+});
+
+app.get('/random-beers', async (req, res) => {
+  punkAPI
+  .getRandom()
+  .then((responseFromAPI) => {
+   const randomBeer = await punkAPI.getBeers();
+     res.render('beers/random-beer.hbs', {beers : responseFromAPI});
+  })
+   .catch(error => console.log(error));
+});
+
+  
+})
+
+
+app.get('/beers/:beerId', (req, res) => {
+punkAPI
+.getBeer(req.params.beerId)
+.then((responseFromAPI) => {
+const responseFromAPI = await punkAPI.getBeer(req.params.beerId);
+res.render('/beers/beer-details.hbs', {beers: responseFromAPI});
+}) 
+.catch(error => console.log(error));
+
+}
+);
+
+
+
+
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
