db9badd1e555286c6f529ec39ca6abe27bf36ca8
Sotiria Dialeti <soti.dialeti@gmail.com>
2021-02-02 21:49:39 +0100

    Pending css to be finished

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
@@ -22,4 +22,48 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      const beerList = {
+        beers: beersFromApi
+      };
+      res.render(`beers`, beerList);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi => {
+      const randomB = {
+        random: responseFromApi
+      };
+
+      res.render(`random-beers`, randomB);
+    })
+
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get('beers/beer-:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then((beerFromApi) => {
+      const selectedBeer = {
+        beer: beerFromApi
+      };
+      res.render(`selected`, selectedBeer);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.listen(4000, () => console.log('🏃‍ on port 4000'));
