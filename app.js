d039b2ee294667b7b99a1288e818a52401b8902b
Rob TC <roberto.taboada@gmail.com>
2021-02-03 22:29:14 -0600

    hasta la iteración 4.2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//1.  IMPORTACIONES
 const express = require('express');
 
 const hbs = require('hbs');
@@ -7,6 +8,9 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+// 2. MIDDLEWARES
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -15,11 +19,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Register the location for handlebars partials here:
 
 // ...
-
+// 3. RUTAS
 // Add the route handlers here:
+
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+
+  const beers = async()=>{
+    const listaCerv = await punkAPI.getBeers()
+    res.render('beers', listaCerv);
+
+  }
+
+  beers();
+
+});
+
+
+app.get('/random-beers', (req, res) => {
+  const randBeers = async()=>{
+    const RandBeer = await punkAPI.getRandom();
+    res.render('random-beers',RandBeer);
+    console.log(RandBeer)
+
+  }
+  randBeers();
+});
+
+//4. LEVANTAMIENTO DEL SERVIDOR
+app.listen(3003, () => console.log('🏃‍ on port 3000'));
