5fcebe65fc80828cdf488742d56342681439e9d8
Mafe Bracho <mafe.bracho@gmail.com>
2021-02-05 16:35:12 +0100

    partially done but without styling

--- app.js
+++ app.js
@@ -14,12 +14,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
+  
 });
+
+app.get('/beers', function (req, res) {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    //console.log(beersFromApi)
+    res.render('beers', {beers: beersFromApi})
+  })
+  .catch(error => console.log(error));
+   
+})
+
+app.get('/random-beers', function (req, res) {
+  punkAPI
+  .getRandom()
+  .then(randomBeer => {
+    console.log(`This is the random beer:`, randomBeer)
+    res.render('random-beers', {beers: randomBeer})
+   //res.render('random-beers')
+  })
+  .catch(error => console.log(error));
+   
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
