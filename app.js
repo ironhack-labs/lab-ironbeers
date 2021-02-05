601f835d176ae2ce9f4072f4c389a77304935399
lilanika <68299182+lilanika@users.noreply.github.com>
2021-02-05 11:01:41 +0100

    lab from yesterday

--- app.js
+++ app.js
@@ -14,12 +14,57 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+// this is needed to be able to use partials
+const handlebars = require('hbs');
+handlebars.registerPartials(__dirname + '/views/partials');
+
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index' ,{ 
+    content: 'This is some content' });
 });
+
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', {beers: beersFromApi});  
+  })
+  .catch(error => console.log(error));
+});
+
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', {beers: beersFromApi});  
+  })
+  .catch(error => console.log(error));
+});
+//whenever a promise happens, two things can are possible
+//one: is successfully ==> then()
+//two: it fails ==> catch()
+
+// const beer1 = punkAPI.getBeer(1)
+
+
+
+
+app.get('/beers/:id', (req, res) => {
+  const godfather = beer.find(function (movie) {
+      return beer.id;
+  })
+  res.render('movieDetails', { clickedMovie: godfather })
+})
+
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
