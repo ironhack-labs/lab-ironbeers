6d2fd5c9164cf572cafaf39377380a1429a07b36
Amalia Zebi <amalia.zebi@b4finance.com>
2021-03-01 18:34:03 +0100

    All iterations plus bonuses and a bit of CSS. Iteration 6 doesnt work completely

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
@@ -21,5 +21,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then((beersFromApi) => {
+      res.render("beers.hbs", {
+        beerList: beersFromApi,
+      });
+    })
+    .catch((dbError) => {
+      res.send(dbError);
+    });
+  });
+
+
+  app.get('/random-beers', (req, res) => {
+    punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer.hbs', {randomBeer:responseFromAPI });
+  })
+  .catch(error => console.log(error));
+  });
+
+
+  app.get('/beers/:id', (req, res) => {
+    punkAPI.getBeers(req.params.id)
+      .then((beer) => {
+        console.log(beer)
+        res.render("beer-details.hbs", { beerSelected:beer });
+      })
+      .catch((dbError) => {
+        res.send(dbError);
+      });
+    });
+  
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
