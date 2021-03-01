a0340eafc6227abec593950e4e3f784fea69f32a
Juan Falconi <juanfalconi86@gmail.com>
2021-03-01 18:12:17 +0100

    mandatory iterations done

--- app.js
+++ app.js
@@ -22,4 +22,27 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+//beers route
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers.hbs', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+//random Beer Route
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      console.log(responseFromAPI)
+      res.render('random-beer.hbs', {randomBeer: responseFromAPI})
+    })
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
