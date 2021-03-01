f6a919557542b2382518143eaea8b4350c887b01
Florian Jam <jam.florian@gmail.com>
2021-03-01 16:52:54 +0100

    done

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 
 app.get('/beers', (req, res, next) => {
   punkAPI.getBeers()
-  .then((arrayOfBeers) => res.render("beers", {arrayOfBeers}))
+  .then((beers) => res.render("beers", {beers}))
 });
 
 app.get('/random-beer', (req, res, next) => {
@@ -38,6 +38,15 @@ app.get('/random-beer', (req, res, next) => {
     })
   })
   .catch((err) => next(err));
+});
+
+app.get('/beers/beer-:id', (req, res, next) => {
+  punkAPI.getBeer(req.params.id)
+  .then((beer) => {
+    const oneBeer = beer[0];
+    res.render("onebeer", {oneBeer})
+  })
+  .catch((err) => next(err))
 });
 
 
