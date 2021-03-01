4601e87698525d729cb2ab80e794be965cbff954
Romain B <romain.bnd@gmail.com>
2021-03-01 18:12:28 +0100

    did all iterations + bonus

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,5 +22,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', async (req, res) => {
+  try {
+    const beersArr = await punkAPI.getBeers()
+    res.render('beers', {beersArr});
+  }
+  catch (err) {
+    console.log(err)
+  }
+});
+
+app.get('/random-beers', async (req, res) => {
+  try {
+    const [ beerRand ] = await punkAPI.getRandom();
+    res.render('randomBeers', {beerRand});
+  }
+  catch (err) {
+    console.log(err)
+  }
+});
+
+app.get('/beers/:id', async (req, res) => {
+  try {
+    const [ beer ] = await punkAPI.getBeer(req.params.id);
+    res.render('beer', {beer});
+
+  }
+  catch (err) {
+    console.log(err)
+  }
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
