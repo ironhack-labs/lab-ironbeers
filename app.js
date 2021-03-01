1f8b6a7833c33f726b6f58ccf33309603a038ddc
Magali <fort_magali@hotmail.fr>
2021-03-01 18:20:04 +0100

    almost done, need to improve the css

--- app.js
+++ app.js
@@ -14,12 +14,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get("/beers", (req, res) => {
+  punkAPI.getBeers() // Returns an array with all beers 
+  .then(apiBeers => 
+    res.render("beers", {apiBeers}))
+  .catch(err => console.log(err))
+})
+
+app.get("/random-beers", (req, res) => {
+  punkAPI.getRandom() // Returns an array of length 1 with a random beer
+  .then(randomApiBeer => 
+    res.render("random-beer", {randomApiBeer}))
+  .catch(err => console.log(err))
+})
+
+app.get("/beers/beer-:id", (req, res) => {
+  punkAPI.getBeer(req.params.id)
+  .then(beer => {
+    res.render("beer-detail", {beer})
+  })
+  .catch(err=>console.log(err))
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
