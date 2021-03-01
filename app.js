4a728dc84164889dcea24bbace3de6bb5812a7b8
Mathieu Jacob <mathieu.jacob@hotmail.fr>
2021-03-01 16:32:36 +0100

    done without bonus yet

--- app.js
+++ app.js
@@ -22,4 +22,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render("beers", {binchettes: beersFromApi});
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(beersFromApi => {
+    res.render("random-beers", {randombinch: beersFromApi});
+    console.log(beersFromApi);
+  })
+  .catch(error => console.log(error));
+});
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
