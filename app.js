15762d4ba52f06ae2a6fbbe9d616b3c9eeae6898
benpin31 <benpin31@gmail.com>
2021-03-01 16:35:56 +0100

    done, no css but i prefere to work on atistify workshop

--- app.js
+++ app.js
@@ -13,13 +13,44 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', async (req, res) => {
+  try {
+    const beers = await punkAPI.getBeers() ;
+    // res.json(beers)
+    res.render("beers.hbs", {beers})
+  } catch(err) {
+    next(err)
+  }
+})
+
+app.get('/beers/:id', async (req, res) => {
+  try {
+    const [ beer ] = await punkAPI.getBeer(req.params.id) ;
+    res.render("random-beer.hbs", {beer})
+    // I should node call random-beer.hbs, not very clean. and do a partial. But I prefere to work on the artsitify workshop !
+
+  } catch(err) {
+    next(err)
+  }
+})
+
+
+app.get('/random-beers', async(req, res) => {
+  try {
+    const [ beer ] = await punkAPI.getRandom() ;
+    res.render("random-beer.hbs", {beer})
+  } catch(err) {
+    next(err)
+  }
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
