f657c158dcf43aa6f4e260419b4ce6b18fab3134
maudfabrello <ma.fabrello@gmail.com>
2021-03-01 18:26:46 +0100

    maudfabrello ironbeers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { Z_BEST_COMPRESSION } = require('zlib');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -13,7 +14,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
 // ...
 
 // Add the route handlers here:
@@ -22,4 +22,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render("beers", {beersFromApi});
+  })
+  .catch(error => console.log(error));
+});
+
+  app.get('/random-beers', (req, res, next) => {
+    punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+        res.render("random-beers", {randombeers: beersFromApi});
+        console.log(beersFromApi);
+      })
+      .catch(error => console.log(error));
+    });
+
+
+app.listen(3000, () => console.log('🏃‍ http://localhost:3000'));
