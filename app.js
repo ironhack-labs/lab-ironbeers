11af34b6030416dd1aabae24bd9b385eb460e7fa
Anna Lefour <anna.lefour@laposte.net>
2021-03-01 18:06:32 +0100

    Done without bonuses

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+// const router = express.Router();
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -20,6 +21,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+//Beers
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersArray => {
+      // console.log(beersArray);
+      res.render('beers.hbs', { listOfBeers: beersArray });
+      console.log('Beers imported!');
+    })
+    .catch(err => console.log(err));
+});
+
+//Random beer
+
+app.get('/random-beer', (req, res) => {
+  const randomBeer = punkAPI.getRandom();
+
+  randomBeer
+    .then(randBeer => {
+      res.render('random-beer.hbs', {beer: randBeer});
+      console.log(randBeer);
+    })
+    .catch(err => console.log(err));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
