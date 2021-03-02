b7f64d9e33723f1cea7cc266c84c14c0531d5a9f
Matthieu Cheynut <matthieucheynut@hotmail.com>
2021-03-02 22:31:28 +0100

    all iterations without css bonuses

--- app.js
+++ app.js
@@ -22,4 +22,27 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  
+  punkAPI.getBeers()
+  .then((beersFromApi) => {
+    // console.log('Beers from the database: ',beersFromApi)
+    res.render("beers",  {beers: beersFromApi} )
+})
+  .catch(error => console.log(error));
+  
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beer => {
+    console.log(beer)
+    res.render("randomBeer", {beer})
+    // res.render("randomBeer",  {randomBeer: RandomBeerFromApi} )
+  // .catch(error => console.log(error));
+})
+.catch(error => console.log(error));
+})
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
