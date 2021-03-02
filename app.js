15a822ae4df0078746e731cbf1124ac9e928f1ed
adri <ad.mallie@gmail.com>
2021-03-02 08:54:01 +0100

    beer commit

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
@@ -21,5 +22,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then((beersFromApi) => {
+    res.render('beers', {ipaDB : beersFromApi});
+  console.log(beersFromApi);
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    // your magic happens here
+    res.render('random-beer', {randomIpa : responseFromAPI});
+    console.log(responseFromAPI);
+  })
+  .catch(error => console.log(error));
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
