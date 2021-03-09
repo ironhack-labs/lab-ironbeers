6c25f939f8e5514b449db4c456a9d8954b27f327
Luca Janssens <lucajanssens@gmail.com>
2021-03-09 18:31:08 +0100

    Without bonuses.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+const randomBeer = punkAPI.getRandom();
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +22,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    // success
+    .then(dataBack => {
+      console.log('dataBack:', dataBack);
+      res.render('beers', { beers: dataBack });
+    })
+    // failure state
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    // success
+    .then(randomBeer => {
+      res.render('random-beer', { randomBeer: randomBeer });
+    })
+    // failure state
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
