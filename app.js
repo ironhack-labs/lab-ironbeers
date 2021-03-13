3ef24ed72f478b03b84a0fd1d0869a63401baf30
cintiathieme <cintiathieme@gmail.com>
2021-03-13 12:10:33 -0300

    interations 1-4

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -14,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
@@ -22,4 +23,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+app.get('/beers', (req,res) =>{
+  punkAPI.getBeers().then((allBeers) => {
+    res.render('beers', {beers: allBeers});
+  })
+  .catch (error => {
+    console.log(error);
+  })
+})
+
+app.get('/random-beer', (req,res) =>{
+    punkAPI.getRandom().then((beer) =>{
+      res.render('random-beer', {beer: beer});
+    })
+    .catch (error => {
+      console.log(error);
+    })
+})
+
+
+app.listen(3400, () => console.log('🏃‍ on port 3400'));
