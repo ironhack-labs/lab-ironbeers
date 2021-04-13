b64abcd39d37bbd2363b22dee3245d01842e3c1d
Anthony Guido <guido.anthony@orange.fr>
2021-04-13 19:36:29 +0200

    Exercise done

--- app.js
+++ app.js
@@ -12,14 +12,46 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
 
-// ...
-
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
-});
+})
+
+app.get('/beers', (req, res) => {
+
+ punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    // console.log('Beers from the database: ', beersFromApi) test
+    res.render('beers-page', { beers: beersFromApi })
+  })
+  .catch(error => console.log(error));
+  
+})
+
+app.get('/beers/:id', (req, res) => {
+
+ punkAPI
+  .getBeer(req.params.id)
+  .then(individualBeer => {
+    console.log("selecting an individual beer: ", individualBeer)
+    res.render('individual-beer', { beer: individualBeer})
+  })
+  .catch(error => console.log(error));
+  
+})
+
+app.get('/random-beer', (req, res) => {
+
+  punkAPI
+  .getRandom()
+  .then(beerFromApi => {
+    // console.log('Random beer from the database: ', beerFromApi) test
+    res.render('random-page', { beer: beerFromApi })
+  })
+  .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
