14039ea9d751064e81e4a15d106ee72c40ff019a
Lorgio Roda <lorgioroda@Lorgios-MacBook-Air.local>
2021-04-13 09:52:07 -0400

    1,2,3 Done♿️

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Module */
 const express = require('express');
 
 const hbs = require('hbs');
@@ -7,19 +8,33 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+/* Middleware */
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* Code */
 // Register the location for handlebars partials here:
-
-// ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beer',(req,res)=> {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beer', beersFromApi)
+  })
+  .catch(error => console.log(error));
+})
+app.get('/random',(req,res)=> {
+  res.render('random-beer')
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
