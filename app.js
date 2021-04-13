a1555f574f08531c4a557fe431ff61e50d69ad55
Thornnk <Thornnk1@gmail.com>
2021-04-13 21:23:07 +0200

    Tried bonus Iteration 6

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
@@ -23,9 +22,8 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+const beersArray = punkAPI.getBeers()
 app.get('/beers', (req, res) => {
-  const beersArray = punkAPI.getBeers()
-
   beersArray.then((beersArray) => {
     console.log(chalk.green(`${beersArray.length} beers received from the database`))
     res.render('beers', {beersArray});
@@ -44,6 +42,13 @@ app.get('/random-beer', (req, res) => {
   randomBeer.catch((error) => {
     console.log(chalk.red('Error: ', error))
   })
+})
+
+app.get('/beers/beer-:id', (req, res) => {
+  const id = req.params.beer
+  console.log('id: ', id)
+  punkAPI.getBeer(id)
+  
 })
 
 app.listen(3002, () => console.log('🏃‍ on port 3002'));
