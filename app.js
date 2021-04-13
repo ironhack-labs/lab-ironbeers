1ce56a29881f7b1ed1960c765cc44422ddbd8ce9
Tigran Harutyunyan <tigrandeboris@outlook.com>
2021-04-13 23:24:59 +0200

    ADD API and some CSS design

--- app.js
+++ app.js
@@ -14,7 +14,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Roots_________________________________________________________________
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {h1: 'hola'});
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then((beersFromApi) => {
+    console.log(beersFromApi);
+    res.render('beers', {beers: beersFromApi})
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log(responseFromAPI);
+    res.render('random-beer', {randomBeer: responseFromAPI[0]});
+  })
+  .catch(error => console.log(error));
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
