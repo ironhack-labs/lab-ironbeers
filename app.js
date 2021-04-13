783b068e57f0e6e4e8fb30cca05e589601da0fc6
Pablo del Barrio <pablodbb@gmail.com>
2021-04-13 16:42:48 +0200

    Iteration 4 done

--- app.js
+++ app.js
@@ -30,7 +30,10 @@ app.get('/beers', (req, res) => {
 });
 
 app.get('/random-beer', (req, res) => {
-  res.render('random-beer');
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => res.render('random-beer', { responseFromAPI }))
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
