82e2a5c4f9c9c697c3803e4f02dd09ec0ac20fb5
MrHose <josemiguel.uk@gmail.com>
2021-04-13 21:04:41 +0200

    Completed without bonuses

--- app.js
+++ app.js
@@ -22,4 +22,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', { allbeers: beersFromApi }))
+  .catch(error => res.render(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(beersFromApi => res.render('random-beer', { randombeer: beersFromApi }))
+  .catch(error => res.render(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+console.log('Servidor levantado');
+
