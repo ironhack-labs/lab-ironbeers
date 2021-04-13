590bfc724f36b14313dfb301a5efc72240bc5371
Yerai Pinedo <yeraipinedog@gmail.com>
2021-04-13 19:53:17 +0200

    done

--- app.js
+++ app.js
@@ -13,13 +13,43 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+
+  res.render('index'); //res.send('SOY EL INDEX')
 });
+
+app.get('/beers', (req, res) => {
+
+  const showBeers = punkAPI.getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers-page', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+
+
+
+
+});
+
+app.get('/random-beers', (req, res) => {
+
+  const showRandomBeer = punkAPI.getRandom()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('random-beers-page', { randomBeer: beersFromApi });
+    })
+    .catch(error => console.log(error));
+
+});
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
