2156aea23a62284af42de843f0c18cabff92eabc
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-04-13 18:02:38 +0200

    Layour barebones and home page done / TODO: navigation btns and navbar

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
+
+//require: cargo los modulos que preciso para realizar la activación
 const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+//realizando la activación de modulos (asignandolos a variables) y generando un servidor
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+//configurando el servidor utilizando los parámetros que da Express 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+//middleware: configura el sitio desde donde se cargarán los archivos
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+// ------ HASTA AQUI ES SETUP - NO HAY NADA QUE REPASAR, SE HACE SOLO UNA VEZ POR COMIENZO DE PROYECTO ------ //
 
-// Add the route handlers here:
-
+// Sgte: Build routes (connect URL to an HTML file, for render)
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  res.render('beers');
+});
+app.get('/random-beers', (req, res) => {
+  res.render('random-beers');
+});
 
+//conecta el servidor con el puerto 3000 (para acceder a cualquier puerto el comando "Localhost");
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
