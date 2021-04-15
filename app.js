1034900960e03f7231ea06966834564f973f4a94
helenetrommelen <helenetrommelen@gmail.com>
2021-04-15 16:03:18 +0200

    Hélène Trommelen & Andy McGinn: completed iteration 1-5 and added styling for home and beers page

--- app.js
+++ app.js
@@ -14,12 +14,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 // Add the route handlers here:
+
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    //console.log('Beers from the database: ', beersFromApi);
+    res.render('beers', {beersFromApi});
+  })
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeer => { 
+    console.log(randomBeer);
+    res.render('random-beer', {randomBeer}); //question: could we do randomBeer[0] to directly access the object?
+  })
+})
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
