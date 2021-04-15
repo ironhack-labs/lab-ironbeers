d53d1b0aa82e4faff0ab7b38deb4f01f070c2080
David Bueno <davidbuenox8@gmail.com>
2021-04-15 15:38:54 +0200

    done but no bonuses nor css

--- app.js
+++ app.js
@@ -22,4 +22,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { beersFromApi })
+    })
+    .catch(error => console.log(error));
+
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      console.log('Random beer from the database: ', randomBeer);
+      res.render('randombeer', { randomBeer })
+    })
+    .catch(error => console.log(error));
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
