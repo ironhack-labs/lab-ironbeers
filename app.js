6bee881c48084bcf824b4553bd33e169ada8d8d7
Chiara Wilden <chiarawilden@gmail.com>
2021-04-15 17:48:27 -0400

    Incomplete Ironbeers Assignment

--- app.js
+++ app.js
@@ -9,17 +9,26 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials("views/partials");
+
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.get('/', (req, res) => res.render("index", {title: "Home"}));
+
+// Unsure How to Properly Execute 
+// app.get('/beers', (req, res, next) => {
+//   const beers = punkAPI.getBeers().then(beersFromApi => {
+//     res.render("beers", {title: "Beers"});
+//     console.log('Beers from the database: ', beersFromApi);
+//     }).catch(error => console.log(error));
+// })
+
+app.get('/beers', (req, res, next) => res.render("beers", {title: "Beers"}));
+app.get('/random-beer', (req, res, next) => res.render("random-beer", {title: "Random Beer"}));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
