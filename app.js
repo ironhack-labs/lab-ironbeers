93897724c152ed20e2bdb30370f820dd6a3a10a2
Pietro Ballarin <pietroballarin@outlook.com>
2021-04-15 15:57:11 +0200

    done without CSS

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(__dirname + '/views/partials')
 // ...
 
 // Add the route handlers here:
@@ -21,5 +21,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then(beers => res.render('beers', {beerList: beers}))
+  .catch(error => console.log(error)); 
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom().then(beer => res.render('random-beer', {randomBeer: beer}))
+  .catch(error => console.log(error)); 
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
