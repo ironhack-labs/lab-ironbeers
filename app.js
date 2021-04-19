f9c8370ec62cf5ee78a7c278e23e27f463bad22a
antonicasasayasg <antoni.casasayas1@gmail.com>
2021-04-19 09:03:27 +0200

    done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,5 +22,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi =>
+      res.render("beers", {beersFromApi})
+    )
+    .catch(error => console.log(error));
+  
+});
+app.get('/random-beer', (req, res) => {
+  
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+     res.render('random-beer', {responseFromAPI});
+    })
+    .catch(error => console.log(error));
+});
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
