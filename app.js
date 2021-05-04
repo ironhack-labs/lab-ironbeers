d42aa2e6a413b8c9d8dfc73479d2a7c7d132dfce
Sophie Krag <sophie.9@live.nl>
2021-05-04 20:18:06 +0200

    done

--- app.js
+++ app.js
@@ -14,12 +14,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials')
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+//async await
+app.get('/beers', async (req, res) => {
+  try {
+    const beersFromAPI = await punkAPI.getBeers()
+    res.render('beers', {
+      beersFromAPI
+    })
+  } catch (err) {
+    console.log(err)
+  }
+});
+
+//promise
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(responseFromAPI => {
+      return res.render('random-beer', {
+        responseFromAPI
+      })
+    })
+    .catch(error => console.log(error))
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
