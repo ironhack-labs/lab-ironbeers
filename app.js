96f5fb53a4821bf303b8f6430e11f7cd5e39c259
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2021-05-10 21:53:01 +0200

    added the ironbeer lab

--- app.js
+++ app.js
@@ -14,12 +14,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(dbResult => {
+      console.log(dbResult);
+      res.render('partials/beers.hbs', { beer: dbResult });
+    })
+    .catch(next);
+});
+
+app.get('/random-Beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('partials/random-Beers.hbs', { randomBeer: responseFromAPI });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
