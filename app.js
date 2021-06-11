bed067c708f397929b61dd25fba0d0d00093c241
Lucia Sanchez <luci.sanchez.ortiz@gmail.com>
2021-06-11 10:30:14 +0200

    lab-done

--- app.js
+++ app.js
@@ -17,9 +17,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ...
 
 // Add the route handlers here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    // console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', {beers:beersFromApi})
+  })
+  .catch(error => console.log(error));
+  
+});
+
+app.get('/randomBeer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log('Beers from the data base: ', responseFromAPI)
+    res.render('randomBeer', {randomBeer:responseFromAPI});
+  })
+  .catch(error => console.log(error));
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
