c992c7588d3d03d375142d3e0147d6ba73e2da7d
Marcos Salvo <marcossalvo95@gmail.com>
2021-06-11 15:36:22 +0200

    Done till iteration 5 (included)

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
+
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -20,6 +23,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then((beersFromAPI) => {
+    res.render('beers', {beersFromAPI})
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeerFromAPI => {
+    const randomBeer = randomBeerFromAPI[0];
+    res.render('random-beer', randomBeer)
+  })
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
