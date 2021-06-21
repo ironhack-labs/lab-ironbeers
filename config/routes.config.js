aaa5b0718e82301c6d3e2f16ce5eb6f6898a9d85
Manuela Ghiulai <gmanuelaromina@gmail.com>
2021-06-21 17:25:20 +0200

    lab done

---
+++ config/routes.config.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const common = require('../controllers/common.controller');
+const beers = require('../controllers/beers.controller');
+
+router.get('/', common.home);
+router.get('/beers', beers.list);
+router.get('/beers/rand', beers.random);
+router.get('/beers/:id', beers.detail);
+
+module.exports = router;
