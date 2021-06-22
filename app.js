9d05a11c4c16796512074b70b3cebda64361991b
Enrique VOZA <enriquevozmediano@yahoo.es>
2021-06-22 22:46:13 +0200

    WDFT0521 ENRIQUEVOZMEDIANO

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(`${__dirname}/views/partials`)
+
 
 // Register the location for handlebars partials here:
 
@@ -20,6 +22,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(responseFromAPI => {
+      
+      res.render('random-beer', responseFromAPI[0])
+    })
+
+    .catch(error => console.log(error));
+
+
+
+
+
+
+});
+
+//endpoint
+app.get('/beers', (req, res) => {
+
+
+  //promesa
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      const beer = { beers: beersFromApi }
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', beer)
+    })
+    .catch(error => console.log(error))
+
+
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
