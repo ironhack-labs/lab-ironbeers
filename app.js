47afb34ee2e2c56ff9705893f92897769cd5a448
Luca TC <lucatuveri@outlook.it>
2021-06-22 20:42:04 +0200

    4.2

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/beers', (req, res) => {
 app.get('/random-beer', (req, res) => {
   punkAPI
     .getRandom()
-    .then(beerFromApi => console.log('random-beer',  beerFromApi[0]))
+    .then(beerFromApi => res.render('random-beer', beerFromApi[0]))
     .catch(error => console.log(error));
 });
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
