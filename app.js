a97b36a434c0fb87a2b2669b17546c69be812bdb
Silviu Dilimot <silviudilimot@gmail.com>
2021-06-22 17:27:46 +0200

    main tasks checked. To Do: bonuses and pagination

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -19,7 +20,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+  // res.send('Test')
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  
+  punkAPI
+    .getBeers()
+    .then( beersFromApi => res.render('beers', {beersFromApi}))
+    .catch(error => console.log(error))
+});
+
+app.get('/random-beer', (req, res) => {
+  // res.send(`Random beer :)`)
+  punkAPI
+  .getRandom()
+  // .then( beerFromApi => console.log(beerFromApi[0].food_pairing))
+  .then( beerFromApi => res.render('random-beer', beerFromApi[0]))
+  .catch( err => console.log(err) )
+
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+
+// *************** HOW DO I GET A BEER? ***********************
+// const beerById = punkAPI.getBeer(:id)
+// const beerByOptions = punkAPI.getBeers(:options)
+// const randomBeer = punkAPI.getRandom()
+// *************** ALL BEERS WITH ABV GREATER THAN 8 **********
+// const strongBeers = punkAPI.getBeers({'abv_gt': 8})
+
+// *************** WHAT IS A FUZZY MATCH? *********************
+// SEE punkAPI.getBeers({'food': algunRegex?})
