a398bf7bcfc93d5e70f9abebbaa2765a954f0047
annasun919 <yuval_sun@yahoo.com.hk>
2021-06-23 20:05:19 +0200

    iteration 3

--- app.js
+++ app.js
@@ -19,7 +19,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
+
+app.get('/beers', (req, res) => {
+ 
+  
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers.hbs', {beersFromApi :beersFromApi} ))
+  
+  .catch(error => console.log(error));
+
+  
+})
+
+
+
+app.get('/random-beers', (req, res) => {
+  res.render('random-beers.hbs');
+
+  
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
