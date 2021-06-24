399f28457169b56dc59e4e6839e10e329618b61c
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-06-24 21:13:33 +0200

    done until 7

--- app.js
+++ app.js
@@ -8,37 +8,21 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 
-//this is a bad idea as it calls the info when the website sets up, not when the client requests it.
-//let getAllBeers;
-//punkAPI.getBeers().then((allData) => getAllBeers = allData); 
-
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-//presetting the dir address for assets used by the hbs files - so then you can simply write it this way:
-//<img src="images/beer.png" alt="beer picture">
-//no need to write ../public/ before the images/beer.png , this prewrites it 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Register the location for handlebars partials here:
 hbs.registerPartials(path.join(__dirname , "/views/partials")) 
 
 
 
-
-// Add the route handlers here:
+//HANDLERS
 
 app.get('/', (req, res) => {res.render('index')});
-
-/* app.all('/beers', (req, res) => {
-punkAPI
-  .getBeers()
-  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
-  .catch(error => console.log(error));
-})
- */
 
 
 app.all("/beers", (req, res) => {
@@ -49,13 +33,20 @@ app.all("/beers", (req, res) => {
 })
 
 
-
 app.all("/random-beer", (req, res) => {
   punkAPI.getRandom()
   .then(data => {
     res.render("randomBeer", data[0])
   })
 })
+
+app.all("/beers/:id" , (req, res) => {
+  punkAPI.getBeer(req.params.id)
+  .then(data => {
+    res.render("onebeer" , data[0])
+  })
+})
+
 
 
 
