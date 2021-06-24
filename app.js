9c580d80389cb5163dcf7eee3bfcfb4aa1fac552
Andrew Oppo <andrew.oppo@gmail.com>
2021-06-24 15:57:09 +0200

    done

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +22,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  const beers = punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beerList: beers});
+      console.log(beerList);
+    })
+    .catch(error => console.log(error));
+  //console.log(beers);
+});
+
+app.get('/random-beer', (req, res) => {
+  const random = punkAPI.getRandom()
+    .then(random => {
+    res.render('random-beer', {randomBeer: random});
+    })
+    .catch(error => console.log(error));
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
