e7aabeb469c5642420a101f5faf60d6a3e25cf68
Raoul Ullens <raoulullens@live.com>
2021-06-24 16:17:43 +0200

    add iterations 1-4

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -20,6 +21,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  // gets the array of 25 beers
+    .getBeers()
+    // it all needs to happen in the .then which is the response
+    // response = beersFromAPI
+    .then(beersFromApi => { 
+      console.log('Beers from the database: ', beersFromApi); 
+      res.render('beers', {beersFromApi})
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    // it all needs to happen in the .then 
+    .then(responseFromApi => {
+      const theBeer = responseFromApi[0];
+      console.log('Random beer appears', responseFromApi[0].name);
+      // alert(responseFromApi[0].name)
+      res.render('random-beer', theBeer)
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
