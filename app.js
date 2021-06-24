5f6a32de8e28ec0c8825336386fc603d7b29df3b
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-06-24 19:28:36 +0200

    mandatory iterations done

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-const getOneBeer;
 
-punkAPI.getBeers()
-.then((data) => data.json)
-.then((allData) => getOneBeer = allData);
+//this is a bad idea as it calls the info when the website sets up, not when the client requests it.
+//let getAllBeers;
+//punkAPI.getBeers().then((allData) => getAllBeers = allData); 
 
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+//presetting the dir address for assets used by the hbs files - so then you can simply write it this way:
+//<img src="images/beer.png" alt="beer picture">
+//no need to write ../public/ before the images/beer.png , this prewrites it 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -30,8 +32,31 @@ hbs.registerPartials(path.join(__dirname , "/views/partials"))
 
 app.get('/', (req, res) => {res.render('index')});
 
-app.all('/beers', (req, res) => {res.render('beersData') , getOneBeer});
+/* app.all('/beers', (req, res) => {
+punkAPI
+  .getBeers()
+  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .catch(error => console.log(error));
+})
+ */
+
+
+app.all("/beers", (req, res) => {
+  punkAPI.getBeers()
+  .then(data => {
+    res.render("beers", {data})
+  })
+})
 
 
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.all("/random-beer", (req, res) => {
+  punkAPI.getRandom()
+  .then(data => {
+    res.render("randomBeer", data[0])
+  })
+})
+
+
+
+app.listen(4000, () => console.log('🏃‍ on port 4000'));
