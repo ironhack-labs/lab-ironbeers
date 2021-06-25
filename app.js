e685049b59b87ce1c456e432e4b5b7492c6d9757
kintrp <philipp.kintrup@googlemail.com>
2021-06-25 14:13:57 +0200

    for Ahmed

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
 hbs.registerPartials(__dirname + '/views/partials');
-
-// ...
 
 // Add the route handlers here:
 
@@ -43,6 +38,7 @@ app.get('/random-beer', (req, res, next) => {
 });
 
 app.get('/', (req, res) => {
+  console.log('Hello')
   res.render('index.hbs');
 });
 
