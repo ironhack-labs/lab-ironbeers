a877cafec5a9f64d4dce971ec2f2825847e85705
or <prietopazjosue@gmail.com>
2021-06-25 17:46:37 +0200

    ironbeers finished

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -19,7 +21,46 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+  //res.render('moviedetail', { clickedMovie: godfather, doctitle: 'movie details' });
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  //let beersArr = punkAPI.getBeers();
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    //console.log('Beers from the database: ', beersFromApi);
+    res.render('beers', {beerList: beersFromApi});
+  })
+  .catch(error => console.log(error));
+});
+  
+
+app.get('/random-beer', (req, res) => {
+  //let beersArr = punkAPI.getBeers();
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', {randomBeer: responseFromAPI});
+  })
+  .catch(error => console.log(error));
+;
+    
+  })
+
+  app.get('/:id', (req, res) => {
+    punkAPI
+    .getBeer(req.params.id)
+    .then(beerNew => {
+      console.log("hi!!!!!x")
+      console.log(beerNew)
+      res.render('beerdetail', {beerDet: beerNew});
+    })
+    .catch(error => console.log(error));
+  ;
+    })  
+//res.render('moviedetail', { clickedMovie: godfather, doctitle: 'movie details' });
+
+
+app.listen(5000, () => console.log('🏃‍ on port 5000'));
