3e54428538e2a29b7764810bbd873717ef9c6cc8
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-06-27 19:09:26 +0200

    lab done

--- app.js
+++ app.js
@@ -12,14 +12,39 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, "./views/partials"))
 
-// ...
 
-// Add the route handlers here:
+//creating the routes
+//iteracion 3
+app.get("/beers", (req, res)=>{
+  punkAPI.getBeers()
+  .then(beersFromApi =>{ 
+    res.render("beers", {objetoArrayDeCervezas : beersFromApi})//render siempre tiene que coger un objeto, así que creamos uno con la key objetoArrayDeCervezas y le asignamos el valor beersFromApi, que es un array.
+    console.log("Beers from the database:", beersFromApi)
+  })
+  .catch(error=> console.log(error));
+})
+//iteracion 4
+app.get("/random-beer", (req, res)=>{
+  punkAPI.getRandom()
+  .then(randomBeer =>{
+    res.render("random-beer", randomBeer[0])//otra manera de convertir el array a objeto
+    console.log("random-beer", randomBeer[0])  
+  })
+  .catch(error=> console.log(error));
+})
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get("/beerProfile/:id", (req, res)=>{
+  punkAPI.getBeer(id)
+  .then(beerId =>{
+    res.render("beerProfileid" , beerId)
+    console.log(beerId)
+  })  
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
