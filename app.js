f6db1456d8a9e0ace2ff3666d1acde62dc5a555f
jxxjxxlxx <92627.p@gmail.com>
2021-07-26 23:55:42 +0200

    286 jinjoo
    done with some errors not solved

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+const beersRouter = require("./routes/beers")
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(__dirname + '/views/partials')
 // ...
 
 // Add the route handlers here:
@@ -21,5 +24,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', async (req,res)=>{
+  try {
+  const beers = await punkAPI.getBeers()
+  res.render('beers', {beers});
+}
+catch(err){ //error appears saying 'try' and 'finally' expected ...?
+  console.log(err)
+}
+});
+
+app.get('/random-beer', (req,res)=> {
+  const random = await punkAPI.getRandom()
+  try {  
+  res.render("rendom-beer", {random})
+  }
+catch (err){ 
+  console.log(err)
+}
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
---
+++ routes/beers.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router();
+
+router.get("/beers", function(req, res, next) {
+    res.render("beers", )
+})
+
+
+
+module.exports = router;
