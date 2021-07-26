3386a40380676c80b3ba2e2a31bf0421fcb0be50
Jessie Panganiban <jessiepanganiban@gmail.com>
2021-07-26 19:52:54 +0200

    cannot extract data from api

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv/config");
+
 const express = require('express');
 
 const hbs = require('hbs');
@@ -12,6 +14,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//app.use("/beers", require("beers"));
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -22,4 +26,60 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+
+app.get("/beers", (req, res, next)=> {
+    punkAPI.getBeers()
+  .then((apiRes) => {
+    const data = {
+      beers: apiRes.results,
+    };
+    console.log(apiRes.results);
+    res.render("beers.hbs", data);
+  })
+  
+  .catch((error) => {
+  next(error);
+  
+
+  });
+
+});
+
+app.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+
+  punkAPI.getBeers(id)
+  .then((apiRes) => {
+    const data = {
+      beers: apiRes,
+    };
+
+    res.render("beers.hbs", data);
+  })
+
+  .catch((error) => {
+    next(error);
+  });
+});
+
+//app.get("/", (req, res, next) => {
+// punkAPI.getBeers()
+//  .then((apiRes) => {
+//    //const data = {
+//      //beers: apiRes.results,
+//
+//    console.log(apiRes.results);
+//    res.render("beers.hbs", { beers: apiRes })
+//  
+//      
+//   // res.render("beers.hbs", data)
+//    
+//  .catch((error) => {
+//    next(error);
+//  
+//
+//  });
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+module.exports = app;
