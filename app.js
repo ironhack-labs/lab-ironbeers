7783988d05d8afcdfc44519a85d7c60221253971
Quentin Marignier <qmarignier@gmail.com>
2021-07-26 16:52:54 +0200

    done, got lazy on the css

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + '/views/partials')
+
 
 // ...
 
@@ -21,5 +23,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', async (req, res) => {
+  try{
+    const beers = await punkAPI.getBeers()
+    res.render('beers', {beers});
+  }
+  catch(err){
+    console.log('Failed loading beers from API', err)
+  }
+});
+
+app.get('/random-beer', async (req, res) => {
+  try{
+    const beer = await punkAPI.getRandom()
+    res.render('random-beer', {beer : beer[0]});
+  }
+  catch(err){
+    console.log('Failed loading random-beer from API', err)
+  }
+});
+
+app.get('/beers/:id', async (req, res) => {
+  const beerId = req.params.id
+  try{
+    const beer = await punkAPI.getBeer(beerId)
+    res.render('random-beer', {beer : beer[0]});
+  }
+  catch(err){
+    console.log('Failed loading random-beer from API', err)
+  }
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
