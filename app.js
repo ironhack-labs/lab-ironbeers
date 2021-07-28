0fe5634b0260887bc225276815a060af3583433b
Peter Mayer <peter_my@gmx.de>
2021-07-28 16:50:14 +0200

    Beers

--- app.js
+++ app.js
@@ -13,13 +13,47 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + '/views/partials');
 
-// ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then( 
+    beersFromApi => {
+      console.log(beersFromApi);
+      res.render("beers", {beers: beersFromApi});
+    })
+  .catch(error => console.log(error));
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then( 
+    beersFromApi => {
+      console.log(beersFromApi);
+      res.render("beers", {beers: beersFromApi});
+    })
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then( 
+    beerFromApi => {
+      console.log(beerFromApi);
+      res.render("random-beer", {beers: beerFromApi});
+    })
+  .catch(error => console.log(error));
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
