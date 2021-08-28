48cc31f30d221b599ae2624c9d392e0d04ffe997
Chris <christaylor269@gmail.com>
2021-08-28 17:39:56 +0200

    done

--- app.js
+++ app.js
@@ -22,4 +22,28 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      //console.log('Beers from the database: ', beersFromApi);
+
+      res.render('beers', {
+        beersFromApi
+      });
+    })
+    .catch();
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', {
+        responseFromAPI
+      });
+    })
+    .catch();
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
