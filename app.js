3bea58df553231ed515c8829ca209403fb217d60
Sara Mansori <sara.mansori.amar@gmail.com>
2021-09-01 00:31:39 +0200

    lab done

--- app.js
+++ app.js
@@ -13,13 +13,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beers: beersFromApi }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(infoBeer => {
+      res.render('info', { infoBeer: infoBeer });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beer-info/:tagId', function (req, res) {
+  punkAPI
+    .getBeer(req.params.tagId)
+    .then(infoBeer => {
+      res.render('info', { infoBeer: infoBeer });
+    })
+    .catch(error => console.log(error));
+});
+
+app.listen(3000, () => console.log('Working‍ on port 3000'));
