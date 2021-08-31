ef527b8f8851db49298f166252fa1bdd07662924
Ivan Espinoza <icaro_19@hotmail.com>
2021-08-31 23:05:48 +0200

    finish work

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -21,5 +23,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('Beers', { theBeers: beers });
+    })
+    .catch(error => console.log(error));
+});
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      res.render('RandomBeers', { theBeer: beer })
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beer/:id', (req, res) => {
+
+  const { id } = req.params
+  punkAPI
+    .getBeer(id)
+    .then(beer => {
+      res.render('beer', { theBeer: beer })
+      // res.render('beer', { theBeer: beer })
+    })
+    .catch(error => console.log(error));
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
