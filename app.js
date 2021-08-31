cd102cf542d357faa899a4cfbf69646a0a0a8594
eduvadillo <87086693+eduvadillo@users.noreply.github.com>
2021-08-31 18:24:38 +0200

    falta itineration 6

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,5 +22,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/', (req, res, next) => res.render('index'));
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beersFromApi }))
+
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => res.render('random-beer', { responseFromAPI }))
+
+    .catch(error => console.log(error));
+});
+
+app.get('/beerCard', (req, res, next) => res.render('beers'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
