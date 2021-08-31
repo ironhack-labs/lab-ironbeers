2509c9b524416e1993dfa6913f99e9988a5d5fed
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-08-31 15:17:15 +0200

    Feat: get beers working

--- app.js
+++ app.js
@@ -14,12 +14,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get ("/beers", (req,res) => {
+    /*Inside the /beers route, call the getBeers() method (
+      the PunkAPI provides this method, and you can find more about it here). 
+      Calling the .getBeers() method returns a promise that should be resolved with an array of 25 beers. */
+    punkAPI
+      .getBeers()
+      .then(beersFromApi => res.render("beers", { beersFromApi }))
+      .catch(error => console.log(error));
+
+})
+
+app.get ("/random-beer", (req,res) => {
+  res.render("random-beer")
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
