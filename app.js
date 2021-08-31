961772c5a82d9e439e29a8e0d1726e0dab1fda63
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-08-31 18:43:58 +0200

    Mandatory iterations done!

--- app.js
+++ app.js
@@ -16,10 +16,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // ...
 
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// Add the route handlers here:
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { theBeers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      console.log('RANDOM BEER: ', beersFromApi);
+      res.render('random-beer', { theBeers: beersFromApi });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
