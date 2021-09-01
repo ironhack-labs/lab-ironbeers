e75f9295d7746151d80c83577ea8d2c7f965aebb
Monika Geiger <geigermonika@hotmail.hu>
2021-09-01 18:20:14 +0200

    lab done
    Co-authored-by: Jessé Vermeulen <JesseVermeulen123@users.noreply.github.com>

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,6 +21,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers();
+  // console.log(beers)
+  res.render('beers', {
+    beers: beers
+  });
+});
+
+app.get('/beer/:id', async (req, res) => {
+  const beer = await punkAPI.getBeer(req.params.id);
+  console.log(beer)
+  res.render('beer', {
+    beer: beer[0]
+  });
+});
+
+app.get('/random-beer', async (req, res) => {
+  let beer
+  try {
+    beer = await punkAPI.getRandom();
+  } catch (error) {
+    console.log(error)
+  }
+  res.render('random-beer', {
+    beer: beer[0]
+  });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
