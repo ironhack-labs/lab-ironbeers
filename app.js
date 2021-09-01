e1c292616e3dd4dd1e943bc790d4aa215cf4ec18
Juandi Mena <juandimena34@gmail.com>
2021-09-01 21:01:21 +0200

    JuandiM-WDFT-AUG21-solved-lab

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
@@ -22,4 +23,16 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.all('/beers', (req, res) => {
+punkAPI.getBeers()
+.then(beers => res.render('beers', {beers}))
+.catch(error => console.log(error));
+});
+
+app.all('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then( oneRandomBeer => res.render('random-beer', oneRandomBeer[0]))
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
