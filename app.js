2d7a99adfe19bf6ed2001ba44a318511f020a2ae
FlavianAlbert <flahribu@gmail.com>
2021-09-01 22:22:59 +0200

    done

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+/**
+ * TOC Express dynamic views
+ * 
+ * 1. require hbs
+ * 2. Set the View Engine
+ * 3. app.all() with the res.render() method
+ * 4. set the views directory to tell express where templated are
+ * 5. require the data for the page
+ * 6. Pass data to the res.render() method
+ * 7. Register the partials directory
+ */
+
 const express = require('express');
 
+// 1. require hbs
 const hbs = require('hbs');
 const path = require('path');
+
+//5. require the data for the page
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+//2. Set the View Engine
 app.set('view engine', 'hbs');
+
+//4. set the views directory to tell express where templated are
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,8 +36,65 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
+//3. app.all() with the res.render() method
+app.all('/', (req, res) => {
   res.render('index');
 });
+
+
+
+
+
+app.all('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi);
+
+    // 6. Pass data to the res.render() method
+    res.render("beers", {beers: beersFromApi})
+  } )
+  .catch(error => console.log(error));})
+
+
+  // Iteration 4
+  app.all('/random-beer', (req, res) => {
+    punkAPI
+    .getRandom()
+    .then(oneBeerFromApi => {
+      console.log('One random Beer from the database: ', oneBeerFromApi);
+  
+    
+      res.render("random-beer", {beer: oneBeerFromApi[0]})
+    } )
+    .catch(error => console.log(error));})
+
+
+
+
+
+
+    /*app.get('/random-beer', async (req, res) => {
+      let beer
+      try {
+        beer = await punkAPI.getRandom();
+      } catch (error) {
+        console.log(error)
+      }
+      res.render('random-beer', {
+        beer: beer[0]
+      });
+    });*/
+
+
+
+
+
+
+
+
+
+
+  
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
