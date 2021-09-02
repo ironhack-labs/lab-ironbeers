e16cda1e92f8e44adcb01fac2d1dec4867850a42
Osvaldo Picazo <odpicazo@gmail.com>
2021-09-02 07:22:23 +0200

    [WDFT - REM - Aug21] Osvaldo Picazo - lab done

--- app.js
+++ app.js
@@ -1,25 +1,85 @@
-const express = require('express');
+/**
+ * TOC Express dynamic views
+ * 
+ * 1. require hbs
+ * 2. Set the View Engine
+ * 3. app.all() with the res.render() method
+ * 4. set the views directory to tell express where templated are
+ * 5. require the data for the page
+ * 6. Pass data to the res.render() method
+ * 7. Register the partials directory
+ */
 
-const hbs = require('hbs');
-const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+ const express = require('express');
 
-const app = express();
-const punkAPI = new PunkAPIWrapper();
-
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
-app.get('/', (req, res) => {
-  res.render('index');
+ // 1. require hbs
+ const hbs = require('hbs');
+ const path = require('path');
+ 
+ //5. require the data for the page
+ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+ 
+ const app = express();
+ const punkAPI = new PunkAPIWrapper();
+ 
+ //2. Set the View Engine
+ app.set('view engine', 'hbs');
+ 
+ //4. set the views directory to tell express where templated are
+ app.set('views', path.join(__dirname, 'views'));
+ 
+ app.use(express.static(path.join(__dirname, 'public')));
+ 
+ // Register the location for handlebars partials here:
+ // 7. Register the partials directory
+ hbs.registerPartials(path.join(__dirname, 'views/partials'));
+ 
+ // Add the route handlers here:
+ 
+// Iteration 2
+//3. app.all() with the res.render() method
+app.all('/', (req, res) => {
+res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+// Iteration 3
+app.all('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi);
+
+    // 6. Pass data to the res.render() method
+    res.render("beers", {beers: beersFromApi})
+  } )
+  .catch(error => console.log(error));})
+ 
+ 
+  // Iteration 4
+  app.all('/random-beer', (req, res) => {
+    punkAPI
+    .getRandom()
+    .then(oneBeerFromApi => {
+      console.log('One random Beer from the database: ', oneBeerFromApi);
+  
+      // 6. Pass data to the res.render() method
+      res.render("randomBeer", {beer: oneBeerFromApi[0]})
+    } )
+    .catch(error => console.log(error));})
+
+// Iteration 6
+app.all('/beers/beer-:id', (req, res) => {
+  const beerId = req.params.id
+  punkAPI
+  .getBeer(beerId)
+  .then(idBeerFromApi => {
+  console.log('One Beer from the Id in database: ', idBeerFromApi);
+
+  // 6. Pass data to the res.render() method
+  res.render("randomBeer", {beer: idBeerFromApi[0]})
+   } )
+  .catch(error => console.log(error));})
+ 
+ 
+ app.listen(3000, () => console.log('🏃‍ on port 3000'));
+ 
