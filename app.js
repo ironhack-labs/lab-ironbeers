2df507c07dc5ae254a4ddd489f5e5af0498234c4
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-09-02 16:32:24 +0200

    iterations 1-5 complete with index css complete

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,7 +20,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { doctitle: 'Home', });
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', { beersFromApi }))
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeerFromApi => res.render('random-beer', { randomBeerFromApi }))
+  .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
