c97c1c5a98778554b4be4db8bc7a920103cd8879
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-09-02 15:37:12 +0200

    complete mandatory steps'

--- app.js
+++ app.js
@@ -22,4 +22,23 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    
+    res.render('beers', {beersList : beersFromApi});
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(responseFromAPI => {
+    res.render('randombeer', {beer: responseFromAPI});
+    // your magic happens here
+  })
+  .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
