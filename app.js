18f1207c5b1fa36161c05b3d8bd533a9ee25f09a
Ludumo Toni <ludumo.toni@outlook.com>
2021-09-06 08:42:14 +0200

    iron beers almost solved

--- app.js
+++ app.js
@@ -14,12 +14,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {title:'Home'});
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render('beers', { beersList : beersFromApi, title:'Beers'});
+  })
+  .catch(error => console.log(error));
+  
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', { beersList : responseFromAPI, title: 'Random Beer'});
+  })
+  .catch(error => console.log(error));
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
