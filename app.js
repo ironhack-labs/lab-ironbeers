b49a211c0f33c65b2bca7987da6725832ee4669f
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-10-13 21:16:34 +0200

    Iteration 1 2 3 4

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
@@ -21,5 +22,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', {beersFromApi});
+  })
+  .catch(error => console.log(error));
+  
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beers', {responseFromAPI})
+  })
+  .catch(error => console.log(error));  
+  
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
