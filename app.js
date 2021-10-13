0642b9bcab25dd18ee755a101d33cb25be2084b0
Alice <alice.jost@outlook.de>
2021-10-13 18:41:01 +0200

    Iteration 3: create beers page with API connection

--- app.js
+++ app.js
@@ -22,4 +22,15 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', {beersFromApi});
+  })
+  .catch(error => console.log(error));
+ 
+});
+
 app.listen(3008, () => console.log('🏃‍ on port 3000'));
