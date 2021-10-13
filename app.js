faf23b3a2b30a588bf25e5b50f9d2fffc8615b43
Geoffrey Laville <laville.geoffrey@gmail.com>
2021-10-13 23:53:34 +0200

    iteration 1 to 3

--- app.js
+++ app.js
@@ -22,4 +22,12 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  res.render('beers');
+});
+
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer');
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
