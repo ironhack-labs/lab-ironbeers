34985388b4e7468dd1f1026c1ec3f2fd517d6a12
jeannette.valencia <jfatima.valencia@gmail.com>
2021-10-14 09:00:23 +0200

    css style

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -37,7 +39,7 @@ app.get('/random-beer', (req, res) => {
   punkAPI
   .getRandom()
   .then(responseFromAPI => {
-    res.render('random-beer', {responseFromApi});
+    res.render('random-beer', {responseFromAPI});
   })
   .catch(error => console.log(error));
 });
