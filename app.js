26c55dfed48907e2ea1a33fb76dd401ee6e9e433
Kati Fe <k-felgitsch@hotmail.com>
2021-10-14 07:29:36 +0100

    created partial

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Add the route handlers here:
 
@@ -30,8 +29,15 @@ app.get('/beers', (req, res) => {
     res.render('beers', beersFromApi)
     })
     .catch(error => console.log(error))
-  
-  
+});
+
+app.get('/beer random', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('beers', beersFromApi)
+  })
+  .catch(error => console.log(error));
 
 });
 
