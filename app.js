b0409a623c7f71f7b61256bf5eb2a3552b0ecc09
Geoffrey Laville <laville.geoffrey@gmail.com>
2021-10-16 23:02:10 +0200

    iteratation 3.2 (bug css)

--- app.js
+++ app.js
@@ -23,11 +23,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/beers', (req, res) => {
-  res.render('beers');
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', {beersFromApi});
+    })
+    .catch(error => console.log(error));
 });
 
 app.get('/random-beer', (req, res) => {
   res.render('random-beer');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(3000);
