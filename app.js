b5fe10999a3ca9854b08eb8b8d7a21213a395842
Michael Seitz <michael.s.seitz@gmail.com>
2021-11-06 19:05:44 +0100

    made a nice nav bar

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { hkdfSync } = require('crypto');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -13,13 +14,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(__dirname+"/views/partials")
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  res.render('beers');
+});
+app.get('/random-beer', (req, res) => {
+  res.render('randm-beer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
