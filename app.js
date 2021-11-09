5609244d7d41581f8e9446d742b86d9dcaca9864
marferbl <fernandezblascomarcos@gmail.com>
2021-11-09 18:04:57 +0100

    DONE

--- app.js
+++ app.js
@@ -11,15 +11,42 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
+//CARGA INDEX
 app.get('/', (req, res) => {
   res.render('index');
+});
+//CARGA BEERS
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log(beersFromApi);
+      res.render('beers', { beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+//CARGA RANDOM BEER
+
+app.get('/randomBeer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      console.log(responseFromAPI);
+      res.render('randomBeer', { responseFromAPI });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/beer-:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then(beerFromApi => {
+      res.render('beerClicked', { beerFromApi });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
