ba3d53dec05af1deb209edbd989d71a301008208
jmsd <sanchezdiezma@gmail.com>
2021-11-09 20:15:50 +0100

    Done

--- app.js
+++ app.js
@@ -18,8 +18,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+// Index --
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// Beers --
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+});
+
+// Random Beers --
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(beersFromApi => {res.render('random-beers', {beersFromApi})})
+  .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+
+
+
+
