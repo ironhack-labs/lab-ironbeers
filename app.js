6ac33fb4a7da46b60b569908f03f3ab30401214b
Eunice Santiago <eunicevsantiago@gmail.com>
2021-11-09 20:38:21 +0100

    Done until Bonus

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+//const randomBeer = PunkAPI.getRandom()
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -18,8 +19,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res, next) => {
+
+  
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error))
+  
+});
+
+app.get('/random-beer', (req, res) => {
+
+  
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => res.render('random-beer', {responseFromAPI}))
+  .catch(error => console.log(error))
+  // .catch(error => console.log(error));
+
+  // res.render('random-beer');
+});
+
+app.listen(3000, () => console.log("escuchando en 3000"))
