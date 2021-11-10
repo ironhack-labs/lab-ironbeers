fc677a3b5832f9be49dba97ff9a3ec586dd87480
Fernando Cases <nandocasesgarcia@gmail.com>
2021-11-10 17:42:38 +0100

    Done, pero ahora me crashea el normal beer, pero funciona el random, tengo que solucionarlo

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 const express = require('express');
-
 const hbs = require('hbs');
+const app = express();
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
-const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      let data = {
+        beer: beers
+      };
+      res.render('beers', data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(beers => {
+      let data = {
+        beer: beers
+      };
+      res.render('randomBeer', data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.listen(3000);
