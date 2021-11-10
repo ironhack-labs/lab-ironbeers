3ca3a01ac6878dda3c646cd75c25766a38695778
Raquel <90280742+raqhidcor@users.noreply.github.com>
2021-11-11 00:01:43 +0100

    lab completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express');
+//Variables
 
+const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
@@ -7,16 +8,21 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+//Middleware for the view engine
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
