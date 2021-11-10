b4cfed4d022ee8f6acedbc5beab7ac8f57326cd3
Analia Lopez <analialopezrodriguez@gmail.com>
2021-11-10 09:01:06 +0100

    lab done

--- app.js
+++ app.js
@@ -22,4 +22,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', {beersFromApi}))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => res.render('random-beer', {beersFromApi}))
+    .catch(error => console.log(error));
+});
+
+app.listen(5000, () => console.log('🏃‍ on port 3000'));
