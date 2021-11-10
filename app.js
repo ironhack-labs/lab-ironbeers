991d6e2a73e7ec9f37dd575c3c460475c7f812b8
TatianaPrada <tatiana.1227@hotmail.com>
2021-11-10 17:19:28 +0000

    interaction 1 and 2 done

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
-
 const hbs = require('hbs');
-const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+//const path = require('path');
+
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+//Middleware for the view engine
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs');
+app.set("views", __dirname + "/views")
+app.use(express.static('public'));
+
+//app.set('views', path.join(__dirname, 'views'));
+//app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Register the location for handlebars partials here:
 
@@ -20,6 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  res.render('beers');
+});
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
