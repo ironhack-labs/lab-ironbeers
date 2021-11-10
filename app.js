4f3130d3eafdb118734fd1c5d2cf1f7b3c79eb7a
Ricardo Franco <ricardofranco14@gmail.com>
2021-11-10 19:13:50 +0000

    done Ricardo e Clara

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(path.join(__dirname, "views/partials"))
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -21,5 +23,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log("Beers from database: ", beersFromApi)
+      res.render('beers', {beersList: beersFromApi});
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi => {
+      console.log("Random Beer: ", responseFromApi)
+      res.render('random-beer', {randomBeer: responseFromApi});
+    })
+    .catch(error => console.log(error));
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
