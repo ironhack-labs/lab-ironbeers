d0acf0ca869cf2dfb95c3daffe0815deb8e25031
TatianaPrada <tatiana.1227@hotmail.com>
2021-11-10 20:32:12 +0000

    lab done

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+
 //Middleware for the view engine
 
 app.set('view engine', 'hbs');
 app.set("views", __dirname + "/views")
 app.use(express.static('public'));
+hbs.registerPartials((__dirname, 'views/partials'));
+
 
 //app.set('views', path.join(__dirname, 'views'));
 //app.use(express.static(path.join(__dirname, 'public')));
@@ -26,11 +30,21 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.render('index');
 });
-app.get('/beers', (req, res) => {
-  res.render('beers');
-});
+
+app.get('/beers', (req, res)=>{
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+})
+
 app.get('/random-beer', (req, res) => {
-  res.render('random-beer');
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => res.render('random-beer', {responseFromAPI}))
+  .catch(error => console.log(error));
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+app.listen(4000, () => console.log('🏃‍ on port 4000'));
