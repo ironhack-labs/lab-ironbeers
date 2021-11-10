884631af7a8dd034ae3a7c4d81102890be9656fd
Federico Altava Redondo <fedealtava@gmail.com>
2021-11-10 19:27:31 +0100

    itinerations 1,2,3,4

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -14,9 +15,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+//Middleware for the partials
+hbs.registerPartials(__dirname + "/views/partials");
+
 
 // Add the route handlers here:
+
+
+
+
+app.get("/beer", (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => res.render("beer",{beersFromApi}))
+      
+    .catch(error => console.log(error));
+  
+});
+
+app.get("/randomBeer", (req, res, next) => {
+  
+  punkAPI
+  .getRandom()
+  .then(beersFromApi => {
+    // your magic happens here
+    res.render("randomBeer",{beersFromApi})
+  })
+  .catch(error => console.log(error));
+});
 
 app.get('/', (req, res) => {
   res.render('index');
