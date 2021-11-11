dbbf63c4181d7bb7853d4467963459262ab434ed
Johannes von Kessel <johannes.v.kessel@web.de>
2021-11-11 14:52:47 +0100

    iteration 2 done

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+app.use(express.static(__dirname + '/public'));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+app.get('/', (req, res) => {
+	res.render('index')
+})
 
 // Register the location for handlebars partials here:
 
