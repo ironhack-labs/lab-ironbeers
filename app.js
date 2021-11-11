6ccd7bf36db0ec68683d3afc9df68583a6000f85
YFarias <yurisfarias@gmail.com>
2021-11-11 10:43:10 +0000

    done

--- app.js
+++ app.js
@@ -21,18 +21,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beer', (req, res) => {});
+app.get('/random-beer', (req, res) => {});
 
 
+//beers
 app.get('/beers', async (req,res) => {
   const beerFromApi = await punkAPI.getBeers();
   res.render('beers', {beers: beerFromApi})
 })
-  
+ 
+//Random Beer
 app.get('/random-beer', async (req,res) => {
   const beerArr = await punkAPI.getRandom();
   console.log(beerArr)
   res.render('randombeer', {beer: beerArr[0]})
 })
+//GEt beer
+app.get('/beers/beer-:beerId', (req,res) => {
+  console.log('req.params.beerId',req.params.beerId)
+  const beerId = req.params.beerId;
+})
+
+
 
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
