a0ee41646b5d33465a8c5903bc54edcf061c8a96
Edgar Uco <edgarin@gmail.com>
2021-11-16 18:10:22 -0600

    lab ironbeers it 1-2 done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
+require("dotenv").config();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -21,5 +21,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get("/views/beers.hbs", (req, res) => {
+  res.render('beers');
+})
+
+app.get('/views/random-beer.hbs', (req, res) => {
+  res.render('random-beer');
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
