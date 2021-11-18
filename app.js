bb2fcc6b6da3f31f4ed40bd4415b38cc8662a2e1
Hector Biminchumo <hectorbiminchumo@gmail.com>
2021-11-18 01:47:36 -0500

    done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//IMPORTACIONES
 const express = require('express');
 
 const hbs = require('hbs');
@@ -7,11 +8,50 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+require('dotenv').config()
 
+//2. MIDDLEWARES
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
+hbs.registerPartials(__dirname + '/views/partials')
+
+
+// 3. RUTAS
+app.get("/random-beer", (req, res)=> {
+    const randomBeers = punkAPI.getRandom()
+    randomBeers.then((beers)=>{
+      res.render("random-beer",{
+          data:beers
+      })
+    })
+    .catch((error)=> {
+      console.log(error);
+    })
+
+
+})
+
+
+app.get("/beers", (req,res)=>{
+
+  //OBTENIENDO LAS PROMESAS  
+  const listBeers = punkAPI.getBeers()
+
+  //EVALUANDO LAS PROMESAS  
+    listBeers.then((beers)=> {
+      res.render("beers",{
+        data:beers
+      })
+    })
+
+    .catch((error)=> {
+      console.log(error);
+    })
+
+
+})
 // Register the location for handlebars partials here:
 
 // ...
@@ -19,7 +59,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('home', {home:'home'});
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+// 4. Server
+app.listen(process.env.PORT, () => {
+console.log(`Listen on port ${process.env.PORT}`);
+})
