ec5643c86dc3c9aa69daa0bf6356af124ab9bc44
SerchPro <proyects_serch@hotmail.com>
2021-11-22 18:55:14 -0600

    partial solved

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +20,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers().then(data => {
+        //console.log(data);
+        res.render('beers', { beers: data });
+    }).catch(error => {
+        console.error(error)
+    });
+
+
+});
+
+app.get('/random-beer', (req, res) => {
+    punkAPI.getRandom().then(data => {
+        let beer = data[0]
+        console.log(beer)
+        res.render('random-beer', { beer });
+    }).catch(error => {
+        console.error(error);
+    });
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
