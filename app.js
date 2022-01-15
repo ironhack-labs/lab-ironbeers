b809d0b9be02b3fc9bb6a690678bf8a05941e1ff
Rachouan Rejeb <info@rachouan.com>
2022-01-15 16:34:18 +0000

    done

--- app.js
+++ app.js
@@ -17,9 +17,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ...
 
 // Add the route handlers here:
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  let beers = []
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    beers = beersFromApi
+    res.render('beers',{ beers })
+  })
+  .catch(error => console.log(error));
+  
+  
+});
+
+app.get('/beers/:beer', (req, res) => {
+  let beer = req.params.beer;
+  console.log(beer)
+  if(beer == 'random'){
+    punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      beer = responseFromAPI[0]
+      res.render('beer',{ beer })
+    })
+    .catch(error => console.log(error));
+  }else{
+    punkAPI
+    .getBeer(beer)
+    .then(responseFromAPI => {
+      // your magic happens here
+      beer = responseFromAPI[0]
+      res.render('beer',{ beer })
+    })
+    .catch(error => console.log(error));
+  }
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
