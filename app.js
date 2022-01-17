193a74403e05a7f5e44b26f637916efdd50022bb
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-01-17 21:11:04 +0100

    finish iteration 1

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + "/views/partials");
+
 // Register the location for handlebars partials here:
 
 // ...
