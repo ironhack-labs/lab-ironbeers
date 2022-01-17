7c410de3075b2713ba048514bf27eecce54c4988
Diogo <diogoascarneiro@hotmail.com>
2022-01-17 14:51:02 +0000

    added partials

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // Register the location for handlebars partials here:
 
 // ...
@@ -20,6 +20,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beers: beersFromApi}))
+  .catch(error => console.log(error));
+  });
+
+app.get('/randomBeer', (req, res) => {
+
+  punkAPI
+  .getRandom()
+  // .then(responseFromAPI => res.render('randomBeer', {responseFromAPI}))
+  .then(responseFromAPI => { 
+    let teste = responseFromAPI[0];
+    console.log(teste);
+    res.render('randomBeer', teste)
+  })
+  .catch(error => console.log(error));
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
