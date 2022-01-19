bc29a45404361eb0ffed9fd4b707121b6d7cbbed
“Giovanni” <giovannitakeshisesta@gmail.com>
2022-01-19 01:29:49 +0100

    iteration 4 ok

--- app.js
+++ app.js
@@ -3,31 +3,42 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
-const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+const app = express();
+
+
 app.set('view engine', 'hbs');
+
+// creates an absolute path pointing to a folder called "views"
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
 
-// ...
-
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/beers', (req, res) => {
-  res.render('beers');
-});
+app.get("/beersPage", (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersArray => {
+      console.log('Beers from the database: ', beersArray)
+      res.render("beersPage", { newname: beersArray })
+    })
+    .catch(error => console.log(error));
+})
 
 app.get('/random', (req, res) => {
-  res.render('random');
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      console.log("sasdasdadsasdasdasdsdfghhfgkjg",randomBeer)
+      res.render('random',{randomBeer});
+    })
+    .catch(error => console.log(error));
 });
 
 
