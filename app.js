dd74f4c79a13e2094d0d90f1bb54f9f3dac0a5fe
Oscar Sangrador <oscarsangrador@gmail.com>
2022-01-23 12:14:32 +0100

    conected hbs pages

--- app.js
+++ app.js
@@ -12,14 +12,26 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // Register the location for handlebars partials here:
 
 // ...
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
+  console.log('index');
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  console.log('beers');
+  res.render('beers');
+});
+
+app.get('/randomBeer', (req, res) => {
+  console.log('random');
+  res.render('randomBeer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
