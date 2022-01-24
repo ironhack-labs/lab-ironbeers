f80d58b1a02d837626535fd72bd8793723c8ebaf
Luisgp01 <73586516+Luisgp01@users.noreply.github.com>
2022-01-24 06:58:59 +0100

    Done

--- app.js
+++ app.js
@@ -19,7 +19,42 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 });
 
+app.get("/beersPage", (req, res) => {
+    punkAPI
+        .getBeers()
+        .then(beersArray => {
+            console.log("Beers from the database: ", beersArray)
+            res.render("beersPage", { newname: beersArray })
+        })
+        .catch(error => console.log(error));
+})
+
+app.get('/random', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(randomBeer => {
+            console.log("plop", randomBeer)
+            res.render('random', { randomBeer });
+        })
+        .catch(error => console.log(error));
+});
+
+app.get("/bonusPage", (req, res) => {
+    punkAPI
+        .getBeers()
+        .then(beersArray => {
+            console.log("Beers from the database: ", beersArray)
+            res.render("bonusPage", { newname: beersArray })
+        })
+        .catch(error => console.log(error));
+});
+
+app.use((req, res) => {
+    res.status(404).send("Not Found!")
+})
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'));
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
