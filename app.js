ae0980214c61d91b05e99c7abe44080c27b77abd
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-01-26 17:31:57 +0100

    Iteration 3

--- app.js
+++ app.js
@@ -14,12 +14,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
+app.get('/', (req,res) => {
+  res.render("index");
+});
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi),
+        res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
 });
 
 app.get('/beers', (req, res, next) => res.render('beers'));
