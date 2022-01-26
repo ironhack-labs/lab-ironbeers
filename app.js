7f0a9deab813ef3eda3deb7c81480b0404c80ab7
László Tarnai <laszlo.tarnai@LEGO.com>
2022-01-26 16:56:59 +0100

    solved lab (without bonus)

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const express = require('express');
 
 const hbs = require('hbs');
+
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
+});
+ 
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render("beers", { beersFromApi }))
+    .catch(error => console.log(error));
+  });
+
+app.get("/random-beer", (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => res.render("random-beer", { responseFromAPI }))
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
