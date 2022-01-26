8cdbb77945e947920501da254086e85e20f2f41c
Ádám Báthori-Kiss <adam.bathori-kiss@lego.com>
2022-01-26 16:56:42 +0100

    Ironbeers

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
+
 const hbs = require('hbs');
+
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
+hbs.registerPartials(__dirname + "/views/partials");
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -21,5 +23,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get("/beers", (req, res, next) => {
+  
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render("beers",{beersFromApi}))
+  .catch(error => console.log(error));
+
+});
+
+app.get("/randombeer", (req, res, next) => {
+  
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI  => res.render("randombeer",{responseFromAPI}))
+  .catch(error => console.log(error));
+
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
