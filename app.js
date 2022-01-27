87a2bb548882d84f33fb65adc696e2c7f59794fb
Oscar Sangrador <oscarsangrador@gmail.com>
2022-01-27 20:16:31 +0100

    added navbar partial and img center

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
@@ -32,6 +33,10 @@ app.get('/beers', (req, res) => {
 app.get('/randomBeer', (req, res) => {
   console.log('random');
   res.render('randomBeer');
+});
+
+app.use((req, res) => {
+  res.status(404).send("Not found");
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
