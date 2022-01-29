7bc69b3c2173e323ff68d14e75c99118ab153cdd
AlejandroGC1990 <alejandro.garcia.carmona@gmail.com>
2022-01-29 19:21:38 +0100

    done

--- app.js
+++ app.js
@@ -13,13 +13,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
-
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
+  console.log('index');
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => res.render('beers', {beers}))
+    .catch((err) => console.log(eer))  
+});
+
+app.get('/randomBeer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => res.render('randomBeer', {randomBeer}))
+    .catch((err) => console.log(err));
+});
+
+app.use((req, res) => {
+  res.status(404).send("Not found");
+});
+
+app.listen(3000, () => console.log(':corredor:‍ on port 3000'));
