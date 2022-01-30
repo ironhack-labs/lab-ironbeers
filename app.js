fdd17f672062d308dbfe19ecf96bad3c28feacf9
ricomasgu <ricard.comas.gomez@gmail.com>
2022-01-30 18:06:57 +0100

    All except completing the styles. while getting the beer on /beers/1 does not apply the style and throw and error.

--- app.js
+++ app.js
@@ -14,12 +14,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', beersFromApi))
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then(beerFromApi => {
+      res.render('beer-id', beerFromApi[0]);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      res.render('random-beer', responseFromAPI[0]);
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
