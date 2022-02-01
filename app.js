0616d86dbf7ccb59a45dbce071f28b24904e5fb5
Emanueljuarez <zerauleuname@gmail.com>
2022-02-01 16:49:56 -0600

    Iteracion 2

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
+
+app.use("/", require("./routes/index"))
 
 app.listen(3000, () => console.log('http://localhost:3000'));
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,14 @@ router.get("/", (req, res) => {
 	res.render("index")
 })
 
+router.get("/beers", (req, res) => {
+	res.render("beers")
+})
+
+router.get("/random-beer", (req, res) => {
+	res.render("random-beer")
+})
+
 //exportacion
 
 module.exports = router
