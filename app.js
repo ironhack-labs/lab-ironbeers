628346c29f11e845eab8b1853f0d288396ab09a0
inesgarper <igperianez@gmail.com>
2022-02-01 19:39:18 +0100

    done except css

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -14,12 +15,52 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+// hbs.registerPartials(`${__dirname}/views/partials/`)
 
 // Add the route handlers here:
 
+// ---- Index Route
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+// ---- Beeers page Route
+app.get('/beers', (req, res) => {
+
+  // beersFromApi es una promesa pendiente
+  const beersFromApi = punkAPI.getBeers()
+
+  // Si la promesa se cumple
+  beersFromApi.then(beers => res.render('beers-page', { theBeers: beers }))
+
+  // Si la promesa no se cumple
+  beersFromApi.catch(error => console.log(error))
+})
+
+// ---- Single Beer page Route
+app.get('/beers/:id', (req, res) => {
+
+  const beerId = req.params.id
+
+  const singleBeer = punkAPI.getBeer(beerId)
+
+  singleBeer.then(beer => res.render('single-beer-page', { theSingleBeer: beer }))
+
+  singleBeer.catch(error => console.log(error))
+})
+
+// ---- Random Beer page Route
+app.get('/random-beer', (req, res) => {
+
+  // randomBeerFromApi es una promesa pendiente
+  const randomBeerFromApi = punkAPI.getRandom()
+
+  // Si la promesa se cumple
+  randomBeerFromApi.then(responseFromApi => res.render('random-beers-page', { theRandomBeer: responseFromApi }))
+
+  // Si la promesa no se cumple
+  randomBeerFromApi.catch(error => console.log(error))
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
