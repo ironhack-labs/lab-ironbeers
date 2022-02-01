8c00242051aa0e33cf79a5f9ea0ff591986f74aa
andredoc <andre.documet@gmail.com>
2022-02-01 22:35:17 +0100

    done

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require('express');
+const express = require('express');           // Requerir Express
 
-const hbs = require('hbs');
+const hbs = require('hbs');                   // llamo a hbs
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
-const app = express();
+const app = express();                        //Instanciar la aplicación a partir de Express
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //Incluir el middleware de directorio con archivos estáticos public
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));   // Necesario para el SETup de partials. Nota: "Colocar / "
 
 // Add the route handlers here:
 
@@ -22,4 +21,38 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => { 
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers.hbs', { theBeers: beersFromApi});
+      console.log(beersFromApi[0])
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/:id',(req,res)=> {
+  punkAPI
+    .getBeer(req.params.id)
+    .then(responseFromAPI=>{
+      res.render('random-beer', { randomFinalBeer: responseFromAPI })
+    })
+    .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res) => {
+  console.log('random choice')
+
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', { randomFinalBeer: responseFromAPI});
+      console.log(responseFromAPI)
+    })
+    .catch(error => console.log(error));
+});
+
+
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'));   //levantar  el servidor en 3000
+
