67d5a93109fc0bbcc41f18a2343a49d157a60f58
Leonardo <mr_kunkel@hotmail.com>
2022-02-01 17:17:08 -0600

    Iteration 5

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname + '/views/partials'));
 
 // Add the route handlers here:
 
@@ -27,7 +27,7 @@ app.get('/beers', async (req, res) => {
   const allBeers = await punkAPI.getBeers();
 
   res.render('beers', {
-    allBeers: allBeers
+    aBeers: allBeers
   });
 
 });
