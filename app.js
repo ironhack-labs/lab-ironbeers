b427cb0e066c3f8528d75b20251b9462b5eaf34e
Anna Ma Porras <amporras85@gmail.com>
2022-02-01 21:27:02 +0100

    done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(`${__dirname}/views/partials/`)
 // ...
 
 // Add the route handlers here:
@@ -21,5 +21,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { theBeers: beersFromApi }))
+    .catch(error => console.log(error))
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('randomBeer', { theBeers: responseFromAPI })
+    })
+    .catch(error => console.log(error))
+})
+
+// app.get('/beerDetails', (req, res) => {
+//   console.dir(req.params.id)
+//   res.render('beer-details', { theBeers: responseFromAPI })
+//   res.send('funciona')
+// })
+
+
+// app.param('beers', function (req, res, id) {
+//   // try to get the user details from the User model and attach it to the request object
+//   Beers.find(id, function (beer) {
+//     if (beer) {
+//       req.beer = beer
+//       next()
+//     } else {
+//       next(new Error('failed to load beer'))
+//     }
+//   })
+// })
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
