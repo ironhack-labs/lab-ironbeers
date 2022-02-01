bc994e9dc007bee68fd1bb3f720361569a4fc4c2
Leonardo <mr_kunkel@hotmail.com>
2022-02-01 15:27:36 -0600

    Iteration 1

--- app.js
+++ app.js
@@ -21,5 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  res.render('beers');
+});
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer');
+});
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(3000, () => console.log(`It's alive on port 3000`));
