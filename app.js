63387f32fc9d1eb136e4a7669d88b1f62d0c7bd1
angelita15 <95506994+angelita15@users.noreply.github.com>
2022-02-01 22:52:43 +0100

    [MAD DE]Ángela rueda

--- app.js
+++ app.js
@@ -10,16 +10,32 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
 
-// ...
-
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  // ruta del navegador
+  // res.render('beers');  //ruta del archivo puedes ponerle o no el hbs PERO IGUAL QUE EN EL ARCHIVO
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+       res.render('beers', { beers: beersFromApi }); })
+    .catch(error => console.log(error));
+});
+
+app.get('/randombeers', (req, res) => {
+punkAPI
+  .getRandom()
+  .then(beersFromApi => {
+    res.render('randombeers', { randombeers: beersFromApi });
+  })
+  .catch(error => console.log(error));
+});
+app.listen(3000, () => console.log(' on port 3000'));
