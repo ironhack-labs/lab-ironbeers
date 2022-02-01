8864cf29c499ab00ada9f04e50a5401889517f7b
Miguel Ortega Ward <miguel.owd@gmail.com>
2022-02-01 17:30:07 +0100

    Lab doner hasta iteración 5 incluida

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const hbs = require('hbs');
+
+
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -11,6 +12,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(`${__dirname}/views/partials/`)
 
 // Register the location for handlebars partials here:
 
@@ -19,7 +21,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers-page', { beerList: beersFromApi }))
+    .catch(error => console.log(error));
+
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      res.render('random-beer-page', { random: beersFromApi })
+    })
+    .catch(error => console.log(error));
+});
+
+app.listen(5005, () => console.log('🏃‍ on port 5005'));
