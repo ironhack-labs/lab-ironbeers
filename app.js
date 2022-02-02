0f8cdace2330f3ea3974b37d61a5e0558409b836
dorrrn <doan7tran@gmail.com>
2022-02-02 22:42:25 +0100

    added css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const { getEnabledCategories } = require('trace_events');
 
@@ -16,14 +16,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Register the location for handlebars partials here:
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-// ...
 
-// Add the route handlers here:
-
+// Route index.hbs
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// Route beers.hbs
 app.get('/beers', (req, res) => {
   punkAPI
     .getBeers()
@@ -34,6 +33,7 @@ app.get('/beers', (req, res) => {
     .catch(error => console.log(error));
 });
 
+// Route randombeer.hbs
 app.get('/random-beer', (req, res) => {
   punkAPI
     .getRandom()
