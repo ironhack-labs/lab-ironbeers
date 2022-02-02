bbd9f4b324fece5702aba5abd4d9019f08be0703
GerardoRegalado <gerardo.regalado24@gmail.com>
2022-02-01 18:00:05 -0600

    se agrega ruta beer

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// importaciones 
 const express = require('express');
 
 const hbs = require('hbs');
@@ -7,6 +8,10 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// Middlewares
+
+
+require("dotenv").config()
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -14,12 +19,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+//hbs.registerPartial(__dirname + "/views/partials")
+
 // ...
+
+
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+
+
+app.get("/", (req, res) => {
+res.render("index");
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get("/beer", (req,res)=> {
+  res.render("beer")
+})
+
+
+
+
+//Servidor
+
+app.listen(3000, () => console.log('servidor activo'));
