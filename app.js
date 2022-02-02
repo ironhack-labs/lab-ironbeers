e4499ad1bce6973d7a238f02baf219329964e341
dorrrn <doan7tran@gmail.com>
2022-02-02 16:21:43 +0100

    added hbs pages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { getEnabledCategories } = require('trace_events');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -22,4 +23,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beersFromApi });
+      console.log('Beers from the database: ', beersFromApi);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('randomBeer', { responseFromAPI });
+      console.log('Random beer from the database: ', responseFromAPI);
+    })
+    .catch(error => console.log(error));
+});
+
+app.listen(4001, () => console.log('🏃‍ on port 4001'));
