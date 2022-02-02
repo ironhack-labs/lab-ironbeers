91fee464396d960d82553df5492814c7d15e4114
Emanueljuarez <zerauleuname@gmail.com>
2022-02-01 18:44:19 -0600

    Iteraciones basicar terminadas

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,6 @@ router.get("/", (req, res) => {
 	res.render("index")
 })
 
-// router.get("/beers", (req, res) => {
-// 	res.render("beers")
-// })
-
 router.get("/beers", async (req, res) => {
 
 	const allBeers = await punkAPI.getBeers()
@@ -23,9 +19,20 @@ router.get("/beers", async (req, res) => {
 	})
 })
 
-router.get("/random-beer", (req, res) => {
-	res.render("random-beer")
+// router.get("/random-beer", (req, res) => {
+// 	res.render("random-beer")
+// })
+
+router.get("/random-beer", async (req, res) => {
+
+	const randomBeer = await punkAPI.getRandom()
+
+	
+	res.render("random-beer", {
+		randomBeer: randomBeer,
+	})
 })
+
 
 //exportacion
 
