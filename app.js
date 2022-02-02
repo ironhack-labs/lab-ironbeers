373ff670086de2faaa8f7d4a0e68e933d5e452ee
Emanueljuarez <zerauleuname@gmail.com>
2022-02-01 18:25:16 -0600

    Iteracion 3

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+//IMPORTACIONES
 const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
 
 const app = express();
-const punkAPI = new PunkAPIWrapper();
+
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -14,14 +15,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname + '/views/partials'));
+
 
 // Add the route handlers here:
+//RUTAS
 
-// app.get('/', (req, res) => {
-//   res.render('index');
-// });
 
 app.use("/", require("./routes/index"))
+
+//SERVIDOR
 
 app.listen(3000, () => console.log('http://localhost:3000'));
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 //importaciones
 const express        = require("express")
 const router         = express.Router()
-
-
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
 //ruteo
 
 router.get("/", (req, res) => {
 	res.render("index")
 })
 
-router.get("/beers", (req, res) => {
-	res.render("beers")
+// router.get("/beers", (req, res) => {
+// 	res.render("beers")
+// })
+
+router.get("/beers", async (req, res) => {
+
+	const allBeers = await punkAPI.getBeers()
+
+	
+	res.render("beers", {
+		allBeers: allBeers,
+	})
 })
 
 router.get("/random-beer", (req, res) => {
