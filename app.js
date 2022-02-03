3cfc4688a38e2a0f55dd5c9b7aa239b97e44b3ba
Bart Huiskamp <bartolameus.h@gmail.com>
2022-02-03 08:51:56 +0100

    barts beer lab

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -14,12 +15,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+app.get("/", function (req, response, next) {
+  response.render("index")
+});
+
+app.get("/beers", function (req, response, next) {
+  punkAPI
+  .getBeers()
+  .then((beersFromApi) => response.render("beers", {beerlist: beersFromApi}))
+  .catch(error => console.log(error))
+
+});
+
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      res.render("random-beer", { random: beersFromApi })
+    })
+  .catch(error => console.log(error));
+})
+
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then((result) => {
+    console.log(result[0])
+    res.render('beers', result)
+  })
+  .catch()
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
