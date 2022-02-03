54148976f3c8f577a68959a72d43cae2c4a7764b
Lamer217 <males.public@gmail.com>
2022-02-03 21:19:51 +0100

    TBA: iteration 6, 7

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+hbs.registerPartial('beerpartial', '{{beerpartial}}');
 
 // Register the location for handlebars partials here:
 
@@ -20,6 +22,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then(beersFromApi => {
+    // console.log('Beers from the database: ', beersFromApi);
+    res.render('beers', {
+      beersArr: beersFromApi
+    });
+  });
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom().then(beer => {
+    // console.log(beer);
+    res.render('random-beer', {
+      ranBeer: beer
+    });
+  });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
