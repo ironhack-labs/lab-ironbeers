db9ed6f7c594da9cfff394be23e1314d6b0d1b10
yoricktf <yorick.tenfeld@gmail.com>
2022-02-04 14:51:49 +0100

    finsished lab

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // Register the location for handlebars partials here:
 
 // ...
@@ -20,6 +20,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(allBeers => res.render('beers', {beers: allBeers}))
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(randomBeer => res.render('random', {beers: randomBeer}))
+  .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+  punkAPI.getBeer(req.params.id)
+  .then(selectBeer => {
+    res.render('specficBeer', {beers: selectBeer})
+  })
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
