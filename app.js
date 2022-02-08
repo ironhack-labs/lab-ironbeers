974a3f801ca67dd98101b57037ab9b3994be74c8
Miguel <info.miguelbanos@gmail.com>
2022-02-08 13:11:46 +0100

    lab complety done, and solve the port issues with 'npx kill-port PORT'

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const res = require('express')
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
@@ -9,7 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
+hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -20,6 +20,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get("/beers", (req, res) => {
+  punkAPI
+    .getBeers()
+      .then(beers => {
+        res.render("beers", { beers: beers })
+      })
+})
+app.get("/random-beer", (req, res) =>{
+  punkAPI
+  .getRandom()
+  .then(beers => {
+    res.render("randomBeer", {beer: beers[0] })
+    
+  })
+  .catch(error => console.log(error));
+})
+
+app.use((req, res) => {
+  res.status(404).send("Not found");
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
