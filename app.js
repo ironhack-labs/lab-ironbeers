2d0c9b66a2111ffbabcac4efdafc82949e82be10
Olena Kraemer <olenakraem@gmail.com>
2022-03-12 14:33:03 +0100

    done

--- app.js
+++ app.js
@@ -18,8 +18,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.render('index');
+});
+
+//Iteration 3.1 - route for the beers page
+app.get('/beers', (_req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beersFromApi }))
+    .catch(err => console.log(err));
+});
+
+//route for the random-beer page
+app.get('/random-beer', (_req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      console.log(beersFromApi), res.render('random-beer', { beersFromApi });
+    })
+
+    .catch(err => console.log(err));
+});
+
+app.get('/beers/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+
+  punkAPI
+    .getBeer(id)
+    .then(beerFromApi => {
+      console.log(beerFromApi[0]);
+      res.render('beer', { beerFromApi: beerFromApi[0] });
+    })
+    .catch(err => console.log(err));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
