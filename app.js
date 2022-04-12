e2793bd2c4b46bc6f90d17c39ecc8da1d82a7ded
Arturo Echanique <arturoechanique@gmail.com>
2022-04-12 22:30:54 +0200

    lab solved

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const req = require('express/lib/request');
 
 const hbs = require('hbs');
 const path = require('path');
@@ -12,6 +13,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -20,6 +23,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+  console.log("i am /")
 });
+
+
+app.get('/beers', (req, res) => {
+
+  console.log("i am beers")
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beersFromApi }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+
+  console.log("i am random beer")
+  punkAPI
+    .getRandom()
+    .then(beers => res.render('beer', { beers }))
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+
+  console.log("el parametro es: ", req.params.id)
+  punkAPI
+    .getBeer(req.params.id)
+    .then(beers => res.render('beer', { beers }))
+    .catch(error => console.log(error));
+});
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
