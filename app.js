a9ddb533cd6f1f6ee34e64521e7e4181b5fc94ad
Gabriela <s.gabriela.bv96@gmail.com>
2022-04-12 23:38:21 +0200

    done

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Register the location for handlebars partials here:
 
@@ -18,8 +19,58 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+//RUTA DEL HOME
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+//RUTA DE LA LISTA DE LAS CERVEZAS
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beersFromApi: beersFromApi });
+      // console.log(beersFromApi);
+    })
+
+    .catch(error => console.log(error));
+});
+
+//RUTA DE LAS CERVEZAS RANDOM
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', { responseFromAPI: responseFromAPI });
+      // console.log(responseFromAPI);
+    })
+    .catch(error => console.log(error));
+});
+
+// RUTA DE "LA CERVEZA ELEGIDA"
+
+app.get('/beer/:beer_id', (req, res) => {
+  const id = req.params.beer_id;
+
+  const theBeer = punkAPI
+    .getBeer(id)
+    
+    .then(theBeer => {
+      res.render('the-beer', { theBeer: theBeer });
+      // console.log(responseFromAPI);
+    })
+    .catch(error => console.log(error));
+
+
+
+
+
+  console.log(theBeer);
+
+  ;
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
