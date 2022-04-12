8ee203983328ad6d78359fcda4d09349fa3f15fe
María Fdez de la Rosa <7mariadlrosa@gmail.com>
2022-04-12 18:59:37 +0200

    doneee

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -21,5 +23,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+app.get("/beers", (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render("beers", { beersFromApi }))
+    .catch(error => console.log(error))
+});
+
+
+app.get("/random-beer", (req, res) => {
+
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render("random-beer", { responseFromAPI})
+    })
+    .catch(error => console.log(error));
+  
+});
+
+// app.get("/jugadores-partials", (req, res) => {
+//   res.render("players-page-partials", { players: players })
+// });
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
