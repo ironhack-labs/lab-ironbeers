700d049f96b5f2d06c77fcfdcd0116c465904755
aBoswell <A.B0ZZW3LL@gmail.com>
2022-04-13 13:34:45 -0400

    beers and randombeer views added

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-hbs.registerPartials(__dirname + "/views/partials");
+//hbs.registerPartials(__dirname + "/views/partials");
 
 // ...
 
@@ -24,11 +24,27 @@ app.get('/', (req, res) => {
 });
 
 app.get('/beers', (req, res) => {
-  res.render('beers');
+  punkAPI.getBeers()
+    .then(beersList => {
+      console.log('beers from api', beersList[0])
+      res.render('beers', { beersList });
+    })
+    .catch(err => {
+      console.log('error', err);
+      res.status(500).send('sorry a server error occured'); // <- force a '500' and "send"/throw a user facing message should the promise fail to fulfill. 
+    })
 });
 
 app.get('/random-beer', (req, res) => {
-  res.render('randomBeer');
+  punkAPI.getRandom()
+    .then(randomBeer => {
+      console.log(randomBeer);
+      res.render('randomBeer', {randomBeer: randomBeer[0]}); // <-- this was needed, prior it would not render
+    })
+    .catch(err => {
+      console.log('error', err);
+      res.status(500).send('sorry could not retreive a random beer');
+    })
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
