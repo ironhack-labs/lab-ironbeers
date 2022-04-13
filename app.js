3ec409960d276a65653489c6e758fe5b0ca2aaf6
mdelgado333 <101524023+mdelgado333@users.noreply.github.com>
2022-04-13 08:58:23 +0200

    done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
@@ -14,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
@@ -22,4 +23,31 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', {responseFromAPI})
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req,res) => {
+  punkAPI
+  .getBeer(req.params.id)
+  .then(beersFromApi => {
+    res.render('beers', {beersFromApi})
+  })
+  .catch(error => console.log(error));
+})
+
+
+
+app.listen(3000, () => console.log('running on port 3000'));
