ea893f9c57b702d192e5eadd0de1e626c133a7e7
Takyoung Woo <takyoung.w@gmail.com>
2022-04-14 17:44:18 +0200

    done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { getEnabledCategories } = require('trace_events');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -13,13 +14,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
 
+punkAPI.getBeer()
+
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log(beersFromApi)
+    res.render('beers', { beerList: beersFromApi });
+  })
+});
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', {randomBeer: responseFromAPI});
+  })
+  .catch(error => console.log(error))
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
