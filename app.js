6313e320d740b42e6533933f503ce77d09d8f44d
Julian Rivera <julianarevircoins@gmail.com>
2022-04-17 00:30:55 -0400

    Still need to do the bonus

--- app.js
+++ app.js
@@ -19,7 +19,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      // console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { beersFromApi });
+    })
+
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('sorry a server error occured');
+    });
+});
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      // console.log('Beers from the database: ', beersFromApi);
+      res.render('random-beer', { randomBeer: randomBeer[0] });
+    })
+
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('sorry a server error occured');
+    });
+});
+
+app.get('/random-beer', (req, res, next) => res.render('random-beer'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
