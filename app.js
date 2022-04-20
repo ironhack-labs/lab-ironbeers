ed57de60b1fd5234d224ea2ecc115de12a87125d
Jawid <41361157+infjay@users.noreply.github.com>
2022-04-20 18:57:48 +0200

    all pages added

--- app.js
+++ app.js
@@ -22,4 +22,23 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi =>{
+  console.log('Beers from the database: ', beersFromApi[0])
+    res.render('beers', beersFromApi)})
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+  console.log('Random Beers from DB', responseFromAPI);
+  res.render('random-beer');
+  })
+  .catch(error => console.log(error));
+  
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
