fc5fe0adc25210f410190c5351e8eb07201fe994
mafaldao21 <mafalda.oliveira.21@icloud.com>
2022-04-20 16:03:43 +0100

    fix path

--- app.js
+++ app.js
@@ -13,13 +13,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+app.get("/random-beer", (req, res) => {
+  res.render("random-beer");
+});
+
+
+
+// Add the route handlers here:
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then( (beersFromApi) => {
+      res.render("beers", {
+        beersFromApi
+      })
+    })
+    .catch(error => console.log(error));
+});
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
