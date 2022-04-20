f58354ccabd96d95ba1e2cd9caf79f99b27e0b17
nash0x <nachoest@gmail.com>
2022-04-19 21:01:43 -0600

    Missing styles

--- app.js
+++ app.js
@@ -15,11 +15,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Register the location for handlebars partials here:
 
 // ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get("/beers", (req, res)=>{
+  punkAPI.getBeers()
+  .then((beersFromApi)=>{
+    //console.log("Beers from the database: ", beersFromApi)
+    res.render("beers",{beersFromApi})
+  })
+  .catch((error)=>{
+    res.send("Error")
+  })
+  
+})
+
+app.get("/random-beer", (req, res)=>{
+  punkAPI.getRandom()
+  .then((responseFromAPI )=>{
+    //console.log("Random beer", responseFromAPI[0])
+    res.render("random-beer",responseFromAPI[0])
+  })
+  .catch(()=>{
+
+  })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
