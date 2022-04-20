4cd123b4dae71618c382d76bc488e025f76413aa
Alexander <alexander.nerz@web.de>
2022-04-20 18:55:37 +0200

    finished iteration  1-4 with some bugs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -18,8 +19,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+
+app.get('/beers', (req, res, next) => {
+  
+  punkAPI
+  .getBeers()
+  .then((beersFromApi) => {
+    console.log(beersFromApi)
+  res.render("beers", {beersArr: beersFromApi});
+  }) 
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res, next) => {
+
+  const randomBeer = PunkAPI.getRandom()
+  .then(responseFromApi => {
+    // console.log("Beers from the database: ", beersFromApi)
+    
+  res.render("randomBeer", {randomBeer});
+  }) 
+  .catch(error => console.log(error));
+});
+
+
+
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(3004, () => console.log('🏃‍ on port 3004'));
