fc6fe84baa39c1799030d48af076077c0dc4f7d4
Alfonso Cano <alfonsoheartagram@gmail.com>
2022-04-24 22:02:54 -0500

    done

--- app.js
+++ app.js
@@ -22,4 +22,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get("/beers", (req, res) => {
+  punkAPI.getBeers()
+  .then((chelas) => {
+    console.log("Beers", chelas)
+    res.render('beers', { chelas })
+  })
+  .catch((error) => console.log("El Error es:", error))
+})
+
+app.get("/random-beer", (req, res) => {
+  punkAPI.getRandom()
+  .then((beer) => {
+    console.log("Beer random", beer)
+    res.render('random-beer', { beer })
+  })
+  .catch(error => console.log("El Error es:", error))
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
