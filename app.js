1fbb93670201bd840c006260be8a0bdbd26492fd
David Garcia <metaphysicaless1489@gmail.com>
2022-04-25 21:06:44 -0600

    beginning

--- app.js
+++ app.js
@@ -13,13 +13,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + 'views')
 
 // ...
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.get("/", (req, res, next) => res.render("index"));
+
+app.get("/beers", (req, res, next) => {
+  punkAPI.getBeers()
+  .then((beers)=> {
+    console.log('beers', beers)
+    res.render('beers', {beers})
+  })
+  .catch(error =>console.log('what is it?', error))
+})
+
+app.get("/random-beer", (req, res, next) => {
+  punkAPI.getRandom()
+  .
+
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
