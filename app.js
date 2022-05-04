fdcd3494958e8f6fb22a814375c6248137e0903e
FranciscoGBento <f_m_bento@hotmail.com>
2022-05-04 18:46:05 +0100

    Iterations

--- app.js
+++ app.js
@@ -14,12 +14,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+//hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      console.log('Beers from the database: ', beers);
+      res.render('beers', {beers});
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/randombeers', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(resApi => {
+    console.log('Random Beer', resApi)
+    res.render('randomBeer', {resApi})
+  })
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
