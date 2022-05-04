51ea8f3412763bb4763e9614882a4944107dfe8a
Pipokinha <pipokinha@superpro.email>
2022-05-04 23:49:49 +0100

    done

--- app.js
+++ app.js
@@ -21,5 +21,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
-
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    console.log('Beers from the database: ', beers)
+    res.render('beer', {beers});})
+  .catch(error => console.log(error));
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeers => {
+    res.render('randomBeer', {randomBeers});
+  })
+  .catch(error => console.log(error));
+  
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
