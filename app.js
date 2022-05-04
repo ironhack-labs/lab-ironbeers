381bbc90a3cbeed69a730f64e5ba52586ce6fe02
AnaPinho1990 <anagonpinho@gmail.com>
2022-05-04 18:46:56 +0100

    done mandatory

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 // Register the location for handlebars partials here:
-
-// ...
-
+hbs.registerPartials(__dirname + '/views/');
 // Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
 });
-
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log(beersFromApi);
+      res.render('beers', { beerList: beersFromApi });
+    })
+    .catch(err => console.log(err));
+});
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      res.render('randombeer', { randomBeer });
+    })
+    .catch(err => console.log(err));
+});
+app.listen(3000, () => console.log(':runner:‍ on port 3000'));
