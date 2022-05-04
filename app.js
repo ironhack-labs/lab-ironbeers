0bd7d0b95ee25ec1ce5d8d9d2af2fd1cbc2ed291
Marina Palma <marina_palma@hotmail.com>
2022-05-04 23:08:32 +0100

    lab

--- app.js
+++ app.js
@@ -14,12 +14,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    console.log('Beers from the database: ', beers)
+  res.render('beers', {beers});
+  })
+
+  .catch(error => console.log(error));
+
+  
+});
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randombeer => {
+    res.render('random-beer', {beer: randombeer[0]});
+  })
+  .catch(error => console.log(error));
+
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
