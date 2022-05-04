775c80cb118a96eb68c156d6e5b8a5b774aa7dac
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-05-04 19:23:02 +0100

    first commit

--- app.js
+++ app.js
@@ -14,12 +14,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then(beersFromApi => {
+    res.render('beers', {beersFromApi})
+  }).catch(error => console.log(error));
+
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom().then(randomBeer => {
+    res.render('random-beer', {randomBeer})
+  }).catch(error => console.log(error));
+
+});
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
