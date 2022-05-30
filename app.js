b68d91ba95f2e481483ef57c710ead03c9fa0f68
ana luz martinez <almadigital.mx@gmail.com>
2022-05-29 18:01:14 -0700

    having trouble fetching the api. went ahead and did more partials

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
-// app.use(express.static(path.join(__dirname, 'public')));
+//It used to be: app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// hbs.registerPartials(path.join(__dirname, 'views/partials'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
@@ -22,4 +22,30 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+//all beers
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beerApi => {
+      res.render('beers', { beerApi });
+    })
+    .catch(error => {
+      console.log('Error fetching api', error);
+    });
+});
+
+//random beers
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeerApi => {
+      res.render('random-beer', { randomBeerApi });
+    })
+    .catch(error => {
+      console.log('Error fetching api', error);
+      res.send('No random beers were found');
+    });
+});
+
+//start server
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
