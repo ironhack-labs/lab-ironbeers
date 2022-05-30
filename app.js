3f6bf5d1be0e4b9cc400279062452645ed0b9b93
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-05-30 18:46:13 -0500

    lab solved wo css

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require('express'); // al ejecutar express lo que tienes es un objeto
 const { get } = require('express/lib/response');
 
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
-const app = express();
+const app = express(); //este es tu servidor. 
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //all static files are in public file.
+//static files can be styles, images.
 
 // Register the location for handlebars partials here:
 
-hbs.registerPartials(path.join(__dirname, 'views/partials'));
+q
 
 // Add the route handlers here:
 
@@ -28,7 +29,7 @@ app.get('/beers', (req, res) => {
 
   punkAPI
   .getBeers()
-  .then(beersFromApi => {/* console.log('Beers from the database: ', beersFromApi) Pregunta cómo consologear esto*/
+  .then(beersFromApi => { /* console.log('Beers from the database: ', beersFromApi)  */
   res.render('beers',{beersFromApi});
 })
   .catch(error => console.log(error));
@@ -44,15 +45,27 @@ app.get('/random-beer', (req, res) => {
 
     res.render('random-beer',{responseFromAPI});
 
-
   })
-  .catch(error => console.log(error));
-
-
-  
+  .catch(error => console.log(error));  
 });
 
+app.get('/:id', (req, res) => {
+punkAPI
+.getBeer(req.params.id)
+.then(beerID => {
+res.render('beersbyid',{beerID});
+})
+.catch(error => console.log(error));
 
+});
+
+//punkAPI.
+
+
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Resource not found</h1>');
+})
 
 
 
