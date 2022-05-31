fc2d99b05be857478b531af5c930cdda8e0042f7
Evgeniia Riaguzova <aglaya_2004@mail.ru>
2022-05-31 18:33:24 +0200

    with problems

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -21,5 +23,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  res.sendFile(__dirname + '/views/beers.hbs')
+
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+     res.render('beers', {beersFromApi})
+  })
+  .catch(error => console.log(error))
+});
+
+app.get('/randomBeer', (req, res) => {
+  res.sendFile(__dirname + '/views/randomBeer.hbs')});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
