429e47be16b492d10132b7af042d8899f64484a4
CarolinaArlone <carolinaarlone@gmail.com>
2022-06-21 18:50:58 +0200

    [MAD DE 0522]Carolina Arlone

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { read } = require('fs');
 
 const hbs = require('hbs');
 const path = require('path');
@@ -12,14 +13,42 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+
+  //res.send('No arriesgo')
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+
+  //res.send('No arriesgo')
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { theBeers: beersFromApi }))
+    .catch(error => console.log(error));
+
+
+});
+
+app.get('/random-beer', (req, res) => {
+
+  //res.send('No arriesgo')
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      console.log(responseFromAPI)
+      res.render('random-beer', { theRandomBeers: responseFromAPI })
+    })
+    .catch(error => console.log(error));
+
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
