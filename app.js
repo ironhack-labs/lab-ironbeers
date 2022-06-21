257c2e516060f2e5c2ff4103979748c936f6586d
Javier Ruiz <javierdeveloper1@gmail.com>
2022-06-21 20:04:23 +0200

    done

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const hbs = require('hbs');
+
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
@@ -19,7 +20,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beersFromApi }))
+    .catch(error => console.log(error));
+
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => res.render('random-beer', { responseFromAPI }))
+    .catch(error => console.log(error));
+
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
