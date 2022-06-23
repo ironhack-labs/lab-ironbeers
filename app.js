fc02b116c08d89fce5a60d6cc2c77ef6db58fd92
luismunoz83 <javier@Castellanas-MBP.home>
2022-06-23 22:47:48 +0200

    done

--- app.js
+++ app.js
@@ -22,4 +22,19 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()
+    .then(beersFromApi => res.render('beers', {beers: beersFromApi}))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+
+  punkAPI.getRandom()
+  .then(responseFromAPI => res.render("random-beer", {responseFromAPI: responseFromAPI[0]}))
+  .catch(error => console.log(error));
+});
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
