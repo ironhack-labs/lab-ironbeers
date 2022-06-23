7ae6c8f66e7e2b44fe63087390c041790fe46c4d
Deniz Saganak <saganak.deniz@gmail.com>
2022-06-23 17:42:00 +0200

    done

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { getEnabledCategories } = require('trace_events');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,5 +26,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+app.get('/beers', function(req, res){
+  punkAPI
+  .getBeers()
+  .then(function(beersFromApi) {
+    console.log(beersFromApi[0])
+    res.render('beers', {beerList: beersFromApi})
+    //res.render's second parameter has to be an object with a key
+    //for the first time we create beersFromAapi
+    //beerList is just a name we give to the object as a key name 
+    //console.log('Beers from the database: ', beersFromApi)
+  })
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beer', function(req, res){
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', {randomBeer: responseFromAPI[0]})
+    //getRandom method always gives one item that's why it
+    //is zero indexed [0]
+
+  })
+  .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
