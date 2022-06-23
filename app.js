88cb687203aefeba86b0b51357f5cf1d9268211e
CrisArranz <c.arranz.arevalo@gmail.com>
2022-06-23 22:01:08 +0200

    First commit - four iterations without styles

--- app.js
+++ app.js
@@ -22,4 +22,18 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => res.render('beers', { beers } ))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => res.render('random-beer', {beer: beer[0]}))
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
