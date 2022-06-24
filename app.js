2c27a7d9e1d727f67bcd8f54222c9099bc3d74cc
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-06-24 12:39:10 +0200

    iteractions 1, 2, 3 and 4 done

--- app.js
+++ app.js
@@ -23,10 +23,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/beers', (req, res) => {  
-  punkAPI.getBeers()
-  .then(beersFromApi => console.log(/* 'Beers from the database: ', beersFromApi */'beers charged'))
-  .catch(error => console.log(error));
-  res.render('beers', beersFromApi);
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', {beers: beersFromApi}))
+    .catch(error => console.log(error));
+  
 });
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => res.render('random-beer', {random: responseFromAPI}))
+    .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
