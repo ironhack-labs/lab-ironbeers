8870b035a789d93344ad445834eea291a340c826
Meryl Kirchhoff <meryl.kirchhoff@gmx.de>
2022-06-25 15:03:30 +0200

    lab ironbeers version1

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { getEnabledCategories } = require('trace_events');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,8 +21,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
   res.render('index');
 });
+
+app.get('/beers', function(req, res) {
+  punkAPI
+  .getBeers()
+  .then(function(beersFromApi) {
+    res.render('beers', {beerList: beersFromApi})
+    console.log('Beers from the database: ', beersFromApi)}
+  )
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beer', function(req, res) {
+  punkAPI
+  .getRandom()
+  .then(function(responseFromApi) {
+    res.render('random-beer', { randomBeer: responseFromApi[0] })
+  })
+  .catch(error => console.log(error));
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
