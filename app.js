563188c3dc7be3c0f94cb62eb93fec6d67d5d6a2
LuisEUM <luiseum95@gmail.com>
2022-06-25 11:39:47 +0200

    Partial Working

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials("partials_absolute_path") // para poder agregar partials creamos esto al principio
+
+// estan son de requisito para el funcionamiento del hbs
+app.set('view engine', 'hbs'); 
+app.set('views', path.join(__dirname, 'views'));
+// hasta aqui los requisitos del hbs
+hbs.registerPartials(__dirname + "/views/partials"); // esta es obligatoria para los partials
 
 
 
@@ -19,15 +24,36 @@ app.get("/", (req, res, next) => {
 });
 
 app.get("/beers", (req, res, next) => {
-  
   punkAPI
   .getBeers()
     .then(beersFromApi => {
       res.render("beers", {beersFromApi}) 
+      console.log(beersFromApi)
     })
     .catch(error => console.log(error));
 
 });
+
+app.get("/random-beer", (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log(responseFromAPI)
+    res.render("random-beer", {responseFromAPI}) 
+  })
+  .catch(error => console.log(error));
+});
+
+// app.get("/beers/:id", (req, res, next) => {
+  
+//   punkAPI
+//   .getBeers()
+//     .then(beersFromApi => {
+//       res.render("beers", {beersFromApi}) 
+//       console.log(beersFromApi)
+//     })
+//     .catch(error => console.log(error));
+// });
 
 
 
