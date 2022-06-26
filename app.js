37faf2ee20aea9b9c7b71f9527092bac8c12e19f
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-06-26 22:22:57 +0200

    no bonus content

--- app.js
+++ app.js
@@ -13,10 +13,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Add the route handlers here:
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', { beerArr: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get ('/random-beers',function(request, response){
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    response.render('random-beer', {beers: responseFromAPI[0]});
+  })
+  .catch(error => console.log(error));
+});
 
 app.get('/', (req, res) => {
   res.render('index');
