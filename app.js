c8662027e8256d24ac739543b803710258a792e8
Moisés Vargas Rangel <vargasrangelaudio@gmail>
2022-06-29 20:39:57 -0500

    Me faltó darle un poquito de style

--- app.js
+++ app.js
@@ -22,4 +22,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+      const randomBeer = punkAPI.getRandom()
+      randomBeer.then(beer => {
+        res.render('random-beers', {beer});
+      })
+      .catch(error => console.log(error));
+    })
+
+  
+  
+
+
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
