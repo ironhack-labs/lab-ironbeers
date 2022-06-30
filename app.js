bf62a2ecf39525c4c004f5ce745c2207dacb8932
unknown <sandrapl.18@gmail.com>
2022-06-29 22:22:24 -0700

    Iron beers done

--- app.js
+++ app.js
@@ -9,17 +9,31 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
 // Register the location for handlebars partials here:
-
 // ...
-
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  const beers = punkAPI.getBeers();
+  beers.then(beersObj => res.render('beers', {beersObj}))
+  //console.log(beersObj);
+.catch(error => console.log(error));
+
+});
+
+app.get('/random-beer', (req, res) => {
+  const randomBeer = punkAPI.getRandom();
+  randomBeer.then(beer => {
+  res.render('random-beer', {beer});
+}).catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
