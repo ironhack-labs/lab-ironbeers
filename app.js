8e7a4fb8e099bca1b03d6a216e0dcf40fedc0644
“Alexandre <coma.alexandre@gmail.com>
2022-07-06 18:39:38 +0200

    Lab done with problem on first bonus

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
@@ -22,4 +22,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      res.render('beers', {beersFromApi})
+    })
+    .catch(error => console.log(error))
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', {responseFromAPI});
+  })
+  .catch(error => console.log(error));
+});
+
+app.listen(3001, () => console.log('🏃‍ on port 3001'));
+
