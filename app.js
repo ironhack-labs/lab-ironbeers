73be2f73a4c919e1a8d4c741c65e2004d66ba6a0
John Doe <lucas.lopez.borchardt@gmail.com>
2022-07-06 18:16:12 +0200

    iteracion 3

--- app.js
+++ app.js
@@ -23,7 +23,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/beers', (req, res) => {
-  res.render('beers')
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {    
+    res.render('beers', {beers: beersFromApi})
+  })
+  .catch (error => console.log(error)) 
+  
 })
 
 app.get('/random-beers', (req, res) => {
