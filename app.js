b031528cb13f4d76377c601294f9a6e7963fa789
Nokeen <cordrepayne@gmail.com>
2022-07-06 01:43:09 -0500

    Solved lab

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { getEnabledCategories } = require('trace_events');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -21,5 +23,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', { "beerArray": beersFromApi })
+    })
+    .catch(err => console.log(err));
+});
+
+app.get('/random-beer', async (req, res, next) => {
+
+  try{
+    let myRandomBeerArray = await punkAPI.getRandom();
+    console.log(myRandomBeerArray);
+    res.render('randomBeer', myRandomBeerArray[0]);
+  }catch (error){
+    console.log(error);
+    res.send('Error retrieving random beer')
+  }
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
