fcf4df8a44763fcc0bab80e871fc730f68906fd2
Nic Solitom <nicsolitom@gmail.com>
2022-07-07 08:40:01 +0200

    Nic Solitom - done

--- app.js
+++ app.js
@@ -22,4 +22,32 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  let myData;
+
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      myData = beersFromApi;
+      res.render('beers', { myData });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      const randomBeer = {};
+      randomBeer.name = beer[0].name;
+      randomBeer.image_url = beer[0].image_url;
+      randomBeer.description = beer[0].description;
+      randomBeer.tagline = beer[0].tagline;
+      randomBeer.food_pairing = beer[0].food_pairing;
+      randomBeer.brewers_tips = beer[0].brewers_tips;
+      res.render('random-beers', randomBeer);
+    })
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
