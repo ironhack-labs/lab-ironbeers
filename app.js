846e4325feba214451737509e5a2ed620dce0eb4
Marina Laporte Boni <marinaolboni@gmail.com>
2022-07-12 14:32:25 +0200

    cant render random beer

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
@@ -12,14 +11,38 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
+hbs.registerPartials(__dirname + '/views/partials'); //partials (ex: navbar || footer)
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  const randomBeer = punkAPI.getRandom();
+  randomBeer
+    .then(beer => {
+      res.render('random-beer', beer);
+    })
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+// app.get("/users/:id", (req, res, next) => {
+//   const idParam = req.params.id;
+
+//   fetch(`https://jsonplaceholder.typicode.com/users/${idParam}`)
+//     .then((response) => response.json())
+//     .then((user) => {
+//       res.render("detail", { user });
+//     });
+// });
