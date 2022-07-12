08e49f042b99ad465c754d23c2a6b6d24f9dc303
Nahum Jaime <nahum.jsz@gmail.com>
2022-07-12 23:53:40 +0200

    lab done, pending bonus

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Register the location for handlebars partials here:
 
@@ -20,6 +22,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  //.then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .then((beers) => res.render("beers", { beers }))
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then((beers) => res.render("random-beer", { beers }))
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
