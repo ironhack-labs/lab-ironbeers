bdc6908c6680ac9096d1f68f9cf38a54b31ead35
Juan De Oliveira <juanabrahamd@gmail.com>
2022-07-13 18:36:31 +0200

    En proceso

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -7,19 +8,31 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname + '/views'));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  res.render('beers');
+});
+
+app.get('/random-beer', (req, res) => {
+  fetch('https://punkapi-javascript-wrapper-demo.glitch.me/beer/random')
+  .then((response) => response.json())
+  res.render('random-beer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
