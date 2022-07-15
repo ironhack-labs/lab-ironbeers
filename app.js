4a419512ae0fccc7457706998adc3f13df168983
Randolph Roy <randolphroy@me.com>
2022-07-15 19:30:18 -0400

    RMT-WEBDEV-JUNE2022-RANDOLPH ROY

--- app.js
+++ app.js
@@ -13,10 +13,33 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials("partials_absolute_path")
 
 // ...
 
 // Add the route handlers here:
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', { "beerArray": beersFromApi })
+    })
+    .catch(err => console.log(err));
+});
+
+app.get('/random-beer', async (req, res, next) => {
+
+  try{
+    let myRandomBeerArray = await punkAPI.getRandom();
+    console.log(myRandomBeerArray);
+    res.render('randomBeer', myRandomBeerArray[0]);
+  }catch (error){
+    console.log(error);
+    res.send('Error retrieving random beer')
+  }
+})
 
 app.get('/', (req, res) => {
   res.render('index');
