5b028e906b205a380b4905efbfbf613e24b6e4ea
idoiafforero <idoia.forero@gmail.com>
2022-07-19 18:06:11 +0200

    First Commit

--- app.js
+++ app.js
@@ -8,18 +8,32 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(__dirname + "/views/partials");
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('home');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{ beers });
+  })
+  .catch(err => console.error(err));
+});
+
+app.get()
+
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
