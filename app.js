17f50454ce695160edb5d0bf1b0e14c67896a9ef
HanhDinh <ms.myhanh89@gmail.com>
2022-07-26 14:30:38 -0400

    solved lab 1-5

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,5 +22,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi =>{
+    console.log ('Beers from the database: ', beersFromApi)
+    res.render('beers.hbs', {"beerArray": beersFromApi})
+  })
+  .catch(err=> {
+    console.log('Error retrieving beers: ', err);
+    res.render('Error retrieving beers');
+  })
+});
+
+app.get('/random-beer', async (request, response, next) => {
+  try{
+    let myRandomBeerArray = await punkAPI.getRandom();
+    console.log(myRandomBeerArray);
+    response.render('randomBeer.hbs', myRandomBeerArray[0]) ;
+  } catch (error){
+    console.log('Error retrieving random beer: ', error);  
+    res.send('error retrieving random beer'); 
+  }
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
