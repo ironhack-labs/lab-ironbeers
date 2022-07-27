a422a70debb08e7239fc9414fccb865e692f14ad
Simão Lima <simaodosreislima@gmail.com>
2022-07-27 17:37:33 +0100

    mandatory iterations done

--- app.js
+++ app.js
@@ -18,8 +18,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get("/beers", (req,res,next) =>{
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render("beers", {beersFromApi}))
+  .catch(error => console.log(error));
+});
+
+app.get("/random-beer", (req,res,next) =>{
+  punkAPI
+  .getRandom()
+  .then(beersFromApi => res.render("random-beer", beersFromApi[0]))
+  .catch(error => console.log(error));
+});
+
+app.listen(3001, () => console.log('🏃‍ on port 3001'));
