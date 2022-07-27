5c868a4bfe89d66798fe4afe9862cbf281cd0ccf
marilu777 <maria.valentes@hotmail.com>
2022-07-27 19:42:55 +0100

    ateasoito

--- app.js
+++ app.js
@@ -14,12 +14,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
+
+
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(beersFromApi => res.render('random-beer', beersFromApi[0]))
+  .catch(error => console.log(error));
+});  
+
+app.listen(3002, () => console.log('🏃‍ on port 3000'));
