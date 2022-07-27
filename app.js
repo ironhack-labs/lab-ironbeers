a16098432f89d7950217119c27c640e56c42b905
Adriana Leitao <adrianaleitao5_8@hotmail.com>
2022-07-27 17:43:49 +0100

    without bonus yet

--- app.js
+++ app.js
@@ -13,10 +13,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
 // Add the route handlers here:
+app.get('/beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(beersFromApi => res.render('random-beer', beersFromApi[0]))
+  .catch(error => console.log(error));
+});
 
 app.get('/', (req, res) => {
   res.render('index');
