a2683e94473702a65cd614f453a31be9ae529c6b
Philip Trachtenberg <philtrachtenberg@gmail.com>
2022-07-27 17:59:38 +0100

    finished first 5 interations (missing css for beers and random beers)

--- app.js
+++ app.js
@@ -14,12 +14,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  let data;
+  
+  punkAPI
+  .getBeers()
+  .then((value) => {
+    data = value;
+    res.render('beers', { data });
+  })
+  .catch(error => console.log(error));
+
+});
+
+app.get('/random-beer', (req, res) => {
+  let random;
+  
+  punkAPI
+  .getRandom()
+  .then((value) => {
+    random = value;
+    res.render('random-beer', { random });
+  })
+  .catch(error => console.log(error));
+  
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
