ad27298ceebb2416a4df077907641cf32c41aa9b
valentin-gago <108815363+valentin-gago@users.noreply.github.com>
2022-08-30 19:13:20 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const { Console } = require('console');
 const express = require('express');
 
 const hbs = require('hbs');
+const { request } = require('http');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -12,14 +14,38 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+const beer = [];
+app.get('/', (_request, response) => {
+  response.render("index");
+});
+app.get("/beers", (_request, response) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      response.render('beers', { beersFromApi });
+     // console.log("beers from the database:", { beersFromApi });
+       
+      })
+    .catch(error => console.log(error));
+});
+  
 
+
+app.get("/random-beers", (_request, response) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+     // console.log("holaaaaaaaa", responseFromAPI)
+      response.render('random-beer', { responseFromAPI });
+    })
+    .catch(error => console.log(error));
+
+});
+  
 // ...
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
