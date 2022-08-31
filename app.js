3fa809fa98039771c6d774764c87901c64144bad
Joanna Zielinska <joannazielinskam@gmail.com>
2022-08-31 16:01:26 +0200

    work in progress

--- app.js
+++ app.js
@@ -14,12 +14,52 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+  
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()  
+  .then(beersFromApi => {
+    //console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', {beers : beersFromApi})
+  })
+  .catch(error => console.log(error))  
+})
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    //console.log(responseFromAPI)
+    res.render('random-beer', {random: responseFromAPI })
+  })
+  .catch(error => console.log(error));
+  
+})
+
+//for (let i = 0; i++ < punkAPI.length; i++)
+
+app.get('/beers/:beerId', (req, res) => {
+  const id = req.params.beerId;
+  console.log(id)
+  punkAPI
+  .getBeer(id)
+  .then(beer => {    
+    
+    res.render('beer', {beer})
+  })
+  
+  
+  
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
