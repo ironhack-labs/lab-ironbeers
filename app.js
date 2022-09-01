7e45cc3c81fb3a63517f873571ce55fe281fad7e
Mauro-Molina <tutowebgoo@gmail.com>
2022-09-01 18:11:14 -0400

    Done!

--- app.js
+++ app.js
@@ -11,15 +11,45 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log(beersFromApi);
+    res.render('beers',{beersFromApi: beersFromApi});
+  })
+  .catch(error => console.log(error));
+ 
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beersFromApi => {
+    res.render('random-beer',{beersFromApi: beersFromApi[0]});
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/beerpartial', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log(beersFromApi);
+    res.render('/views/partials/beerpartial',{beersFromApi: beersFromApi});
+  })
+  .catch(error => console.log(error));
+ 
+});
+
+
+app.get('/', (req, res) => {
+  let info ={
+    img: "/images/beer.png",
+    beer: "/beers",
+    randomBeer: "random-beer"
+  }
+  res.render('index', info);
+});
+
+app.listen(8000, () => console.log('🏃‍ on port 8000'));
