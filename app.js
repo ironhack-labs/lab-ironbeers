a8dd502f9ed6e2d23044d77f89eb5801849cfa92
nayibibs <nayibibs@gmail.com>
2022-09-01 18:11:36 -0400

    ok

--- app.js
+++ app.js
@@ -14,12 +14,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
-
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
+    res.render('index');
+ });
+
+
+// ruta beers localhost:3000/beers
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then(cervezas => {
+    console.log(cervezas);
+    res.render('beers', {cervezas: cervezas});
+  })
+  .catch(err =>console.log(err));
+  
+  });
+
+  app.get('/random-beer', (req, res) => {
+    punkAPI.getRandom().then(cerveza => {
+      console.log(cerveza);
+      res.render('random-beer', {cerveza: cerveza[0]});
+    })
+    .catch(err =>console.log(err));
+    
+    });
+
+
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
