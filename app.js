ed6b445d1fa0bbec43a3f5ff9cf136407945e396
jdlms <97018563+jdlms@users.noreply.github.com>
2022-09-03 17:27:54 +0200

    first push

--- app.js
+++ app.js
@@ -22,4 +22,23 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beersFromApi: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeerFromApi => {
+      res.render('random-beer', { randomBeerFromApi: randomBeerFromApi });
+      console.log(randomBeerFromApi);
+    })
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
