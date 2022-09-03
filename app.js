eedb7d00471185365fd470361c7f2f1183acd1b8
Patrick-Groot <patrick@candis.io>
2022-09-03 15:42:24 +0200

    Iterations 1-4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const hbs = require('hbs');
+const { restart } = require('nodemon');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -14,12 +15,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    doctitle: "Index"
+  });
+});
+
+app.get('/beers', async (req, res) => {  
+  try {
+    const beersFromApi = await punkAPI.getBeers();
+    console.log("Beers from the database: ", beersFromApi)
+  res.render('beers', {
+    doctitle: "Beers",
+    beers: beersFromApi
+  });   
+  } catch(error) {res.send("There was an error. Sorry! :'(")}
+});
+
+app.get('/random-beer', async (req, res) => {  
+  try {
+    const randomBeerFromApi = await punkAPI.getRandom();
+    console.log("Random Beer from the database: ", randomBeerFromApi)
+  res.render('randomBeer', {
+    doctitle: "Random Beer",
+    beer: randomBeerFromApi[0]
+  });   
+  } catch(error) {res.send("There was an error. Sorry! :'(")}
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
