736b091fa11f224583e84127bc706730b4d3c43c
gused95 <gused.gm@gmail.com>
2022-09-03 21:37:10 -0500

    Completed lab ! :tada:

--- app.js
+++ app.js
@@ -22,4 +22,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+//Ruta Beers localhost:3000/beers
+app.get("/beers", (req,res) => {
+  punkAPI.getBeers() //pides
+  .then(cervezas => { //recibes datos
+      console.log(cervezas);
+      res.render("beers", { cervezas: cervezas}) //envias
+    })
+    .catch(err => console.log(err)); //manda msj si hay error
+});
+
+//Ruta Beers localhost:3000/beers
+app.get("/random-beer", (req,res) => {
+  punkAPI
+  .getRandom() //pides
+  .then(cerveza => { //recibes datos
+      console.log(cerveza);
+      res.render("random-beer", { cerveza: cerveza[0]})
+    })
+    .catch(err => console.log(err));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
