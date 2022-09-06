140c0c55fbe17adee57aa2f94088d175e862571e
Malkiat singh <malkiatcheema02@gmail.com>
2022-09-06 10:08:23 +0200

    Malkiat singh

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
-app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs');
 
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // Register the location for handlebars partials here:
 
 // ...
@@ -22,4 +23,21 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+app.get('/beers', (req, res, ) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {res.render('beers',{beers:beersFromApi})
+  
+})
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res, ) => {
+punkAPI.getRandom().then(responseFromAPI => 
+  {res.render('random-beer',{beer:responseFromAPI[0]})
+    // your magic happens here
+  })
+  .catch(error => console.log(error));
+})
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.listen(4000, () => console.log('🏃‍ on port 4000'));
