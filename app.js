d154f91e5ca6512c128805d7dedbe4983216d800
Wilfried Wurch <wil@wurch.de>
2022-09-28 17:20:17 +0200

    3. Iteration done

--- app.js
+++ app.js
@@ -22,4 +22,21 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log(beersFromApi[0]);
+    const data = {beers: beersFromApi};
+    res.render('beers', data);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  
+});
+
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer');
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
