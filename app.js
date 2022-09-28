f88ac66dc304e0d22963f45da6b4b68e228b3fc7
Saskia Peach <saskiapeach@gmail.com>
2022-09-28 20:17:47 +0100

    partials added

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Register the location for handlebars partials here:
 
