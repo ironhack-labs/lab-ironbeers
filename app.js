50f44b753ec76c0e43e64f58f58461dc13e666e0
Liz Manresa <manresa.liz@gmail.com>
2022-09-28 18:55:24 +0200

    mandatory iterations done

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+
 // ...
 
 // Add the route handlers here:
@@ -21,5 +22,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beersFromAPI => {
+    console.log('Beers from the database: ', beersFromAPI);
+
+    const data = {
+      beers: beersFromAPI
+    }
+    res.render('beers', data)
+  })
+  .catch()
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(responseFromAPI => {
+
+    const data = {
+      randomBeerImage: responseFromAPI[0].image_url,
+      randomBeerName: responseFromAPI[0].name,
+      randomBeerDescription: responseFromAPI[0].description,
+      randomBeerTagline: responseFromAPI[0].tagline,
+      randomBeerTips: responseFromAPI[0].brewers_tips,
+      randomBeersFoodPairing: responseFromAPI[0].food_pairing
+
+    }
+
+    res.render('random-beer', data) 
+    
+    
+  })
+  .catch(error => console.log(error))
+})
+
+// app.get('/random-beer', (req, res) => {
+//   punkAPI.getRandom()
+//   .then(beersFromApi => {
+//     res.render('random-beer', beersFromApi)
+//     console.log(randomBeer)
+//   })
+//   .catch(error => console.log(error))
+// })
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
