1806548f64b830765f7f8dbe58a9be08dad40598
barbaraagra <babs.agra@gmail.com>
2022-10-26 16:12:35 +0100

    solved lab

--- app.js
+++ app.js
@@ -18,8 +18,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the API:', beersFromApi)
+      res.render('beers', { beersFromApi });
+    })
+    .catch(error => console.log(error))
+});
+
 app.get('/', (req, res) => {
   res.render('index');
-});
+})
+
+app.get('/randombeer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      res.render('randombeer', randomBeer[0])
+    })
+    .catch(error => console.log(error));
+})
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
