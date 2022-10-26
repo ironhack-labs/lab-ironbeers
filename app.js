d81500c16a96d3e623ccd6ea5352d5ad0d6d7522
Camila <agarcamila@gmail.com>
2022-10-26 22:14:38 +0100

    handlebar lab

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
@@ -14,12 +12,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromAPI => {
+    console.log('Beers from the API:', beersFromAPI)
+    res.render('beers', {beersFromAPI});
+  })
+  .catch(err => 
+    console.log(err))
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beerFromAPI => {
+    console.log('Beers from the API:', beerFromAPI)
+    let beer = beerFromAPI[0]
+    res.render('randomBeer', beer);
+  })
+  .catch(err => 
+    console.log(err))
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
