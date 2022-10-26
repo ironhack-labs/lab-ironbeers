5f4e3cd4c5cfec2d4f83d8b2c070053ae9eeb71d
Miguel Quiaios <miguelquiaios12@gmail.com>
2022-10-26 18:10:22 +0100

    lab

--- app.js
+++ app.js
@@ -22,4 +22,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers() 
+  .then(beersFromApi => {
+    console.log('Beers from the Api', beersFromApi)
+    res.render('beers', { beersFromApi });
+  })
+  .catch(error => console.log(error))
+ 
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom() 
+  .then(beersFromApi => {
+    console.log('Beers from the Api', beersFromApi)
+    let beer = beersFromApi[0];
+    res.render('random-beer', beer);
+  })
+  .catch(error => console.log(error))
+ 
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
