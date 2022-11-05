a54b6e738473c9d2a12f1f71919fd34bed4b4440
R3alistic <acunhax@gmail.com>
2022-11-05 15:58:20 +0000

    solved lab

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { getRandomValues } = require('crypto');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -13,13 +14,33 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  const beerID = punkAPI.getBeers({'ids':'beer'})
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi),
+        res.render('beers', { beersFromApi })
+    })
+    .catch(error => console.log(error));
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      console.log('Random beer here: ', responseFromAPI)
+      // your magic happens here
+      res.render('random-beer', {responseFromAPI});
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
