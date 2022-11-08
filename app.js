a845950e036bedc282b8272d4c8fca5352d5c5d3
PedroSG00 <suarezgallardopedro@gmail.com>
2022-11-08 21:12:53 +0100

    done

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(__dirname + "/views/partials")
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -20,6 +22,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+
+});
+app.get('/beers', (req, res) => {
+
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beersInfo: beersFromApi });
+    })
+    .catch(error => console.log(error));
+
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers/:id', (req, res) => {
+
+  punkAPI
+    .getBeer(req.params.id)
+    .then(beersFromApi => {
+      console.log(beersFromApi)
+      res.render("beers", { beersInfo: beersFromApi })
+    })
+    .catch(error => console.log(error));
+
+});
+
+
+app.get('/randomBeer', (req, res) => {
+
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // console.log(responseFromAPI[0])
+      res.render("randomBeer", { randomBeer: responseFromAPI })
+    })
+    .catch(error => console.log(error));
+
+
+});
+
+
+app.listen(5005, () => console.log('🏃‍ on port 5005'));
