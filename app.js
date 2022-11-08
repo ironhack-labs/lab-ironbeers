4b160c7d3869b9fa2605fafa1e3f7a367372cf8c
YanxiaWu <yanxiawusilva@gmail.com>
2022-11-08 19:09:27 +0100

    parte obligatoria terminado

--- app.js
+++ app.js
@@ -22,4 +22,30 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+        res.render('beers', {allBeers: beersFromApi});
+    })
+  .catch(error => console.log(error));
+
+});
+
+
+app.get('/random-beer', (req, res) => {
+punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log(responseFromAPI)
+  res.render('random-beer',{randomBeers:responseFromAPI});
+  })
+  .catch(error => console.log(error));
+  
+});
+
+
+
+app.listen(5005, () => console.log('🏃‍ on port 5005'));
