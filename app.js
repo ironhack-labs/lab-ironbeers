497ab35091826326bca7bb70363687673b68eb57
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2022-11-08 23:44:15 +0100

    done

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+let id_beers = []
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Register the location for handlebars partials here:
 
@@ -20,6 +23,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers })
+
+      beers.map(beer => {
+        app.get('/beers/beer-' + beer.id, (req, res) => {
+          punkAPI
+            .getBeer(beer.id)
+            .then(beer => {
+              res.render('specificBeer', { beer })
+
+            })
+            .catch(error => console.log(error));
+        });
+      });
+
+    })
+    .catch(error => console.log(error));
+});
+
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      res.render('random-beer', { randomBeer });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
