964501b21c88965a9e1774fc7ca006d2b79236e0
Miguel A. Garcia <miguelangelgarcia994@gmail.com>
2022-11-09 07:43:21 +0100

    almost done

--- app.js
+++ app.js
@@ -21,5 +21,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers-page', { beersarray: beersFromApi })
+    })
+
+        .catch(error => console.log(error));    
+  
+  
+  
+})
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+  
+  .then(responseFromApi => {
+    res.render('random-beer-page', { beersarray: responseFromApi })
+    console.log(responseFromApi)
+    
+})
+  .catch(error => console.log(error));
+ 
+  
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
