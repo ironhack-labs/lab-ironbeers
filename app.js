bc73996a7dd17922d0ee1689a5225cfd0b3f8ed1
Angela Salazar <angelasalazargarcia04@gmail.com>
2022-11-09 17:16:14 +0100

    BERLIN OCT2022 Angela Salazar solved No Css

--- app.js
+++ app.js
@@ -12,11 +12,34 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* _,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_ */
+
+
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+ 
 
 // ...
 
 // Add the route handlers here:
+app.get('/beers', (req, res, next) => {
+punkAPI
+  .getBeers(25)
+  .then(beersFromApi => res.render('beers', {beerlist: beersFromApi}))
+  .catch(error => console.log("error :("+error))
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', {beerRandom: responseFromAPI})
+  })
+  .catch(error => console.log("error :("+error))
+})
+
+/* _,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_ */
 
 app.get('/', (req, res) => {
   res.render('index');
