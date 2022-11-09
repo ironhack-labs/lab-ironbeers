51320cb8787f95cb7d7ba0be56488c51b382a36f
Vika Shcherbak <vikashcherbaka@gmail.com>
2022-11-09 22:19:40 +0100

    solved lab

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
+const { ppid } = require('process');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
@@ -9,17 +10,27 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-// Register the location for handlebars partials here:
+app.get("/", (req, res, next) => res.render("index"));
 
-// ...
+app.get("/beers", (req, res, next) => {
+  let beers = punkAPI.getBeers();
+  beers.then(beers => {
+    let x = beers.slice(0, 25);
+    
+    res.render("beers", {x});
+  }).catch(error => console.log(error));
+})
 
-// Add the route handlers here:
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.get("/random-beer", (req, res, next) => {
+  const randomBeer = punkAPI.getRandom();
+  randomBeer.then(randomBeer => {
+    let a = randomBeer;
+    console.log(randomBeer);
+    res.render("random-beer", {a});
+  }).catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
