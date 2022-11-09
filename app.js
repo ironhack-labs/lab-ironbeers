39500c71f3f2dfaa1e7c01f2ce9d5043c6fa8f84
Carolina Acuna <carolina.acuna90@gmail.com>
2022-11-09 21:59:34 +0100

    make prettier and check partials

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, "/views/partials/beerpartials.hbs"));
 
 // Add the route handlers here:
 
@@ -22,4 +23,28 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+
+app.get('/beers', (req, res) => {
+ punkAPI
+  .getBeers() 
+  .then((beersFromApi) => {
+    console.log(beersFromApi)
+  res.render('beers', {beer: beersFromApi}); 
+})
+.catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeer => {
+    res.render('random-beer', {randomBeer});
+})
+  .catch(error => console.log(error));
+});
+
+/* app.get('beerpartial', (req, res) => {
+  res.render('beerapartial')
+})
+ */
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
