5b6d6833a2cc85281c3d7bce5b804ca9ba6e950e
Alexis Tellez <alexiztg@gmail.com>
2022-11-10 03:14:37 -0600

    done

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +24,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get("/beers", (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beersfromApi => { 
+    console.log('Beers from the database: ', beersfromApi);
+    res.render('beers', {beersfromApi})
+  }) 
+  .catch(error => console.log(error));
+});
+
+app.get("/random-beer", (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => { 
+    console.log('One beer from the database: ', responseFromAPI);
+    res.render('random-beer', {responseFromAPI})
+  }) 
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
