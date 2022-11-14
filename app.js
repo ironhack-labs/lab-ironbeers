0aa71cf40c3d4adfe4b93e87ce3db5807507c2fe
Micaela Rosadio <rosadio.karen@gmail.com>
2022-11-14 18:31:59 +0100

    Solutions for lab tasks 1 - 5

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
-const punkAPI = new PunkAPIWrapper();
+const punkAPI = new PunkAPIWrapper(); 
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 /******************************/
 /*          Routes            */
 /******************************/
-
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 hbs.registerPartials(__dirname + "/views/partials");
