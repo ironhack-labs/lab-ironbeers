d69f19e353466988d08954487802b42a24ce715e
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2022-12-14 18:55:46 +0100

    done

--- app.js
+++ app.js
@@ -22,4 +22,31 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      const data = {
+        beersFromApi: beersFromApi
+      };
+      console.log(data);
+      res.render('beers', data);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi => {
+      const data = {
+        responseFromApi
+      };
+      console.log(data);
+
+      res.render('random-beer', data);
+    })
+    .catch(error => console.log(error));
+});
+
+app.listen(3003, () => console.log('🏃‍ on port 3003'));
