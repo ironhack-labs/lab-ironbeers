3ec35042ac542421e693b98e8d362efc17a1ec1d
Carlos Sosa <csosa@simpletg.com>
2023-01-06 00:56:13 -0500

    added beer details route

--- app.js
+++ app.js
@@ -39,9 +39,21 @@ app.get('/random-beer', (req, res) => {
   punkAPI
     .getRandom()
     .then(beer => {
-      console.log({ beer: beer });
+      console.log({ beer });
       if (!beer[0].image_url) beer[0].image_url = './images/beer.png';
-      res.render('random-beer', { beer: beer[0] });
+      res.render('beer_detail', { beer: beer[0] });
+    })
+    .catch(err => console.log(err));
+});
+
+app.get('/beer/:beer_id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.beer_id)
+    .then(beer => {
+      console.log({ beer });
+      if (!beer[0].image_url) beer[0].image_url = './images/beer.png';
+      res.render('beer_detail', { beer: beer[0], goback: true });
+      // res.send(beers);
     })
     .catch(err => console.log(err));
 });
