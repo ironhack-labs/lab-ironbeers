d808951399b93460ccb0849c72a8309256171cf6
unknown <roxan.m.alma18@gmail.com>
2023-01-12 15:44:05 +0100

    done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+// esta require es donde están los datos de las cervezas.
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -13,13 +14,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);// el API de las cervezas
+// no poner aquí el res.render porque el codigo que le sigue no funciona
+      const data = { beers: beersFromApi }; // se hace una constante porque llamamos a muchos datos
+      res.render('beers', data);// ponerlo aquí para que funcione la promesa/codigo
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(responseFromAPI => {
+      console.log('Random beer from the database: ', responseFromAPI);
+
+      res.render('random-beer', responseFromAPI[0]);// no se forma una constante porque en este caso se llama de forma aleatoria un dato.
+      //se pone [0] porque queremos que solo llame un dato de tantos.
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
---
+++ public/notes.js
@@ -0,0 +1,23 @@
+/*
+La carpeta app.js es la carpeta principal donde renderizamos las subcarpetas, seguimos las rutas de cada una de ellas y 
+además dentro llamamos a la API para obtener los datos y con ello poder cumplir la promesa.
+
+
+En layout ponemos todo el HTML5, styless.css y un body {{body}} para que funcione.
+
+
+Las siguientes subcarpetas (beers.hbs, random.hbs) hbs realizamos las funciones para obtener los datos que queremos.
+
+
+En index.hbs lo que se hizo es poner más datos y se ligó sus  enlaces a los enlaces de la carpeta layout.
+
+
+
+Luego de crear las carpetas, se creó una carpeta dentro de views llamada card.hbs, que es donde llamamos los datos que 
+queremos ver en el localhost:3000.
+
+
+
+
+
+*/
