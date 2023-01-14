db85bab1c91939640642964321dc5733524ec723
Barbara Rodrigues <barbara.rodrigues@cabify.com>
2023-01-14 14:44:50 +0100

    done class

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const hbs = require('hbs');
+const hbs = require('hbs'); // html enriquecido
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -13,13 +13,37 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
 
-app.get('/', (req, res, next) => {res.render('index')});
-app.get('/beers', (req, res, next) => {res.render('beers')});
-app.get('/random-beer', (req, res, next) => {res.render('random-beer')});
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+
+      const data = {
+        beers: beersFromApi
+      };
+      res.render('beers', data);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(responseFromAPI => {
+      console.log('Random beer from the database: ', responseFromAPI);
+
+      res.render('random-beer', responseFromAPI[0]);
+    })
+    .catch(error => console.log(error));
+});
+
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'));  //// npm run dev - localhost:3000
