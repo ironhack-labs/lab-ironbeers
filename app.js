516891c26d8d449c3023e0fb4f6febd6b3763544
Secil <secilegen@gmail.com>
2023-01-16 14:48:02 +0100

    lab complete, iteration 1 - bonus iteration 6

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
 
+// Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + '/views/partials');
 // ...
 
 // Add the route handlers here:
@@ -21,5 +22,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) =>{
+  punkAPI.getBeers()
+  .then(beersFromApi => { 
+    console.log(beersFromApi)
+    res.render('beers', {beers: beersFromApi})
+  })
+  .catch(error => console.log(error));
+  
+});
+
+app.get('/random-beer', (req,res) => {
+  punkAPI.getRandom()
+  .then((beer)=>{
+    const [randomBeer] = beer
+    console.log(randomBeer)
+    res.render('random-beer', randomBeer);
+  })
+});
+
+app.get('/beers/:beerId', (req,res)=>{
+  punkAPI.getBeer(req.params.beerId)
+  .then(beer =>{
+    const [detailedBeer] = beer
+    console.log(detailedBeer)
+    res.render('beer-details', detailedBeer)
+  })
+  .catch(err=>console.log(err))
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
