1d53ddf2aa4b3eb0d151be780b796d3573784cd9
Erin <erica@Erin.local>
2023-01-20 17:18:49 +0100

    Done mandatory without styles

--- app.js
+++ app.js
@@ -22,4 +22,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', {
+        beers: beersFromApi
+      });
+    })
+    .catch(error => console.log('error'));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(random => {
+      res.render('random-beer', { random });
+    })
+    .catch(error => console.log('error'));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
