874c3d0e9206ef08ba4ea17eaab83c9982dfcb26
elena992 <elena.fernandez1992@gmail.com>
2023-01-21 02:17:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,13 +13,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+
+      const data = {
+        beers: beersFromApi
+      };
+      res.render('beers', data);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(responseFromAPI => {
+      console.log('Random beer from the database: ', responseFromAPI);
+
+      res.render('random-beer', responseFromAPI[0]);
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
