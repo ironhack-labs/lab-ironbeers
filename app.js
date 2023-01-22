c631e7adeca61ba83140f86543b911c7803f33ad
sergiocataladuran <sergio.catala.duran@gmail.com>
2023-01-22 13:02:20 +0100

    Completado

--- app.js
+++ app.js
@@ -13,13 +13,41 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      let beers = [];
+      beersFromApi.forEach(beer => {
+        beers.push(beer);
+      });
+      console.log(beers);
+      res.render('beers', { beers });
+    })
+    .catch(error => console.log(error));
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      let beer = randomBeer[0];
+      console.log(beer);
+      res.render('randomBeer', beer);
+    })
+    .catch(error => console.log(error));
+});
+app.get('/beers/:beerId', (req, res) => {
+  punkAPI.getBeer(req.params.beerId).then(beer => {
+    let indexedBeer = beer[0];
+  });
+}); 
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
