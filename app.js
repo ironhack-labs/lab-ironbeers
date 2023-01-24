0daa1189b9c6856d8685c75f0c5c10d8d0132965
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2023-01-24 17:55:29 +0100

    Mandatory iterations done

--- app.js
+++ app.js
@@ -18,8 +18,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+
+
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+    punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi);
+    res.render('beers', {beersFromApi});
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeerFromAPI => {
+    res.render('random-beer', {
+      beer: randomBeerFromAPI[0]
+    });
+  })
+  .catch(error => console.log(error))
+})
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
