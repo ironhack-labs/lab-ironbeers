07e75fd61ee1d6b7f2571528b14c475c87d9f562
silvia Edreira <silviaedreiragg@gmail.com>
2023-01-25 17:33:05 +0100

    lab done

--- app.js
+++ app.js
@@ -7,16 +7,12 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -36,8 +32,5 @@ app.get('/random-beer', (req, res) => {
   res.render('random-beer', {responseFromAPI}))
   .catch(error => console.log(error))
 });
-
-
-
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
