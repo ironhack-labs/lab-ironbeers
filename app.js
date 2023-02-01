69673e5935586d5fb07d9baac97641af34214013
HenrikH <henrik.hellem@live.com>
2023-02-01 23:34:54 +0100

    Iteration 5 done - working on iteration 6

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
-
 app.get('/', (req, res) => {
   const image = '/images/beer.png';
   res.render('index', { image: image });
@@ -27,7 +25,7 @@ app.get('/beers', (req, res) => {
   punkAPI
     .getBeers()
     .then(beersFromApi => {
-      res.render('beers', { beerList : beersFromApi });
+      res.render('beers', { beerList : beersFromApi, extended : false });
     })
     .catch(error => console.log(error));
 });
@@ -36,9 +34,17 @@ app.get('/random-beer', (req, res) => {
   punkAPI
     .getRandom()
     .then(beer => {
-      res.render('random-beer', { randomBeer : beer[0] });
+      res.render('random-beer', { randomBeer : beer[0] , extended : true});
     })
     .catch(error => console.log(error));
 });
+
+app.get('/beers/beer-:beerId', (req, res) => {
+  punkAPI
+  .getBeer(req.params.beerId)
+  .then(beer => {
+    res.send('beer', { selectBeer : beer[0] , extended : true});
+  })
+})
 
 app.listen(5000, () => console.log('🏃‍ on port 5000'));
