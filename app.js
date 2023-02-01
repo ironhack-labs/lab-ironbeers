82e18f95fb6b7056c2dab1b7bae56bb4ddbd5752
Neilly <neilpilarca@gmail.com>
2023-02-01 16:34:09 +0100

    initial commit

--- app.js
+++ app.js
@@ -13,13 +13,44 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + '/views/partials');
 
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  // define the variables
+  const imageUrl = '/images/beer.png';
+  const user = 'neil';
+
+  //   render the view
+  //   syntax: render(<name of view file>, <data in one object>)
+  res.render('home', {
+    imageUrl,
+    user
+  });
+});
+
+app.get('/beers', (req, res) => {
+  //using async/await + try/catch
+  punkAPI
+    .getBeers()
+    .then(data => {
+      res.render('beers', { data });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(data => {
+      // your magic happens here
+      console.log(data);
+      res.render('random', { data });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
