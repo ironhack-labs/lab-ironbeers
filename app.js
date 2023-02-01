8d7af5ce4ce7aa6c863a5e94f7d51945fda5adf6
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-01 13:50:43 +0100

    style home page

--- app.js
+++ app.js
@@ -13,13 +13,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-// Add the route handlers here:
+// Route handlers
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const data = { image: 'images/beer.png' };
+  res.render('index', data);
+});
+
+app.get('/beers', (req, res) => {
+  const data = punkAPI.getBeers();
+  console.log(data);
+  // .then(beersFromApi =>
+  //   console.log('Beers from the database: ', beersFromApi)
+  // )
+  // .catch(error => console.log(error));
+  res.render('beers', data);
+});
+
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
