3e3a9c7bb7207f08c035bde13cd9104c94168993
OFKIR Mohamed <mohamed_ofkir@outlook.fr>
2023-02-01 17:27:07 +0100

    first save

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { getEnabledCategories } = require('trace_events');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -13,13 +14,32 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+// hbs.registerPartials(__dirname + "/views/partials")
 
-// ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get("/beers", (req,res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render("beers", {beers} ) 
+    })
+    .catch(error=> console.log(error))
+})
+app.get("/random-beers", (req,res) => {
+  punkAPI
+    .getRandom()
+    .then(rbeer => {
+      console.log(rbeer)
+      res.render("randombeers", {rbeer: rbeer[0]} )
+  })
+    .catch(err => console.log(err))
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
