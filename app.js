0fd49acc4df70e7fa3cbda910d37a7609f0bcb50
AGUSTINA PATT <agustinapatt@gmail.com>
2023-02-02 12:24:45 +0100

    solved lab

--- app.js
+++ app.js
@@ -19,7 +19,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const beerImage = "/images/beer.png"
+  const link1 = "Check the beers!"
+  const link2 = "Check a Random Beer"
+  res.render('index', {image: beerImage, link1, link2});
+  
 });
+
+app.get("/beers", (req, res) => {
+  const beersArr = punkAPI.getBeers()
+  .then(beersFromApi => res.render("beers", {beersArr: beersFromApi}))
+  .catch(error => console.log(error));
+  })
+
+app.get("/random-beer", (req, res) => {
+  const randomBeer = punkAPI.getRandom()
+  randomBeer.then(beer => {
+    res.render("random-beer", {randomBeer: beer[0]})
+  })
+  .catch(error => console.log(error));
+})
+
+//  punkAPI
+//  .getRandom()
+//  .then(beers => {
+//      beers[0].random = true
+//      res.render("random-beer", {beer: beers[0]})
+//})
+
+// app.get("/beer-i3f4d34s34b", (req, res) => {
+//   const beerId = punkAPI.getBeers({'id': 8})
+//   res.render("beer-i3f4d34s34b", beerId: beerId)
+// })
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
