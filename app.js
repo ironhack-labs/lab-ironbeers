e2c0b82c87e6e55af8eb306558a13a59e2307b73
ArturoRubin <116040655+ArturoRubin@users.noreply.github.com>
2023-02-01 21:11:30 -0600

    Commit
    Intente terminar el lab, pero no logro hacer que funcione. Me avienta un error

--- app.js
+++ app.js
@@ -18,8 +18,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req,res) => res.render("index"))
+
+
+app.get("/beers", (req, res)=>{
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render("beers",{
+      beers:beersFromApi,
+    });
+  })
+  .catch(error => console.log(error));
 });
+
+app.get("/random-beer", (req,res) => res.render("random.beer"))
+
+punkAPI
+.getRandom()
+  .then(responseFromAPI => {
+    console.log("random", responseFromAPI)
+    res.render("random-beers",{
+      random:responseFromAPI[0],
+    })
+  })
+  .catch(error => console.log(error));
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
