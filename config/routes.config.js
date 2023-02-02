3810542a3f9b2054a71211d7e5de8700a07e7957
Alba Greciano <albagreciano@Alba-Greciano-2.local>
2023-02-02 18:41:42 +0100

    error

--- config/routes.config.js
+++ config/routes.config.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const beers = require('../controllers/beers.controller');
 
-router.get('/beers', beers.beersls);
+router.get('/beers', beers.list);
 router.get('/random', beers.random);
 
 module.exports = router;
