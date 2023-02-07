8ac03f48a43e2fa557cbceaa5905aee9dbdf2628
EmilianoCC <emiliano93cc@gmail.com>
2023-02-07 00:10:07 -0600

    commit complete

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { getEnabledCategories } = require('trace_events');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -20,6 +21,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => console.log(error));
+});
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(rbeer => {
+      console.log(rbeer);
+      res.render('randombeers', { rbeer: rbeer[0] });
+    })
+    .catch(err => console.log(err));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
