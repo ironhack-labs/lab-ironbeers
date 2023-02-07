edbb976c0c411fbcba3b6b57413b6b5818bab77c
jelloreda <jaimelloredavelasco@gmail.com>
2023-02-07 19:49:31 +0100

    done

--- app.js
+++ app.js
@@ -14,12 +14,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      // console.log(beersFromApi)
+      res.render('beers', { beersFromApi })
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      res.render('random-beers', { randomBeer });
+    })
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
