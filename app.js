17811e3a4312c81d2226009c0e4e93a228822a99
“Adrian <johndoe@example.com>
2023-02-07 20:09:42 +0100

    done

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 // ...
 
 // Add the route handlers here:
@@ -21,5 +23,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', { allBeers: beersFromApi })
+    })
+    .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req,res)=> {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+res.render('random-beer', {pickBeer: responseFromAPI})
+    })
+    .catch(error => console.log(error));
+})
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
