699d17c5728819e2196d32487df451501b32c6cb
Raphaël Malet <malet.raphael@gmail.com>
2023-02-07 17:47:04 +0100

    modif apostrophe

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const express = require(‘express’);
-const hbs = require(‘hbs’);
-const path = require(‘path’);
-const PunkAPIWrapper = require(‘punkapi-javascript-wrapper’);
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-app.set(‘view engine’, ‘hbs’);
-app.set(‘views’, path.join(__dirname, ‘views’));
-app.use(express.static(path.join(__dirname, ‘public’)));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 // Register the location for handlebars partials here:
 // Register the location for partials
-hbs.registerPartials(path.join(__dirname, ‘views/partials’));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // Add the route handlers here:
-app.get(‘/’, (req, res) => {
-  res.render(‘index’);
+app.get('/', (req, res) => {
+  res.render('index');
 });
-app.get(‘/beers’, (req, res) => {
+app.get('/beers', (req, res) => {
   punkAPI
-  .getBeers()
-  .then(beersFromApi => {
-  res.render(‘beers’, { beers: beersFromApi });
-  })
-  .catch(error => console.log(error));
-  });
-app.get(‘/random-beer’, (req, res) => {
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+app.get('/random-beer', (req, res) => {
   punkAPI
-  .getRandom()
-  .then(beersFromApi => {
-  res.render(‘random-beer’, { beers: beersFromApi });
-  })
-  .catch(error => console.log(error));
-  });
-app.listen(3000, () => console.log(‘:coureur-à-pied:‍ on port 3000’));
+    .getRandom()
+    .then(beersFromApi => {
+      res.render('random-beer', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+app.listen(3000, () => console.log(':coureur-à-pied:‍ on port 3000'));
