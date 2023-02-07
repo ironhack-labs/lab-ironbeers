0c02d29832e6348f93b24434754338aafb393b65
Daniel Yanez <ingdanielyanez@gmail.com>
2023-02-07 17:59:46 +0100

    [MAD DE 0123] DANIEL YANEZ

--- app.js
+++ app.js
@@ -11,15 +11,37 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Register the location for handlebars partials here:
 
 // ...
-
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index')
 });
+// Add the route handlers here:
+app.get('/beers', (req, res) => {
+  // console.log('hola beer')
+  //const getBeers = PunkAPI.getBeers()
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', { beers: beersFromApi })
+    })
+    .catch(error => console.log(error))
+
+})
+app.get('/randomBeer', (req, res) => {
+  // console.log('hola random beer')
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('randomBeer', { beers: beersFromApi })
+    })
+    .catch(error => console.log(error))
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
