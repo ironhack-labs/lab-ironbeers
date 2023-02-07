80ea5f933ec29aa0aedecf70b6071609514dd9fe
Gimena Motto <gimenapimba@gmail.com>
2023-02-07 22:00:43 +0100

    done

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
@@ -22,4 +23,26 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', { beers: beersFromApi })
+    })
+    .catch(error => console.log(error))
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      console.log('lo q hay acá:', responseFromAPI)
+      res.render('random-beer.hbs', { random: responseFromAPI })
+    })
+    .catch(error => console.log(error))
+
+})
+
+app.listen(5005, () => console.log('🏃‍ on port 5005'));
