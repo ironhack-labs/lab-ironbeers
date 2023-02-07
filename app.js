5e1ca886c1a5fe8d707fae324886b95bb3cba44d
Cristina Gutiérrez <cgutierrezgm@gmail.com>
2023-02-07 20:10:25 +0100

    v1

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 // ...
 
@@ -21,5 +22,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beersData: beersFromApi })
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      res.render('random-beer', { randomBeerData: responseFromAPI })
+    })
+    .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
