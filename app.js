75c82da9590024550ee976ac5941305e3626ba5b
Jizzus <miguel.angelo.jesus@hotmail.com>
2023-02-08 17:40:10 +0000

    lab done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +20,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then((getBeers) => {
+    res.render('beers', { getBeers });
+  })
+  .catch((err) =>console.log(err));
+});
+
+app.get('/beers/:id', (req, res) => {
+  punkAPI.getBeer(req.params.id)
+  .then((getBeer) => {
+    res.render('beers-id', { getBeer });
+  })
+  .catch((err) =>console.log(err));
+});
+
+app.get('/random-beer', (req, res) => {
+  /* let data = {
+    getRandom: true
+  } */
+  punkAPI.getRandom()
+  .then((getRandom) => {
+    /* let id = req.params.getRandom
+    console.log(id); */
+    res.render('random-beer', { getRandom }, /* id */);
+  })
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
