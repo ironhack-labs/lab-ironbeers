fa327df6a4d66ea052220dcadde6f560261dc3b2
Tiago Rato <tiagorato13@hotmail.com>
2023-02-08 21:09:07 +0000

    submit lab minimal css

--- app.js
+++ app.js
@@ -14,12 +14,46 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+
+//BEER
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then((beersFromApi) => { 
+    console.log('Beers from the database: ', beersFromApi)
+    res.render('beers', {beer: beersFromApi})
+})
+  .catch(error => console.log(error))
+  });
+
+  app.get('/beer/:id', (req, res) => {
+    const beerId = req.params.id;
+
+    punkAPI
+    .getBeer(beerId)
+    .then(responseFromAPI => {
+      res.render('random-beer', {random: responseFromAPI});
+    })
+    .catch(error => console.log(error));
+  })
+
+
+  //RANDOM BEER
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', {random: responseFromAPI});
+  })
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
