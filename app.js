15d42edd079840b4c8324b1b3fd413d91448c399
Rocio Salgado <salgadorocio@hotmail.com>
2023-02-08 17:42:45 +0100

    solves lab

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const express = require('express');
-
+const express = require('express')
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
@@ -16,10 +15,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // ...
 
+hbs.registerPartials(__dirname + "/views/partials");
+
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/random-beer', async (req, res) => {
+  let randomBeer = await punkAPI.getRandom()
+  res.render('random-beer', {random: randomBeer[0]});
+});
+
+app.get('/beers', async (req, res) => {
+  let beers = await punkAPI.getBeers()
+  let arrayOfBeers = {beers: beers}
+  res.render('beers', arrayOfBeers);
+});
+
+app.get('/beer/:id', async (req, res) => {
+  let id = req.params.id;
+  let beer = await punkAPI.getBeer(id)
+  let beerObject = {random: beer[0]}
+  console.log(beerObject)
+  res.render('random-beer', beerObject);
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
