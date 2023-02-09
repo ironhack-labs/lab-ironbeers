d97d5c38c147463f2ccb86f429df300772b79c04
Mahmoud Laban <mahmoud.laban@gmail.com>
2023-02-09 20:33:26 +0000

    in progress

--- app.js
+++ app.js
@@ -13,13 +13,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then((beersFromApi) => {
+      console.log('Get Beers from DB: ', beersFromApi)
+      res.render('beers', {beersFromApi: beersFromApi});
+    })
+    .catch(error => console.log(error))
+});
+
+app.get("/beers/:id",(req,res) => {
+  punkAPI.getBeer(req.params.id)
+    .then(beer => {
+      console.log("Beer:", beer)
+    res.render("beer",{beerItem: beer[0]})
+    })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
