014190c8962d05b8923cebe976ca756ef39e5b0d
Mehdi Eddira <m.eddira75@outlook.fr>
2023-02-09 01:13:57 +0100

    add iterations 1 , 2 , 3 beers

--- app.js
+++ app.js
@@ -13,13 +13,56 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+  console.log('Hello from /');
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res, next) => {
+  // try {
+    // const beers = punkAPI.getBeers()
+    // console.log(beers)
+      punkAPI
+        .getBeers()
+        .then(beersFromApi => {
+          res.locals.beers = [];
+          beersFromApi.forEach(element => {
+            res.locals.beers.push({
+              id: element.id,
+              image_url: element.image_url,
+              name: element.name,
+              description: element.description,
+              tagline: element.tagline
+            });
+          });
+          console.log(res.locals.beers)
+          res.render('beers',{
+          beers : res.locals.beers,
+          }) ;
+        })
+        .catch(error => {
+          res.statusCode = 500;
+          res.render('server-error', {code: 500, message: error.message});
+        });
+})
+
+// app.get("/random-beer", (req, res) => {
+//   console.log(req.query);
+//   res.render("random-beer", {
+//     navbar: true
+//   });
+// });
+
+app.use((error, req, res, next) => {
+  res.statusCode = 500;
+  console.log(error.message);
+  res.render('server-error', { code: res.statusCode, message: error.message });
+});
+
+app.listen(3000, () => console.log('🏃‍ on port 3000 http://localhost:3000'));
