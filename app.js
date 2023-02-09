0c7486f69fc3fee444e0909b28a0bc3347486c77
Marisha Deroubaix <deroubaix.marisha@gmail.com>
2023-02-09 00:12:37 +0000

    commiting lab

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 // Register the location for handlebars partials here:
 
@@ -20,6 +21,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beers =>  {
+    res.render('beers', { beers });
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/random', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    // your magic happens here
+    res.render('random', beers[0]);
+  })
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
