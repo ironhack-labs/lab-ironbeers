c953f5068936e5d1283ec8ece371fa8e8693cf37
maxbrandt96 <max.brandtk@gmail.com>
2023-02-13 00:27:27 -0600

    Lab Terminado

--- app.js
+++ app.js
@@ -22,4 +22,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then((beers) => {
+    res.render('beers', { beers });
+  });
+});
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getBeers().then((beers) => {
+    const randomBeer = beers[Math.floor(Math.random() * beers.length)];
+    res.render('random-beer', { beer: randomBeer });
+  }).catch((error) => {
+    console.log(error);
+  });
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
