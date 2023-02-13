f97f8a529e8ae49ef0616da192b56594ec092878
Marisha Deroubaix <deroubaix.marisha@gmail.com>
2023-02-13 09:02:04 +0000

    commiting bonus 6

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+//for the image? but its a form example
+/* const bodyParser = require('body-parser') */
+//for the image? but its a form example
+//make the app use the bodyparser
+/* app.use(bodyPerser.urlencoded({extended: true})) */
+
+
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(__dirname + '/views/partials')
@@ -23,13 +32,39 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/beers', (req, res) => {
+/* app.get('/beers', (req, res) => {
   punkAPI.getBeers()
     .then(beers =>  {
     res.render('beers', { beers });
+    console.log("Response is ", beers)
   })
   .catch(error => console.log(error));
-});
+}); */
+
+app.get('/beers', async (req, res)=> {
+  try {
+    //get the beers from th e API 
+    let beersFromApi = await punkAPI.getBeers()
+    //console.log(beers)
+    // Send the beers to the view
+    res.render('beers', {beers: beersFromApi})
+  } catch (error) {
+    
+  }
+})
+
+
+app.get('/beers/:beerId', (req, res) => {
+  punkAPI
+  .getBeer(req.params.beerId)
+  .then((beersInfo) => {
+    //console.log('beer info' , beersInfo.beers)
+   
+    res.render('info', {beers: beersInfo})
+  })
+  .catch((error) => console.log(error));
+})
+
 
 app.get('/random', (req, res) => {
   punkAPI.getRandom()
