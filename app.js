7d02d75fdb612777f034980b89f28dd3244723bc
andrehcarvalho6 <andre.hcarvalho1@gmail.com>
2023-03-15 18:11:45 +0000

    all iterations and bonus done by both

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -21,5 +21,56 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (request, response, next) => {
+  punkAPI
+    .getBeers()
+    .then(beerArr => {
+      const data = {
+        beers: beerArr
+      };
+      response.render('beers', data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get('/random-beer', (request, response, next) => {
+  punkAPI
+    .getRandom()
+    .then(beerArr => {
+      const data = {
+        randomBeer: beerArr
+      };
+      console.log(data);
+      response.render('random-beer', data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+//attemp to make iteration 6
+
+app.get('/beers/:id', (req, res) => {
+  const beerId = req.params.id;
+  // const beerIds = getObjectById(beerId);
+  punkAPI
+    .getBeer(beerId)
+    .then(beerArr => {
+      // const data = {
+      //   beerClick: beerArr
+      // };
+      const [data] = beerArr;
+      console.log(data);
+      res.render('beerClick', data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+//
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
