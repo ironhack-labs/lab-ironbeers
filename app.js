19b9e053f982ee56ca209e69a23bab7639a4b648
Cristian Palao <cristianpalaocamarasa@gmail.com>
2023-03-15 15:33:07 +0100

    Implemented full iteration 3

--- app.js
+++ app.js
@@ -18,8 +18,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+//GET /
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+// GET /beers
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+    .then((beersRetrieved) => {
+
+      const data = {
+        beers: beersRetrieved
+      }
+
+      console.log(data.beers[1])
+
+      res.render('beers', data);
+
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
