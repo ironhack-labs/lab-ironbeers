77d0ab6d7f856cecb4a985a171089b803c05e6c9
burak algan <burakalgan@gmail.com>
2023-03-15 18:57:51 +0100

    done without bonuses

--- app.js
+++ app.js
@@ -22,4 +22,44 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (request, response, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      const banana = { beers: beersFromApi };
+      response.render('beers', banana);
+      // console.log('Beers from the database: ', beersFromApi[0]);
+    })
+    .catch(error => console.log(error));
+});
+// app.get('/random-beer', (request, response, next) => {
+//   punkAPI
+//     .getRandom()
+//     .then(beersFromApi => {
+//       const banana = { beers: beersFromApi };
+//       response.render('random-beer', banana);
+//       // console.log('Beers from the database: ', beersFromApi[0]);
+//     })
+//     .catch(error => console.log(error));
+// });
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+
+    .getRandom()
+
+    .then(responseFromApi => {
+      console.log(responseFromApi);
+
+      const randomBeerArr = [...responseFromApi];
+
+      const data = {
+        beers: randomBeerArr
+      };
+
+      res.render('random-beer', data);
+    })
+
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
