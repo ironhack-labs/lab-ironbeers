dba591e0a8a6be5e18de738f412ef69b2e04fb9f
Bartosz Dubinski <bartas.de@gmail.com>
2023-03-15 17:00:31 +0100

    iteration 1-4 done

--- app.js
+++ app.js
@@ -18,6 +18,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+app.get("/beers", (request, response, next) => {
+  punkAPI.getBeers()
+  .then( (beerArr) => {
+     
+      const data = {
+          beers: beerArr
+      }
+  
+
+      //console.log(beerArr) //remember to console log & understand what you have ;)
+
+
+      response.render("beers", data);
+
+  })
+  .catch( (error) => {
+      //error.... houston, no beers in the fridge
+  });
+})
+
+app.get("/random-beer", (request, response, next) => {
+  punkAPI.getRandom()
+  .then( (responseFromAPI) => {
+    const data = {
+      randomBeer: responseFromAPI
+    }
+  response.render("random-beer", data);
+   console.log(responseFromAPI)
+  })
+  .catch(error => console.log(error));
+
+  });
+
+
 app.get('/', (req, res) => {
   res.render('index');
 });
