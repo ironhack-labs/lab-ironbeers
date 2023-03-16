43192aa7c9fd346ae360364a8ac80f75be9cffb1
Hristos Pappasgit config --global user.name Hristos <hristos.pappas@icloud.com>
2023-03-16 10:05:06 +0800

    all mandatory iterations solved

--- app.js
+++ app.js
@@ -13,13 +13,41 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
-// ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get("/beers", (req, res) => {
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+  punkAPI.getBeers()
+  .then( (beerArr) => {
+
+    const data = {
+      beers: beerArr,
+    }
+    
+    res.render("beers", data)
+  })
+  .catch(error => console.log(error));
+  
+})
+app.get("/random-beer", (req, res) => {
+
+  punkAPI.getRandom()
+  .then( (randomBeer) => {
+    const data = {
+      beer: randomBeer[0],
+    }
+
+    res.render("random-beer", data)
+  })
+  .catch(error => console.log(error));
+})
+
+
+app.listen(3002, () => console.log('🏃‍ on port 3002'));
+
