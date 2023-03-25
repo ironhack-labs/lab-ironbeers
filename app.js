6761df1075171e35c6f08fc808c9ea2316e8f559
Max Strandberg <maax.strandberg@gmail.com>
2023-03-25 16:59:54 +0100

    Max, Yo-Jia

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
@@ -20,6 +21,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  
+  //const beerArr =punkAPI
+  punkAPI
+ 
+  .getBeers()
+  .then(beersFromApi =>{ 
+  
+    res.render('beers', {beersFromApi})
+   })
+   
+  .catch(error => console.log(error));
+  
+});
+app.get('/random-beer', (req, res) => {
+ 
+
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    // your magic happens here
+    let randomBeer = responseFromAPI[0]
+    console.log( randomBeer)
+     res.render('random-beer',{randomBeer})
+  })
+  .catch(error => console.log(error));
+
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
