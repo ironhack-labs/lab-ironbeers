fed516bbbdbe226f43b0aaefa12ace88fbf32880
raqueldfrutos <raqueldfrutos@gmail.com>
2023-04-04 21:08:30 +0200

    added comments

--- app.js
+++ app.js
@@ -25,14 +25,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/beers', (req, res) => {
-  const beerArr = punkAPI.getBeers();
-
-  beerArr
-    .then(beerArr => {
-      res.render('beers', { beerArr });
+  // const beerArr = punkAPI.getBeers();
+  punkAPI.getBeers() // funcion que llama a la base de datos de beers
+    .then((beerArr) => {  // la promesa escucha y cuando tiene los datos,  el .then() ejecuta una funcion que como parametro está pasando el array de datos
+      res.render('beers', { beers:beerArr }); // la función lo que hace es renderizar el array. "beers" será la key que yo luego pasaré a la plantilla .hbs 
       console.log('Beers from the database: ', beerArr);
     })
-
     .catch(error => console.log(error));
 });
 
