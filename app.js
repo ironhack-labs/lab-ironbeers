56402a3f71aa9a86dcae67a85303dabb1acbc3ad
IreneValor <115938535+IreneValor@users.noreply.github.com>
2023-04-09 15:21:46 +0200

    done

--- app.js
+++ app.js
@@ -22,5 +22,28 @@ app;
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', async (req, res) => {
+  try {
+    const beers = await punkAPI.getBeers();
+    res.render('beers', { beers });
+  } catch (err) {
+    res.send('<h1>Something went wrong</h1>').status(404);
+  }
+});
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    const [randBeer] = await punkAPI.getRandom();
+    res.render('random-beer', { ...randBeer, single: true });
+  } catch (err) {
+    res.send('<h1>Something went wrong</h1>').status(404);
+  }
+});
+
+app.get('/beers/:id', async (req, res) => {
+  const { id } = req.params;
+  const [beer] = await punkAPI.getBeer(id);
+  res.render('random-beer', { ...beer, single: true });
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
