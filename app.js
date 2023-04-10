24c8161880422f7e18c3f229e082b81c85183313
“Zuzana <zuzana@getyourguide.com>
2023-04-10 21:18:31 +0200

    done, incl. bonuses

--- app.js
+++ app.js
@@ -12,10 +12,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
@@ -23,25 +22,37 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/beers', (req, res, next) => {
-  const allBeer = punkAPI.getBeers();
-  allBeer.then((beersFromApi) => {
-    console.log('Beers from the database: ', beersFromApi);
-    let beerData = {beers: beersFromApi};
-    res.render('beers', beerData);
-  })
-    .catch((err) => console.log("there was an error", err));
+app.get('/beers', async (req, res) => {
+  try {
+    const allBeers = await punkAPI.getBeers();
+    // console.log(allBeers);
+    res.render('beers', {allBeers});
+  }
+  catch (err) {console.log("There was an error", err)}
 });
 
-app.get('/random-beer', (req, res, next) => {
-  const randomBeer = punkAPI.getRandom();
-  randomBeer.then((beersFromApi) => {
-    console.log('Random beer ', beersFromApi);
-    let randomBeerData = {randomBeer: beersFromApi};
-    res.render('random-beer', randomBeerData);
-  })
-    .catch((err) => console.log("there was an error", err));
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    const randomBeer = await punkAPI.getRandom();
+    // console.log(randomBeer);
+    res.render('random-beer', {randomBeer});
+  }
+  catch (err) {console.log("There was an error", err)}
 });
+
+app.get('/beer/:id', async (req, res) => {
+    const id = req.params.id;
+    punkAPI.getBeer(id)
+    .then(function(beer) {
+      res.render('newbeer', { beer});
+    })
+  }
+ 
+);
+
+
+// res.render('newbeer', { beer: beer[0] });
 
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
