f08911e89bbb9ed6eeb3cd69cdcda4a5dcb40d97
HeimLi <127536715+HeimLi@users.noreply.github.com>
2023-04-12 16:40:42 +0200

    Iteration2

--- app.js
+++ app.js
@@ -10,16 +10,28 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+// Iteration 2
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+const imageBeer = "/images/beer.png"
+
+  res.render('index', {imageBeer});
+
+});
+
+app.get('/beers', (req, res) => {
+  res.render('Beers');
+});
+
+app.get('/beer-random', (req, res) => {
+  res.render('Beer-Random');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
