7121a187b96ce594f4c1c692be96cd220eb8fff7
gunnar-miklis <gunnar.miklis@gmail.com>
2023-04-12 18:07:03 +0200

    done

--- app.js
+++ app.js
@@ -12,14 +12,28 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 });
+app.get('/beers', async (req, res) => {
+    try {
+        const response = await punkAPI.getBeers();
+        res.render( 'beers', { beers: response } );
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
+app.get('/random-beer', async (req, res) => {
+    try {
+        const response = await punkAPI.getRandom();
+        res.render( 'random-beer', { randomBeer: response[0] } )
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
