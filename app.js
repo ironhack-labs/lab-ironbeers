73c354f17b683ab262d98ef951de138ed6bc71eb
Fabian Hurtado Volkmann <fabian04berlin@outlook.de>
2023-04-12 16:29:30 +0200

    complete iteration 3

--- app.js
+++ app.js
@@ -21,8 +21,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
-// app.get('/beer', (req, res) => {
-//   res.render('test');
-// });
+app.get('/beers', (req, res) => {  
+  // punkAPI.getBeers().then(beers => console.log(beers)).catch(error => console.log(error))
+
+
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beers: beersFromApi}))
+  .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
