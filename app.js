55f4269a7d64cf0321e9950193ef95a306c0e5b3
Lilly Liefers <lillyliefers@me.com>
2023-04-12 17:07:24 +0200

    solved lab

--- app.js
+++ app.js
@@ -22,4 +22,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      /*     console.log('Beers from the database: ', beersFromApi) */
+      res.render('beers', { beers:beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      
+    })
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
