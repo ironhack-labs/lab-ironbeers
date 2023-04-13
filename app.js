0165f36021b85aca13aeeba3fad00220a651db99
beckjosh <Joshbeck17@gmail.com>
2023-04-13 09:14:42 +0200

    "Ironbeers Finished"

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
+// const beerImage = loadImage("")
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
@@ -21,5 +21,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+  let beers = punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beers: beersFromApi }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  let beers = punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', { beers: responseFromAPI })
+    })
+    .catch(error => console.log(error));
+});
+
+app.listen(3001, () => console.log('🏃‍ on port 3001'));
