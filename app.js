af38c05828252147f2d87e64b38c71e484bf03ad
Matt Krebs <mattekrebs@gmail.com>
2023-05-24 16:39:15 +0200

    WDPT Matt Krebs

--- app.js
+++ app.js
@@ -14,12 +14,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req,res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .catch(error => console.log(error));
+  res.render('beers');
+});
+
+app.get('/random-beer', (req,res) => {
+  res.render('random-beer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
