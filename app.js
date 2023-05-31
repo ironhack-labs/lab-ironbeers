c6fd8816f4f928a2f95953426bbba8e5d86d2acd
Joao Faria <joaorafaria@gmail.com>
2023-05-31 18:04:10 +0100

    bonus iteration 5 done

--- app.js
+++ app.js
@@ -11,12 +11,10 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
