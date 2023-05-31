e123229b21288b5a108f3ec3d38c3391fd23a67c
Dok <pierredok@outlook.fr>
2023-05-31 17:44:47 +0200

    We done All iteration without Bonus.

--- app.js
+++ app.js
@@ -22,4 +22,35 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+
+
+
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+  
+  res.render('beers', {beer:beersFromApi})
+
+  })
+  .catch(error => {console.log(error)})
+
+})
+
+
+
+
+app.get('/random-beer', (req, res) => {
+
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+
+    res.render('random', {randomBeer:responseFromAPI});
+
+  })
+  .catch(error => console.log(error));
+
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
