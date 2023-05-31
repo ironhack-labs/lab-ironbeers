0d86ab04d5c5e515409c53f726d22a73273c4b1e
Joao Faria <joaorafaria@gmail.com>
2023-05-31 17:22:37 +0100

    iteration 1 to 4 done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -19,7 +20,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const data = {
+    imageFile: 'beer.png'
+  };
+  res.render('index', data);
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersArr => {
+      // console.log(beersArr[0]);
+      const beers = { beers: beersArr };
+      res.render('beers', beers);
+    })
+    .catch(error => {
+      console.log('There was an error', error);
+    });
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      const beer = responseFromAPI[0];
+      res.render('random-beer', beer);
+    })
+
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
