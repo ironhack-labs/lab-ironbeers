8ac92e25dcc59c7e3414bcf4ef43cb230bcf59cd
Jani Ojala <jani@stptrade.com>
2023-05-31 18:57:34 +0200

    lab done

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -20,6 +22,58 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(responseFromAPI => {
+      const data = {
+        beers: responseFromAPI
+      };
+      console.log(data);
+      res.render('beers', data);
+    })
+    .catch(error => {
+      console.log('error', error);
+    });
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      const data = {
+        beer: responseFromAPI
+      };
+      res.render('random-beer', data);
+    })
+    .catch(error => {
+      console.log('error', error);
+    });
+});
+
+// now we are defining a new route called beerdetails
+// and we are dynamically passing the ID with the colon (:)
+
+app.get('/beerdetails/:id', (req, res) => {
+  //res.send(req.params);
+  // if we do a console log, we can see the request params
+  //console.log(req.params);
+
+  punkAPI
+    // when calling GetBeer, we can pass in the ID, but we should use the req.params.id variable to ensure we pass in the correct one (dynamically)
+    .getBeer(req.params.id)
+    .then(responseFromAPI => {
+      const data = {
+        beer: responseFromAPI
+      };
+      // console.log(data);
+      res.render('beerdetails', data);
+    })
+    .catch(error => {
+      console.log('error', error);
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
