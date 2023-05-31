d4c5a05b8c7dd76bb103fb37768dc8396dd155db
Gretil <gretil_15@hotmail.com>
2023-05-31 17:58:08 +0200

    all done except styling.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
+
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
@@ -14,6 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
@@ -27,6 +29,19 @@ app.get('/beers', (req, res) => {
     .getBeers()
     .then(beersFromApi => {
       res.render('beers', beersFromApi);
+      // res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+  console.log(req.params.id);
+  punkAPI
+    .getBeer(req.params.id.split('-')[1])
+    .then(beerFromApi => {
+      res.render('beer', beerFromApi[0]);
+
+      console.log(beerFromApi[0]);
       // res.render('beers', { beers: beersFromApi });
     })
     .catch(error => console.log(error));
