8cb00c99ad4f0d39e131fd6e9e9640ca1abe9bd8
linhvnde <linh.nh.tyty@gmail.com>
2023-05-31 16:31:27 +0200

    iteration 3 beers.hbs done together

--- app.js
+++ app.js
@@ -22,4 +22,17 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      const data = {
+        beers: beersFromApi
+      };
+
+      console.log('Beers from the database: ', data);
+      res.render('beers', data);
+    })
+    .catch(error => console.log(error));
+});
 app.listen(3000, () => console.log('🏃‍ on http://localhost:3000/ '));
