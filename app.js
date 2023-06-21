478436a591f36c48ba767c552ccac91a95676deb
Mirza Abdullah Baig <abdullah.baig2013@gmail.com>
2023-06-21 18:26:36 +0200

    Attemped

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('/views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,6 +19,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/randomBeer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      const randomBer = responseFromAPI[0];
+      res.render('randomBeer', {
+        beer: randomBer
+      });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/index', (req, res) => {
   res.render('index');
 });
 
