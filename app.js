1e11310a181077152a5260d20b1df72981dbd703
Aykut Kav <ayktkav@gmail.com>
2023-06-21 20:29:51 +0200

    wip

--- app.js
+++ app.js
@@ -22,4 +22,17 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer');
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
