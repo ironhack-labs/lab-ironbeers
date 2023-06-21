eeef3fa9be75b9511d6d692e7c546e40c8a7b7e0
alexanderley <alexander.ley.inbox@gmail.com>
2023-06-21 17:00:31 +0200

    lab done

--- app.js
+++ app.js
@@ -12,14 +12,31 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', async (req, res) => {
+  try {
+    const beers = await punkAPI.getBeers();
+    console.log('beers', { beers });
+    res.render('beers', { beers });
+  } catch (error) {
+    console.log(error);
+    res.render('error');
+  }
+});
+
+app.get('/randomBeer', async (req, res) => {
+  // res.render('randomBeer');
+  try {
+    const randomBeer = await punkAPI.getRandom();
+    console.log('random beer: ', randomBeer);
+    res.render('randomBeer', { randomBeer });
+  } catch (error) {
+    console.log(error);
+    res.render('error');
+  }
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
