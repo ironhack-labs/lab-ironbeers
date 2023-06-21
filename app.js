3fc920665c15bca3277924cae1737562085aaf0c
Andy Cockroft <acockroft1994@gmail.com>
2023-06-21 16:40:14 +0200

    stuck on random beer

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
@@ -22,4 +22,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res) => {
+  try {
+    const beers = await punkAPI.getBeers({ abv_gt: 0 });
+    console.log('Beers from the database: ', beers);
+    res.render('beers', { beers });
+  } catch (error) {
+    console.log(error);
+    res.render('error');
+  }
+});
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    const randomBeer = await punkAPI.getRandom();
+    console.log('Random Beer: ', randomBeer);
+    res.render('random-beer', { randomBeer });
+  } catch (error) {
+    console.log(error);
+    res.render('error');
+  }
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
