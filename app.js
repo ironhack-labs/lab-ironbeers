2438b88ee8000e24a18e393ea09de621d83968c1
Mary Bonilla <marybonillaseverino@gmail.com>
2023-06-27 09:53:42 +0200

    primera y segunda iteracion

--- app.js
+++ app.js
@@ -14,12 +14,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/partials");
+app.use(express.static(path.join(__dirname, "public")));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get ('/beers', (req, res) => {
+  res.render('beers')
+});
+
+app.get ('/random-beer', (req, res) => {
+  res.render('random-beer')
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
