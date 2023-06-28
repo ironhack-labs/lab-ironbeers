ad4d4850fe9f560793ccf7b8bc246cd062c506a2
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-06-28 02:22:40 +0200

    Iteration 1 y 2

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+app.get('/', (req, res) => res.render('index'));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/beers', (req, res) => {
+  res.render('beers');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
