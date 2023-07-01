5a0be7693489f93932ab60a562347ecd1a4bbfea
Sheila Hernández <shey.hdez1999@gmail.com>
2023-07-01 07:08:20 +0200

    faltan cositas

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const hbs = require('hbs');
-const path = require('path');
+const hbs = require("hbs");
+const path = require("path");
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
@@ -12,14 +12,20 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => res.render("beers", { beersFromApi }))
+    .catch(err => console.log(err));
+})
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(responseFromAPI =>   res.render("random-beer",{ responseFromAPI }))
+  .catch(err => console.log(err));
+})
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+app.listen(3001, () => console.log('🏃‍ on port 3001'));
