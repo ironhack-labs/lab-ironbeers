5ded9866df748a86a1350c3300f8ecc4877b4449
TiagoLobo-source <65892221+TiagoLobo-source@users.noreply.github.com>
2023-07-02 23:37:21 +0100

    bug fixed getrandom -4 iter complete

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-const beersAPI = punkAPI.getBeers()
-const randomBeer = punkAPI.getRandom()
+let beersAPI = punkAPI.getBeers();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // Register the location for handlebars partials here:
 
@@ -25,22 +23,18 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 app.get('/beers', (req, res) => {
-    beersAPI
-      .then(beersFromApi => res.render('beers', { data: beersFromApi }))
-      .catch(error => console.log(error));
+  beersAPI
+    .then(beersFromApi => res.render('beers', { data: beersFromApi }))
+    .catch(error => console.log(error));
 });
 app.get('/random-beer', (req, res) => {
-
-randomBeer.then(beer =>{ res.render('randombeer', { data: beer[0]})
-console.log(beer[0])})
-  .catch(error => console.log(error));
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      res.render('randombeer', { data: beer[0] });
+      console.log(beer[0]);
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(4000, () => console.log('🏃‍ on port 4000'));
-
-punkAPI
-.getRandom()
-  .then(responseFromAPI => {
-    // your magic happens here
-  })
-  .catch(error => console.log(error));
