5860e86a249c9c72726df54a2bec8931b438cf65
Marcos Francesquini <francesquini.personal@gmail.com>
2023-07-04 07:05:20 +0200

    founding beers

--- app.js
+++ app.js
@@ -13,13 +13,36 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+//hbs.registerPartials(__dirname + '/views/partials');
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+app.get('/beers', (req, res) => {
+  const getBeers = punkAPI.getBeers([1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25])
+  getBeers.then(beers => {
+    res.render('beers', { beers });
+  }).catch(error => {
+    res.render('error');
+  });
+});
+
+app.get('/randombeer', (req, res) => {
+  const randomBeer = punkAPI.getRandom()
+
+  randomBeer.then(beer => {
+    console.log(beer[0].name)
+  })
+  //res.render('randombeer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
