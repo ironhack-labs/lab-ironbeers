787a38c5beb6775f6202607d58cca6587f02ea1d
0000nebu <neboa.fresco@gmail.com>
2023-07-22 09:31:33 +0200

    WIP interacion 4

--- app.js
+++ app.js
@@ -16,7 +16,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // ...
 
+
+
 // Add the route handlers here:
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandomBeers(beers)
+    .then((beers) => {
+      const randomBeer= Math.floor(Math.random() * beers.length);
+      res.render('random-beers', beers[randomBeer])
+    })
+    .catch((err) => {
+      console.error('Error fetching beers from the PunkAPI');
+    });
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers({ per_page: 25 })
+    .then((beers) => {
+      res.render('beers', { beers });
+    })
+    .catch((err) => {
+      console.error('Error fetching beers from the PunkAPI');
+    });
+});
 
 app.get('/', (req, res) => {
   res.render('index');
