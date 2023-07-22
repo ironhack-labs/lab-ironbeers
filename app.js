ad4e641cbd40cc5783ba99611f3ee52e052d398f
0000nebu <neboa.fresco@gmail.com>
2023-07-22 10:41:41 +0200

    images

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Add the route handlers here:
-
+/*
 app.get('/random-beer', (req, res, next) => {
   punkAPI.getRandomBeers(beers)
     .then((beers) => {
@@ -30,10 +30,20 @@ app.get('/random-beer', (req, res, next) => {
       console.error('Error fetching beers from the PunkAPI');
     });
 });
+*/
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandomBeers()
+.then(beer => {
+  alert(beer[0].name)
+});
+ 
+});
+
 
 app.get('/beers', (req, res, next) => {
   punkAPI.getBeers({ per_page: 25 })
     .then((beers) => {
+      console.log(beers)
       res.render('beers', { beers });
     })
     .catch((err) => {
