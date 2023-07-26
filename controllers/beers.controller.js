a0710aa4980f469ed2de737b521e524ae9640ae3
Arturo Art <arturovt90@gmail.com>
2023-07-26 21:26:44 +0200

    done

---
+++ controllers/beers.controller.js
@@ -0,0 +1,24 @@
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
+
+module.exports.home = (req, res) => {
+    res.render('index');
+};
+
+module.exports.list = (req, res) => {
+punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+            res.render('beers/list', { beersFromApi });
+        })
+    .catch(error => console.log(error));
+};
+
+module.exports.random = (req, res) => {
+    punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('beers/random', { beer: responseFromAPI[0] });
+  })
+  .catch(error => console.log(error));
+}
