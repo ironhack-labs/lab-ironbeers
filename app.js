0431c41be38a9ea40017dce134227c8dbb96de52
Andre Pinto <andre.marpinto@gmail.com>
2023-08-02 21:17:05 +0100

    [FT-LIS-072023]André_Pinto

--- app.js
+++ app.js
@@ -13,13 +13,51 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+//beers route
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log(beersFromApi)
+    res.render('beers', {beersFromApi})
+    })
+    .catch(error => console.log(error));
+});
+
+//Random beers
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+    // your magic happens here
+    res.render('random-beer', {responseFromAPI})
+      
+    })
+    .catch(error => console.log(error));
+})
+
+
+app.get('/beers/:beerId', (req, res) => {
+  punkAPI
+    .getBeer(req.params.beerId)
+    .then(responseFromAPI => {
+    // your magic happens here
+    res.render('beerdetails', {responseFromAPI})
+      
+    })
+    .catch(error => console.log(error));
+})
+
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
