0fb38d8b681c737cafc647d9e7137aa35a5f86d2
Rodrigo Erthal <rrerthal@gmail.com>
2023-08-23 20:24:03 +0200

    iteration 3 completed

--- app.js
+++ app.js
@@ -18,8 +18,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    const cervas = {
+      beers:beersFromApi
+    };
+res.render("beers",cervas);
+})
+.catch(error => console.log(error));
+});
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+app.listen(3001, () => console.log('🏃‍ on port 3001'));
