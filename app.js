fd83fe4058879473bc624603a2d8958d7f945b11
Kim Idsinga <kimidsinga@gmail.com>
2023-08-23 19:21:19 +0200

    done

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -20,6 +22,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      const beerListData = {
+        listOfBeers: beersFromApi
+      };
+      // console.log('Beers from the database: ', beersFromApi);
+      // console.log(data.name);
+      res.render('beers', beerListData);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeerFromApi => {
+      const randomBeerData = {
+        randomBeer: randomBeerFromApi
+      };
+      console.log(randomBeerData)
+      res.render('random-beer', randomBeerData);
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
