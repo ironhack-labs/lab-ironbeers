5967e7b40fabc659690b9d1c5a094707fd0e55e3
Utku Cikmaz <utkucikmaz@gmail.com>
2023-08-23 18:09:11 +0200

    mandatory part without any css

--- app.js
+++ app.js
@@ -17,6 +17,46 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ...
 
 // Add the route handlers here:
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      const beerDataArray = [];
+
+      beersFromApi.forEach(element => {
+        const dataBeer = {
+          name: element.name,
+          image: element.image_url,
+          description: element.description,
+          tagline: element.tagline
+        };
+        beerDataArray.push(dataBeer);
+      });
+
+      res.render('beers', { beersFromApi: beerDataArray });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      const data = responseFromAPI[0];
+
+      const randomBeerData = {
+        image: data.image_url,
+        name: data.name,
+        description: data.description,
+        tagline: data.tagline,
+        foodPairing: data.food_pairing,
+        brewerTips: data.brewers_tips
+      };
+      console.log(randomBeerData);
+      res.render('random-beer', randomBeerData);
+    })
+    .catch(error => console.log(error));
+});
 
 app.get('/', (req, res) => {
   res.render('index');
