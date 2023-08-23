068710d24cf9245af4f9cc2b408b357bd4fa76e2
Rodrigo Erthal <rrerthal@gmail.com>
2023-08-23 21:57:08 +0200

    iteration 4 completed

--- app.js
+++ app.js
@@ -24,16 +24,26 @@ app.get('/', (req, res) => {
 });
 app.get('/beers', (req, res) => {
   punkAPI
-  .getBeers()
-  .then(beersFromApi => {
-    const cervas = {
-      beers:beersFromApi
-    };
-res.render("beers",cervas);
-})
-.catch(error => console.log(error));
+    .getBeers()
+    .then(beersFromApi => {
+      const cervas = {
+        beers: beersFromApi
+      };
+      res.render("beers", cervas);
+    })
+    .catch(error => console.log(error));
 });
 
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      const randomBeer = responseFromAPI[0];
+
+      res.render("random-beer", { randomBeer });
+    })
+    .catch(error => console.log(error));
+});
 
 
 app.listen(3001, () => console.log('🏃‍ on port 3001'));
