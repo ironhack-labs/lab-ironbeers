6fa6511586238780fcc56b09136474c4a54c4c49
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-08-30 17:03:02 +0200

    wip

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
-const punkAPI = new PunkAPIWrapper();
+const PunkAPIWrapper =require("punkapi-javascript-wrapper");
+const punkAPI = new PunkAPIWrapper()
+
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +21,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
-app.get("/beers", (req, res, next) => res.render("/beers"));
 
-app.get("/random-beer", (req, res, next) => res.render("/random-beer"));
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      console.log(beersFromApi);
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('Error fetching beers from the API.');
+    });
+});
+
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer');
+});
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
 
