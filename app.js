9a4e19564cdf21cdf5816407c300172734eae4e0
Gaspar Bonari <gasparbonari@gmail.com>
2023-09-03 13:11:36 +0200

    Done

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Add the route handlers here:
 
@@ -26,10 +26,20 @@ app.get('/beers', (req, res) => {
   punkAPI
   .getBeers()
   .then(beers =>{
-    console.log('beers: ', beers)
     res.render('beers', {beers});
   })
   .catch(error => console.log(error));
 });
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    const randomBeer = responseFromAPI[0];
+    res.render('random-beer', { randomBeer });
+  })
+  .catch(error => console.log(error));
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
