5ee34fc8870efa639b73a38383e6e153e5ff2de2
chloe <marin.ducarme@gmail.com>
2023-10-10 02:50:26 +0200

    done

--- app.js
+++ app.js
@@ -12,14 +12,45 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
 
-// ...
+app.get('/random-beer', async (req, res) => {
+  try { 
+    const getmyRandomBeer = await punkAPI.getRandom();
+    const randomBeer = getmyRandomBeer.map((beer) => {
+      return {
+        name: beer.name,
+        image: beer.image,
+        description: beer.description,
+        tagline: beer.tagline,
+        foodpairing: beer.foodpairing,
+        brewertips: beer.brewertips
+      }
+    })
+    res.render('random-beer', { getmyRandomBeer: randomBeer });
+} catch(error) {
+  console.log(error);
+}
+})
 
-// Add the route handlers here:
+app.get('/beers', async (req, res) => {
+  try {
+    const beers = await punkAPI.getBeers();
+    const firstTwentyFive = beers.slice(0, 25).map((beer) => {
+      return {
+        name: beer.name,
+        image: beer.image,
+        description: beer.description,
+        tagline: beer.tagline
+      };
+    });
+    res.render('beers', { beers: firstTwentyFive });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(5000, () => console.log('🏃‍ on port 5000'));
