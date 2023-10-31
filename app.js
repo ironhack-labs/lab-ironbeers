2d7d2bbcff0833ce221004457340348a02687682
Tania Zubrienko <tatiana.zubrienko@gmail.com>
2023-10-31 19:06:45 +0100

    done

--- app.js
+++ app.js
@@ -13,13 +13,38 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 // Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
-});
+})
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beer: beersFromApi }))
+    .catch(error => console.log(error));
+
+})
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', { randomBeer: responseFromAPI })
+    })
+    .catch(error => console.log(error));
+})
+
+
+
+app.get('/beers/:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then(responseFromAPI => {
+      res.render('beer', responseFromAPI[0])
+    })
+    .catch(error => console.log(error));
+})
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'))
