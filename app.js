43c53a9964a3d6d90ea8b308d40869ee5ff4a2cf
PMisar <147728448+PMisar@users.noreply.github.com>
2023-11-08 17:10:23 +0100

    done

--- app.js
+++ app.js
@@ -22,4 +22,23 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi }); 
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      const randomBeer = responseFromAPI[0]; 
+      res.render('random-beer', { randomBeer }); 
+    })
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
