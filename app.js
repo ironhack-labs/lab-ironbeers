87ac5d1cb5795b34fe1758a4fe21ce0d76394e07
gizemayegul <gizemayazyegul@gmail.com>
2023-11-08 22:25:17 +0100

    solved lab

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(__dirname);
 
 // Register the location for handlebars partials here:
 
@@ -20,6 +23,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  const beers = punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('An error occurred while fetching data.');
+    });
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randombeer => {
+      res.render('randombeer', { beers: randombeer });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('An error occurred while fetching data.');
+    });
+});
+
+app.get('/beer-info/:beerId', (req, res) => {
+  const beerId = req.params.beerId;
+
+  punkAPI
+    .getBeer(beerId)
+    .then(beer => {
+      res.render('beers-info', { beer });
+      console.log(beer);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('An error occurred while fetching data.');
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
