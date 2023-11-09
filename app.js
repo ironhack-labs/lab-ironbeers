c3d73f5f9ab762ce37e8a32c44f967832ed56612
Isin Kosemen <isinkosemen@gmail.com>
2023-11-09 09:35:04 +0100

    done

--- app.js
+++ app.js
@@ -15,11 +15,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Register the location for handlebars partials here:
 
 // ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', async (req, res) => {
+  try {
+    const beersArray = await punkAPI.getBeers();
+    res.render('beers', { beers: beersArray });
+  } catch (error) {
+    console.error('Error fetching beers:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    const randomBeer = await punkAPI.getRandom();
+    res.render('random-beer', { beer: randomBeer[0] });
+  } catch (error) {
+    console.error('Error fetching random beer:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
