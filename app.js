a1f6e2efec979bf12a6f51d35e91d5e71a30ee76
Vladimir Ilovaca <vladoilovaca@gmail.com>
2023-11-20 18:34:17 +0100

    Half way

--- app.js
+++ app.js
@@ -19,7 +19,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render('beers.hbs', { beers: beersFromApi});
+  })
+  .catch(error => console.log(error))
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI 
+  .getRandom()
+  .then(responseFromApi => {
+    res.render('/random-beer.hbs', { beer: responseFromApi});
+  })
+  .catch(error => console.log(error))
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
