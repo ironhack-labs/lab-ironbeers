5d4d2599c5dc00fde3535382957af71516107335
alberto-sclocchi <scloks75tv@gmail.com>
2023-12-16 22:09:04 -0500

    solve lab plus bonus 6

--- app.js
+++ app.js
@@ -12,14 +12,32 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+
+});
+
+app.get("/random-beer", (req, res, next)=>{
+  punkAPI.getRandom()
+  .then((responseFromAPI)=>{
+      console.log (responseFromAPI);
+      res.render('random-beer', {beer: responseFromAPI[0]});
+  })
+})
+
+app.get('/beers/:beerID', (req, res) => {
+  punkAPI.getBeer(req.params.beerID).then((theBeer)=>{
+    res.render('random-beer', {beer: theBeer[0]})
+  });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
