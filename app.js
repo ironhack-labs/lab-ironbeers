d538562dbd43102e04d8a89a0c21fa413847bd9f
Marcelo Via <mvia@outlook.com>
2023-12-16 23:21:57 -0500

    Project Completed - Marcelo V.

--- app.js
+++ app.js
@@ -22,4 +22,36 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+
+app.get('/beers', (req, res) => {
+  
+  punkAPI.getBeers()
+    .then((beersFromApi) => {
+      // console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', {allTheBeers: beersFromApi});
+  });
+    
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then((randomBeerFromApi) => {
+    // console.log('Random Beer from the database: ', randomBeerFromApi)
+    res.render('random-beer', {randomBeer: randomBeerFromApi[0]});
+  });
+
+});
+
+
+app.get('/beers/:theID', (req,res) => {
+  punkAPI.getBeer(req.params.theID)
+  .then((beerDetails) => {
+    
+    res.render("beer-details", {theBeer: beerDetails[0]})
+    // console.log("Details of Beer are: ", beerDetails)
+  
+  });
+  
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
