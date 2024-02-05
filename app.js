a59db66961d18f15f1e04830a16ac61e65c9ad53
Jesús Sánchez <jsxexu38@gmail.com>
2024-02-05 15:50:37 +0100

    solved without bonus

--- app.js
+++ app.js
@@ -22,4 +22,23 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res)=> {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render('beers', { beers: beersFromApi});
+  })
+  .catch(error => console.log(error))
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    const random = responseFromAPI[0]
+    res.render('random-beer', {random})
+  })
+  .catch(error => console.log(error));
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
