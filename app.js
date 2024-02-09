77e586354557e40fddc9ab3e1f5d37c80f337ac6
IvanZScode <sayonbra@gmail.com>
2024-02-09 17:06:11 +0100

    Done without the BONUS 6

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const hbs = require('hbs');
+
 const path = require('path');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+
 
 // Configure HBS view engine
 app.set('view engine', 'hbs');
@@ -13,22 +16,25 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('/beers', (req, res) => {
-  res.render('beers');
+  punkAPI.getBeers().then((beers) => {
+    console.debug(beers);
+    res.render('beers', {beers: beers});
+  })
 });
 
 app.get('/random-beer', (req, res) => {
-  res.render('random');
-});
+  punkAPI.getRandom().then(beers => {
+    console.log(beers)
+    res.render('random-beer', {beers: beers});
+  })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
