3060d27617625cfd7bb393d722251951c1969c49
martab <mbsuarez.92@gmail.com>
2024-02-10 09:24:54 +0100

    done

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const app = express();
 
 const hbs = require('hbs');
+
 const path = require('path');
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
-const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
@@ -12,20 +14,29 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
 
 
-app.get('/random-beer', (req, res) => {
-  punkAPI.getRandom().then(beer => {
-    console.log(beer)
-    res.render('beer-detail', beer)
+app.get('/', (req, res) => {
+  res.render('index');
+  res.render('home');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then((beers) => {
+    console.debug(beers);
+    res.render('beers', {beers: beers});
   })
-})
+});
 
+
+//codigo julio
+//app.get('/random-beer', (req, res) => {
+ // punkAPI.getRandom().then(beer => {
+ //   console.log(beer)
+ //   res.render('beer-detail', beer)
+ //   res.render('random-beer', {beers: beers});
+//  })
+//})
 
 
 
