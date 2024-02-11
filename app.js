c723bc4cac8fa7bc991df153b093c9c3f1762d16
martab <mbsuarez.92@gmail.com>
2024-02-11 22:55:08 +0100

    solved code mistakes

--- app.js
+++ app.js
@@ -1,48 +1,39 @@
 const express = require('express');
-const app = express();
 
 const hbs = require('hbs');
-
 const path = require('path');
-
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(`${__dirname}/views/partials`);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.get('/', (req, res) => {
   res.render('index');
-  res.render('home');
 });
 
-app.get('/beers', (req, res) => {
-  punkAPI.getBeers().then((beers) => {
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then((beers) => {
     console.debug(beers);
-    res.render('beers', {beers: beers});
+    res.render('beers', {beers: beers})
   })
-});
+  .catch((error) => nextTick(error));
+})
 
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then((beer) => {
+    res.render('random-beer', {beer: beer[0]});
+  })
+  .catch((error) => console.log(error));
+})
 
-//codigo julio
-//app.get('/random-beer', (req, res) => {
- // punkAPI.getRandom().then(beer => {
- //   console.log(beer)
- //   res.render('beer-detail', beer)
- //   res.render('random-beer', {beers: beers});
-//  })
-//})
-
-
-
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
