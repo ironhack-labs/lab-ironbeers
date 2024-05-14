d030a66797e907f1d9612cf922f41cc301a0cb9b
Bruno-Zamperoni <bzamperoni@gmai.com>
2024-05-14 15:45:01 -0300

    Done

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
-const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -14,12 +12,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+app.get('/', (req, res, next) => res.render('index'));
+
+app.get('/breweries', (req, res, next) =>
+  fetch('https://api.openbrewerydb.org/v1/breweries?per_page=25')
+    .then((response) => response.json())
+    .then((response) => res.render('breweries', {breweries: response}))
+    .catch((err) => {
+      console.error('Error fetching data:', err);
+      res.status(500).send('Error fetching data');
+    })
+);
+
+app.get('/random-breweries', (req, res, next) =>
+  fetch('https://api.openbrewerydb.org/v1/breweries/random')
+    .then((response) => response.json())
+    .then((response) => res.render('random-breweries', {brewery: response}))
+    .catch((err) => {
+      console.error('Error fetching data:', err);
+      res.status(500).send('Error fetching data');
+    })
+);
 
 // Add the route handlers here:
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
