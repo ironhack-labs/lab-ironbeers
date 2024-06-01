807b04015849de9caab0be7e16fa0ab4166c8ef1
amirvaknin <amir6661@gmail.com>
2024-06-01 15:34:17 +0200

    initial commit

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const axios = require('axios');
 
 const app = express();
-const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -14,12 +13,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  axios.get('https://api.openbrewerydb.org/v1/breweries')
+    .then(response => res.render('beers', {data: response.data}))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  axios.get('https://api.openbrewerydb.org/v1/breweries/random')
+    .then(response => res.render('random-beer', {data: response.data}))
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
