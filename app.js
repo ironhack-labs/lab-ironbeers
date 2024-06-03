e3da4d1d13d5573df3c1e7ea34de29ed66f884ae
Aditya Raikar <raikaraditya22@gmail.com>
2024-06-03 18:43:40 +0200

    done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios')
 
 const hbs = require('hbs');
 const path = require('path');
@@ -20,6 +21,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  axios.get('https://api.openbrewerydb.org/v1/breweries')
+        .then((response)=>{
+            console.log('response', response.data)
+            res.render('beers', {id: response.data})
+        })
+        .catch((err)=>console.log(err))
+});
+
+app.get('/random-beers', (req, res) => {
+  axios.get('https://api.openbrewerydb.org/v1/breweries/random')
+        .then((response)=>{
+            console.log('response', response.data)
+            res.render('random-beers', {id: response.data})
+        })
+        .catch((err)=>console.log(err))
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
