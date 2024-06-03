9e90dc0ef78c2eb460f960a4217d450186c0b61f
Aditya Raikar <raikaraditya22@gmail.com>
2024-06-03 18:48:44 +0200

    done again

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('home');
 });
 
 app.get('/beers', (req, res) => {
